30a668cfa469427cb5bb46dc5d4a5a74101fc435
Lucas C. Abbas <lucasbear9@gmail.com>
2025-01-31 17:20:54 -0900

    1/31/2025 5:20 PM

---
+++ src/core/base_object.cpp
@@ -0,0 +1,7 @@
+//
+// Created by lucas on 1/31/2025.
+//
+
+#include "base_object.h"
+
+using namespace newhaven_core;
---
+++ src/core/base_object.h
@@ -0,0 +1,18 @@
+//
+// Created by lucas on 1/31/2025.
+//
+
+#ifndef NH_OBJECT_H
+#define NH_OBJECT_H
+
+#include <lua.h>
+
+namespace newhaven_core
+{
+    static void generateBaseObjectUsertype(lua_State* L) {}
+
+    class BaseObject {
+    };
+}
+
+#endif //NH_OBJECT_H
