bc073b6ede05e430d7e324310f8a82610aa866a1
Lucas C. Abbas <lucasbear9@gmail.com>
2025-02-01 18:57:56 -0900

    Enhance Lua binding by adding property and method support for Godot classes

--- src/core/lua_bind.cpp
+++ src/core/lua_bind.cpp
@@ -5,6 +5,7 @@
 
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
+#include <godot_cpp/classes/json.hpp>
 
 using namespace godot;
 using namespace newhaven_core;
@@ -15,41 +16,86 @@ void newhaven_core::bind_class_to_lua(sol::state& lua, const String& class_name)
         godot::UtilityFunctions::print("Class not found: ", class_name);
         return;
     }
-
     // Create a new Lua usertype
-    lua.new_usertype<godot::Object>(
+    sol::usertype<godot::Object> ut = lua.new_usertype<godot::Object>(
         class_name.utf8().get_data(),
-        sol::constructors<>()
+        sol::constructors<>(),
+        "new", sol::factories([class_name]() { 
+            return ClassDB::instantiate( class_name ); 
+            }
+        )
     );
 
+    TypedArray<Dictionary> property_list = ClassDB::class_get_property_list(class_name);
+    TypedArray<Dictionary>* property_list_ptr = &property_list;
+    
+    // Get property list
+    for (int i = 0; i < property_list_ptr->size(); i++) {
+        Dictionary property = property_list[i];
+        String property_name = property["name"];
+        godot::UtilityFunctions::print("Binding property: ", property_name);
+        auto get_func = [property_name]( godot::Object *obj ) { return obj->get( property_name ); };
+        auto set_func = [property_name]( godot::Object *obj, const Variant &value ) {
+            obj->set( property_name, value );
+        };
+        ut.set( property_name.utf8().get_data(), sol::property( get_func, set_func ) );
+    }
+
     // Get method list
-    List<MethodInfo> method_list;
-    List<MethodInfo>* method_list_ptr = &method_list;
-    ClassDB::class_get_method_list(class_name, method_list_ptr);
+    TypedArray<Dictionary> method_list = ClassDB::class_get_method_list(class_name);
+    TypedArray<Dictionary>* method_list_ptr = &method_list;
+    for (int i = 0; i < method_list_ptr->size(); i++)
+    {   
+        Dictionary method = method_list[i];
+        //godot::UtilityFunctions::print(JSON::stringify(method));
+
+        String method_name = method["name"];
+        godot::UtilityFunctions::print("Binding method: ", method_name);
+        auto func = [class_name, method](godot::Object* obj, sol::variadic_args va) {
+                Array args;
+                for (const auto& arg : va) {
+                    args.append(arg);
+                }
+                return obj->callv(method["name"], args);
+        };
+
+        //sol::function lua_func = sol::function::;
+
+        ut.set(method_name.utf8().get_data(), func);
+        /*if (!(ut.get)) {
+            godot::UtilityFunctions::print("Failed to bind method: ", method_name);
+        }*/
+    }
+    
 
     // Iterate through methods and bind them
-    for (const MethodInfo& method : method_list) {
+    /*for (const auto& method : method_list) {
         godot::UtilityFunctions::print("Binding method: ", method.name);
-        lua[class_name.utf8().get_data()][((String)method.name).utf8().get_data()] =
-            [class_name, method](godot::Object* obj, sol::variadic_args va) {
+        auto func = [class_name, method](godot::Object* obj, sol::variadic_args va) {
                 Array args;
                 for (const auto& arg : va) {
                     args.append(arg);
                 }
                 return obj->call(method.name, args);
         };
-    }
+        lua[class_name.utf8().get_data()][((String)method.name).utf8().get_data()] = func;
+
+        if (!(lua[class_name.utf8().get_data()][((String)method.name).utf8().get_data()] == func)) {
+            godot::UtilityFunctions::print("Failed to bind method: ", method.name);
+        }
+    }*/
 }
 
 void newhaven_core::bind_all_godot_classes(sol::state& lua) {
     auto all_classes = ClassDB::get_class_list();
 
     for (const String& class_name : all_classes) {
-        godot::UtilityFunctions::print(class_name);
+        godot::UtilityFunctions::print("binding class: ", class_name);
         if (class_name == String("OS") || class_name == String("DisplayServer") ||
-            class_name == String("RenderingDevice") || class_name == String("FileAccess")) {
+            class_name == String("RenderingDevice") || class_name == String("FileAccess")
+            || class_name == String("Engine") || class_name == String("SceneTree")){
             continue; // Exclude core engine classes
-            }
+        }
         bind_class_to_lua(lua, class_name);
     }
 }
@@ -81,6 +127,8 @@ void newhaven_core::initialize_lua(sol::state& lua) {
 			        v=t[v]
 			        if type(v)=="table" and not seen[v] then
 				        dump(v,i.."\t")
+                    elseif type(v)=="userdata" and not seen[v] then
+                        dump(getmetatable(v),i.."\t")
 			        end
 		        end
 	        end
