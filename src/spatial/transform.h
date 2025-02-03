5256b4beb03db6a0eaf3dec70571390f2667dbf8
Lucas C. Abbas <lucasbear9@gmail.com>
2025-02-03 05:01:24 -0900

    Refactor SpatialTransform class for improved readability and functionality

--- src/spatial/transform.h
+++ src/spatial/transform.h
@@ -3,7 +3,9 @@
 
 #include <godot_cpp/variant/transform3d.hpp>
 #include <godot_cpp/classes/node3d.hpp>
+#include <godot_cpp/classes/scene_tree.hpp>
 #include <vector>
+#include <sol/sol.hpp>
 
 #include "../core/scene_system.h"
 
@@ -12,58 +14,137 @@ using namespace newhaven_core;
 
 namespace newhaven_spatial
 {
+    void bindSpatialTransform( sol::state& lua );
+
     class SpatialTransform : public Component
     {
     private:
         struct Data
         {
-            mutable Transform3D global_transform;
-            mutable Transform3D local_transform;
-            mutable EulerOrder euler_rotation_order = EulerOrder::YXZ;
-            mutable Vector3 euler_rotation;
+            mutable Transform3D globalTransform;
+            mutable Transform3D localTransform;
+            mutable EulerOrder eulerRotationOrder = EulerOrder::YXZ;
+            mutable Vector3 eulerRotation;
             mutable Vector3 scale = Vector3( 1, 1, 1 );
 
             Viewport *viewport = nullptr;
 
-            bool top_level = false;
-            bool inside_world = false;
+            bool topLevel = false;
+            bool insideWorld = false;
 
-            RID visibility_parent;
+            RID visibilityParent;
 
             SpatialTransform *parentTransform = nullptr;
             std::vector<SpatialTransform *> children;
 
-            bool ignore_notification = false;
-            bool notify_local_transform = false;
-            bool notify_transform = false;
+            bool ignoreNotification = false;
+            bool notifyLocalTransform = false;
+            bool notifyTransform = false;
 
             bool visible = true;
-            bool disable_scale = false;
+            bool disableScale = false;
         } data;
 
     protected:
-        _FORCE_INLINE_ void  set_ignore_transform_notification( bool ignore ) { data.ignore_notification = ignore; }
+        _FORCE_INLINE_ void  setIgnoreTransformNotification( bool ignore ) { data.ignoreNotification = ignore; }
 
-        _FORCE_INLINE_ void _update_local_transform() const;
-        _FORCE_INLINE_ void _update_rotation_and_scale() const;
+        _FORCE_INLINE_ void _updateLocalTransform() const;
+        _FORCE_INLINE_ void _updateRotationAndScale() const;
 
         void _notification( int p_what );
 
     public:
         enum
         {
-            NOTIFICATION_TRANSFORM_CHANGED = SceneTree::NOTIFICATION_TRANSFORM_CHANGED,
+            NOTIFICATION_TRANSFORM_CHANGED = 2000,
             NOTIFICATION_ENTER_WORLD = 41,
             NOTIFICATION_EXIT_WORLD = 42,
             NOTIFICATION_VISIBILITY_CHANGED = 43,
             NOTIFICATION_LOCAL_TRANSFORM_CHANGED = 44,
         };
 
-        SpatialTransform *get_parent_transform() const;
+        SpatialTransform *getParentTransform() const;
 
-        Ref<World3D> get_world() const;
+        Ref<World3D> getWorld() const;
 
-        void set_position(const Vector3 &p_position);
+        void setPosition(const Vector3 &pPosition);
+
+        void setRotationOrder(EulerOrder pOrder);
+        void setRotation(const Vector3 &pEulerRad);
+        void setRotationDegrees(const Vector3 &pEulerDeg);
+        void setScale(const Vector3 &pScale);
+
+        void setGlobalPosition(const Vector3 &pPosition);
+        void setGlobalBasis(const Basis &pBasis);
+        void setGlobalRotation(const Vector3 &pEulerRad);
+        void setGlobalRotation_degrees(const Vector3 &pEulerDeg);
+
+        Vector3 getPosition() const;
+
+        EulerOrder getRotationOrder() const;
+        Vector3 getRotation() const;
+        Vector3 getRotationDegrees() const;
+        Vector3 getScale() const;
+
+        Vector3 getGlobalPosition() const;
+        Basis getGlobalBasis() const;
+        Vector3 getGlobalRotation() const;
+        Vector3 getGlobalRotationDegrees() const;
+
+        void setTransform(const Transform3D &pTransform) const;
+        void setBasis(const Basis &p_basis) const;
+        void setQuaternion(const Quaternion &pQuaternion) const;
+        void setGlobalTransform(const Transform3D &pTransform) const;
+
+        void setAsTopLevel(bool pEnabled);
+        void setAsTopLevelKeepLocal(bool pEnabled);
+        bool isScaleDisabled() const;
+
+        _FORCE_INLINE_ bool isInsideWorld() const { return data.insideWorld; }
+
+        Transform3D getRelativeTransform(const Entity *pParent) const;
+
+        void rotate(const Vector3 &pAxis, real_t pAngle);
+        void rotateX(real_t pAngle);
+        void rotateY(real_t pAngle);
+        void rotateZ(real_t pAngle);
+        void translate(const Vector3 &pOffset);
+        void scale(const Vector3 &pRatio);
+
+        void rotateObjectLocal(const Vector3 &pAxis, real_t pAngle);
+        void scaleObjectLocal(const Vector3 &pScale);
+        void translateObjectLocal(const Vector3 &pOffset);
+
+        void globalRotate(const Vector3 &pAxis, real_t pAngle);
+        void globalScale(const Vector3 &pScale);
+        void globalTranslate(const Vector3 &pOffset);
+
+        void lookAt(const Vector3 &pTarget, const Vector3 &pUp = Vector3(0, 1, 0), bool pUseModelFront = false);
+        void lookAtFromPosition(const Vector3 &pPos, const Vector3 &pTarget, const Vector3 &pUp = Vector3(0, 1, 0), bool pUseModelFront = false);
+
+        Vector3 toLocal(Vector3 pGlobal) const;
+        Vector3 toGlobal(Vector3 pLocal) const;
+
+        void setNotifyTransform(bool pEnabled);
+        bool isTransformNotificationEnabled() const;
+
+        void setNotifyLocalTransform(bool pEnabled);
+        bool isLocalTransformNotificationEnabled() const;
+
+        void orthonormalize();
+        void setIdentity();
+
+        void setVisible(bool pVisible);
+        void show();
+        void hide();
+        bool isVisible() const;
+        bool isVisibleInScene() const;
+
+        void forceUpdateTransform();
+
+        void setVisibilityParent(const std::string &pPath);
+
+        SpatialTransform();
     };
 }
 
