3876f995d8b591db1e31f9fb1363951c53a18ffb
Lucas C. Abbas <lucasbear9@gmail.com>
2025-02-12 18:23:26 -0900

    Initialize V8 engine in App class and manage its lifecycle

--- src/app.cpp
+++ src/app.cpp
@@ -6,6 +6,7 @@
 
 #include <godot_cpp/variant/utility_functions.hpp>
 #include <godot_cpp/classes/engine.hpp>
+#include <libplatform/libplatform.h>
 
 #include"core/lua_bind.h"
 #include "core/scene_system.h"
@@ -29,6 +30,11 @@ App::App() {
 
 App::~App() {
     //UtilityFunctions::print("App destructor");
+
+    isolate->Dispose();
+    v8::V8::Dispose();
+    //v8::V8::ShutdownPlatform();
+    delete platform;
 }
 
 void App::_ready() {
@@ -252,6 +258,32 @@ void App::_ready() {
 
         printScene(scene)
     )");
+}
+
+void App::init_v8() {
+    v8::V8::InitializeICUDefaultLocation(".");
+    v8::V8::InitializeExternalStartupData(".");
+    platform = v8::platform::NewDefaultPlatform().release();
+    v8::V8::InitializePlatform(platform);
+    v8::V8::Initialize();
+
+    v8::Isolate::CreateParams create_params;
+    create_params.array_buffer_allocator = v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+    isolate = v8::Isolate::New(create_params);
+
+    {
+        v8::Isolate::Scope isolate_scope(isolate);
+        v8::Local<v8::Context> context = v8::Context::New(isolate);
+        v8::Context::Scope context_scope(context);
+
+        v8::Local<v8::String> source = v8::String::NewFromUtf8Literal(isolate, "'Hello from V8!'");
+        v8::Local<v8::Script> script = v8::Script::Compile(context, source).ToLocalChecked();
+        v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+
+        v8::String::Utf8Value utf8(isolate, result);
+        std::string str(*utf8);
+        UtilityFunctions::print(str.c_str());
+    }
 }
 
 void App::start() {
