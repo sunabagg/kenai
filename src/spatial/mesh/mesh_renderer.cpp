e3f920a88f31fd2ce9b364a7cecd3d8ca2fda81e
Lucas C. Abbas <lucasbear9@gmail.com>
2025-02-12 18:28:21 -0900

    revert mistakes

--- src/spatial/mesh/mesh_renderer.cpp
+++ src/spatial/mesh/mesh_renderer.cpp
@@ -3,24 +3,22 @@
 using namespace godot;
 using namespace newhaven_core;
 
-namespace newhaven_spatial_mesh {
-    void bindMeshRenderer(sol::state& lua) {
-        lua.new_usertype<MeshRenderer>(
-            "MeshRenderer",
-            sol::constructors<MeshRenderer()>(),
-            sol::base_classes, sol::bases<Component>(),
-            "create_convex_collision", &MeshRenderer::createConvexCollision,
-            "create_debug_tangents", &MeshRenderer::createDebugTangents,
-            "create_trimesh_collision", &MeshRenderer::createTrimeshCollision,
-            "find_blend_shape_by_name", &MeshRenderer::findBlendShapeByName,
-            "get_blend_shape_count", &MeshRenderer::getBlendShapeCount,
-            "get_blend_shape_value", &MeshRenderer::getBlendShapeValue,
-            "get_surface_override_material_count", &MeshRenderer::getSurfaceOverideMaterialCount,
-            "set_blend_shape_value", &MeshRenderer::setBlendShapeValue,
-            "getFromEntity", [](Entity* entity) {
-                Component* component = entity->getComponent("MeshRenderer");
-                return static_cast<MeshRenderer*>(component);
-            }
-        );
-    }
+void newhaven_spatial_mesh::bindMeshRenderer(sol::state& lua) {
+    lua.new_usertype<MeshRenderer>(
+        "MeshRenderer",
+        sol::constructors<MeshRenderer()>(),
+        sol::base_classes, sol::bases<Component>(),
+        "create_convex_collision", &MeshRenderer::createConvexCollision,
+        "create_debug_tangents", &MeshRenderer::createDebugTangents,
+        "create_trimesh_collision", &MeshRenderer::createTrimeshCollision,
+        "find_blend_shape_by_name", &MeshRenderer::findBlendShapeByName,
+        "get_blend_shape_count", &MeshRenderer::getBlendShapeCount,
+        "get_blend_shape_value", &MeshRenderer::getBlendShapeValue,
+        "get_surface_override_material_count", &MeshRenderer::getSurfaceOverideMaterialCount,
+        "set_blend_shape_value", &MeshRenderer::setBlendShapeValue,
+        "getFromEntity", [](Entity* entity) {
+            Component* component = entity->getComponent("MeshRenderer");
+            return static_cast<MeshRenderer*>(component);
+        }
+    );
 }
