2b6e16846c35a293bb789f98b9fb08dc36dba7be
Lucas C. Abbas <lucasbear9@gmail.com>
2025-02-13 21:34:34 -0900

    Add IVisualInstance class and binding for Lua integration

---
+++ src/spatial/i_visual_instance.cpp
@@ -0,0 +1,16 @@
+#include "i_visual_instance.h"
+
+void newhaven_spatial::bindVisualInstance(sol::state& lua) {
+    lua.new_usertype<IVisualInstance>(
+        "IVisualInstance",
+        sol::constructors<IVisualInstance()>(),
+        sol::base_classes, sol::bases<Component>(),
+        "layerMask", sol::property(&IVisualInstance::getLayerMask, &IVisualInstance::setLayerMask),
+        "sortingOffset", sol::property(&IVisualInstance::getSortingOffset, &IVisualInstance::setSortingOffset),
+        "sortingUseAabbCenter", sol::property(&IVisualInstance::getSortingUseAabbCenter, &IVisualInstance::setSortingUseAabbCenter),
+        "getAabb", &IVisualInstance::getAabb,
+        "onGetAabb", &IVisualInstance::onGetAabb,
+        "getLayerMaskValue", &IVisualInstance::getLayerMaskValue,
+        "setLayerMaskValue", &IVisualInstance::setLayerMaskValue
+    );
+}
---
+++ src/spatial/i_visual_instance.h
@@ -0,0 +1,61 @@
+#ifndef I_VISUAL_INSTANCE_H
+#define I_VISUAL_INSTANCE_H
+
+#include <godot_cpp/classes/visual_instance3d.hpp>
+
+#include "../core/scene_system.h"
+#include "transform.h"
+
+using namespace godot;
+using namespace newhaven_core;
+
+namespace newhaven_spatial
+{
+    void bindVisualInstance(sol::state& lua);
+
+    class IVisualInstance : public Component
+    {
+    protected:
+        VisualInstance3D* node;
+    public:
+        int getLayerMask() {
+            return node->get_layer_mask();
+        }
+        void setLayerMask(int mask) {
+            node->set_layer_mask(mask);
+        }
+
+        float getSortingOffset() {
+            return node->get_sorting_offset();
+        }
+        void setSortingOffset(float offset) {
+            node->set_sorting_offset(offset);
+        }
+
+        bool getSortingUseAabbCenter() {
+            return node->is_sorting_use_aabb_center();
+        }
+
+        void setSortingUseAabbCenter(bool use) {
+            node->set_sorting_use_aabb_center(use);
+        }
+
+        virtual AABB onGetAabb() {
+            return node->get_aabb();
+        }
+
+        AABB getAabb() {
+            return onGetAabb();
+        }
+
+        bool getLayerMaskValue(int layer) {
+            return node->get_layer_mask_value(layer);
+        }
+
+        void setLayerMaskValue(int layer, bool value) {
+            node->set_layer_mask_value(layer, value);
+        }
+    };
+}
+
+#endif // I_VISUAL_INSTANCE_H
