8e8c7e8e7acf9322f08690cfa165a37ab22245de
Lucas C. Abbas <lucasbear9@gmail.com>
2025-02-20 15:38:58 -0900

    Add Transform3D class with native bindings and transformation methods

---
+++ sunaba/core/Transform3D.hx
@@ -0,0 +1,23 @@
+package sunaba.core;
+
+@:native("Transform3D")
+extern class Transform3D {
+    public var basis : Basis;
+    public var origin : Vector3;
+    public function new(basis : NativeBasis, origin : NativeVector3);
+    @:native("new")
+    public static function zero() : Transform3D;
+    public function affineInverse() : Transform3D;
+    public function interpolateWith(tform : Transform3D, c : Float) : Transform3D;
+    public function isEqualApprox(tform : Transform3D) : Bool;
+    public function lookingAt(target : NativeVector3, up : NativeVector3 = new Vector3(0, 1, 0)) : Transform3D;
+    public function orthonormalized() : Transform3D;
+    public function rotated(axis : NativeVector3, angle : Float) : Transform3D;
+    public function rotatedLocal(axis : NativeVector3, angle : Float) : Transform3D;
+    public function scaled(scale : NativeVector3) : Transform3D;
+    public function scaledLocal(scale : NativeVector3) : Transform3D;
+    public function translated(translation : NativeVector3) : Transform3D;
+    public function translatedLocal(translation : NativeVector3) : Transform3D;
+    @:native("tostring")
+    public function toString() : String;
+}
