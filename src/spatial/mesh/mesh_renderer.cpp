2a91d5e543715e5b11bcbe1bcdd37743ce16d531
Lucas C. Abbas <lucasbear9@gmail.com>
2025-02-20 17:44:18 -0900

    Refactor MeshRenderer Lua bindings for consistency and add generic getComponent method in Behavior class

--- src/spatial/mesh/mesh_renderer.cpp
+++ src/spatial/mesh/mesh_renderer.cpp
@@ -8,14 +8,14 @@ void sunaba::spatial::mesh::bindMeshRenderer(sol::state& lua) {
         "MeshRenderer",
         sol::constructors<MeshRenderer()>(),
         sol::base_classes, sol::bases<IGeometryInstance, IVisualInstance, Component>(),
-        "create_convex_collision", &MeshRenderer::createConvexCollision,
-        "create_debug_tangents", &MeshRenderer::createDebugTangents,
-        "create_trimesh_collision", &MeshRenderer::createTrimeshCollision,
-        "find_blend_shape_by_name", &MeshRenderer::findBlendShapeByName,
-        "get_blend_shape_count", &MeshRenderer::getBlendShapeCount,
-        "get_blend_shape_value", &MeshRenderer::getBlendShapeValue,
-        "get_surface_override_material_count", &MeshRenderer::getSurfaceOverideMaterialCount,
-        "set_blend_shape_value", &MeshRenderer::setBlendShapeValue,
+        "createConvexCollision", &MeshRenderer::createConvexCollision,
+        "createDebugTangents", &MeshRenderer::createDebugTangents,
+        "createTrimeshCollision", &MeshRenderer::createTrimeshCollision,
+        "findBlendShapeByName", &MeshRenderer::findBlendShapeByName,
+        "getBlendShapeCount", &MeshRenderer::getBlendShapeCount,
+        "getBlendShapeValue", &MeshRenderer::getBlendShapeValue,
+        "getSurfaceOverrideMaterialCount", &MeshRenderer::getSurfaceOverideMaterialCount,
+        "setBlendShapeValue", &MeshRenderer::setBlendShapeValue,
         "getFromEntity", [](Entity* entity) {
             return entity->getComponentByT<MeshRenderer>();
         }
--- sunaba/core/Behavior.hx
+++ sunaba/core/Behavior.hx
@@ -23,4 +23,29 @@ class Behavior {
     public function onUpdate(deltaTime : Float) : Void {}
 
     public function onPhysicsUpdate(delatTime : Float) : Void {}
+
+    @:generic
+    public function getComponent<T>(entity : Entity = null) : T {
+        if (entity == null) {
+            entity = component.entity;
+        }
+        try {
+            var comp = entity.getComponent(component.getType());
+            if (comp != null) {
+                return cast comp;
+            }
+        }
+        catch (e : Dynamic) {}
+        try {
+            var userComp = entity.getUserComponent(component.getType());
+            if (userComp != null) {
+                return cast userComp;
+            }
+            return null;
+        }
+        catch (e : Dynamic) {}
+
+        throw "Component not found";
+        return null;
+    }
 }
