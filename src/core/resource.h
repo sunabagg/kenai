9563a3f8c00713ff761d5a2a9c1d5c5c8f65af38
Lucas C. Abbas <lucasbear9@gmail.com>
2025-02-26 10:16:00 -0900

    Refactor Resource class to use raw pointers instead of smart pointers for GodotResource

--- src/core/resource.h
+++ src/core/resource.h
@@ -14,31 +14,31 @@ namespace sunaba::core {
 
     class Resource : public BaseObject {
     private:
-        godot::Ref<GodotResource> resource = nullptr; // Pointer to the Resource instance
+    GodotResource* resource = nullptr; // Pointer to the Resource instance
     public:
         // Constructor with Ref<GodotResource> parameter
-        Resource(godot::Ref<GodotResource> res) : resource(res) {
+        Resource(GodotResource* res) : resource(res) {
         }
 
         // Constructor with no parameters
         Resource() {
-            auto r  = memnew(GodotResource);
-            resource = godot::Ref<GodotResource>(r);
+            resource  = memnew(GodotResource);
+            //resource = godot::Ref<GodotResource>(r);
         }
 
         // Getter for the Resource node
-        godot::Ref<GodotResource> getResource() {
+        GodotResource* getResource() {
             return resource;
         }
 
         // Setter for the Resource node
-        void setResource(godot::Ref<GodotResource> res) {
+        void setResource(GodotResource* res) {
             resource = res;
             resource->reference();
         }
 
         Resource* duplicate(bool subresources = false) {
-            return new Resource(resource->duplicate(subresources));
+            return new Resource(resource->duplicate(subresources).ptr());
         }
 
         void emitChanged() {
