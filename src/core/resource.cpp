01e142e56380be0397f551b74cc91a7704462e01
Lucas C. Abbas <lucasbear9@gmail.com>
2025-02-26 09:55:56 -0900

    Add base class binding for Resource in Lua integration

--- src/core/resource.cpp
+++ src/core/resource.cpp
@@ -3,6 +3,7 @@
 void sunaba::core::bindResource(sol::state &lua) {
     lua.new_usertype<sunaba::core::Resource>("Resource",
         sol::constructors<sunaba::core::Resource()>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject>(),
         "duplicate", &sunaba::core::Resource::duplicate,
         "emitChanged", &sunaba::core::Resource::emitChanged
     );
