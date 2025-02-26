73badcff4341e9da42140692c5507cefa4dc0ef5
Lucas C. Abbas <lucasbear9@gmail.com>
2025-02-26 12:06:27 -0900

    Add conversion functions for Lua and Godot types in convert_godot_lua

---
+++ src/core/convert_godot_lua.cpp
@@ -0,0 +1,118 @@
+#include "convert_godot_lua.h"
+
+using namespace godot;
+
+namespace sunaba::core {
+    template<typename ref_t>
+    Variant to_variant(const sol::basic_object<ref_t>& object) {
+        if (object.is<bool>()) {
+            return object.template as<bool>();
+        }
+        else if (object.is<std::string>()) {
+            return String(object.template as<std::string>().c_str());
+        }
+        else if (object.is<int64_t>()) {
+            return object.template as<int64_t>();
+        }
+        else if (object.is<double>()) {
+            return object.template as<double>();
+        }
+        else {
+            return Variant();
+        }
+    }
+    
+    Variant to_variant(const sol::object& object) {
+        return to_variant<>(object);
+    }
+    
+    Variant to_variant(const sol::stack_object& object) {
+        return to_variant<>(object);
+    }
+    
+    Variant to_variant(const sol::stack_proxy_base& proxy) {
+        return to_variant<>(sol::stack_object(proxy.lua_state(), proxy.stack_index()));
+    }
+    
+    
+    
+    Variant to_variant(lua_State *L, int index) {
+        return to_variant(sol::stack_object(L, index));
+    }
+    
+    sol::stack_object to_lua(lua_State *lua_state, const Variant& value) {
+        switch (value.get_type()) {
+            case Variant::NIL:
+                sol::stack::push(lua_state, sol::nil);
+                break;
+    
+            case Variant::BOOL:
+                sol::stack::push(lua_state, (bool) value);
+                break;
+    
+            case Variant::INT:
+                sol::stack::push(lua_state, (int64_t) value);
+                break;
+    
+            case Variant::FLOAT:
+                sol::stack::push(lua_state, (double) value);
+                break;
+    
+            case Variant::STRING:
+                sol::stack::push(lua_state, (String) value);
+                break;
+    
+            case Variant::STRING_NAME:
+                sol::stack::push(lua_state, (StringName) value);
+                break;
+            
+            default:
+                sol::stack::push(lua_state, value);
+                break;
+        }
+        return sol::stack_object(lua_state, -1);
+    }
+
+    Array to_array(const sol::variadic_args& args) {
+        Array array;
+        for (const auto& arg : args) {
+            array.append(to_variant(arg));
+        }
+        return array;
+    }
+
+    Array to_array(const sol::table& table) {
+        Array array;
+        for (const auto& pair : table) {
+            array.append(to_variant(pair.second));
+        }
+        return array;
+    }
+
+    Dictionary to_dictionary(const sol::table& table) {
+        Dictionary dictionary;
+        for (const auto& pair : table) {
+            dictionary[to_variant(pair.first)] = to_variant(pair.second);
+        }
+        return dictionary;
+    }
+
+    sol::table to_table(sol::state_view& state, const Dictionary& dictionary) {
+        sol::table table = state.create_table();
+        for (int i = 0; i < dictionary.size(); ++i) {
+            const Variant& key = dictionary.keys()[i];
+            const Variant& value = dictionary[key];
+            table[to_lua(state, key)] = to_lua(state, value);
+        }
+        return table;
+    }
+
+    sol::table to_table(sol::state_view& state, const Array& array) {
+        sol::table table = state.create_table();
+        for (int i = 0; i < array.size(); ++i) {
+            const Variant& value = array[i];
+            table[i] = to_lua(state, value);
+        }
+        return table;
+    }
+}
---
+++ src/core/convert_godot_lua.h
@@ -0,0 +1,27 @@
+#ifndef CONVERT_GODOT_LUA_H
+#define CONVERT_GODOT_LUA_H
+
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+using namespace godot;
+
+namespace sunaba::core {
+    Variant to_variant(const sol::object& object);
+    Variant to_variant(const sol::stack_object& object);
+    Variant to_variant(const sol::stack_proxy_base& object);    
+    Variant to_variant(const sol::protected_function_result& function_result);
+    Variant to_variant(const sol::load_result& load_result);
+    Variant to_variant(lua_State *L, int index);
+    template<typename ref_t>
+    Variant to_variant(const sol::basic_object<ref_t>& object);
+    sol::stack_object to_lua(lua_State *lua_state, const Variant& value);
+
+    Array to_array(const sol::variadic_args& args);
+    Array to_array(const sol::table& table);
+    Dictionary to_dictionary(const sol::table& table);
+    sol::table to_table(sol::state_view& state, const Dictionary& dictionary);
+    sol::table to_table(sol::state_view& state, const Array& array);
+}
+
+#endif // CONVERT_GODOT_LUA_H
