5c70e67d2f4532f712e923628bc1f7356322f692
Lucas C. Abbas <lucasbear9@gmail.com>
2025-02-26 16:51:44 -0900

    Add ImageTexture class and Lua binding for integration

---
+++ src/core/image_texture.cpp
@@ -0,0 +1,14 @@
+#include "image_texture.h"
+
+void sunaba::core::bindImageTexture(sol::state &lua) {
+    lua.new_usertype<sunaba::core::ImageTexture>("ImageTexture",
+        sol::constructors<sunaba::core::ImageTexture()>(),
+        sol::base_classes, sol::bases<sunaba::core::Texture2D>(),
+        "image", sol::property(
+            &sunaba::core::ImageTexture::getImage, 
+            &sunaba::core::ImageTexture::setImage
+        ),
+        "createFromImage", &sunaba::core::ImageTexture::createFromImage,
+        "getFormat", &sunaba::core::ImageTexture::getFormat
+    );
+}
---
+++ src/core/image_texture.h
@@ -0,0 +1,73 @@
+#ifndef SUNABA_IMAGE_TEXTURE_H
+#define SUNABA_IMAGE_TEXTURE_H
+
+#include <godot_cpp/classes/image_texture.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotImageTexture godot::ImageTexture
+
+#include "texture2d.h"
+
+#include "io/binary_data.h"
+#include "io/io_index.h"
+
+using namespace godot;
+using namespace sunaba::core::io;
+
+namespace sunaba::core {
+    void bindImageTexture(sol::state &lua);
+
+    class ImageTexture : public Texture2D {
+    private:
+        GodotImageTexture* imageTexture = nullptr; // Pointer to the ImageTexture instance
+    public:
+        // Constructor with Ref<GodotImageTexture> parameter
+        ImageTexture(GodotImageTexture* imgTex) {
+            setImageTexture(imgTex);
+        }
+
+        // Constructor with no parameters
+        ImageTexture() {
+            setImageTexture(memnew(GodotImageTexture));
+        }
+
+        // Getter for the ImageTexture node
+        GodotImageTexture* getImageTexture() {
+            return imageTexture;
+        }
+
+        // Setter for the ImageTexture node
+        void setImageTexture(GodotImageTexture* imgTex) {
+            imageTexture = imgTex;
+            imageTexture->reference();
+            setTexture2D(imageTexture);
+        }
+
+        Image* getImage() {
+            return new Image(imageTexture->get_image().ptr());
+        }
+
+        void setImage(Image* image) {
+            imageTexture->set_image(image->getImage());
+        }
+
+        static ImageTexture* createFromImage(Image* image, bool generateMipmaps = false) {
+            return new ImageTexture(GodotImageTexture::create_from_image(image->getImage(), generateMipmaps));
+        }
+
+        int getFormat() {
+            return imageTexture->get_format();
+        }
+
+        void setSizeOverride(Vector2 size) {
+            imageTexture->set_size_override(size);
+        }
+
+        void update(Image* Image) {
+            imageTexture->update(Image->getImage());
+        }
+    };
+}
+
+#endif // SUNABA_IMAGE_TEXTURE_H
