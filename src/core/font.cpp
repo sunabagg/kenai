1616697146e4c7f3713e91deee6cac56395d4406
Lucas C. Abbas <lucasbear9@gmail.com>
2025-02-26 17:31:41 -0900

    Add Font class and Lua binding for integration

---
+++ src/core/font.cpp
@@ -0,0 +1,34 @@
+#include "font.h"
+
+void sunaba::core::bindFont(sol::state &lua) {
+    lua.new_usertype<sunaba::core::Font>("Font",
+        sol::constructors<sunaba::core::Font()>(),
+        sol::base_classes, sol::bases<sunaba::core::Resource>(),
+        "fallbacks", sol::property(
+            &sunaba::core::Font::getFallbacks, 
+            &sunaba::core::Font::setFallbacks
+        ),
+        "getAscent", &sunaba::core::Font::getAscent,
+        "getCharSize", &sunaba::core::Font::getCharSize,
+        "getDescent", &sunaba::core::Font::getDescent,
+        "getFaceCount", &sunaba::core::Font::getFaceCount,
+        "getFontName", &sunaba::core::Font::getFontName,
+        "getFontStyle", &sunaba::core::Font::getFontStyle,
+        "getFontWeight", &sunaba::core::Font::getFontWeight,
+        "getHeight", &sunaba::core::Font::getHeight,
+        "getMultilineStringSize", &sunaba::core::Font::getMultilineStringSize,
+        "getOpentypeFeatures", &sunaba::core::Font::getOpentypeFeatures,
+        "getOtNameStrings", &sunaba::core::Font::getOtNameStrings,
+        "getSpacing", &sunaba::core::Font::getSpacing,
+        "getStringSize", &sunaba::core::Font::getStringSize,
+        "getSupportedChars", &sunaba::core::Font::getSupportedChars,
+        "getSupportedFeatureList", &sunaba::core::Font::getSupportedFeatureList,
+        "getSupportedVariationList", &sunaba::core::Font::getSupportedVariationList,
+        "getUnderlinePosition", &sunaba::core::Font::getUnderlinePosition,
+        "getUnderlineThickness", &sunaba::core::Font::getUnderlineThickness,
+        "hasChar", &sunaba::core::Font::hasChar,
+        "isLanguageSupported", &sunaba::core::Font::isLanguageSupported,
+        "isScriptSupported", &sunaba::core::Font::isScriptSupported,
+        "setCacheCapacity", &sunaba::core::Font::setCacheCapacity,
+    );
+}
---
+++ src/core/font.h
@@ -0,0 +1,153 @@
+#ifndef SUNABA_FONT_H
+#define SUNABA_FONT_H
+
+#include <godot_cpp/classes/font.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotFont godot::Font
+
+#include "resource.h"
+#include "convert_godot_lua.h"
+
+using namespace godot;
+
+namespace sunaba::core {
+    void bindFont(sol::state &lua);
+
+    class Font : public Resource {
+    private:
+        GodotFont* font = nullptr; // Pointer to the Font instance
+    public:
+        // Constructor with Ref<GodotFont> parameter
+        Font(GodotFont* fnt) {
+            setFont(fnt);
+        }
+
+        // Constructor with no parameters
+        Font() {
+            setFont(memnew(GodotFont));
+        }
+
+        // Getter for the Font node
+        GodotFont* getFont() {
+            return font;
+        }
+
+        // Setter for the Font node
+        void setFont(GodotFont* fnt) {
+            font = fnt;
+            font->reference();
+            setResource(font);
+        }
+
+        std::vector<Font*> getFallbacks() {
+            auto fontList = font->get_fallbacks();
+            std::vector<Font*> fonts;
+            for (int i = 0; i < fontList.size(); ++i) {
+                auto fVar = fontList[i];
+                auto f = (Ref<GodotFont>)fVar;
+                fonts.push_back(new Font(f.ptr()));
+            }
+            return fonts;
+        }
+
+        void setFallbacks(const std::vector<Font*>& fallbacks) {
+            Array fontList;
+            for (const auto& f : fallbacks) {
+                fontList.append(f->getFont());
+            }
+            font->set_fallbacks(fontList);
+        }
+
+        float getAscent(int fontSize = 16) {
+            return font->get_ascent(fontSize);
+        }
+
+        Vector2 getCharSize(int chr, int fontSize) {
+            return font->get_char_size(chr, fontSize);
+        }
+
+        float getDescent(int fontSize = 16) {
+            return font->get_descent(fontSize);
+        }
+
+        float getFaceCount() {
+            return font->get_face_count();
+        }
+
+        std::string getFontName() {
+            return String(font->get_font_name()).utf8().get_data();
+        }
+
+        int getFontStyle() {
+            return font->get_font_style();
+        }
+
+        int getFontWeight() {
+            return font->get_font_weight();
+        }
+
+        float getHeight(int fontSize = 16) {
+            return font->get_height(fontSize);
+        }
+
+        Vector2 getMultilineStringSize(const std::string &text, int alignment= 0.0f, float width = -1.0f, int fontSize = 16, int maxLines = -1, int brkFlags = 3, int justificationFlags = 3, int direction = 0, int orientation = 0) {
+            return font->get_multiline_string_size(text.c_str(), static_cast<HorizontalAlignment>(alignment), width, fontSize, maxLines, brkFlags, justificationFlags, static_cast<TextServer::Direction>(direction), static_cast<TextServer::Orientation>(orientation));
+        }
+
+        sol::table getOpentypeFeatures(sol::state_view lua_state) {
+            return to_table(lua_state, font->get_opentype_features());
+        }
+
+        sol::table getOtNameStrings(sol::state_view lua_state) {
+            return to_table(lua_state, font->get_ot_name_strings());
+        }
+
+        int getSpacing(int spacing) {
+            return font->get_spacing(static_cast<TextServer::SpacingType>(spacing));
+        }
+
+        Vector2 getStringSize(const std::string& text, int alignment = 0, float width = 0, int fontSize = 16, int justificationFlags = 3, int direction = 0, int orientation = 0) {
+            return font->get_string_size(text.c_str(), static_cast<HorizontalAlignment>(alignment), width, fontSize, justificationFlags, static_cast<TextServer::Direction>(direction), static_cast<TextServer::Orientation>(orientation));
+        }
+
+        std::string getSupportedChars() {
+            return String(font->get_supported_chars()).utf8().get_data();
+        }
+
+        sol::table getSupportedFeatureList(sol::state_view lua_state) {
+            return to_table(lua_state, font->get_supported_feature_list());
+        }
+
+        sol::table getSupportedVariationList(sol::state_view lua_state) {
+            return to_table(lua_state, font->get_supported_variation_list());
+        }
+
+        float getUnderlinePosition(int fontSize = 16) {
+            return font->get_underline_position(fontSize);
+        }
+
+        float getUnderlineThickness(int fontSize = 16) {
+            return font->get_underline_thickness(fontSize);
+        }
+
+        bool hasChar(int chr) {
+            return font->has_char(chr);
+        }
+
+        bool isLanguageSupported(const std::string& lang) {
+            return font->is_language_supported(lang.c_str());
+        }
+
+        bool isScriptSupported(const std::string& script) {
+            return font->is_script_supported(script.c_str());
+        }
+
+        void setCacheCapacity(int singleLine, int multiLine) {
+            font->set_cache_capacity(singleLine, multiLine);
+        }
+    };
+}
+
+#endif // SUNABA_FONT_H
