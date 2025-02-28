3f6c32ea471b58db2f791580b9fc3824caddc391
Lucas C. Abbas <lucasbear9@gmail.com>
2025-02-28 12:15:16 -0900

    Add AnimatedTexture class and Lua bindings for animated texture operations

---
+++ src/core/animated_texture.cpp
@@ -0,0 +1,35 @@
+#include "animated_texture.h"
+
+void sunaba::core::bindAnimatedTexture(sol::state &lua) {
+    lua.new_usertype<sunaba::core::AnimatedTexture>("AnimatedTexture",
+        sol::constructors<sunaba::core::AnimatedTexture()>(),
+        sol::base_classes, sol::bases<sunaba::core::Texture2D>(),
+        "currentFrame", sol::property(
+            &sunaba::core::AnimatedTexture::getCurrentFrame, 
+            &sunaba::core::AnimatedTexture::setCurrentFrame
+        ),
+        "frames", sol::property(
+            &sunaba::core::AnimatedTexture::getFrames, 
+            &sunaba::core::AnimatedTexture::setFrames
+        ),
+        "oneShot", sol::property(
+            &sunaba::core::AnimatedTexture::getOneShot, 
+            &sunaba::core::AnimatedTexture::setOneShot
+        ),
+        "frames", sol::property(
+            &sunaba::core::AnimatedTexture::getFrames, 
+            &sunaba::core::AnimatedTexture::setFrames
+        ),
+        "speedScale", sol::property(
+            &sunaba::core::AnimatedTexture::getSpeedScale, 
+            &sunaba::core::AnimatedTexture::setSpeedScale
+        ),
+        "getFrameDuration", &sunaba::core::AnimatedTexture::getFrameDuration,
+        "getFrameTexture", &sunaba::core::AnimatedTexture::getFrameTexture,
+        "setFrameDuration", &sunaba::core::AnimatedTexture::setFrameDuration,
+        "setFrameTexture", &sunaba::core::AnimatedTexture::setFrameTexture,
+        "cast", [](Resource* instance) {
+            return new AnimatedTexture(static_cast<GodotAnimatedTexture*>(instance->getResource()));
+        }
+    );
+}
---
+++ src/core/animated_texture.h
@@ -0,0 +1,102 @@
+#ifndef ANIMATED_TEXTURE_H
+#define ANIMATED_TEXTURE_H
+
+#include <godot_cpp/classes/animated_texture.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotAnimatedTexture godot::AnimatedTexture
+
+#include "texture2d.h"
+#include "convert_godot_lua.h"
+
+using namespace godot;
+
+namespace sunaba::core {
+    void bindAnimatedTexture(sol::state &lua);
+
+    class AnimatedTexture : public Texture2D {
+    private:
+        GodotAnimatedTexture* animatedTexture = nullptr; // Pointer to the AnimatedTexture instance
+    public:
+        // Constructor with Ref<GodotAnimatedTexture> parameter
+        AnimatedTexture(GodotAnimatedTexture* animatedTexture) {
+            setAnimatedTexture(animatedTexture);
+        }
+
+        // Constructor with no parameters
+        AnimatedTexture() {
+            setAnimatedTexture(memnew(GodotAnimatedTexture));
+        }
+
+        // Getter for the AnimatedTexture node
+        GodotAnimatedTexture* getAnimatedTexture() {
+            return animatedTexture;
+        }
+
+        // Setter for the AnimatedTexture node
+        void setAnimatedTexture(GodotAnimatedTexture* animatedTexture) {
+            this->animatedTexture = animatedTexture;
+            animatedTexture->reference();
+            setTexture(animatedTexture);
+        }
+
+        int getCurrentFrame() {
+            return animatedTexture->get_current_frame();
+        }
+
+        void setCurrentFrame(int frame) {
+            animatedTexture->set_current_frame(frame);
+        }
+
+        int getFrames() {
+            return animatedTexture->get_frames();
+        }
+
+        void setFrames(int frames) {
+            animatedTexture->set_frames(frames);
+        }
+
+        bool getOneShot() {
+            return animatedTexture->get_one_shot();
+        }
+
+        void setOneShot(bool oneShot) {
+            animatedTexture->set_one_shot(oneShot);
+        }
+
+        bool getPause() {
+            return animatedTexture->get_pause();
+        }
+
+        void setPause(bool paused) {
+            animatedTexture->set_pause(paused);
+        }
+
+        float getSpeedScale() {
+            return animatedTexture->get_speed_scale();
+        }
+
+        void setSpeedScale(float speedScale) {
+            animatedTexture->set_speed_scale(speedScale);
+        }
+
+        float getFrameDuration(int frame) {
+            return animatedTexture->get_frame_duration(frame);
+        }
+
+        Texture2D* getFrameTexture(int frame) {
+            return new Texture2D(animatedTexture->get_frame_texture(frame).ptr());
+        }
+
+        void setFrameDuration(int frame, float duration) {
+            animatedTexture->set_frame_duration(frame, duration);
+        }
+
+        void setFrameTexture(int frame, Texture2D* texture) {
+            animatedTexture->set_frame_texture(frame, texture->getTexture2D());
+        }
+    };
+}
+
+#endif // ANIMATED_TEXTURE_H
