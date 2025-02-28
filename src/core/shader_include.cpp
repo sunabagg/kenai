016725c5a6c8ef336a37034704e14a6822d53ff1
Lucas C. Abbas <lucasbear9@gmail.com>
2025-02-28 13:36:33 -0900

    Add ShaderInclude class and Lua bindings for shader include operations

---
+++ src/core/shader_include.cpp
@@ -0,0 +1,15 @@
+#include "shader_include.h"
+
+void sunaba::core::bindShaderInclude(sol::state &lua) {
+    lua.new_usertype<sunaba::core::ShaderInclude>("ShaderInclude",
+        sol::constructors<sunaba::core::ShaderInclude()>(),
+        sol::base_classes, sol::bases<sunaba::core::Resource>(),
+        "code", sol::property(
+            &sunaba::core::ShaderInclude::getCode, 
+            &sunaba::core::ShaderInclude::setCode
+        ),
+        "cast", [](Resource* instance) {
+            return new ShaderInclude(static_cast<GodotShaderInclude*>(instance->getResource()));
+        }
+    );
+}
---
+++ src/core/shader_include.h
@@ -0,0 +1,53 @@
+#ifndef SHADER_INCLUDE_H
+#define SHADER_INCLUDE_H
+
+#include <godot_cpp/classes/shader_include.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotShaderInclude godot::ShaderInclude
+
+#include "resource.h"
+
+using namespace godot;
+
+namespace sunaba::core {
+    void bindShaderInclude(sol::state &lua);
+
+    class ShaderInclude : public Resource {
+    private:
+        GodotShaderInclude* shaderInclude = nullptr; // Pointer to the Shader instance
+    public:
+        // Constructor with Ref<GodotShader> parameter
+        ShaderInclude(GodotShaderInclude* shaderInclude) {
+            setShader(shaderInclude);
+        }
+
+        // Constructor with no parameters
+        ShaderInclude() {
+            setShader(memnew(GodotShaderInclude));
+        }
+
+        // Getter for the Shader node
+        GodotShaderInclude* getShader() {
+            return shaderInclude;
+        }
+
+        // Setter for the Shader node
+        void setShader(GodotShaderInclude* shaderInclude) {
+            this->shaderInclude = shaderInclude;
+            shaderInclude->reference();
+            setResource(shaderInclude);
+        }
+
+        void setCode(const String &code) {
+            shaderInclude->set_code(code);
+        }
+
+        String getCode() const {
+            return shaderInclude->get_code();
+        }
+    };
+}
+
+#endif
