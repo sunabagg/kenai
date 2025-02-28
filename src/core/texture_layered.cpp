d925d64275bfb3692d7aeb5681fbc5fbe69904c8
Lucas C. Abbas <lucasbear9@gmail.com>
2025-02-28 14:17:30 -0900

    Add TextureLayered class and Lua bindings for layered texture operations

---
+++ src/core/texture_layered.cpp
@@ -0,0 +1,17 @@
+#include "texture_layered.h"
+
+void sunaba::core::bindTextureLayered(sol::state &lua) {
+    lua.new_usertype<sunaba::core::TextureLayered>("TextureLayered",
+        sol::constructors<sunaba::core::TextureLayered()>(),
+        sol::base_classes, sol::bases<sunaba::core::Texture>(),
+        "getFormat", &sunaba::core::TextureLayered::getFormat,
+        "getHeight", &sunaba::core::TextureLayered::getHeight,
+        "getLayerData", &sunaba::core::TextureLayered::getLayerData,
+        "getLayeredType", &sunaba::core::TextureLayered::getLayeredType,
+        "getLayers", &sunaba::core::TextureLayered::getLayers,
+        "getWidth", &sunaba::core::TextureLayered::getWidth,
+        "cast", [](Resource* instance) {
+            return new TextureLayered(static_cast<GodotTextureLayered*>(instance->getResource()));
+        }
+    );
+}
---
+++ src/core/texture_layered.h
@@ -0,0 +1,75 @@
+#ifndef TEXTURE_LAYERED_H
+#define TEXTURE_LAYERED_H
+
+#include <godot_cpp/classes/texture_layered.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotTextureLayered godot::TextureLayered
+
+#include "texture.h"
+#include "image.h"
+#include "convert_godot_lua.h"
+
+using namespace godot;
+
+namespace sunaba::core {
+    void bindTextureLayered(sol::state &lua);
+
+    class TextureLayered : public Texture {
+    private:
+        GodotTextureLayered* textureLayered = nullptr; // Pointer to the TextureLayered instance
+    public:
+        // Constructor with Ref<GodotTextureLayered> parameter
+        TextureLayered(GodotTextureLayered* textureLayered) {
+            setTextureLayered(textureLayered);
+        }
+
+        // Constructor with no parameters
+        TextureLayered() {
+            setTextureLayered(memnew(GodotTextureLayered));
+        }
+
+        // Getter for the TextureLayered node
+        GodotTextureLayered* getTextureLayered() {
+            return textureLayered;
+        }
+
+        // Setter for the TextureLayered node
+        void setTextureLayered(GodotTextureLayered* textureLayered) {
+            this->textureLayered = textureLayered;
+            textureLayered->reference();
+            setTexture(textureLayered);
+        }
+
+        int getFormat() {
+            return textureLayered->get_format();
+        }
+
+        int getHeight() {
+            return textureLayered->get_height();
+        }
+
+        Image* getLayerData(int layer) {
+            return new Image(textureLayered->get_layer_data(layer).ptr());
+        }
+
+        int getLayeredType() {
+            return textureLayered->get_layered_type();
+        }
+
+        int getLayers() {
+            return textureLayered->get_layers();
+        }
+
+        int getWidth() {
+            return textureLayered->get_width();
+        }
+
+        bool hasMipmaps() {
+            return textureLayered->has_mipmaps();
+        }
+    };
+}
+
+#endif // TEXTURE_LAYERED_H
