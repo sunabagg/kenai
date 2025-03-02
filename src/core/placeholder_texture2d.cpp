a4c93306356cc52101447ea753f5bee6ea470a31
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-02 11:09:25 -0900

    Add PlaceholderTexture2D class and Lua bindings for placeholder texture handling

---
+++ src/core/placeholder_texture2d.cpp
@@ -0,0 +1,15 @@
+#include "placeholder_texture2d.h"
+
+void sunaba::core::bindPlaceholderTexture2D(sol::state &lua) {
+    lua.new_usertype<sunaba::core::PlaceholderTexture2D>("PlaceholderTexture2D",
+        sol::constructors<sunaba::core::PlaceholderTexture2D()>(),
+        sol::base_classes, sol::bases<sunaba::core::Texture2D>(),
+        "size", sol::property(
+            &sunaba::core::PlaceholderTexture2D::getSize, 
+            &sunaba::core::PlaceholderTexture2D::setSize
+        ),
+        "cast", [](Resource* instance) {
+            return new PlaceholderTexture2D(static_cast<GodotPlaceholderTexture2D*>(instance->getResource()));
+        }
+    );
+}
---
+++ src/core/placeholder_texture2d.h
@@ -0,0 +1,53 @@
+#ifndef PLACEHOLDER_TEXTURE2D_H
+#define PLACEHOLDER_TEXTURE2D_H
+
+#include <godot_cpp/classes/placeholder_texture2d.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotPlaceholderTexture2D godot::PlaceholderTexture2D
+
+#include "texture2d.h"
+
+using namespace godot;
+
+namespace sunaba::core {
+    void bindPlaceholderTexture2D(sol::state &lua);
+
+    class PlaceholderTexture2D : public Texture2D {
+    private:
+        GodotPlaceholderTexture2D* placeholderTexture2D = nullptr; // Pointer to the PlaceholderTexture2D instance
+    public:
+        // Constructor with Ref<GodotPlaceholderTexture2D> parameter
+        PlaceholderTexture2D(GodotPlaceholderTexture2D* placeholderTexture2D) {
+            setPlaceholderTexture2D(placeholderTexture2D);
+        }
+
+        // Constructor with no parameters
+        PlaceholderTexture2D() {
+            setPlaceholderTexture2D(memnew(GodotPlaceholderTexture2D));
+        }
+
+        // Getter for the PlaceholderTexture2D node
+        GodotPlaceholderTexture2D* getPlaceholderTexture2D() {
+            return placeholderTexture2D;
+        }
+
+        // Setter for the PlaceholderTexture2D node
+        void setPlaceholderTexture2D(GodotPlaceholderTexture2D* placeholderTexture2D) {
+            this->placeholderTexture2D = placeholderTexture2D;
+            placeholderTexture2D->reference();
+            setTexture(placeholderTexture2D);
+        }
+
+        Vector2 getSize() {
+            return placeholderTexture2D->get_size();
+        }
+
+        void setSize(Vector2 size) {
+            placeholderTexture2D->set_size(size);
+        }
+    };
+}
+
+#endif // PLACEHOLDER_TEXTURE2D_H
