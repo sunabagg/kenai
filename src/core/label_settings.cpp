a672a205f2153e159348f9d2f700fe3e6bd9bfe8
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-01 20:05:41 -0900

    Add LabelSettings class and Lua bindings for font and styling properties

---
+++ src/core/label_settings.cpp
@@ -0,0 +1,17 @@
+#include "label_settings.h"
+
+void sunaba::core::bindLabelSettings(sol::state &lua) {
+    lua.new_usertype<LabelSettings>("LabelSettings",
+        sol::constructors<LabelSettings(), LabelSettings(GodotLabelSettings*)>(),
+        sol::base_classes, sol::bases<Resource>(),
+        "font", sol::property(&LabelSettings::getFont, &LabelSettings::setFont),
+        "fontColor", sol::property(&LabelSettings::getFontColor, &LabelSettings::setFontColor),
+        "fontSize", sol::property(&LabelSettings::getFontSize, &LabelSettings::setFontSize),
+        "lineSpacing", sol::property(&LabelSettings::getLineSpacing, &LabelSettings::setLineSpacing),
+        "outlineColor", sol::property(&LabelSettings::getOutlineColor, &LabelSettings::setOutlineColor),
+        "outlineSize", sol::property(&LabelSettings::getOutlineSize, &LabelSettings::setOutlineSize),
+        "shadowColor", sol::property(&LabelSettings::getShadowColor, &LabelSettings::setShadowColor),
+        "shadowOffset", sol::property(&LabelSettings::getShadowOffset, &LabelSettings::setShadowOffset),
+        "shadowSize", sol::property(&LabelSettings::getShadowSize, &LabelSettings::setShadowSize)
+    );
+}
---
+++ src/core/label_settings.h
@@ -0,0 +1,119 @@
+#ifndef LABEL_SETTINGS_H
+#define LABEL_SETTINGS_H
+
+#include <godot_cpp/classes/label_settings.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotLabelSettings godot::LabelSettings
+
+#include "resource.h"
+#include "convert_godot_lua.h"
+#include "font.h"
+
+using namespace godot;
+
+namespace sunaba::core {
+    void bindLabelSettings(sol::state &lua);
+
+    class LabelSettings : public Resource {
+    private:
+        GodotLabelSettings* labelSettings = nullptr; // Pointer to the LabelSettings instance
+    public:
+        // Constructor with Ref<GodotLabelSettings> parameter
+        LabelSettings(GodotLabelSettings* labelSettings) {
+            setLabelSettings(labelSettings);
+        }
+
+        // Constructor with no parameters
+        LabelSettings() {
+            setLabelSettings(memnew(GodotLabelSettings));
+        }
+
+        // Getter for the LabelSettings node
+        GodotLabelSettings* getLabelSettings() {
+            return labelSettings;
+        }
+
+        // Setter for the LabelSettings node
+        void setLabelSettings(GodotLabelSettings* labelSettings) {
+            this->labelSettings = labelSettings;
+            labelSettings->reference();
+            setResource(labelSettings);
+        }
+
+        Font* getFont() {
+            return new Font(labelSettings->get_font().ptr());
+        }
+
+        void setFont(Font* font) {
+            labelSettings->set_font(Ref<GodotFont>(font->getFont()));
+        }
+
+        Color getFontColor() {
+            return labelSettings->get_font_color();
+        }
+
+        void setFontColor(Color color) {
+            labelSettings->set_font_color(color);
+        }
+
+        int getFontSize() {
+            return labelSettings->get_font_size();
+        }
+
+        void setFontSize(int size) {
+            labelSettings->set_font_size(size);
+        }
+
+        float getLineSpacing() {
+            return labelSettings->get_line_spacing();
+        }
+
+        void setLineSpacing(float spacing) {
+            labelSettings->set_line_spacing(spacing);
+        }
+
+        Color getOutlineColor() {
+            return labelSettings->get_outline_color();
+        }
+
+        void setOutlineColor(Color color) {
+            labelSettings->set_outline_color(color);
+        }
+
+        int getOutlineSize() {
+            return labelSettings->get_outline_size();
+        }
+
+        void setOutlineSize(int size) {
+            labelSettings->set_outline_size(size);
+        }
+
+        Color getShadowColor() {
+            return labelSettings->get_shadow_color();
+        }
+
+        void setShadowColor(Color color) {
+            labelSettings->set_shadow_color(color);
+        }
+
+        Vector2 getShadowOffset() {
+            return labelSettings->get_shadow_offset();
+        }
+
+        void setShadowOffset(Vector2 offset) {
+            labelSettings->set_shadow_offset(offset);
+        }
+
+        int getShadowSize() {
+            return labelSettings->get_shadow_size();
+        }
+
+        void setShadowSize(int size) {
+            labelSettings->set_shadow_size(size);
+        }
+    };
+}
+
+#endif // LABEL_SETTINGS_H
