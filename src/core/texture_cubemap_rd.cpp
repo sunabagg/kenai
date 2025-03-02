4781a3717f8ceb7ae5f69c543c4653c0aa3f8120
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-02 10:34:02 -0900

    Add constructors for TextureCubemapRD Lua bindings

--- src/core/texture_cubemap_rd.cpp
+++ src/core/texture_cubemap_rd.cpp
@@ -2,6 +2,7 @@
 
 void sunaba::core::bindTextureCubemapRD(sol::state &lua) {
     lua.new_usertype<sunaba::core::TextureCubemapRD>("TextureCubemapRD",
+        sol::constructors<sunaba::core::TextureCubemapRD()>(),
         sol::base_classes, sol::bases<sunaba::core::TextureLayeredRD>(),
         "cast", [](sunaba::core::Resource* instance) {
             return new TextureCubemapRD(static_cast<GodotTextureCubemapRD*>(instance->getResource()));
