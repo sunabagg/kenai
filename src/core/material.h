b6e6a461a2ccd184444c821d36bdadd0b4cf37fc
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-05 16:52:56 -0900

    Add Material class and header for resource management in Godot

---
+++ src/core/material.h
@@ -0,0 +1,63 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+#include <godot_cpp/classes/material.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotMaterial godot::Material
+
+#include "resource.h"
+#include "convert_godot_lua.h"
+
+using namespace godot;
+
+namespace sunaba::core {
+    void bindMaterial(sol::state_view& lua);
+
+    class Material : public Resource {
+    private:
+        GodotMaterial* material;
+    public:
+
+        Material(GodotMaterial* material) {
+            setMaterial(material);
+        }
+
+        Material() {
+            setMaterial(memnew(GodotMaterial));
+        }
+
+        GodotMaterial* getMaterial() {
+            return material;
+        }
+
+        void setMaterial(GodotMaterial* m) {
+            material = m;
+            material->reference();
+            setResource(material);
+        }
+
+        Material* getNextPass() {
+            return new Material(material->get_next_pass());
+        }
+
+        void setNextPass(Material* next) {
+            material->set_next_pass(next->getMaterial());
+        }
+
+        int getRenderPriority() {
+            return material->get_render_priority();
+        }
+
+        void setRenderPriority(int priority) {
+            material->set_render_priority(priority);
+        }
+
+        Resource* createPlaceholder() {
+            return new Material(material->create_placeholder().ptr());
+        }
+    };
+}
+
+#endif // MATERIAL_H
