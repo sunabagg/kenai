0b5ca765a5fa0be27670d582f3c1d7a3cf4ddee5
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-06 15:37:02 -0900

    Add FogMaterial class and associated files for spatial rendering

---
+++ src/spatial/fog_material.cpp
@@ -0,0 +1,10 @@
+#include "fog_material.h"
+
+void sunaba::spatial::bindFogMaterial(sol::state_view &lua) {
+    lua.new_usertype<FogMaterial>("FogMaterial",
+        sol::constructors<FogMaterial()>(),
+        sol::base_classes, sol::bases<sunaba::core::Material>(),
+        "albedo", sol::property(&FogMaterial::getAlbedo, &FogMaterial::setAlbedo),
+        "density", sol::property(&FogMaterial::getDensity, &FogMaterial::setDensity),
+        "edgeFade", sol::property(&FogMaterial::getEdgeFade, &FogMaterial::setEdgeFade));
+}
---
+++ src/spatial/fog_material.h
@@ -0,0 +1,84 @@
+#ifndef FOG_MATERIAL_H
+#define FOG_MATERIAL_H
+
+#include <godot_cpp/classes/fog_material.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotFogMaterial godot::FogMaterial
+
+#include "../core/material.h"
+#include "../core/convert_godot_lua.h"
+
+
+
+using namespace godot;
+
+namespace sunaba::spatial {
+    void bindFogMaterial(sol::state_view& lua);
+
+    class FogMaterial : public sunaba::core::Material {
+    private:
+        GodotFogMaterial* fogMaterial;
+    public:
+
+        FogMaterial(GodotFogMaterial* material) {
+            setFogMaterial(material);
+        }
+
+        FogMaterial() {
+            setFogMaterial(memnew(GodotFogMaterial));
+        }
+
+        GodotFogMaterial* getFogMaterial() {
+            return fogMaterial;
+        }
+
+        void setFogMaterial(GodotFogMaterial* m) {
+            fogMaterial = m;
+            fogMaterial->reference();
+            setMaterial(fogMaterial);
+        }
+
+        Color getAlbedo() {
+            return fogMaterial->get_albedo();
+        }
+
+        void setAlbedo(Color albedo) {
+            fogMaterial->set_albedo(albedo);
+        }
+
+        float getDensity() {
+            return fogMaterial->get_density();
+        }
+
+        void setDensity(float density) {
+            fogMaterial->set_density(density);
+        }
+
+        float getEdgeFade() {
+            return fogMaterial->get_edge_fade();
+        }
+
+        void setEdgeFade(float edgeFade) {
+            fogMaterial->set_edge_fade(edgeFade);
+        }
+
+        Color getEmission() {
+            return fogMaterial->get_emission();
+        }
+
+        void setEmission(Color emission) {
+            fogMaterial->set_emission(emission);
+        }
+
+        float getHeightFalloff() {
+            return fogMaterial->get_height_falloff();
+        }
+
+        void setHeightFalloff(float heightFalloff) {
+            fogMaterial->set_height_falloff(heightFalloff);
+        }
+    };
+}
+#endif
