7e48659bf368aec0c3a6e1f5f8239c3f5ce00b2a
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-08 13:56:50 -0900

    Fix cast function in bindNoise to use Object::cast_to for resource conversion

--- src/core/noise.cpp
+++ src/core/noise.cpp
@@ -12,7 +12,7 @@ void sunaba::core::bindNoise(sol::state_view& lua) {
             "getSeamlessImage", &Noise::getSeamlessImage,
             "getSeamlessImage3d", &Noise::getSeamlessImage3d,
             "cast", [](Resource* instance) {
-                return new Noise(dynamic_cast<GodotNoise*>(instance->getResource()));
+                return new Noise(Object::cast_to<GodotNoise*>(instance->getResource()));
             }
     );
 }
