0c8bd8a9a379952cca6ac3fb4049fc1c6ff94fab
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-08 10:52:08 -0900

    Refactor Noise Lua bindings to include BaseObject in class hierarchy

--- src/core/noise.cpp
+++ src/core/noise.cpp
@@ -3,7 +3,7 @@
 void sunaba::core::bindNoise(sol::state_view& lua) {
         lua.new_usertype<Noise>("Noise",
             sol::constructors<Noise(), Noise(GodotNoise*)>(),
-            sol::base_classes, sol::bases<sunaba::core::Resource>(),
+            sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource>(),
             "getImage", &Noise::getImage,
             "getImage3d", &Noise::getImage3d,
             "getNoise1d", &Noise::getNoise1d,
