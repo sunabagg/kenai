b08f11ef83ef3a893ddd838638cf4d1d0995f72d
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-08 11:07:42 -0900

    Refactor FogMaterial Lua bindings to include BaseObject in class hierarchy

--- src/spatial/fog_material.cpp
+++ src/spatial/fog_material.cpp
@@ -3,7 +3,7 @@
 void sunaba::spatial::bindFogMaterial(sol::state_view &lua) {
     lua.new_usertype<FogMaterial>("FogMaterial",
         sol::constructors<FogMaterial()>(),
-        sol::base_classes, sol::bases<sunaba::core::Material>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::core::Material>(),
         "albedo", sol::property(&FogMaterial::getAlbedo, &FogMaterial::setAlbedo),
         "density", sol::property(&FogMaterial::getDensity, &FogMaterial::setDensity),
         "edgeFade", sol::property(&FogMaterial::getEdgeFade, &FogMaterial::setEdgeFade));
