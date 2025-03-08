69a9fa72ea23da2c33c50df1addd6e4f9d2b250b
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-07 16:45:40 -0900

    Add InputEventJoypadMotion class and Lua bindings for joypad motion input events

---
+++ src/input/input_event_joypad_motion.cpp
@@ -0,0 +1,13 @@
+#include "input_event_joypad_motion.h"
+
+void sunaba::input::bindInputEventJoypadMotion(sol::state_view& lua) {
+    lua.new_usertype<InputEventJoypadMotion>("InputEventJoypadMotion",
+        sol::constructors<InputEventJoypadMotion(), InputEventJoypadMotion(GodotInputEventJoypadMotion*)>(),
+        sol::base_classes, sol::bases<sunaba::input::InputEvent>(),
+        "axis", sol::property(&InputEventJoypadMotion::getAxis, &InputEventJoypadMotion::setAxis),
+        "axisValue", sol::property(&InputEventJoypadMotion::getAxisValue, &InputEventJoypadMotion::setAxisValue),
+        "cast", [](sunaba::core::Resource* instance) {
+            return new InputEventJoypadMotion(static_cast<GodotInputEventJoypadMotion*>(instance->getResource()));
+        }
+    );
+}
---
+++ src/input/input_event_joypad_motion.h
@@ -0,0 +1,58 @@
+#ifndef INPUT_EVENT_JOYPAD_MOTION_H
+#define INPUT_EVENT_JOYPAD_MOTION_H
+
+#include <godot_cpp/classes/input_event_joypad_motion.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotInputEventJoypadMotion godot::InputEventJoypadMotion
+
+#include "input_event.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+namespace sunaba::input {
+    void bindInputEventJoypadMotion(sol::state_view& lua);
+
+    class InputEventJoypadMotion : public sunaba::input::InputEvent {
+    private:
+        GodotInputEventJoypadMotion* event;
+    public:
+        InputEventJoypadMotion(GodotInputEventJoypadMotion* e) {
+            setInputEventJoypadMotion(e);
+        }
+
+        InputEventJoypadMotion() {
+            setInputEventJoypadMotion(memnew(GodotInputEventJoypadMotion));
+        }
+
+        GodotInputEventJoypadMotion* getInputEventJoypadMotion() const {
+            return event;
+        }
+
+        void setInputEventJoypadMotion(GodotInputEventJoypadMotion* e) {
+            event = e;
+            event->reference();
+            setInputEvent(e);
+        }
+
+        int getAxis() {
+            return event->get_axis();
+        }
+
+        void setAxis(int axis) {
+            event->set_axis(static_cast<JoyAxis>(axis));
+        }
+
+        float getAxisValue() {
+            return event->get_axis_value();
+        }
+
+        void setAxisValue(float value) {
+            event->set_axis_value(value);
+        }
+    };
+}
+
+#endif // INPUT_EVENT_JOYPAD_MOTION_H
