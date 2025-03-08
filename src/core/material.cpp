2a2e65aa0c660da113b21ced07d32b0af75cb025
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-08 10:54:12 -0900

    Refactor Material Lua bindings to include BaseObject in class hierarchy

--- src/core/material.cpp
+++ src/core/material.cpp
@@ -3,6 +3,7 @@
 void sunaba::core::bindMaterial(sol::state_view& lua) {
     lua.new_usertype<Material>("Material",
         sol::constructors<Material(), Material(GodotMaterial*)>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource>(),
         "nextPass", sol::property(&Material::getNextPass, &Material::setNextPass),
         "renderPriority", sol::property(&Material::getRenderPriority, &Material::setRenderPriority),
         "createPlaceholder", &Material::createPlaceholder
