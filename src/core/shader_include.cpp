402f8c2d13f9cdb3b3b3bd3f695091d59b670a9c
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-08 10:48:17 -0900

    Refactor ShaderInclude Lua bindings to include BaseObject in class hierarchy

--- src/core/shader_include.cpp
+++ src/core/shader_include.cpp
@@ -2,7 +2,7 @@
 
 void sunaba::core::bindShaderInclude(sol::state &lua) {
     lua.new_usertype<sunaba::core::ShaderInclude>("ShaderInclude",
-        sol::constructors<sunaba::core::ShaderInclude()>(),
+        sol::constructors<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::core::ShaderInclude()>(),
         sol::base_classes, sol::bases<sunaba::core::Resource>(),
         "code", sol::property(
             &sunaba::core::ShaderInclude::getCode, 
