86c44e3c2f2482b8510c1118c0880cf66b22e694
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-08 10:43:49 -0900

    Refactor TextureCubemapRD Lua bindings to include BaseObject in class hierarchy

--- src/core/texture_cubemap_rd.cpp
+++ src/core/texture_cubemap_rd.cpp
@@ -3,7 +3,7 @@
 void sunaba::core::bindTextureCubemapRD(sol::state &lua) {
     lua.new_usertype<sunaba::core::TextureCubemapRD>("TextureCubemapRD",
         sol::constructors<sunaba::core::TextureCubemapRD()>(),
-        sol::base_classes, sol::bases<sunaba::core::TextureLayeredRD>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::core::Texture, sunaba::core::TextureLayered, sunaba::core::TextureLayeredRD>(),
         "cast", [](sunaba::core::Resource* instance) {
             return new TextureCubemapRD(dynamic_cast<GodotTextureCubemapRD*>(instance->getResource()));
         }
