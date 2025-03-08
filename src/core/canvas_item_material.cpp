60c9e0bf8e80279669ed3b1b31aebf1f88fafc69
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-08 11:04:56 -0900

    Refactor CanvasItemMaterial Lua bindings to include BaseObject in class hierarchy

--- src/core/canvas_item_material.cpp
+++ src/core/canvas_item_material.cpp
@@ -3,7 +3,7 @@
 void sunaba::core::bindCanvasItemMaterial(sol::state_view &lua) {
     lua.new_usertype<CanvasItemMaterial>("CanvasItemMaterial",
         sol::constructors<CanvasItemMaterial()>(),
-        sol::base_classes, sol::bases<sunaba::core::Material>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::core::Material>(),
         "blendMode", sol::property(&CanvasItemMaterial::getBlendMode, &CanvasItemMaterial::setBlendMode),
         "lightMode", sol::property(&CanvasItemMaterial::getLightMode, &CanvasItemMaterial::setLightMode),
         "particlesAnimHFrames", sol::property(&CanvasItemMaterial::getParticlesAnimHFrames, &CanvasItemMaterial::setParticlesAnimHFrames));
