3fe48d64e3cd27eb80115137cff0cd851ce4127c
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-08 10:24:46 -0900

    Add InputEventMouseButton class and Lua bindings for mouse button input events

---
+++ src/input/input_event_mouse_button.cpp
@@ -0,0 +1,16 @@
+#include "input_event_mouse_button.h"
+
+void sunaba::input::bindInputEventMouseButton(sol::state_view& lua) {
+    lua.new_usertype<InputEventMouseButton>("InputEventMouseButton",
+        sol::constructors<InputEventMouseButton(), InputEventMouseButton(GodotInputEventMouseButton*)>(),
+        sol::base_classes, sol::bases<sunaba::input::InputEvent>(),
+        "buttonIndex", sol::property(&InputEventMouseButton::getButtonIndex, &InputEventMouseButton::setButtonIndex),
+        "canceled", sol::property(&InputEventMouseButton::isCanceled, &InputEventMouseButton::setCanceled),
+        "doubleclick", sol::property(&InputEventMouseButton::isDoubleclick, &InputEventMouseButton::setDoubleclick),
+        "factor", sol::property(&InputEventMouseButton::getFactor, &InputEventMouseButton::setFactor),
+        "pressed", sol::property(&InputEventMouseButton::isPressed, &InputEventMouseButton::setPressed),
+        "cast", [](sunaba::core::Resource* instance) {
+            return new InputEventMouseButton(dynamic_cast<GodotInputEventMouseButton*>(instance->getResource()));
+        }
+    );
+}
---
+++ src/input/input_event_mouse_button.h
@@ -0,0 +1,82 @@
+#ifndef INPUT_EVENT_MOUSE_BUTTON_H
+#define INPUT_EVENT_MOUSE_BUTTON_H
+
+#include <godot_cpp/classes/input_event_mouse_button.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotInputEventMouseButton godot::InputEventMouseButton
+
+#include "input_event.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+namespace sunaba::input {
+    void bindInputEventMouseButton(sol::state_view& lua);
+
+    class InputEventMouseButton : public sunaba::input::InputEvent {
+    private:
+        GodotInputEventMouseButton* event;
+    public:
+        InputEventMouseButton(GodotInputEventMouseButton* e) {
+            setInputEventMouseButton(e);
+        }
+
+        InputEventMouseButton() {
+            setInputEventMouseButton(memnew(GodotInputEventMouseButton));
+        }
+
+        GodotInputEventMouseButton* getInputEventMouseButton() const {
+            return event;
+        }
+
+        void setInputEventMouseButton(GodotInputEventMouseButton* e) {
+            event = e;
+            event->reference();
+            setInputEvent(e);
+        }
+
+        int getButtonIndex() {
+            return event->get_button_index();
+        }
+
+        void setButtonIndex(int index) {
+            event->set_button_index(static_cast<MouseButton>(index));
+        }
+
+        bool isCanceled() {
+            return event->is_canceled();
+        }
+
+        void setCanceled(bool cancel) {
+            event->set_canceled(cancel);
+        }
+
+        bool isDoubleclick() {
+            return event->is_double_click();
+        }
+
+        void setDoubleclick(bool doubleclick) {
+            event->set_double_click(doubleclick);
+        }
+
+        float getFactor() {
+            return event->get_factor();
+        }
+
+        void setFactor(float factor) {
+            event->set_factor(factor);
+        }
+
+        bool isPressed() {
+            return event->is_pressed();
+        }
+
+        void setPressed(bool pressed) {
+            event->set_pressed(pressed);
+        }
+    };
+}
+
+#endif // INPUT_EVENT_MOUSE_BUTTON_H
