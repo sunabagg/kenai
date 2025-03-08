15dfe9ebf1420f35184a3b0bb3db25eaa3143c06
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-08 11:26:35 -0900

    Add Lua bindings for InputEventWithModifiers class

--- src/input/input_event_with_modifiers.cpp
+++ src/input/input_event_with_modifiers.cpp
@@ -1,1 +1,16 @@
 #include "input_event_with_modifiers.h"
+
+void sunaba::input::bindInputEventWithModifiers(sol::state_view& lua) {
+    lua.new_usertype<InputEventWithModifiers>("InputEventWithModifiers",
+        sol::constructors<InputEventWithModifiers(), InputEventWithModifiers(GodotInputEventWithModifiers*)>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::input::InputEvent, sunaba::input::InputEventFromWindow>(),
+        "altPressed", sol::property(&InputEventWithModifiers::getAltPressed, &InputEventWithModifiers::setAltPressed),
+        "commandOrControlAutoremap", sol::property(&InputEventWithModifiers::getCommandOrControlAutoremap, &InputEventWithModifiers::setCommandOrControlAutoremap),
+        "ctrlPressed", sol::property(&InputEventWithModifiers::isCtrlPressed, &InputEventWithModifiers::setCtrlPressed),
+        "metaPressed", sol::property(&InputEventWithModifiers::isMetaPressed, &InputEventWithModifiers::setMetaPressed),
+        "shiftPressed", sol::property(&InputEventWithModifiers::isShiftPressed, &InputEventWithModifiers::setShiftPressed),
+        "cast", [](sunaba::core::Resource* instance) {
+            return new InputEventWithModifiers(dynamic_cast<GodotInputEventWithModifiers*>(instance->getResource()));
+        }
+    );
+}
