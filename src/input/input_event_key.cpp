a14d6d76ac594969c6e05fae11e3a0e76b7411be
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-07 17:02:36 -0900

    Add InputEventKey class and Lua bindings for key input events

---
+++ src/input/input_event_key.cpp
@@ -0,0 +1,23 @@
+#include "input_event_key.h"
+
+void sunaba::input::bindInputEventKey(sol::state_view& lua) {
+    lua.new_usertype<InputEventKey>("InputEventKey",
+        sol::constructors<InputEventKey(), InputEventKey(GodotInputEventKey*)>(),
+        sol::base_classes, sol::bases<sunaba::input::InputEvent>(),
+        "echo", sol::property(&InputEventKey::isEcho, &InputEventKey::setEcho),
+        "keyLabel", sol::property(&InputEventKey::getKeyLabel, &InputEventKey::setKeyLabel),
+        "location", sol::property(&InputEventKey::getLocation, &InputEventKey::setLocation),
+        "physicalKeycode", sol::property(&InputEventKey::getPhysicalKeycode, &InputEventKey::setPhysicalKeycode),
+        "unicode", sol::property(&InputEventKey::getUnicode, &InputEventKey::setUnicode),
+        "asTextKeyLabel", &InputEventKey::asTextKeyLabel,
+        "asTextKeycode", &InputEventKey::asTextKeycode,
+        "asTextLocation", &InputEventKey::asTextLocation,
+        "asTextPhysicalKeycode", &InputEventKey::asTextPhysicalKeycode,
+        "getKeyLabelWithModifiers", &InputEventKey::getKeyLabelWithModifiers,
+        "getKeycodeWithModifiers", &InputEventKey::getKeycodeWithModifiers,
+        "getPhysicalKeycodeWithModifiers", &InputEventKey::getPhysicalKeycodeWithModifiers,
+        "cast", [](sunaba::core::Resource* instance) {
+            return new InputEventKey(dynamic_cast<GodotInputEventKey*>(instance->getResource()));
+        }
+    );
+}
---
+++ src/input/input_event_key.h
@@ -0,0 +1,119 @@
+#ifndef INPUT_EVENT_KEY_H
+#define INPUT_EVENT_KEY_H
+
+#include <godot_cpp/classes/input_event_key.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+#include <string>
+
+#define GodotInputEventKey godot::InputEventKey
+
+#include "input_event.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+namespace sunaba::input {
+    void bindInputEventKey(sol::state_view& lua);
+
+    class InputEventKey : public sunaba::input::InputEvent {
+    private:
+        GodotInputEventKey* event;
+    public:
+        InputEventKey(GodotInputEventKey* e) {
+            setInputEventKey(e);
+        }
+
+        InputEventKey() {
+            setInputEventKey(memnew(GodotInputEventKey));
+        }
+
+        GodotInputEventKey* getInputEventKey() const {
+            return event;
+        }
+
+        void setInputEventKey(GodotInputEventKey* e) {
+            event = e;
+            event->reference();
+            setInputEvent(e);
+        }
+
+        bool isEcho() {
+            return event->is_echo();
+        }
+
+        void setEcho(bool echo) {
+            event->set_echo(echo);
+        }
+
+        int getKeyLabel() {
+            return event->get_key_label();
+        }
+
+        void setKeyLabel(int key) {
+            event->set_key_label(static_cast<Key>(key));
+        }
+
+        int getLocation() {
+            return event->get_location();
+        }
+
+        void setLocation(int location) {
+            event->set_location(static_cast<KeyLocation>(location));
+        }
+
+        int getPhysicalKeycode() {
+            return event->get_physical_keycode();
+        }
+
+        void setPhysicalKeycode(int key) {
+            event->set_physical_keycode(static_cast<Key>(key));
+        }
+
+        bool isPressed() {
+            return event->is_pressed();
+        }
+
+        void setPressed(bool pressed) {
+            event->set_pressed(pressed);
+        }
+
+        int getUnicode() {
+            return event->get_unicode();
+        }
+
+        void setUnicode(int unicode) {
+            event->set_unicode(unicode);
+        }
+
+        std::string asTextKeyLabel() {
+            return event->as_text_key_label().utf8().get_data();
+        }
+
+        std::string asTextKeycode() {
+            return event->as_text_keycode().utf8().get_data();
+        }
+
+        std::string asTextLocation() {
+            return event->as_text_location().utf8().get_data();
+        }
+
+        std::string asTextPhysicalKeycode() {
+            return event->as_text_physical_keycode().utf8().get_data();
+        }
+
+        int getKeyLabelWithModifiers() {
+            return event->get_key_label_with_modifiers();
+        }
+
+        int getKeycodeWithModifiers() {
+            return event->get_keycode_with_modifiers();
+        }
+
+        int getPhysicalKeycodeWithModifiers() {
+            return event->get_physical_keycode_with_modifiers();
+        }
+    };
+}
+
+#endif // INPUT_EVENT_KEY_H
