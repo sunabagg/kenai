9195b5ea5f7d71e750c4ff442f5ec8924cc7ffda
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-07 16:26:57 -0900

    Add InputEventFromWindow class and Lua bindings for window input events

---
+++ src/input/input_event_from_window.cpp
@@ -0,0 +1,12 @@
+#include "input_event_from_window.h"
+
+void sunaba::input::bindInputEventFromWindow(sol::state_view& lua) {
+    lua.new_usertype<InputEventFromWindow>("InputEventFromWindow",
+        sol::constructors<InputEventFromWindow(), InputEventFromWindow(GodotInputEventFromWindow*)>(),
+        sol::base_classes, sol::bases<sunaba::input::InputEvent>(),
+        "windowId", sol::property(&InputEventFromWindow::getWindowId, &InputEventFromWindow::setWindowId),
+        "cast", [](sunaba::core::Resource* instance) {
+            return new InputEventFromWindow(static_cast<GodotInputEventFromWindow*>(instance->getResource()));
+        }
+    );
+}
---
+++ src/input/input_event_from_window.h
@@ -0,0 +1,51 @@
+#ifndef INPUT_EVENT_FROM_WINDOW_H
+#define INPUT_EVENT_FROM_WINDOW_H
+
+#include <godot_cpp/classes/input_event_from_window.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotInputEventFromWindow godot::InputEventFromWindow
+
+#include "input_event.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+namespace sunaba::input {
+    void bindInputEventFromWindow(sol::state_view& lua);
+
+    class InputEventFromWindow : public sunaba::input::InputEvent {
+    private:
+        GodotInputEventFromWindow* event;
+    public:
+        InputEventFromWindow(GodotInputEventFromWindow* e) {
+            setInputEventFromWindow(e);
+        }
+
+        InputEventFromWindow() {
+            setInputEventFromWindow(memnew(GodotInputEventFromWindow));
+        }
+
+        GodotInputEventFromWindow* getInputEventFromWindow() const {
+            return event;
+        }
+
+        void setInputEventFromWindow(GodotInputEventFromWindow* e) {
+            event = e;
+            event->reference();
+            setInputEvent(e);
+        }
+
+        int getWindowId() {
+            return event->get_window_id();
+        }
+
+        void setWindowId(int id) {
+            event->set_window_id(id);
+        }
+
+    };
+}
+
+#endif // INPUT_EVENT_FROM_WINDOW_H
