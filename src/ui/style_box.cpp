a87b8696d03963b056e7a0e4cce10a8bdfaf272c
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-09 12:06:02 -0800

    Add StyleBox class and Lua bindings for style management

---
+++ src/ui/style_box.cpp
@@ -0,0 +1,24 @@
+#include "style_box.h"
+
+void sunaba::ui::bindStyleBox(sol::state &lua) {
+    using namespace sol;
+    using namespace sunaba::ui;
+
+    lua.new_usertype<StyleBox>("StyleBox",
+        sol::constructors<StyleBox()>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource>(),
+        "contentMarginTop", sol::property(&StyleBox::getContentMarginTop, &StyleBox::setContentMarginTop),
+        "contentMarginBottom", sol::property(&StyleBox::getContentMarginBottom, &StyleBox::setContentMarginBottom),
+        "contentMarginLeft", sol::property(&StyleBox::getContentMarginLeft, &StyleBox::setContentMarginLeft),
+        "contentMarginRight", sol::property(&StyleBox::getContentMarginRight, &StyleBox::setContentMarginRight),
+        "getContentMargin", &StyleBox::getContentMargin,
+        "getMargin", &StyleBox::getMargin,
+        "getMinimumSize", &StyleBox::getMinimumSize,
+        "getOffset", &StyleBox::getOffset,
+        "setContentMargin", &StyleBox::setContentMargin,
+        "setContentMarginAll", &StyleBox::setContentMarginAll,
+        "cast", [](Resource* p_resource) {
+            return new StyleBox(godot::Object::cast_to<GodotStyleBox>(p_resource.getResource()));
+        }
+    );
+}
---
+++ src/ui/style_box.h
@@ -0,0 +1,102 @@
+#ifndef STYLE_BOX_H
+#define STYLE_BOX_H
+
+#include <godot_cpp/classes/style_box.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotStyleBox godot::StyleBox
+
+#include "../core/resource.h"
+#include "canvas_item.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+namespace sunaba::ui {
+    void bindStyleBox(sol::state &lua);
+
+    class StyleBox : public Resource {
+    private:
+        GodotStyleBox* style_box = nullptr; // Pointer to the StyleBox instance
+    public:
+        // Constructor with Node* parameter
+        StyleBox(GodotStyleBox* p_node) {
+            setStyleBox(p_node);
+        }
+    
+        // Constructor with no parameters
+        StyleBox() {
+            setStyleBox(memnew(GodotStyleBox));
+        }
+
+        // Getter for the StyleBox node
+        GodotStyleBox* getStyleBox() {
+            return style_box;
+        }
+
+        // Setter for the StyleBox node
+        void setStyleBox(GodotStyleBox* p_node) {
+            style_box = p_node;
+            setResource(style_box);
+        }
+
+        float getContentMarginTop() {
+            return style_box->get_content_margin(SIDE_TOP);
+        }
+
+        void setContentMarginTop(float margin) {
+            style_box->set_content_margin(SIDE_TOP, margin);
+        }
+
+        float getContentMarginBottom() {
+            return style_box->get_content_margin(SIDE_BOTTOM);
+        }
+
+        void setContentMarginBottom(float margin) {
+            style_box->set_content_margin(SIDE_BOTTOM, margin);
+        }
+
+        float getContentMarginLeft() {
+            return style_box->get_content_margin(SIDE_LEFT);
+        }
+
+        void setContentMarginLeft(float margin) {
+            style_box->set_content_margin(SIDE_LEFT, margin);
+        }
+
+        float getContentMarginRight() {
+            return style_box->get_content_margin(SIDE_RIGHT);
+        }
+
+        void setContentMarginRight(float margin) {
+            style_box->set_content_margin(SIDE_RIGHT, margin);
+        }
+
+        float getContentMargin(int margin) {
+            return style_box->get_content_margin(static_cast<Side>(margin));
+        }
+
+        float getMargin(int margin) {
+            return style_box->get_margin(static_cast<Side>(margin));
+        }
+
+        Vector2 getMinimumSize() {
+            return style_box->get_minimum_size();
+        }
+
+        Vector2 getOffset() {
+            return style_box->get_offset();
+        }
+
+        void setContentMargin(int margin, float value) {
+            style_box->set_content_margin(static_cast<Side>(margin), value);
+        }
+
+        void setContentMarginAll(float offset) {
+            style_box->set_content_margin_all(offset);
+        }
+    };
+}
+
+#endif // STYLE_BOX_H
