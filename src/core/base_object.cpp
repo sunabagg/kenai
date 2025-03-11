2f2e391cbd4f689c7a35a173486b07d39b5d6a52
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-11 11:33:37 -0800

    Implement BaseObject JavaScript class with constructor, finalizer, and free method

--- src/core/base_object.cpp
+++ src/core/base_object.cpp
@@ -11,6 +11,66 @@ namespace sunaba::core
         "BaseObject",
         .finalizer = jsBaseObjectFinalizer,
     };
+
+    void jsBaseObjectFinalizer(JSContext* rt, JSValue value ) {
+        BaseObject* baseObj = (BaseObject*)JS_GetOpaque(value, BaseObject::baseObjectClassID);
+        if (baseObj) {
+            if (baseObj->hasBeenFreed()) {
+                delete baseObj;
+            }
+        }
+    }
+
+    JSValue jsBaseObjectConstructor(JSContext* ctx, JSValueConst newTarget,  int argc, JSValueConst* argv) {
+        if (argc != 0) return JS_EXCEPTION;
+
+        BaseObject* obj = new BaseObject();
+
+        JSValue objVal = JS_NewObjectClass(ctx, BaseObject::baseObjectClassID);
+        JS_SetOpaque(objVal, obj);
+
+        return objVal;
+    }
+
+    JSValue jsBaseObjectFree(JSContext* ctx, JSValueConst this_val, int argc, JSValueConst* argv) {
+        BaseObject* obj = (BaseObject*)JS_GetOpaque(this_val, BaseObject::baseObjectClassID);
+        if (obj) {
+            obj->free();
+        }
+        return JS_UNDEFINED;
+    }
+
+    const JSCFunctionListEntry BaseObject::baseObjectProtoFuncs[] = {
+        JS_CFUNC_DEF("free", 0, jsBaseObjectFree),
+        JS_PROP_STRING_DEF("[[Class]]", "BaseObject", JS_PROP_CONFIGURABLE),
+        JS_PROP_STRING_DEF("className", "BaseObject", JS_PROP_CONFIGURABLE),
+    };
+
+    void registerBaseObject(JSRuntime* rt, JSContext* ctx) {
+        JS_NewClassID(rt, &BaseObject::baseObjectClassID);
+        JS_NewClass(rt, BaseObject::baseObjectClassID, &BaseObject::baseObjectJsClass);
+
+        JSValue proto = JS_NewObject(ctx);
+        JS_SetPropertyFunctionList(
+            ctx, 
+            proto, 
+            BaseObject::baseObjectProtoFuncs, 
+            sizeof(BaseObject::baseObjectProtoFuncs)
+        );
+
+        JSValue constructor = JS_NewCFunction2(
+            ctx, 
+            jsBaseObjectConstructor, 
+            "BaseObject", 
+            0, 
+            JS_CFUNC_constructor, 
+            0
+        );
+        JS_SetConstructor(ctx, constructor, proto);
+        JS_SetClassProto(ctx, BaseObject::baseObjectClassID, proto);
+
+        
+    }
 }
 
 using namespace sunaba::core;
--- src/core/base_object.h
+++ src/core/base_object.h
@@ -13,20 +13,27 @@ namespace sunaba::core
     //static void generateBaseObjectUsertype(lua_State* L);
 
     void registerBaseObject(JSContext* ctx);
-    void jsBaseObjectFinalizer(JSRuntime* rt, JSValue val) {}
+    void jsBaseObjectFinalizer(JSRuntime* rt, JSValue val);
     JSValue jsBaseObjectConstructor(JSContext* ctx, JSValueConst newTarget,  int argc, JSValueConst* argv);
+    JSValue jsBaseObjectFreeFunc(JSContext* ctx, JSValueConst this_val, int argc, JSValueConst* argv);
 
     class BaseObject {
+    private:
+        bool freed = false;
     public:
         virtual void onNotification( int p_what ) {}
 
         void notification(const int p_notification, bool p_reversed = false);
 
-        bool freed = false;
+        bool hasBeenFreed() {
+            return freed;
+        }
 
         static JSClassID baseObjectClassID;
 
         static const JSClassDef baseObjectJsClass;
+
+        static const JSCFunctionListEntry baseObjectProtoFuncs[];
 
         virtual void onFree() {}
         
