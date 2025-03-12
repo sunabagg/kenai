5d46cb3b767bc6f8b578d359fe1beb6fa80752af
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-12 13:03:50 -0800

    Add AudioStreamPolyphonic class and Lua bindings for polyphonic audio management

---
+++ src/audio/audio_stream_polyphonic.cpp
@@ -0,0 +1,16 @@
+#include "audio_stream_polyphonic.h"
+
+void sunaba::audio::bindAudioStreamPolyphonic(sol::state_view& lua) {
+    lua.new_usertype<AudioStreamPolyphonic>("AudioStreamPolyphonic",
+        sol::constructors<AudioStreamPolyphonic()>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, AudioStream>(),
+        "cast", [](sunaba::core::Resource* resource) { 
+            return new AudioStreamPolyphonic(
+                Object::cast_to<GodotAudioStreamPolyphonic>(
+                    resource->getResource()
+                )
+            ); 
+        },
+        "polyphony", sol::property(&AudioStreamPolyphonic::getPolyphony, &AudioStreamPolyphonic::setPolyphony)
+    );
+}
---
+++ src/audio/audio_stream_polyphonic.h
@@ -0,0 +1,50 @@
+#ifndef AUDIO_STREAM_POLYPHONIC_H
+#define AUDIO_STREAM_POLYPHONIC_H
+
+#include <godot_cpp/classes/audio_stream_polyphonic.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotAudioStreamPolyphonic godot::AudioStreamPolyphonic
+
+#include "audio_stream.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+namespace sunaba::audio {
+    void bindAudioStreamPolyphonic(sol::state_view& lua);
+
+    class AudioStreamPolyphonic : public AudioStream {
+    private:
+        GodotAudioStreamPolyphonic* audioStreamPolyphonic;
+    public:
+        AudioStreamPolyphonic() {
+            setAudioStreamPolyphonic(memnew(GodotAudioStreamPolyphonic));
+        }
+
+        AudioStreamPolyphonic(GodotAudioStreamPolyphonic* a) {
+            setAudioStreamPolyphonic(a);
+        }
+
+        GodotAudioStreamPolyphonic* getAudioStreamPolyphonic() {
+            return audioStreamPolyphonic;
+        }
+
+        void setAudioStreamPolyphonic(GodotAudioStreamPolyphonic* a) {
+            audioStreamPolyphonic = a;
+            audioStreamPolyphonic->reference();
+            setAudioStream(audioStreamPolyphonic);
+        }
+
+        int getPolyphony() {
+            return audioStreamPolyphonic->get_polyphony();
+        }
+
+        void setPolyphony(int polyphony) {
+            audioStreamPolyphonic->set_polyphony(polyphony);
+        }
+    };
+}
+
+#endif // AUDIO_STREAM_POLYPHONIC_H
