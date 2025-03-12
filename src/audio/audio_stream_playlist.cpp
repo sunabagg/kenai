1caf69557cdba700e109894402be04941ad683aa
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-12 13:00:12 -0800

    Add AudioStreamPlaylist class and Lua bindings for audio stream management

---
+++ src/audio/audio_stream_playlist.cpp
@@ -0,0 +1,21 @@
+#include "audio_stream_playlist.h"
+
+void sunaba::audio::bindAudioStreamPlaylist(sol::state_view& lua) {
+    lua.new_usertype<AudioStreamPlaylist>("AudioStreamPlaylist",
+        sol::constructors<AudioStreamPlaylist()>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, AudioStream>(),
+        "cast", [](sunaba::core::Resource* resource) { 
+            return new AudioStreamPlaylist(
+                Object::cast_to<GodotAudioStreamPlaylist>(
+                    resource->getResource()
+                )
+            ); 
+        },
+        "fadeTime", sol::property(&AudioStreamPlaylist::getFadeTime, &AudioStreamPlaylist::setFadeTime),
+        "loop", sol::property(&AudioStreamPlaylist::getLoop, &AudioStreamPlaylist::setLoop),
+        "shuffle", sol::property(&AudioStreamPlaylist::getShuffle, &AudioStreamPlaylist::setShuffle),
+        "streamCount", sol::property(&AudioStreamPlaylist::getStreamCount),
+        "getListStream", &AudioStreamPlaylist::getListStream,
+        "setListStream", &AudioStreamPlaylist::setListStream,
+    );
+}
---
+++ src/audio/audio_stream_playlist.h
@@ -0,0 +1,84 @@
+#ifndef AUDIO_STREAM_PLAYLIST_H
+#define AUDIO_STREAM_PLAYLIST_H
+
+#include <godot_cpp/classes/audio_stream_playlist.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotAudioStreamPlaylist godot::AudioStreamPlaylist
+
+#include "audio_stream.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+namespace sunaba::audio {
+    void bindAudioStreamPlaylist(sol::state_view& lua);
+
+    class AudioStreamPlaylist : public AudioStream {
+    private:
+        GodotAudioStreamPlaylist* audioStreamPlaylist;
+    public:
+        AudioStreamPlaylist() {
+            setAudioStreamPlaylist(memnew(GodotAudioStreamPlaylist));
+        }
+
+        AudioStreamPlaylist(GodotAudioStreamPlaylist* a) {
+            setAudioStreamPlaylist(a);
+        }
+
+        GodotAudioStreamPlaylist* getAudioStreamPlaylist() {
+            return audioStreamPlaylist;
+        }
+
+        void setAudioStreamPlaylist(GodotAudioStreamPlaylist* a) {
+            audioStreamPlaylist = a;
+            audioStreamPlaylist->reference();
+            setAudioStream(audioStreamPlaylist);
+        }
+
+        float getFadeTime() {
+            return audioStreamPlaylist->get_fade_time();
+        }
+
+        void setFadeTime(float fadeTime) {
+            audioStreamPlaylist->set_fade_time(fadeTime);
+        }
+
+        bool getLoop() {
+            return audioStreamPlaylist->has_loop();
+        }
+
+        void setLoop(bool loop) {
+            audioStreamPlaylist->set_loop(loop);
+        }
+
+        bool getShuffle() {
+            return audioStreamPlaylist->get_shuffle();
+        }
+
+        void setShuffle(bool shuffle) {
+            audioStreamPlaylist->set_shuffle(shuffle);
+        }
+
+        int getStreamCount() {
+            return audioStreamPlaylist->get_stream_count();
+        }
+
+        void setStreamCount(int streamCount) {
+            audioStreamPlaylist->set_stream_count(streamCount);
+        }
+
+        float getBpm() {
+            return audioStreamPlaylist->get_bpm();
+        }
+
+        AudioStream* getListStream(int index) {
+            return new AudioStream(audioStreamPlaylist->get_list_stream(index));
+        }
+
+        void setListStream(int index, AudioStream* stream) {
+            audioStreamPlaylist->set_list_stream(index, stream->getAudioStream());
+        }
+    };
+}
