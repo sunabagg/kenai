3ea04a7972e41d6865d26693a7c024de91b07b95
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-14 15:34:48 -0800

    Add BoxOccluder3D class and Lua binding for integration

---
+++ src/spatial/box_occluder3d.cpp
@@ -0,0 +1,12 @@
+#include "box_occluder3d.h"
+
+void sunaba::spatial::bindBoxOccluder3D(sol::state_view& lua) {
+    lua.new_usertype<sunaba::spatial::BoxOccluder>("BoxOccluder3D",
+        sol::constructors<BoxOccluder()>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, Occluder3D>(),
+        "size", sol::property(&BoxOccluder::getSize, &BoxOccluder::setSize),
+        "cast", [](sunaba::core::Resource* instance) {
+            return new BoxOccluder(godot::Object::cast_to<GodotBoxOccluder3D>(instance->getResource()));
+        }
+    );
+}
---
+++ src/spatial/box_occluder3d.h
@@ -0,0 +1,49 @@
+#ifndef BOX_OCCLUDER3D_H
+#define BOX_OCCLUDER3D_H
+
+#include <godot_cpp/classes/box_occluder3d.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotBoxOccluder3D godot::BoxOccluder3D
+
+#include "occluder3d.h"
+
+using namespace godot;
+
+namespace sunaba::spatial {
+    void bindBoxOccluder3D(sol::state_view& lua);
+
+    class BoxOccluder : public Occluder3D {
+    private:
+        GodotBoxOccluder3D* boxOccluder = nullptr;
+    public:
+        BoxOccluder() {
+            setBoxOccluder3D(memnew(GodotBoxOccluder3D));
+        }
+
+        BoxOccluder(GodotBoxOccluder3D* bo) {
+            setBoxOccluder3D(bo);
+        }
+
+        GodotBoxOccluder3D* getBoxOccluder3D() {
+            return boxOccluder;
+        }
+
+        void setBoxOccluder3D(GodotBoxOccluder3D* bo) {
+            boxOccluder = bo;
+            boxOccluder->reference();
+            setOccluder3D(boxOccluder);
+        }
+
+        Vector3 getSize() {
+            return boxOccluder->get_size();
+        }
+
+        void setSize(Vector3 size) {
+            boxOccluder->set_size(size);
+        }
+    };
+}
+
+#endif // BOX_OCCLUDER3D_H
