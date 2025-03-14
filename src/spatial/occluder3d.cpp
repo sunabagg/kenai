43d14cc2eb8f2670c27ae5f70b2e58d18cc4c899
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-14 13:14:35 -0800

    Add Occluder3D class and Lua bindings for integration with spatial system

---
+++ src/spatial/occluder3d.cpp
@@ -0,0 +1,14 @@
+#include "occluder3d.h"
+
+void sunaba::spatial::bindOccluder3D(sol::state_view& lua) {
+    lua.new_usertype<sunaba::spatial::Occluder3D>("Occluder3D",
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource>(),
+        sol::constructors<Occluder3D()>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource>(),
+        "getIndices", &Occluder3D::getIndices,
+        "getVertices", &Occluder3D::getVertices,
+        "cast", [](sunaba::core::Resource* instance) {
+            return new Occluder3D(godot::Object::cast_to<GodotOccluder3D>(instance->getResource()));
+        }
+    );
+}
---
+++ src/spatial/occluder3d.h
@@ -0,0 +1,58 @@
+#ifndef OCCLUDER3D_H
+#define OCCLUDER3D_H
+
+#include <godot_cpp/classes/occluder3d.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotOccluder3D godot::Occluder3D
+
+#include "../core/resource.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+namespace sunaba::spatial {
+    void bindOccluder3D(sol::state_view& lua);
+
+    class Occluder3D : public sunaba::core::Resource {
+    private:
+        GodotOccluder3D* occluder = nullptr;
+    public:
+        Occluder3D() {
+            setOccluder3D(memnew(GodotOccluder3D));
+        }
+
+        Occluder3D(GodotOccluder3D* o) {
+            setOccluder3D(o);
+        }
+
+        GodotOccluder3D* getOccluder3D() {
+            return occluder;
+        }
+
+        void setOccluder3D(GodotOccluder3D* o) {
+            occluder = o;
+            occluder->reference();
+            setResource(occluder);
+        }
+
+        std::vector<int> getIndices() {
+            std::vector<int> indices;
+            PackedInt32Array arr = occluder->get_indices();
+            for (int i = 0; i < arr.size(); i++) {
+                indices.push_back(arr[i]);
+            }
+            return indices;
+        }
+
+        std::vector<Vector3> getVertices() {
+            std::vector<Vector3> vertices;
+            PackedVector3Array arr = occluder->get_vertices();
+            for (int i = 0; i < arr.size(); i++) {
+                vertices.push_back(arr[i]);
+            }
+            return vertices;
+        }
+    };
+}
