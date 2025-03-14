a0b204fa385c9e66361305c9e831b93eaffefea9
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-14 15:44:52 -0800

    Add ArrayOccluder3D class and Lua binding for integration

---
+++ src/spatial/array_occluder3d.cpp
@@ -0,0 +1,14 @@
+#include "array_occluder3d.h"
+
+void sunaba::spatial::bindArrayOccluder3D(sol::state_view& lua) {
+    lua.new_usertype<sunaba::spatial::ArrayOccluder>("ArrayOccluder3D",
+        sol::constructors<ArrayOccluder()>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, Occluder3D>(),
+        "indicies", sol::property(&ArrayOccluder::getIndices, &ArrayOccluder::setIndices),
+        "vertices", sol::property(&ArrayOccluder::getVertices, &ArrayOccluder::setVertices),
+        "setArrays", &ArrayOccluder::setArrays,
+        "cast", [](sunaba::core::Resource* instance) {
+            return new ArrayOccluder(godot::Object::cast_to<GodotArrayOccluder3D>(instance->getResource()));
+        }
+    );
+}
---
+++ src/spatial/array_occluder3d.h
@@ -0,0 +1,69 @@
+#ifndef ARRAY_OCCLUDER3D_H
+#define ARRAY_OCCLUDER3D_H
+
+#include <godot_cpp/classes/array_occluder3d.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotArrayOccluder3D godot::ArrayOccluder3D
+
+#include "occluder3d.h"
+
+using namespace godot;
+
+namespace sunaba::spatial {
+    void bindArrayOccluder3D(sol::state_view& lua);
+
+    class ArrayOccluder : public Occluder3D {
+    private:
+        GodotArrayOccluder3D* arrayOccluder = nullptr;
+    public:
+        ArrayOccluder() {
+            setArrayOccluder3D(memnew(GodotArrayOccluder3D));
+        }
+
+        ArrayOccluder(GodotArrayOccluder3D* ao) {
+            setArrayOccluder3D(ao);
+        }
+
+        GodotArrayOccluder3D* getArrayOccluder3D() {
+            return arrayOccluder;
+        }
+
+        void setArrayOccluder3D(GodotArrayOccluder3D* ao) {
+            arrayOccluder = ao;
+            arrayOccluder->reference();
+            setOccluder3D(arrayOccluder);
+        }
+
+        void setIndices(std::vector<int> indices) {
+            PackedInt32Array arr;
+            for (int i = 0; i < indices.size(); i++) {
+                arr.push_back(indices[i]);
+            }
+            arrayOccluder->set_indices(arr);
+        }
+
+        void setVertices(std::vector<Vector3> vertices) {
+            PackedVector3Array arr;
+            for (int i = 0; i < vertices.size(); i++) {
+                arr.push_back(vertices[i]);
+            }
+            arrayOccluder->set_vertices(arr);
+        }
+
+        void setArrays(std::vector<Vector3> vertices, std::vector<int> indices) {
+            PackedInt32Array indicesArr;
+            for (int i = 0; i < indices.size(); i++) {
+                indicesArr.push_back(indices[i]);
+            }
+            PackedVector3Array verticesArr;
+            for (int i = 0; i < vertices.size(); i++) {
+                verticesArr.push_back(vertices[i]);
+            }
+            arrayOccluder->set_arrays(verticesArr, indicesArr);
+        }
+    };
+}
+
+#endif // ARRAY_OCCLUDER3D_H
