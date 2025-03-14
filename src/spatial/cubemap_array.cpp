7bfbb3fb96619f7d63f6dd55d7350d75bc3cdfc6
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-14 14:03:11 -0800

    Fix binding for CubemapArray in Lua integration by correcting base class declarations

--- src/spatial/cubemap_array.cpp
+++ src/spatial/cubemap_array.cpp
@@ -2,7 +2,6 @@
 
 void sunaba::spatial::bindCubemapArray(sol::state_view& lua) {
     lua.new_usertype<sunaba::spatial::CubemapArray>("CubemapArray",
-        sol::base_classes, sol::bases<sunaba::core::ImageTextureLayered>(),
         sol::constructors<CubemapArray()>(),
         sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::core::Texture, sunaba::core::TextureLayered, sunaba::core::ImageTextureLayered>(),
         "createPlaceholder", &CubemapArray::createPlaceholder,
