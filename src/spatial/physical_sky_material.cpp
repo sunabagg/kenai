19a76ca0c5cdd7b4c50c7d217511388d8f4af684
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-14 15:10:28 -0800

    Add PhysicalSkyMaterial class and binding for Lua integration

---
+++ src/spatial/physical_sky_material.cpp
@@ -0,0 +1,22 @@
+#include "physical_sky_material.h"
+
+void sunaba::spatial::bindPhysicalSkyMaterial(sol::state_view& lua) {
+    lua.new_usertype<sunaba::spatial::PhysicalSkyMaterial>("PhysicalSkyMaterial",
+        sol::constructors<PhysicalSkyMaterial()>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::core::Material>(),
+        "energyMultiplier", sol::property(&PhysicalSkyMaterial::getEnergyMultiplier, &PhysicalSkyMaterial::setEnergyMultiplier),
+        "groundColor", sol::property(&PhysicalSkyMaterial::getGroundColor, &PhysicalSkyMaterial::setGroundColor),
+        "mieCoefficient", sol::property(&PhysicalSkyMaterial::getMieCoefficient, &PhysicalSkyMaterial::setMieCoefficient),
+        "mieColor", sol::property(&PhysicalSkyMaterial::getMieColor, &PhysicalSkyMaterial::setMieColor),
+        "mieEccentricity", sol::property(&PhysicalSkyMaterial::getMieEccentricity, &PhysicalSkyMaterial::setMieEccentricity),
+        "nightSky", sol::property(&PhysicalSkyMaterial::getNightSky, &PhysicalSkyMaterial::setNightSky),
+        "rayleighCoefficient", sol::property(&PhysicalSkyMaterial::getRayleighCoefficient, &PhysicalSkyMaterial::setRayleighCoefficient),
+        "rayleighColor", sol::property(&PhysicalSkyMaterial::getRayleighColor, &PhysicalSkyMaterial::setRayleighColor),
+        "sunDiskScale", sol::property(&PhysicalSkyMaterial::getSunDiskScale, &PhysicalSkyMaterial::setSunDiskScale),
+        "turbidity", sol::property(&PhysicalSkyMaterial::getTurbidity, &PhysicalSkyMaterial::setTurbidity),
+        "useDebanding", sol::property(&PhysicalSkyMaterial::getUseDebanding, &PhysicalSkyMaterial::setUseDebanding),
+        "cast", [](sunaba::core::Resource* instance) {
+            return new PhysicalSkyMaterial(godot::Object::cast_to<GodotPhysicalSkyMaterial>(instance->getResource()));
+        }
+    );
+}
---
+++ src/spatial/physical_sky_material.h
@@ -0,0 +1,135 @@
+#ifndef PHYSICAL_SKY_MATERIAL_H
+#define PHYSICAL_SKY_MATERIAL_H
+
+#include <godot_cpp/classes/physical_sky_material.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotPhysicalSkyMaterial godot::PhysicalSkyMaterial
+
+#include "../core/material.h"
+#include "../core/texture2d.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+#define Texture2D sunaba::core::Texture2D
+
+namespace sunaba::spatial {
+    void bindPhysicalSkyMaterial(sol::state_view& lua);
+
+    class PhysicalSkyMaterial : public sunaba::core::Material {
+    private:
+        GodotPhysicalSkyMaterial* material = nullptr;
+    public:
+        PhysicalSkyMaterial(GodotPhysicalSkyMaterial* m) {
+            setPhysicalSkyMaterial(m);
+        }
+
+        PhysicalSkyMaterial() {
+            setPhysicalSkyMaterial(memnew(GodotPhysicalSkyMaterial));
+        }
+
+        GodotPhysicalSkyMaterial* getPhysicalSkyMaterial() {
+            return material;
+        }
+
+        void setPhysicalSkyMaterial(GodotPhysicalSkyMaterial* m) {
+            material = m;
+            material->reference();
+            setMaterial(material);
+        }
+
+        float getEnergyMultiplier() {
+            return material->get_energy_multiplier();
+        }
+
+        void setEnergyMultiplier(float multiplier) {
+            material->set_energy_multiplier(multiplier);
+        }
+
+        Color getGroundColor() {
+            return material->get_ground_color();
+        }
+
+        void setGroundColor(Color color) {
+            material->set_ground_color(color);
+        }
+
+        float getMieCoefficient() {
+            return material->get_mie_coefficient();
+        }
+
+        void setMieCoefficient(float coefficient) {
+            material->set_mie_coefficient(coefficient);
+        }
+
+        Color getMieColor() {
+            return material->get_mie_color();
+        }
+
+        void setMieColor(Color color) {
+            material->set_mie_color(color);
+        }
+
+        float getMieEccentricity() {
+            return material->get_mie_eccentricity();
+        }
+
+        void setMieEccentricity(float eccentricity) {
+            material->set_mie_eccentricity(eccentricity);
+        }
+
+        Texture2D* getNightSky() {
+            return new Texture2D(
+                material->get_night_sky().ptr()
+            );
+        }
+
+        void setNightSky(Texture2D* texture) {
+            material->set_night_sky(texture->getTexture2D());
+        }
+
+        float getRayleighCoefficient() {
+            return material->get_rayleigh_coefficient();
+        }
+
+        void setRayleighCoefficient(float coefficient) {
+            material->set_rayleigh_coefficient(coefficient);
+        }
+
+        Color getRayleighColor() {
+            return material->get_rayleigh_color();
+        }
+
+        void setRayleighColor(Color color) {
+            material->set_rayleigh_color(color);
+        }
+
+        float getSunDiskScale() {
+            return material->get_sun_disk_scale();
+        }
+
+        void setSunDiskScale(float scale) {
+            material->set_sun_disk_scale(scale);
+        }
+
+        float getTurbidity() {
+            return material->get_turbidity();
+        }
+
+        void setTurbidity(float turbidity) {
+            material->set_turbidity(turbidity);
+        }
+
+        bool getUseDebanding() {
+            return material->get_use_debanding();
+        }
+
+        void setUseDebanding(bool enable) {
+            material->set_use_debanding(enable);
+        }
+    };
+}
+
+#endif // PHYSICAL_SKY_MATERIAL_H
