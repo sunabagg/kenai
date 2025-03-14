7a48a4deb8d6ffe4bcba7d8e705f26f674af5c7b
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-14 13:18:44 -0800

    Add OrmMaterial3D class and Lua bindings for integration with spatial system

---
+++ src/spatial/orm_material3d.cpp
@@ -0,0 +1,12 @@
+#include "orm_material3d.h"
+
+void sunaba::spatial::bindOrmMaterial3D(sol::state_view& lua) {
+    lua.new_usertype<sunaba::spatial::OrmMaterial3D>("OrmMaterial3D",
+        sol::base_classes, sol::bases<sunaba::spatial::BaseMaterial3D>(),
+        sol::constructors<OrmMaterial3D(), OrmMaterial3D(GodotOrmMaterial3D*)>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::core::Material, sunaba::spatial::BaseMaterial3D>(),
+        "cast", [](sunaba::core::Resource* instance) {
+            return new OrmMaterial3D(godot::Object::cast_to<GodotOrmMaterial3D>(instance->getResource()));
+        }
+    );
+}
---
+++ src/spatial/orm_material3d.h
@@ -0,0 +1,42 @@
+#ifndef ORM_MATERIAL3D_H
+#define ORM_MATERIAL3D_H
+
+#include <godot_cpp/classes/orm_material3d.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotOrmMaterial3D godot::ORMMaterial3D
+
+#include "base_material3d.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+namespace sunaba::spatial {
+    void bindOrmMaterial3D(sol::state_view& lua);
+
+    class OrmMaterial3D : public sunaba::spatial::BaseMaterial3D {
+    private:
+        GodotOrmMaterial3D* material = nullptr;
+    public:
+        OrmMaterial3D(GodotOrmMaterial3D* m) {
+            setOrmMaterial(m);
+        }
+
+        OrmMaterial3D() {
+            setOrmMaterial(memnew(GodotOrmMaterial3D));
+        }
+
+        GodotOrmMaterial3D* getOrmMaterial() {
+            return material;
+        }
+
+        void setOrmMaterial(GodotOrmMaterial3D* m) {
+            material = m;
+            material->reference();
+            setBaseMaterial3D(material);
+        }
+    };
+}
+
+#endif // ORM_MATERIAL3D_H
