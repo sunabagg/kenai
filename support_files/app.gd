3fcd90da64abee112e943a0048d397b3759d4515
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-14 10:14:41 -0800

    Added new assets

--- support_files/app.gd
+++ support_files/app.gd
@@ -3,6 +3,7 @@ extends App
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	get_window().theme = load("res://addons/lite/light.tres")
 	start("res://test3")
 
 
