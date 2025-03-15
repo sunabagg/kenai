4ba90818e250338947dc9a02726a6bee1bf62cfb
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-14 16:02:38 -0800

    Add QuadOccluder3D class and Lua binding for integration

---
+++ src/spatial/quad_occluder3d.h
@@ -0,0 +1,49 @@
+#ifndef QUAD_OCCLUDER3D_H
+#define QUAD_OCCLUDER3D_H
+
+#include <godot_cpp/classes/quad_occluder3d.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotQuadOccluder3D godot::QuadOccluder3D
+
+#include "occluder3d.h"
+
+using namespace godot;
+
+namespace sunaba::spatial {
+    void bindQuadOccluder3D(sol::state_view& lua);
+
+    class QuadOccluder3D : public Occluder3D {
+    private:
+        GodotQuadOccluder3D* quadOccluder = nullptr;
+    public:
+    QuadOccluder3D() {
+            setQuadOccluder3D(memnew(GodotQuadOccluder3D));
+        }
+
+        QuadOccluder3D(GodotQuadOccluder3D* qo) {
+            setQuadOccluder3D(qo);
+        }
+
+        GodotQuadOccluder3D* getQuadOccluder3D() {
+            return quadOccluder;
+        }
+
+        void setQuadOccluder3D(GodotQuadOccluder3D* qo) {
+            quadOccluder = qo;
+            quadOccluder->reference();
+            setOccluder3D(quadOccluder);
+        }
+
+        Vector2 getSize() {
+            return quadOccluder->get_size();
+        }
+        
+        void setSize(Vector2 size) {
+            quadOccluder->set_size(size);
+        }
+    };
+}
+
+#endif // QUAD_OCCLUDER3D_H
