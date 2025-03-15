3fa149a8fb85c117db32acf76dcc6d10a5fc2c72
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-14 16:53:10 -0800

    Add Skin class and Lua binding for integration

---
+++ src/spatial/skin.cpp
@@ -0,0 +1,22 @@
+#include "skin.h"
+
+void sunaba::spatial::bindSkin(sol::state_view& lua) {
+    lua.new_usertype<sunaba::spatial::Skin>("Skin",
+        sol::constructors<Skin()>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource>(),
+        "addBind", &Skin::addBind,
+        "addNamedBind", &Skin::addNamedBind,
+        "clearBinds", &Skin::clearBinds,
+        "getBindBone", &Skin::getBindBone,
+        "getBindCount", &Skin::getBindCount,
+        "getBindName", &Skin::getBindName,
+        "getBindPose", &Skin::getBindPose,
+        "setBindBone", &Skin::setBindBone,
+        "setBindCount", &Skin::setBindCount,
+        "setBindName", &Skin::setBindName,
+        "setBindPose", &Skin::setBindPose,
+        "cast", [](sunaba::core::Resource* instance) {
+            return new Skin(godot::Object::cast_to<GodotSkin>(instance->getResource()));
+        }
+    );
+}
---
+++ src/spatial/skin.h
@@ -0,0 +1,85 @@
+#ifndef SKIN_H
+#define SKIN_H
+
+#include <godot_cpp/classes/skin.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotSkin godot::Skin
+
+#include "../core/resource.h"
+
+using namespace godot;
+
+namespace sunaba::spatial {
+    void bindSkin(sol::state_view& lua);
+
+    class Skin : public sunaba::core::Resource {
+    private:
+        GodotSkin* skin = nullptr;
+    public:
+        Skin() {
+            setSkin(memnew(GodotSkin));
+        }
+
+        Skin(GodotSkin* s) {
+            setSkin(s);
+        }
+
+        GodotSkin* getSkin() {
+            return skin;
+        }
+
+        void setSkin(GodotSkin* s) {
+            skin = s;
+            skin->reference();
+            setResource(skin);
+        }
+
+        void addBind(int bone, Transform3D pose) {
+            skin->add_bind(bone, pose);
+        }
+
+        void addNamedBind(std::string name, Transform3D pose) {
+            skin->add_named_bind(name.c_str(), pose);
+        }
+
+        void clearBinds() {
+            skin->clear_binds();
+        }
+
+        int getBindBone(int bindIndex) {
+            return skin->get_bind_bone(bindIndex);
+        }
+
+        int getBindCount() {
+            return skin->get_bind_count();
+        }
+
+        std::string getBindName(int bindIndex) {
+            return String(skin->get_bind_name(bindIndex)).utf8().get_data();
+        }
+
+        Transform3D getBindPose(int bindIndex) {
+            return skin->get_bind_pose(bindIndex);
+        }
+
+        void setBindBone(int bindIndex, int bone) {
+            skin->set_bind_bone(bindIndex, bone);
+        }
+
+        void setBindCount(int bindCount) {
+            skin->set_bind_count(bindCount);
+        }
+
+        void setBindName(int bindIndex, std::string name) {
+            skin->set_bind_name(bindIndex, name.c_str());
+        }
+
+        void setBindPose(int bindIndex, Transform3D pose) {
+            skin->set_bind_pose(bindIndex, pose);
+        }
+    };
+}
+
+#endif // SKIN_H
