47298974356d114c5c5074da0822ee84676c479f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-15 12:31:44 -0800

    Add CameraAttributesPractical class and Lua bindings for advanced settings

---
+++ src/spatial/camera_attributes_practical.cpp
@@ -0,0 +1,20 @@
+#include "camera_attributes_practical.h"
+
+void sunaba::spatial::bindCameraAttributesPractical(sol::state_view& lua) {
+    lua.new_usertype<CameraAttributesPractical>("CameraAttributesPractical",
+        sol::constructors<CameraAttributesPractical()>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::spatial::CameraAttributes>(),
+        "autoExposureMaxSensitivity", sol::property(&CameraAttributesPractical::getAutoExposureMaxSensitivity, &CameraAttributesPractical::setAutoExposureMaxSensitivity),
+        "autoExposureMinSensitivity", sol::property(&CameraAttributesPractical::getAutoExposureMinSensitivity, &CameraAttributesPractical::setAutoExposureMinSensitivity),
+        "dofBlurAmount", sol::property(&CameraAttributesPractical::getDofBlurAmount, &CameraAttributesPractical::setDofBlurAmount),
+        "dofBlurFarDistance", sol::property(&CameraAttributesPractical::getDofBlurFarDistance, &CameraAttributesPractical::setDofBlurFarDistance),
+        "dofBlurFarEnabled", sol::property(&CameraAttributesPractical::getDofBlurFarEnabled, &CameraAttributesPractical::setDofBlurFarEnabled),
+        "dofBlurFarTransition", sol::property(&CameraAttributesPractical::getDofBlurFarTransition, &CameraAttributesPractical::setDofBlurFarTransition),
+        "dofBlurNearDistance", sol::property(&CameraAttributesPractical::getDofBlurNearDistance, &CameraAttributesPractical::setDofBlurNearDistance),
+        "dofBlurNearEnabled", sol::property(&CameraAttributesPractical::getDofBlurNearEnabled, &CameraAttributesPractical::setDofBlurNearEnabled),
+        "dofBlurNearTransition", sol::property(&CameraAttributesPractical::getDofBlurNearTransition, &CameraAttributesPractical::setDofBlurNearTransition),
+        "cast", [](sunaba::core::Resource* res) {
+            return new CameraAttributesPractical(Object::cast_to<GodotCameraAttributesPractical>(res->getResource()));
+        }
+    );
+}
---
+++ src/spatial/camera_attributes_practical.h
@@ -0,0 +1,114 @@
+#ifndef CAMERA_ATTRIBUTES_PRACTICAL_H
+#define CAMERA_ATTRIBUTES_PRACTICAL_H
+
+#include <godot_cpp/classes/camera_attributes_practical.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotCameraAttributesPractical godot::CameraAttributesPractical
+
+#include "camera_attributes.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+namespace sunaba::spatial {
+    void bindCameraAttributesPractical(sol::state_view& lua);
+
+    class CameraAttributesPractical : public CameraAttributes {
+    private:
+        GodotCameraAttributesPractical* cameraAttributesPractical = nullptr;
+    public:
+        CameraAttributesPractical() {
+            setCameraAttributesPractical(memnew(GodotCameraAttributesPractical));
+        }
+
+        CameraAttributesPractical(GodotCameraAttributesPractical* cap) {
+            setCameraAttributesPractical(cap);
+        }
+
+        GodotCameraAttributesPractical* getCameraAttributesPractical() {
+            return cameraAttributesPractical;
+        }
+
+        void setCameraAttributesPractical(GodotCameraAttributesPractical* cap) {
+            cameraAttributesPractical = cap;
+            cameraAttributesPractical->reference();
+            setResource(cameraAttributesPractical);
+        }
+
+        float getAutoExposureMaxSensitivity() {
+            return cameraAttributesPractical->get_auto_exposure_max_sensitivity();
+        }
+
+        void setAutoExposureMaxSensitivity(float maxSensitivity) {
+            cameraAttributesPractical->set_auto_exposure_max_sensitivity(maxSensitivity);
+        }
+
+        float getAutoExposureMinSensitivity() {
+            return cameraAttributesPractical->get_auto_exposure_min_sensitivity();
+        }
+
+        void setAutoExposureMinSensitivity(float minSensitivity) {
+            cameraAttributesPractical->set_auto_exposure_min_sensitivity(minSensitivity);
+        }
+
+        float getDofBlurAmount() {
+            return cameraAttributesPractical->get_dof_blur_amount();
+        }
+
+        void setDofBlurAmount(float blurAmount) {
+            cameraAttributesPractical->set_dof_blur_amount(blurAmount);
+        }
+
+        float getDofBlurFarDistance() {
+            return cameraAttributesPractical->get_dof_blur_far_distance();
+        }
+
+        void setDofBlurFarDistance(float farDistance) {
+            cameraAttributesPractical->set_dof_blur_far_distance(farDistance);
+        }
+
+        bool getDofBlurFarEnabled() {
+            return cameraAttributesPractical->is_dof_blur_far_enabled();
+        }
+
+        void setDofBlurFarEnabled(bool enabled) {
+            cameraAttributesPractical->set_dof_blur_far_enabled(enabled);
+        }
+
+        float getDofBlurFarTransition() {
+            return cameraAttributesPractical->get_dof_blur_far_transition();
+        }
+
+        void setDofBlurFarTransition(float farTransition) {
+            cameraAttributesPractical->set_dof_blur_far_transition(farTransition);
+        }
+
+        float getDofBlurNearDistance() {
+            return cameraAttributesPractical->get_dof_blur_near_distance();
+        }
+
+        void setDofBlurNearDistance(float nearDistance) {
+            cameraAttributesPractical->set_dof_blur_near_distance(nearDistance);
+        }
+
+        bool getDofBlurNearEnabled() {
+            return cameraAttributesPractical->is_dof_blur_near_enabled();
+        }
+
+        void setDofBlurNearEnabled(bool enabled) {
+            cameraAttributesPractical->set_dof_blur_near_enabled(enabled);
+        }
+
+        float getDofBlurNearTransition() {
+            return cameraAttributesPractical->get_dof_blur_near_transition();
+        }
+
+        void setDofBlurNearTransition(float nearTransition) {
+            cameraAttributesPractical->set_dof_blur_near_transition(nearTransition);
+        }
+    };
+}
+
+#endif // CAMERA_ATTRIBUTES_PRACTICAL_H
