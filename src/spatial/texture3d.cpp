7c19dd949537a64d70bfe67e3b8efa188d69563a
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-14 17:18:41 -0800

    Add Texture3D class and Lua binding for integration

---
+++ src/spatial/texture3d.cpp
@@ -0,0 +1,18 @@
+#include "texture3d.h"
+
+void sunaba::spatial::bindTexture3D(sol::state_view& lua) {
+    lua.new_usertype<sunaba::spatial::Texture3D>("Texture3D",
+        sol::constructors<Texture3D()>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, Resource, Texture>(),
+        "createPlaceholder", &Texture3D::createPlaceholder,
+        "getData", &Texture3D::getData,
+        "getDepth", &Texture3D::getDepth,
+        "getFormat", &Texture3D::getFormat,
+        "getHeight", &Texture3D::getHeight,
+        "getWidth", &Texture3D::getWidth,
+        "hasMipmaps", &Texture3D::hasMipmaps,
+        "cast", [](Resource* instance) {
+            return new Texture3D(godot::Object::cast_to<GodotTexture3D>(instance->getResource()));
+        }
+    );
+}
---
+++ src/spatial/texture3d.h
@@ -0,0 +1,84 @@
+#ifndef TEXTURE3D_H
+#define TEXTURE3D_H
+
+#include <godot_cpp/classes/texture3d.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotTexture3D godot::Texture3D
+
+#include "../core/texture.h"
+#include "../core/image.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+#define Texture sunaba::core::Texture
+#define Resource sunaba::core::Resource
+
+namespace sunaba::spatial {
+    void bindTexture3D(sol::state_view& lua);
+
+    class Texture3D : public Texture {
+    private:
+        GodotTexture3D* texture = nullptr;
+    public:
+        Texture3D() {
+            setTexture3D(memnew(GodotTexture3D));
+        }
+
+        Texture3D(GodotTexture3D* t) {
+            setTexture3D(t);
+        }
+
+        GodotTexture3D* getTexture3D() {
+            return texture;
+        }
+
+        void setTexture3D(GodotTexture3D* t) {
+            texture = t;
+            texture->reference();
+            setTexture(texture);
+        }
+
+        Resource* createPlaceholder() {
+            return new Resource(
+                texture->create_placeholder().ptr()
+            );
+        }
+
+        std::vector<sunaba::core::Image> getData() {
+            std::vector<sunaba::core::Image> images;
+            TypedArray<GodotImage> arr = texture->get_data();
+            for (int i = 0; i < arr.size(); i++) {
+                Ref<GodotImage> img = arr[i];
+                if (img.is_valid()) {
+                    images.push_back(sunaba::core::Image(img.ptr()));
+                }
+            }
+            return images;
+        }
+
+        int getDepth() {
+            return texture->get_depth();
+        }
+
+        int getFormat() {
+            return texture->get_format();
+        }
+
+        int getHeight() {
+            return texture->get_height();
+        }
+
+        int getWidth() {
+            return texture->get_width();
+        }
+
+        bool hasMipmaps() {
+            return texture->has_mipmaps();
+        }
+    };
+}
+
+#endif // TEXTURE3D_H
