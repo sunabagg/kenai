90291d41cd7e8ebb650f4d06a7fb77bdd66110ca
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-14 17:06:24 -0800

    Add StandardMaterial3D class and Lua binding for integration

---
+++ src/spatial/standard_material3d.h
@@ -0,0 +1,41 @@
+#ifndef STANDARD_MATERIAL3D_H
+#define STANDARD_MATERIAL3D_H
+
+#include <godot_cpp/classes/standard_material3d.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotStandardMaterial3D godot::StandardMaterial3D
+
+#include "base_material3d.h"
+
+using namespace godot;
+
+namespace sunaba::spatial {
+    void bindStandardMaterial3D(sol::state_view& lua);
+
+    class StandardMaterial3D : public BaseMaterial3D {
+    private:
+        GodotStandardMaterial3D* material = nullptr;
+    public:
+        StandardMaterial3D() {
+            setStandardMaterial3D(memnew(GodotStandardMaterial3D));
+        }
+
+        StandardMaterial3D(GodotStandardMaterial3D* m) {
+            setStandardMaterial3D(m);
+        }
+
+        GodotStandardMaterial3D* getStandardMaterial3D() {
+            return material;
+        }
+
+        void setStandardMaterial3D(GodotStandardMaterial3D* m) {
+            material = m;
+            material->reference();
+            setBaseMaterial3D(material);
+        }
+    };
+}
+
+#endif // STANDARD_MATERIAL3D_H
