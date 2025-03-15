96a3bcd144dbfcfa0834cfc8132905922f5e7160
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-15 12:03:24 -0800

    Add CameraAttributes class and Lua bindings for auto exposure settings

---
+++ src/spatial/camera_attributes.cpp
@@ -0,0 +1,15 @@
+#include "camera_attributes.h"
+
+void sunaba::spatial::bindCameraAttributes(sol::state_view& lua) {
+    lua.new_usertype<CameraAttributes>("CameraAttributes",
+        sol::constructors<CameraAttributes()>(),
+        "autoExposureEnabled", sol::property(&CameraAttributes::getAutoExposureEnabled, &CameraAttributes::setAutoExposureEnabled),
+        "autoExposureScale", sol::property(&CameraAttributes::getAutoExposureScale, &CameraAttributes::setAutoExposureScale),
+        "autoExposureSpeed", sol::property(&CameraAttributes::getAutoExposureSpeed, &CameraAttributes::setAutoExposureSpeed),
+        "exposureMultiplier", sol::property(&CameraAttributes::getExposureMultiplier, &CameraAttributes::setExposureMultiplier),
+        "exposureSensitivity", sol::property(&CameraAttributes::getExposureSensitivity, &CameraAttributes::setExposureSensitivity),
+        "cast", [](sunaba::core::Resource* res) {
+            return new CameraAttributes(Object::cast_to<GodotCameraAttributes>(res->getResource()));
+        }
+    );
+}
---
+++ src/spatial/camera_attributes.h
@@ -0,0 +1,81 @@
+#ifndef CAMERA_ATTRIBUTES_H
+#define CAMERA_ATTRIBUTES_H
+
+#include <godot_cpp/classes/camera_attributes.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotCameraAttributes godot::CameraAttributes
+
+#include "../core/resource.h"
+
+using namespace godot;
+
+namespace sunaba::spatial {
+    void bindCameraAttributes(sol::state_view& lua);
+
+    class CameraAttributes : public sunaba::core::Resource {
+    private:
+        GodotCameraAttributes* cameraAttributes = nullptr;
+    public:
+        CameraAttributes() {
+            setCameraAttributes(memnew(GodotCameraAttributes));
+        }
+
+        CameraAttributes(GodotCameraAttributes* ca) {
+            setCameraAttributes(ca);
+        }
+
+        GodotCameraAttributes* getCameraAttributes() {
+            return cameraAttributes;
+        }
+
+        void setCameraAttributes(GodotCameraAttributes* ca) {
+            cameraAttributes = ca;
+            cameraAttributes->reference();
+            setResource(cameraAttributes);
+        }
+
+        bool getAutoExposureEnabled() {
+            return cameraAttributes->is_auto_exposure_enabled();
+        }
+
+        void setAutoExposureEnabled(bool enabled) {
+            cameraAttributes->set_auto_exposure_enabled(enabled);
+        }
+
+        float getAutoExposureScale() {
+            return cameraAttributes->get_auto_exposure_scale();
+        }
+
+        void setAutoExposureScale(float scale) {
+            cameraAttributes->set_auto_exposure_scale(scale);
+        }
+
+        float getAutoExposureSpeed() {
+            return cameraAttributes->get_auto_exposure_speed();
+        }
+
+        void setAutoExposureSpeed(float speed) {
+            cameraAttributes->set_auto_exposure_speed(speed);
+        }
+
+        float getExposureMultiplier() {
+            return cameraAttributes->get_exposure_multiplier();
+        }
+
+        void setExposureMultiplier(float multiplier) {
+            cameraAttributes->set_exposure_multiplier(multiplier);
+        }
+
+        float getExposureSensitivity() {
+            return cameraAttributes->get_exposure_sensitivity();
+        }
+
+        void setExposureSensitivity(float sensitivity) {
+            cameraAttributes->set_exposure_sensitivity(sensitivity);
+        }
+    };
+}
+
+#endif // CAMERA_ATTRIBUTES_H
