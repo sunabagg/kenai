dcff5e69a1134935f42e63591b6ce5508dd59088
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-14 17:23:28 -0800

    Add Texture3DRD class and Lua binding for integration

---
+++ src/spatial/texture3drd.cpp
@@ -0,0 +1,11 @@
+#include "texture3drd.h"
+
+void sunaba::spatial::bindTexture3DRD(sol::state_view& lua) {
+    lua.new_usertype<sunaba::spatial::Texture3DRD>("Texture3DRD",
+        sol::constructors<Texture3DRD()>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, Resource, Texture, Texture3D>(),
+        "cast", [](sunaba::core::Resource* instance) {
+            return new Texture3DRD(godot::Object::cast_to<GodotTexture3DRD>(instance->getResource()));
+        }
+    );
+}
---
+++ src/spatial/texture3drd.h
@@ -0,0 +1,41 @@
+#ifndef TEXTURE3DRD_H
+#define TEXTURE3DRD_H
+
+#include <godot_cpp/classes/texture3drd.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotTexture3DRD godot::Texture3DRD
+
+#include "texture3d.h"
+
+using namespace godot;
+
+#define Texture3D sunaba::spatial::Texture3D
+
+namespace sunaba::spatial {
+    void bindTexture3DRD(sol::state_view& lua);
+
+    class Texture3DRD : public Texture3D {
+    private:
+        GodotTexture3DRD* texture = nullptr;
+    public:
+        Texture3DRD() {
+            setTexture3DRD(memnew(GodotTexture3DRD));
+        }
+
+        Texture3DRD(GodotTexture3DRD* t) {
+            setTexture3DRD(t);
+        }
+
+        GodotTexture3DRD* getTexture3DRD() {
+            return texture;
+        }
+
+        void setTexture3DRD(GodotTexture3DRD* t) {
+            texture = t;
+            texture->reference();
+            setTexture3D(texture);
+        }
+    };
+}
