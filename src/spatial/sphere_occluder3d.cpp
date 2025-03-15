a9d4d4e6e2cd5a3f3b576358a67562aef31d02d9
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-14 17:05:03 -0800

    Add SphereOccluder3D class and Lua binding for integration

---
+++ src/spatial/sphere_occluder3d.cpp
@@ -0,0 +1,12 @@
+#include "sphere_occluder3d.h"
+
+void sunaba::spatial::bindSphereOccluder3D(sol::state_view& lua) {
+    lua.new_usertype<sunaba::spatial::SphereOccluder3D>("SphereOccluder3D",
+        sol::constructors<SphereOccluder3D()>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::spatial::Occluder3D>(),
+        "radius", sol::property(&SphereOccluder3D::getRadius, &SphereOccluder3D::setRadius),
+        "cast", [](sunaba::core::Resource* instance) {
+            return new SphereOccluder3D(godot::Object::cast_to<GodotSphereOccluder3D>(instance->getResource()));
+        }
+    );
+}
---
+++ src/spatial/sphere_occluder3d.h
@@ -0,0 +1,49 @@
+#ifndef SPHERE_OCCLUDER3D_H
+#define SPHERE_OCCLUDER3D_H
+
+#include <godot_cpp/classes/sphere_occluder3d.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotSphereOccluder3D godot::SphereOccluder3D
+
+#include "occluder3d.h"
+
+using namespace godot;
+
+namespace sunaba::spatial {
+    void bindSphereOccluder3D(sol::state_view& lua);
+
+    class SphereOccluder3D : public Occluder3D {
+    private:
+        GodotSphereOccluder3D* sphere_occluder = nullptr;
+    public:
+        SphereOccluder3D() {
+            setSphereOccluder3D(memnew(GodotSphereOccluder3D));
+        }
+
+        SphereOccluder3D(GodotSphereOccluder3D* o) {
+            setSphereOccluder3D(o);
+        }
+
+        GodotSphereOccluder3D* getSphereOccluder3D() {
+            return sphere_occluder;
+        }
+
+        void setSphereOccluder3D(GodotSphereOccluder3D* o) {
+            sphere_occluder = o;
+            sphere_occluder->reference();
+            setResource(sphere_occluder);
+        }
+
+        float getRadius() {
+            return sphere_occluder->get_radius();
+        }
+
+        void setRadius(float radius) {
+            sphere_occluder->set_radius(radius);
+        }
+    };
+}
+
+#endif // SPHERE_OCCLUDER3D_H
