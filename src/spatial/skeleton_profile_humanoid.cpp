bf5df4f8cce8125b7c60ae2de44fc4e8386acde4
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-14 16:43:55 -0800

    Add SkeletonProfileHumanoid class and Lua binding for integration

---
+++ src/spatial/skeleton_profile_humanoid.cpp
@@ -0,0 +1,16 @@
+#include "skeleton_profile_humanoid.h"
+#include "../core/resource.h"
+
+void sunaba::spatial::bindSkeletonProfileHumanoid(sol::state_view& lua) {
+    lua.new_usertype<sunaba::spatial::SkeletonProfileHumanoid>("SkeletonProfileHumanoid",
+        sol::constructors<SkeletonProfileHumanoid()>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, SkeletonProfile>(),
+        "boneSize", sol::property(&SkeletonProfileHumanoid::getBoneSize, &SkeletonProfileHumanoid::setBoneSize),
+        "groupSize", sol::property(&SkeletonProfileHumanoid::getGroupSize, &SkeletonProfileHumanoid::setGroupSize),
+        "rootBone", sol::property(&SkeletonProfileHumanoid::getRootBone, &SkeletonProfileHumanoid::setRootBone),
+        "scaleBaseBone", sol::property(&SkeletonProfileHumanoid::getScaleBaseBone, &SkeletonProfileHumanoid::setScaleBaseBone),
+        "cast", [](sunaba::core::Resource* instance) {
+            return new SkeletonProfileHumanoid(godot::Object::cast_to<GodotSkeletonProfile>(instance->getResource()));
+        }
+    );
+}
---
+++ src/spatial/skeleton_profile_humanoid.h
@@ -0,0 +1,73 @@
+#ifndef SKELETON_PROFILE_H
+#define SKELETON_PROFILE_H
+
+#include <godot_cpp/classes/skeleton_profile.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotSkeletonProfile godot::SkeletonProfile
+
+#include "skeleton_profile.h"
+
+using namespace godot;
+
+namespace sunaba::spatial {
+    void bindSkeletonProfileHumanoid(sol::state_view& lua);
+
+    class SkeletonProfileHumanoid : public SkeletonProfile {
+    private:
+        GodotSkeletonProfile* skeletonProfile = nullptr;
+    public:
+        SkeletonProfileHumanoid() {
+            setSkeletonProfile(memnew(GodotSkeletonProfile));
+        }
+
+        SkeletonProfileHumanoid(GodotSkeletonProfile* sp) {
+            setSkeletonProfile(sp);
+        }
+
+        GodotSkeletonProfile* getSkeletonProfile() {
+            return skeletonProfile;
+        }
+
+        void setSkeletonProfile(GodotSkeletonProfile* sp) {
+            skeletonProfile = sp;
+            skeletonProfile->reference();
+            setSkeletonProfile(skeletonProfile);
+        }
+
+        int getBoneSize() {
+            return skeletonProfile->get_bone_size();
+        }
+
+        void setBoneSize(int size) {
+            skeletonProfile->set_bone_size(size);
+        }
+
+        int getGroupSize() {
+            return skeletonProfile->get_group_size();
+        }
+
+        void setGroupSize(int size) {
+            skeletonProfile->set_group_size(size);
+        }
+
+        std::string getRootBone() {
+            return String(skeletonProfile->get_root_bone()).utf8().get_data();
+        }
+
+        void setRootBone(std::string name) {
+            skeletonProfile->set_root_bone(String(name.c_str()));
+        }
+
+        std::string getScaleBaseBone() {
+            return String(skeletonProfile->get_scale_base_bone()).utf8().get_data();
+        }
+
+        void setScaleBaseBone(std::string name) {
+            skeletonProfile->set_scale_base_bone(String(name.c_str()));
+        }
+    };
+}
+
+#endif // SKELETON_PROFILE_H
