21572ceef8c5c601f3a73a2452ead0191407a3d8
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-16 10:32:30 -0800

    Add input binding functions for improved input event handling

---
+++ src/input/bind_input_classes.cpp
@@ -0,0 +1,36 @@
+#include "bind_input_classes.h"
+#include "input_event.h"
+#include "input_event_from_window.h"
+#include "input_event_with_modifiers.h"
+#include "input_event_action.h"
+#include "input_event_gesture.h"
+#include "input_event_magnify_gesture.h"
+#include "input_event_pan_gesture.h"
+#include "input_event_joypad_button.h"
+#include "input_event_joypad_motion.h"
+#include "input_event_key.h"
+#include "input_event_mouse.h"
+#include "input_event_mouse_button.h"
+#include "input_event_mouse_motion.h"
+#include "input_event_screen_drag.h"
+#include "input_event_screen_touch.h"
+#include "shortcut.h"
+
+void sunaba::input::bindInputClasses(sol::state& lua) {
+    bindInputEvent(lua);
+    bindInputEventFromWindow(lua);
+    bindInputEventWithModifiers(lua);
+    bindInputEventAction(lua);
+    bindInputEventGesture(lua);
+    bindInputEventMagnifyGesture(lua);
+    bindInputEventPanGesture(lua);
+    bindInputEventJoypadButton(lua);
+    bindInputEventJoypadMotion(lua);
+    bindInputEventKey(lua);
+    bindInputEventMouse(lua);
+    bindInputEventMouseButton(lua);
+    bindInputEventMouseMotion(lua);
+    bindInputEventScreenDrag(lua);
+    bindInputEventScreenTouch(lua);
+    bindShortcut(lua);
+}
---
+++ src/input/bind_input_classes.h
@@ -0,0 +1,10 @@
+#ifndef BIND_INPUT_CLASSES_H
+#define BIND_INPUT_CLASSES_H
+
+#include <sol/sol.hpp>
+
+namespace sunaba::input {
+    void bindInputClasses(sol::state& lua);
+}
+
+#endif // BIND_INPUT_CLASSES_H
