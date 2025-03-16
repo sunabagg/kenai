ceee0b125ca640095ea0519f36f5f2f557f28661
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-16 15:42:16 -0800

    Add drawMultilineString method to CanvasItem for advanced text rendering

--- src/core/canvas_item.h
+++ src/core/canvas_item.h
@@ -245,6 +245,31 @@ namespace sunaba::core {
             }
             canvas_item->draw_multiline_colors(pointsArray, colorsArray, width, antialiased);
         }
+
+        void drawMultilineString(Font* font, Vector2 pos, std::string text, int alignment = 0, float width = -1, int fontSize = 16, int maxLines = -1, Color modulate = Color(1, 1, 1, 1), std::vector<int> brkFlags = {3}, std::vector<int> justificationFlags = {3}, int direction = 0, int orientation = 0) {
+            BitField<TextServer::LineBreakFlag> brkFlagsBitField = NULL;
+            for (int flag : brkFlags) {
+                brkFlagsBitField = static_cast<TextServer::LineBreakFlag>(static_cast<int>(brkFlagsBitField) | flag);
+            }
+            BitField<TextServer::JustificationFlag> justificationFlagsBitField = NULL;
+            for (int flag : justificationFlags) {
+                justificationFlagsBitField = static_cast<TextServer::JustificationFlag>(static_cast<int>(justificationFlagsBitField) | flag);
+            }
+            canvas_item->draw_multiline_string(
+                font->getFont(), 
+                pos, 
+                text.c_str(), 
+                static_cast<HorizontalAlignment>(alignment), 
+                width, 
+                fontSize, 
+                maxLines, 
+                modulate, 
+                brkFlagsBitField, 
+                justificationFlagsBitField, 
+                static_cast<TextServer::Direction>(direction), 
+                static_cast<TextServer::Orientation>(orientation)
+            );
+        }
     };
 }
 
