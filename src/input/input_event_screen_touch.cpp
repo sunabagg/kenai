f1c0e2f7a9c62799a213d0099893e336b193ea0d
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-16 10:32:36 -0800

    Add binding function for InputEventScreenTouch to enhance Lua integration

--- src/input/input_event_screen_touch.cpp
+++ src/input/input_event_screen_touch.cpp
@@ -1,1 +1,31 @@
 #include "input_event_screen_touch.h"
+
+void sunaba::input::bindInputEventScreenTouch(sol::state_view& lua) {
+    lua.new_usertype<sunaba::input::InputEventScreenTouch>("InputEventScreenTouch",
+        sol::constructors<InputEventScreenTouch()>(),
+        sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::input::InputEvent, sunaba::input::InputEventFromWindow>(),
+        "canceled", sol::property(
+            &InputEventScreenTouch::isCanceled,
+            &InputEventScreenTouch::setCanceled
+        ),
+        "doubleTap", sol::property(
+            &InputEventScreenTouch::isDoubleTap,
+            &InputEventScreenTouch::setDoubleTap
+        ),
+        "index", sol::property(
+            &InputEventScreenTouch::getIndex,
+            &InputEventScreenTouch::setIndex
+        ),
+        "position", sol::property(
+            &InputEventScreenTouch::getPosition,
+            &InputEventScreenTouch::setPosition
+        ),
+        "pressed", sol::property(
+            &InputEventScreenTouch::isPressed,
+            &InputEventScreenTouch::setPressed
+        ),
+        "cast", [](sunaba::core::Resource* instance) {
+            return new InputEventScreenTouch(godot::Object::cast_to<GodotInputEventScreenTouch>(instance->getResource()));
+        }
+    );
+}
