bb9cf36cf4845b9845d0f837221cf63b4b3b71be
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-16 16:40:28 -0800

    Add canvas_item.cpp file for core canvas item functionality

---
+++ src/core/canvas_item.cpp
@@ -0,0 +1,102 @@
+#include "canvas_item.h"
+
+namespace sunaba::core {
+    void CanvasItemProxy::_draw() {
+        if (canvas_item_element != nullptr) {
+            canvas_item_element->draw();
+        }
+    }
+
+    void bindCanvasItem(sol::state& lua) {
+        lua.new_usertype<CanvasItem>(
+            "CanvasItem",
+            sol::constructors<CanvasItem(), CanvasItem(CanvasItemNode*)>(),
+            sol::base_classes, sol::bases<Element>(),
+            sol::meta_function::garbage_collect, sol::destructor([](CanvasItem* c) { }),
+            "clipChildren", sol::property(
+                CanvasItem::getClipChildren,
+                CanvasItem::setClipChildren
+            ),
+            "lightMask", sol::property(
+                CanvasItem::getLightMask,
+                CanvasItem::setLightMask
+            ),
+            "material", sol::property(
+                CanvasItem::getMaterial,
+                CanvasItem::setMaterial
+            ),
+            "modulate", sol::property(
+                CanvasItem::getModulate,
+                CanvasItem::setModulate
+            ),
+            "selfModulate", sol::property(
+                CanvasItem::getSelfModulate,
+                CanvasItem::setSelfModulate
+            ),
+            "showBehindParent", sol::property(
+                CanvasItem::getShowBehindParent,
+                CanvasItem::setShowBehindParent
+            ),
+            "textureFilter", sol::property(
+                CanvasItem::getTextureFilter,
+                CanvasItem::setTextureFilter
+            ),
+            "textureRepeat", sol::property(
+                CanvasItem::getTextureRepeat,
+                CanvasItem::setTextureRepeat
+            ),
+            "topLevel", sol::property(
+                CanvasItem::isTopLevel,
+                CanvasItem::setTopLevel
+            ),
+            "useParentMaterial", sol::property(
+                CanvasItem::getUsesParentMaterial,
+                CanvasItem::setUseParentMaterial
+            ),
+            "visibilityLayer", sol::property(
+                CanvasItem::getVisibilityLayer,
+                CanvasItem::setVisibilityLayer
+            ),
+            "visible", sol::property(
+                CanvasItem::isVisible,
+                CanvasItem::setVisible
+            ),
+            "ySortEnabled", sol::property(
+                CanvasItem::getYSortEnabled,
+                CanvasItem::setYSortEnabled
+            ),
+            "zAsRelative", sol::property(
+                CanvasItem::isZAsRelative,
+                CanvasItem::setZAsRelative
+            ),
+            "zIndex", sol::property(
+                CanvasItem::getZIndex,
+                CanvasItem::setZIndex
+            ),
+            "drawAnimationSlice", &CanvasItem::drawAnimationSlice,
+            "drawArc", &CanvasItem::drawArc,
+            "drawChar", &CanvasItem::drawChar,
+            "drawCharOutline", &CanvasItem::drawCharOutline,
+            "drawCircle", &CanvasItem::drawCircle,
+            "drawColoredPolygon", &CanvasItem::drawColoredPolygon,
+            "drawLine", &CanvasItem::drawLine,
+            "drawDashedLine", &CanvasItem::drawDashedLine,
+            "drawEndAnimation", &CanvasItem::drawEndAnimation,
+            "drawLcdTextureRectRegion", &CanvasItem::drawLcdTextureRectRegion,
+            "drawMsdfTextureRectRegion", &CanvasItem::drawMsdfTextureRectRegion,
+            "drawMultiline", &CanvasItem::drawMultiline,
+            "drawMultilineColors", &CanvasItem::drawMultilineColors,
+            "drawMultilineString", &CanvasItem::drawMultilineString,
+            "drawMultilineStringOutline", &CanvasItem::drawMultilineStringOutline,
+            "drawPolygon", &CanvasItem::drawPolygon,
+            "drawPolyline", &CanvasItem::drawPolyline,
+            "drawPolylineColors", &CanvasItem::drawPolylineColors,
+            "drawPrimitive", &CanvasItem::drawPrimitive,
+            "drawRect", &CanvasItem::drawRect,
+            "drawRect", &CanvasItem::drawRect,
+            "drawTexture", &CanvasItem::drawTexture,
+            "drawTextureRect", &CanvasItem::drawTextureRect,
+            "drawTextureRectRegion", &CanvasItem::drawTextureRectRegion,
+        );
+    }
+}
