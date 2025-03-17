aaf71b2276db4b96da1f6ef047c2382102607352
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-17 10:46:47 -0800

    Refactor Entity class and introduce EntityNative for improved component management

--- sunaba/core/Entity.hx
+++ sunaba/core/Entity.hx
@@ -1,40 +1,91 @@
 package sunaba.core;
 
-@:native("Entity")
-extern class Entity extends BaseObject {
-    public var name : String;
+import sunaba.native.core.EntityNative;
 
-    public function new();
+class Entity extends BaseObject {
+    public var entInstance(get, default): EntityNative;
+    public function get_entInstance(): EntityNative {
+        return cast instance;
+    }
+    public function new() {
+        instance = new EntityNative();
+        if (instance == null) {
+            throw "Entity.new() returned null";
+        }
+        pushToStack();
+        entInstance = instance;
+    }
+    public static function fromInstance(instance: EntityNative): Entity {
+        if (instance == null) {
+            throw "Entity.fromInstance() received null instance";
+        }
+        var obj = new Entity();
+        obj.instance = instance;
+        return obj;
+    }
 
-    public function addComponent(comp: Component, name : String) : Void;
+    public var name(get, set): String;
+    public function get_name(): String {
+        return entInstance.name;
+    }
+    public function set_name(value: String): Void {
+        entInstance.name = value;
+    }
 
-    public function hasComponent(comp : Behavior) : Bool;
 
-    public function hasComponentByName(name : String) : Bool;
+    public function addComponent(comp: Component, name: String): Void {
+        entInstance.addComponent(comp.compInstance, name);
+    }
 
-    public function removeComponent(comp : Component) : Void;
+    public function hasComponent(comp: Component): Bool {
+        return entInstance.hasComponent(comp);
+    }
 
-    public function removeUserComponent(comp : Behavior) : Void;
-    
-    public function getUserComponent(type : Class<Behavior>) : Behavior;
+    public function hasComponentByName(name: String): Bool {
+        return entInstance.hasComponentByName(name);
+    }
+    public function removeComponent(comp: Component): Void {
+        entInstance.removeComponent(comp.compInstance);
+    }
 
-    public function getComponent(type : Class<Component>) : Component;
+    public function getUserComponent(type: Class<Component>): Component {
+        return Component.fromInstance(entInstance.getUserComponent(type));
+    }
 
-    public function getComponentByName(name : String) : Component;
+    public function getComponentByName(name : String) : Component {
+        return Component.fromInstance(entInstance.getComponentByName(name));
+    }
 
-    public function getComponentsByType(type : Class<Component>) : lua.Table<Int, Component>;
+    public function getComponentsByType(type : Class<Component>, ) : Array<Component> {
+        var arr = entInstance.getUserComponentsByType(type);
+        var result = new Array<Component>();
+        for (comp in arr) {
+            result.push(comp);
+        }
+        return result;
+    }
 
-    public function getUserComponentsByType(type : Class<Behavior>) : lua.Table<Int, Behavior>;
+    public function addChild(child : Entity) : Void {
+        entInstance.addChild(child.entInstance);
+    }
 
-    public function addChild(child: Entity): Void;
+    public function removeChild(child : Entity) : Void {
+        entInstance.removeChild(child.entInstance);
+    }
 
-    public function removeChild(child: Entity): Void;
+    public function hasChild(child : Entity) : Bool {
+        return entInstance.hasChild(child.entInstance);
+    }
 
-    public function hasChild(child: Entity): Bool;
+    public function find(path : String) : Entity {
+        return Entity.fromInstance(entInstance.find(path));
+    }
 
-    public function find(path : String) : Entity;
+    public function getChildCount() : Int {
+        return entInstance.getChildCount();
+    }
 
-    public function getChildCount() : Int;
-
-    public function getChild(index: Int) : Entity;
+    public function getChild(index: Int) : Entity {
+        return Entity.fromInstance(entInstance.getChild(index));
+    }
 }
---
+++ sunaba/native/core/EntityNative.hx
@@ -0,0 +1,41 @@
+package sunaba.native.core;
+
+import sunaba.core.Component;
+
+@:native("Entity")
+extern class EntityNative extends BaseObjectNative {
+    public var name : String;
+
+    public function new();
+
+    public function addComponent(comp: ComponentNative, name : String) : Void;
+
+    public function hasComponent(comp : Component) : Bool;
+
+    public function hasComponentByName(name : String) : Bool;
+
+    public function removeComponent(comp : ComponentNative) : Void;
+
+    
+    public function getUserComponent(type : Class<Component>) : Component;
+
+    public function getComponent(type : Class<ComponentNative>) : Component;
+
+    public function getComponentByName(name : String) : Component;
+
+    public function getComponentsByType(type : Class<ComponentNative>) : Array<ComponentNative>;
+
+    public function getUserComponentsByType(type : Class<ComponentNative>) : Array<Component>;
+
+    public function addChild(child: EntityNative): Void;
+
+    public function removeChild(child: EntityNative): Void;
+
+    public function hasChild(child: EntityNative): Bool;
+
+    public function find(path : String) : EntityNative;
+
+    public function getChildCount() : Int;
+
+    public function getChild(index: Int) : EntityNative;
+}
