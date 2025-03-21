6cc15b3c0ba65e76578b23fe763e338f597f3f66
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-20 19:22:53 -0800

    Add castFrom method to Resource class for type casting

--- sunaba/core/Resource.hx
+++ sunaba/core/Resource.hx
@@ -4,4 +4,6 @@ extern class Resource extends BaseObject {
     public function duplicate() : Resource;
     public function emitChanged() : Void;
     public function isNull() : Bool;
+    @:native("cast")
+    public function castFrom(type: Dynamic) : Resource;
 }
