36749b4d2224dd91a1711c2d3ec198a9417f9470
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-20 19:40:14 -0800

    Add Font class with methods for font metrics and character support

---
+++ sunaba/core/Font.hx
@@ -0,0 +1,27 @@
+package sunaba.core;
+
+extern class Font extends Resource {
+    public var fallbacks: Array<Font>;
+    public function getAscent(fontSize : Int) : Float;
+    public function getCharSize(chr : Int, fontsize : Int) : Vector2;
+    public function getDescent(fontSize : Int) : Float;
+    public function getFaceCount() : Int;
+    public function getFontName() : String;
+    public function getFontStyle() : Int;
+    public function getFontWeight() : Int;
+    public function getHeight(fontSize : Int) : Float;
+    public function getMultilineStringSize(text : String, alignment : Float = 0.0, width : Float = -1.0, fontSize : Float = 16, maxLines: Int = -1, brkFlags : Int = 3, justificationFlags : Int = 3, direction : Int = 0, orientation : Int = 0) : Vector2;
+    public function getOpentypeFeatures() : Map<Dynamic, Dynamic>;
+    public function getOtNameStrings() : Map<Dynamic, Dynamic>;
+    public function getSpacing(spacing : Int) : Int;
+    public function getStringSize(text : String, alignment : Int = 0, width : Float = 0, fontSize : Int = 16, justificationFlags : Int = 3, direction : Int = 0, orientation : Int = 0) : Vector2;
+    public function getSupportedChars() : String;
+    public function getSupportedFeatureList() : Map<Dynamic, Dynamic>;
+    public function getSupportedVariationList() : Map<Dynamic, Dynamic>;
+    public function getUnderlinePosition(fontSize : Int) : Float;
+    public function getUnderlineThickness(fontSize : Int) : Float;
+    public function hasChar(chr : Int) : Bool;
+    public function isLanguageSupported(lang : String) : Bool;
+    public function isScriptSupported(script : String) : Bool;
+    public function setCacheCapacity(singleline : Int, multiline : Int) : Void;
+}
