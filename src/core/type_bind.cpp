ba27df213d60a7a189bb917b81661a5466b48de0
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-20 19:59:13 -0800

    Add constructors for Rect2 and Rect2i to support additional initialization options

--- src/core/type_bind.cpp
+++ src/core/type_bind.cpp
@@ -65,26 +65,35 @@ void sunaba::core::bind_base_types(sol::state& lua) {
         "Rect2",
         sol::constructors<
             Rect2(), 
+            Rect2(Rect2i),
             Rect2(Vector2, Vector2), 
             Rect2(float, float, float, float)>(),
-        "position", &Rect2::position,
-        "size", &Rect2::size,
-        "abs", &Rect2::abs,
-        "encloses", &Rect2::encloses,
-        "expand", &Rect2::expand,
-        "getArea", &Rect2::get_area,
-        "getCenter", &Rect2::get_center,
-        "grow", &Rect2::grow,
-        "growIndividual", &Rect2::grow_individual,
-        "growSide", &Rect2::grow_side,
-        "hasArea", &Rect2::has_area,
-        "hasPoint", &Rect2::has_point,
-        "intersection", &Rect2::intersection,
-        "intersects", &Rect2::intersects,
-        "isEqualApprox", &Rect2::is_equal_approx,
-        //"isFinite", &Rect2::is_finite,
-        "merge", &Rect2::merge,
-        "tostring", [](const Rect2& r) { return std::string((r.operator String()).utf8().get_data()); }
+            "end", sol::property(
+                [](Rect2& self) { 
+                    return self.get_end(); 
+                }, 
+                [](Rect2& self, Vector2 value) { 
+                    self.set_end(value); 
+                }
+            ),
+            "position", &Rect2::position,
+            "size", &Rect2::size,
+            "abs", &Rect2::abs,
+            "encloses", &Rect2::encloses,
+            "expand", &Rect2::expand,
+            "getArea", &Rect2::get_area,
+            "getCenter", &Rect2::get_center,
+            "grow", &Rect2::grow,
+            "growIndividual", &Rect2::grow_individual,
+            "growSide", &Rect2::grow_side,
+            "hasArea", &Rect2::has_area,
+            "hasPoint", &Rect2::has_point,
+            "intersection", &Rect2::intersection,
+            "intersects", &Rect2::intersects,
+            "isEqualApprox", &Rect2::is_equal_approx,
+            //"isFinite", &Rect2::is_finite,
+            "merge", &Rect2::merge,
+            "tostring", [](const Rect2& r) { return std::string((r.operator String()).utf8().get_data()); }
     );
 
     lua.new_usertype<godot::Vector2i>(
@@ -114,7 +123,9 @@ void sunaba::core::bind_base_types(sol::state& lua) {
         "Rect2i", 
         sol::constructors<
             Rect2i(), 
-            Rect2i(Vector2i, Vector2i)>(), 
+            Rect2i(Vector2i, Vector2i),
+            Rect2i(Rect2),
+            Rect2i(int, int, int, int)>(),
             "position", &Rect2i::position, 
             "size", &Rect2i::size,
             "abs", &Rect2i::abs,
