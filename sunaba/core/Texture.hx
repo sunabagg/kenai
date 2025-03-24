11b4b8adbdceb6b2bdf747eb95cdfc208e69f0d9
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-24 12:02:12 -0800

    Add castFrom method to Texture class for dynamic type casting

--- sunaba/core/Texture.hx
+++ sunaba/core/Texture.hx
@@ -2,6 +2,7 @@ package sunaba.core;
 
 @:native("Texture")
 extern class Texture extends Resource {
+    public static function castFrom(type: Dynamic) : Texture;
 }
 
 abstract TextureAbstract(Texture) from Texture to Texture {
