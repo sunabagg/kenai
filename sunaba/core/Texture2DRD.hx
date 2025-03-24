97526dd078e34ecf02b0737a38ba798bc974c664
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-24 12:17:44 -0800

    Enhance Texture2DRD.fromResource method to return null for null textures

--- sunaba/core/Texture2DRD.hx
+++ sunaba/core/Texture2DRD.hx
@@ -8,6 +8,10 @@ extern class Texture2DRD extends Texture2D {
 abstract Texture2DRDAbstract(Texture2DRD) from Texture2DRD to Texture2DRD {
     @:from
     public static function fromResource(resource: Resource) : Texture2DRDAbstract {
-        return Texture2DRD.castFrom(resource);
+        var txt2drd = Texture2DRD.castFrom(resource);
+        if (txt2drd.isNull()) {
+            return null;
+        }
+        return txt2drd;
     } 
 }
