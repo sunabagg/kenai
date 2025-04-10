4ee6e3a112109ef24c93b4075d1653f6fd7c474f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-10 13:53:12 -0800

    Add Translation class and TranslationAbstract for localization support

---
+++ sunaba/core/Translation.hx
@@ -0,0 +1,27 @@
+package sunaba.core;
+
+@:native("Translation")
+extern class Translation extends Resource {
+    public var locale: String;
+    public function addMessage(srcMsg: String, xlatedMsg: String, n: Int, context: String = ""): Void;
+    public function addPluralMessage(srcMsg: String, xlatedMsg: String, n: Int, context: String = ""): Void;
+    public function eraseMessage(srcMsg: String, context: String): Void;
+    public function getMessage(srcMsg: String, context: String = ""): String;
+    public function getMessageCount(): Int;
+    public function getMessageList(): Array<String>;
+    public function getPluralMessage(srcMsg: String, srcPluralMsg: String, n: Int, context: String): String;
+    public function getTRanslatedMessageList(): Array<String>;
+    @:native("cast")
+    public static function castFrom(type: Dynamic) : Translation;
+}
+
+abstract TranslationAbstract(Translation) from Translation to Translation {
+    @:from
+    public static function fromResource(resource: Resource) : TranslationAbstract {
+        var translation = Translation.castFrom(resource);
+        if (translation.isNull()) {
+            return null;
+        }
+        return translation;
+    } 
+} // end package sunaba.core
