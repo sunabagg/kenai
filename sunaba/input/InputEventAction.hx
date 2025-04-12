a9c96cf9c563fd753c748a7f430b19709df42e07
mintkat <lucasbear9@gmail.com>
2025-04-12 14:52:05 -0800

    Add InputEventActionAbstract to facilitate resource conversion to InputEventAction

--- sunaba/input/InputEventAction.hx
+++ sunaba/input/InputEventAction.hx
@@ -6,4 +6,17 @@ extern class InputEventAction extends InputEvent {
     public var eventIndex: Int;
     public var pressed: Bool;
     public var strength: Float;
+    @:native("cast")
+    public static function castFrom(resource: Dynamic): InputEventAction;
+}
+
+abstract InputEventActionAbstract(InputEventAction) from InputEventAction to InputEventAction {
+    @:from
+    public static function fromResource(resource: Resource) : InputEventActionAbstract {
+        var inputEventAction = InputEventAction.castFrom(resource);
+        if (inputEventAction.isNull()) {
+            return null;
+        }
+        return inputEventAction;
+    }
 }
