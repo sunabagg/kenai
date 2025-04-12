d5674dc62abd0303e425695e4b825c56dfa1623c
mintkat <lucasbear9@gmail.com>
2025-04-12 14:53:59 -0800

    Add castFrom method and abstract conversion for InputEventJoypadMotion

--- sunaba/input/InputEventJoypadMotion.hx
+++ sunaba/input/InputEventJoypadMotion.hx
@@ -4,4 +4,17 @@ package sunaba.input;
 extern class InputEventJoypadMotion extends InputEvent {
     public var axis: Int;
     public var axisValue: Float;
+    @:native("cast")
+    public static function castFrom(resource: Dynamic): InputEventJoypadMotion;
 }
+
+abstract InputEventJoypadMotionAbstract(InputEventJoypadMotion) from InputEventJoypadMotion to InputEventJoypadMotion {
+    @:from
+    public static function fromResource(resource: Resource) : InputEventJoypadMotionAbstract {
+        var inputEventJoypadMotion = InputEventJoypadMotion.castFrom(resource);
+        if (inputEventJoypadMotion.isNull()) {
+            return null;
+        }
+        return inputEventJoypadMotion;
+    }
+} // end package sunaba.input
