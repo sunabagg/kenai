2393a4f676e570634314bbc6b4ef267fdc2d9d94
mintkat <lucasbear9@gmail.com>
2025-04-12 15:08:50 -0800

    Add InputEventPanGesture class and abstract conversion to InputEventPanGestureAbstract

---
+++ sunaba/input/InputEventPanGesture.hx
@@ -0,0 +1,19 @@
+package sunaba.input;
+
+@:native("InputEventPanGesture")
+extern class InputEventPanGesture extends InputEventGesture {
+    public var delta: Vector2;
+    @:native("cast")
+    public function castFrom(res: Dynamic): InputEventPanGesture;
+}
+
+abstract InputEventPanGestureAbstract(InputEventPanGesture) from InputEventPanGesture to InputEventPanGesture {
+    @:from
+    public static function fromResource(resource: Resource) : InputEventPanGestureAbstract {
+        var inputEventPanGesture = InputEventPanGesture.castFrom(resource);
+        if (inputEventPanGesture.isNull()) {
+            return null;
+        }
+        return inputEventPanGesture;
+    }
+} // end package sunaba.input
