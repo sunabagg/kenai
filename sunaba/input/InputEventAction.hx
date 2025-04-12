14f4b87bd0498d1fbdd80622f59c97122d3de9e6
mintkat <lucasbear9@gmail.com>
2025-04-12 14:24:35 -0800

    Add InputEventAction class to represent input actions in the sunaba.input package

---
+++ sunaba/input/InputEventAction.hx
@@ -0,0 +1,8 @@
+package sunaba.input;
+
+class InputEventAction extends InputEvent {
+    public var action: String;
+    public var eventIndex: Int;
+    public var pressed: Bool;
+    public var strength: Float;
+}
