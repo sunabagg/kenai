087d90d5fa3c77883d84c35796e37868b1cbf57b
mintkat <lucasbear9@gmail.com>
2025-04-12 14:53:35 -0800

    Add castFrom method to InputEventGesture and implement abstract conversion to InputEventGestureAbstract

--- sunaba/input/InputEventGesture.hx
+++ sunaba/input/InputEventGesture.hx
@@ -3,4 +3,17 @@ package sunaba.input;
 @:native("InputEventGesture")
 extern class InputEventGesture extends InputEventWithModifiers {
     public var position: Vector2;
+    @:native("cast")
+    public function castFrom(res: Dynamic): InputEventGesture;
+}
+
+abstract InputEventGestureAbstract(InputEventGesture) from InputEventGesture to InputEventGesture {
+    @:from
+    public static function fromResource(resource: Resource) : InputEventGestureAbstract {
+        var inputEventGesture = InputEventGesture.castFrom(resource);
+        if (inputEventGesture.isNull()) {
+            return null;
+        }
+        return inputEventGesture;
+    }
 }
