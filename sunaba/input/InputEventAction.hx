b414a9a044b535608dd00e586e348d7e3ce07dc6
mintkat <lucasbear9@gmail.com>
2025-04-12 14:25:18 -0800

    Add native metadata to InputEventAction class for improved integration

--- sunaba/input/InputEventAction.hx
+++ sunaba/input/InputEventAction.hx
@@ -1,6 +1,7 @@
 package sunaba.input;
 
-class InputEventAction extends InputEvent {
+@:native("InputEventAction")
+extern class InputEventAction extends InputEvent {
     public var action: String;
     public var eventIndex: Int;
     public var pressed: Bool;
