4c37a1db013ec6e3b6ca1b50488938ac5730a11b
mintkat <lucasbear9@gmail.com>
2025-04-12 14:52:39 -0800

    Add castFrom method to InputEventWithModifiers and implement abstract conversion to InputEventWithModifiersAbstract

--- sunaba/input/InputEventWithModifiers.hx
+++ sunaba/input/InputEventWithModifiers.hx
@@ -9,4 +9,17 @@ extern class InputEventWithModifiers extends InputEventFromWindow {
     public var shiftPressed: Bool;
     public function getModifiersMask(): Int;
     public function isCommandOrControlPressed(): Bool;
+    @:native("cast")
+    public function castFrom(res: Dynamic): InputEventWithModifiers;
 }
+
+abstract InputEventWithModifiersAbstract(InputEventWithModifiers) from InputEventWithModifiers to InputEventWithModifiers {
+    @:from
+    public static function fromResource(resource: Resource) : InputEventWithModifiersAbstract {
+        var inputEventWithModifiers = InputEventWithModifiers.castFrom(resource);
+        if (inputEventWithModifiers.isNull()) {
+            return null;
+        }
+        return inputEventWithModifiers;
+    }
+} // end package sunaba.input
