446ca8d4a93b3f7a6d692479326d4a74cd08670f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-19 10:34:08 -0800

    Add Lua bindings for Dictionary type with various methods and meta-functions

--- src/core/type_bind.cpp
+++ src/core/type_bind.cpp
@@ -287,6 +287,44 @@ void sunaba::core::bind_base_types(sol::state& lua) {
         "getOrAdd", &Dictionary::get_or_add,
         "has", &Dictionary::has,
         "hasAll", &Dictionary::has_all,
+        "hash", &Dictionary::hash,
+        "isEmpty", &Dictionary::is_empty,
+        "isReadOnly", &Dictionary::is_read_only,
+        "keys", &Dictionary::keys,
+        "makeReadOnly", &Dictionary::make_read_only,
+        "merge", &Dictionary::merge,
+        "merged", &Dictionary::merged,
+        "recursiveEqual", &Dictionary::recursive_equal,
+        "set", &Dictionary::set,
+        "size", &Dictionary::size,
+        "sort", &Dictionary::sort,
+        "values", &Dictionary::values,
+        "__pairs", [](Dictionary& dict) {
+            return sol::as_function([&dict, i = 0](sol::this_state& s) mutable -> std::tuple<sol::object, sol::object> {
+            if (i < dict.size()) {
+                return { sol::make_object(s, dict.keys()[i]), sol::make_object(s, dict.values()[i++]) };
+            } else {
+                return { sol::lua_nil, sol::lua_nil };
+            }
+            });
+        },
+        "__ipairs", [](Dictionary& dict) {
+            return sol::as_function([](sol::this_state& s, Dictionary& d, int i) -> std::tuple<sol::object, sol::object> {
+                i++;
+                if (i <= d.size()) {
+                    return { sol::make_object(s, d.keys()[i - 1]), sol::make_object(s, d.values()[i - 1]) };
+                }
+                return { sol::lua_nil, sol::lua_nil };
+            });
+        },
+        sol::meta_function::index, [](Dictionary& dict, const Variant& key) { return dict.get(key); },
+        sol::meta_function::new_index, [](Dictionary& dict, const Variant& key, const Variant& value) {
+            dict.set(key, value);
+        },
+        sol::meta_function::length, &Dictionary::size,
+        sol::meta_function::to_string, [](const Dictionary& dict) {
+            return "<Dictionary size=" + std::to_string(dict.size()) + ">";
+        }
     );
 
     lua.new_usertype<Vector2>(
