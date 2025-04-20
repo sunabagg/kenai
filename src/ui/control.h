8e41bb3d6c896defb953126c42ee0d4da5272d9f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-20 01:09:01 -0800

    Add hasThemeConstant method to Control class for theme constant retrieval

--- src/ui/control.h
+++ src/ui/control.h
@@ -562,6 +562,10 @@ namespace sunaba::ui {
             bool hasThemeColor(std::string name, std::string type = String(StringName("&amp;&quot;&quot;")).utf8().get_data()) {
                 return control->has_theme_color(name.c_str(), type.c_str());
             }
+
+            bool hasThemeConstant(std::string name, std::string type = String(StringName("&amp;&quot;&quot;")).utf8().get_data()) {
+                return control->has_theme_constant(name.c_str(), type.c_str());
+            }
     };
 }
 
