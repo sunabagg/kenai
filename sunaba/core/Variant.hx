0d277134e10bed5f96ee58857c4329e8e425591f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-19 19:43:24 -0800

    Add VariantAbstract class for enhanced type conversion and resource handling

--- sunaba/core/Variant.hx
+++ sunaba/core/Variant.hx
@@ -91,3 +91,231 @@ extern class Variant {
     @:native("tostring")
     public function toString():String;
 }
+
+abstract VariantAbstract(Variant) from Variant to Variant {
+    @:from
+    public static function fromString(value:String):VariantAbstract {
+        return Variant.fromString(value);
+    }
+    @:to
+    public static function toString(value:VariantAbstract):String {
+        return value.asString();
+    }
+    @:from
+    public static function fromInt(value:Int):VariantAbstract {
+        return Variant.fromInt(value);
+    }
+    @:to
+    public static function toInt(value:VariantAbstract):Int {
+        return value.asInt();
+    }
+    @:from
+    public static function fromFloat(value:Float):VariantAbstract {
+        return Variant.fromFloat(value);
+    }
+    @:to
+    public static function toFloat(value:VariantAbstract):Float {
+        return value.asFloat();
+    }
+    @:from
+    public static function fromBool(value:Bool):VariantAbstract {
+        return Variant.fromBool(value);
+    }
+    @:to
+    public static function toBool(value:VariantAbstract):Bool {
+        return value.asBool();
+    }
+    @:from
+    public static function fromVector2(value:Vector2):VariantAbstract {
+        return Variant.fromVector2(value);
+    }
+    @:to
+    public static function toVector2(value:VariantAbstract):Vector2 {
+        return value.asVector2();
+    }
+    @:from
+    public static function fromVector3(value:Vector3):VariantAbstract {
+        return Variant.fromVector3(value);
+    }
+    @:to
+    public static function toVector3(value:VariantAbstract):Vector3 {
+        return value.asVector3();
+    }
+    @:from
+    public static function fromVector4(value:Vector4):VariantAbstract {
+        return Variant.fromVector4(value);
+    }
+    @:to
+    public static function toVector4(value:VariantAbstract):Vector4 {
+        return value.asVector4();
+    }
+    @:from
+    public static function fromVector2i(value:Vector2i):VariantAbstract {
+        return Variant.fromVector2i(value);
+    }
+    @:to
+    public static function toVector2i(value:VariantAbstract):Vector2i {
+        return value.asVector2i();
+    }
+    @:from
+    public static function fromVector3i(value:Vector3i):VariantAbstract {
+        return Variant.fromVector3i(value);
+    }
+    @:to
+    public static function toVector3i(value:VariantAbstract):Vector3i {
+        return value.asVector3i();
+    }
+    @:from
+    public static function fromVector4i(value:Vector4i):VariantAbstract {
+        return Variant.fromVector4i(value);
+    }
+    @:to
+    public static function toVector4i(value:VariantAbstract):Vector4i {
+        return value.asVector4i();
+    }
+    @:from
+    public static function fromColor(value:Color):VariantAbstract {
+        return Variant.fromColor(value);
+    }
+    @:to
+    public static function toColor(value:VariantAbstract):Color {
+        return value.asColor();
+    }
+    @:from
+    public static function fromRect2(value:Rect2):VariantAbstract {
+        return Variant.fromRect2(value);
+    }
+    @:to
+    public static function toRect2(value:VariantAbstract):Rect2 {
+        return value.asRect2();
+    }
+    @:from
+    public static function fromRect2i(value:Rect2i):VariantAbstract {
+        return Variant.fromRect2i(value);
+    }
+    @:to
+    public static function toRect2i(value:VariantAbstract):Rect2i {
+        return value.asRect2i();
+    }
+    @:from
+    public static function fromBasis(value:Basis):VariantAbstract {
+        return Variant.fromBasis(value);
+    }
+    @:to
+    public static function toBasis(value:VariantAbstract):Basis {
+        return value.asBasis();
+    }
+    @:from
+    public static function fromTransform2D(value:Transform2D):VariantAbstract {
+        return Variant.fromTransform2D(value);
+    }
+    @:to
+    public static function toTransform2D(value:VariantAbstract):Transform2D {
+        return value.asTransform2D();
+    }
+    @:from
+    public static function fromTransform3D(value:Transform3D):VariantAbstract {
+        return Variant.fromTransform3D(value);
+    }
+    @:to
+    public static function toTransform3D(value:VariantAbstract):Transform3D {
+        return value.asTransform3D();
+    }
+    @:from
+    public static function fromPlane(value:Plane):VariantAbstract {
+        return Variant.fromPlane(value);
+    }
+    @:to
+    public static function toPlane(value:VariantAbstract):Plane {
+        return value.asPlane();
+    }
+    @:from
+    public static function fromQuaternion(value:Quaternion):VariantAbstract {
+        return Variant.fromQuatrernion(value);
+    }
+    @:to
+    public static function toQuaternion(value:VariantAbstract):Quaternion {
+        return value.asQuaternion();
+    }
+    @:from
+    public static function fromElement(value:Element):VariantAbstract {
+        return Variant.fromElement(value);
+    }
+    @:to
+    public static function toElement(value:VariantAbstract):Element {
+        return value.asElement();
+    }
+    @:from
+    public static function fromResource(value:Resource):VariantAbstract {
+        return Variant.fromResource(value);
+    }
+    @:to
+    public static function toResource(value:VariantAbstract):Resource {
+        return value.asResource();
+    }
+    @:from
+    public static function fromByteArray(value:BinaryData):VariantAbstract {
+        return Variant.fromByteArray(value);
+    }
+    @:to
+    public static function toByteArray(value:VariantAbstract):BinaryData {
+        return value.asByteArray();
+    }
+    @:from
+    public static function fromIntArray(value:Table<Int, Int>):VariantAbstract {
+        return Variant.fromIntArray(value);
+    }
+    /*@:to
+    public static function toIntArray(value:VariantAbstract):Table<Int, Int> {
+        return value.asIntArray();
+    }
+    @:from
+    public static function fromIntArray64(value:Array<Int>):VariantAbstract {
+        return Variant.fromIntArray64(value);
+    }
+    */
+    @:to
+    public static function toIntArray64(value:VariantAbstract):Array<Int> {
+        return value.asIntArray64();
+    }
+    /*@:from
+    public static function fromFloatArray(value:Table<Int, Float>):VariantAbstract {
+        return Variant.fromFloatArray(value);
+    }
+    @:to
+    public static function toFloatArray(value:VariantAbstract):Table<Int, Float> {
+        return value.asFloatArray();
+    }*/
+    @:from
+    public static function fromFloatArray64(value:Array<Float>):VariantAbstract {
+        return Variant.fromFloatArray64(value);
+    }
+    @:to
+    public static function toFloatArray64(value:VariantAbstract):Array<Float> {
+        return value.asFloatArray64();
+    }
+    @:from
+    public static function fromStringArray(value:Table<Int, String>):VariantAbstract {
+        return Variant.fromStringArray(value);
+    }
+    @:to
+    public static function toStringArray(value:VariantAbstract):Table<Int, String> {
+        return value.asStringArray();
+    }
+    @:from
+    public static function fromVector2Array(value:Table<Int, Vector2>):VariantAbstract {
+        return Variant.fromVector2Array(value);
+    }
+    @:to
+    public static function toVector2Array(value:VariantAbstract):Table<Int, Vector2> {
+        return value.asVector2Array();
+    }
+    @:from
+    public static function fromVector3Array(value:Table<Int, Vector3>):VariantAbstract {
+        return Variant.fromVector3Array(value);
+    }
+    @:to
+    public static function toVector3Array(value:VariantAbstract):Table<Int, Vector3> {
+        return value.asVector3Array();
+    }
+}
