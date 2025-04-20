44f8064619ec378cfd3ef2c32645f8e89866f416
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-20 12:49:29 -0800

    Add BaseObjectProxy class for managing BaseObject instances

---
+++ src/core/base_object_proxy.h
@@ -0,0 +1,23 @@
+#ifndef BASE_OBJECT_PROXY_H
+#define BASE_OBJECT_PROXY_H
+
+#include <godot_cpp/classes/object.hpp>
+#include "base_object.h"
+
+using namespace godot;
+
+namespace sunaba::core {
+    class BaseObjectProxy : public Object {
+        GDCLASS(BaseObjectProxy, Object);
+    protected:
+        static void _bind_methods() {
+        }
+    public:
+        BaseObject* base_object = nullptr;
+
+        BaseObjectProxy() {
+        }
+    };
+}
+
+#endif // BASE_OBJECT_PROXY_H
