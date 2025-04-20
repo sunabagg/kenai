91db5a8630f13ca1628e479c3215a7157b4218a5
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-20 01:24:18 -0800

    Add setOffset method to Control class for setting offset values

--- src/ui/control.h
+++ src/ui/control.h
@@ -682,6 +682,10 @@ namespace sunaba::ui {
             void setGlobalPosition(Vector2 position, bool keepOffset = false) {
                 control->set_global_position(position, keepOffset);
             }
+
+            void setOffset(int side, float value) {
+                control->set_offset(static_cast<godot::Side>(side), value);
+            }
     };
 }
 
