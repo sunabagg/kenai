5de1d82f97c5b07d1a4082be3e71387dfcc2ae6e
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-20 04:15:31 -0800

    Add hasThemeIconOverride function to Control class

--- sunaba/ui/Control.hx
+++ sunaba/ui/Control.hx
@@ -93,4 +93,5 @@ extern class Control extends CanvasItem {
     public function hasThemeFontSize(name:String,?type:String):Bool;
     public function hasThemeFontSizeOverride(name:String):Bool;
     public function hasThemeIcon(name:String,?type:String):Bool;
+    public function hasThemeIconOverride(name:String):Bool;
 }
