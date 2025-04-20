0a24a970ca0204b8a4100b12075eb5fec19463fe
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-20 04:23:16 -0800

    Add setAnchorsAndOffsetsPreset function and update setAnchorAndOffset signature in Control class

--- sunaba/ui/Control.hx
+++ sunaba/ui/Control.hx
@@ -107,5 +107,6 @@ extern class Control extends CanvasItem {
     public function removeThemeStyleboxOverride(name:String):Void;
     public function resetSize():Void;
     public function setAnchor(side:Int, anchor:Float, ?keepOffset:Bool, ?pushOppositeAnchor:Bool):Void;
-    public function setAnchorAndOffset(side:Int, anchor:Float, offset:Float):Void;
+    public function setAnchorAndOffset(side:Int, anchor:Float, offset:Float, ?pushOppositeAnchor:Bool):Void;
+    public function setAnchorsAndOffsetsPreset(preset:Int, ?resizeMode:Int, margin:Int):Void;
 }
