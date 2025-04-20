38e875bcf900410e66d2f9845d4cc8d199dc4b98
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-20 04:14:40 -0800

    Add hasThemeFontOverride function to Control class

--- sunaba/ui/Control.hx
+++ sunaba/ui/Control.hx
@@ -89,4 +89,5 @@ extern class Control extends CanvasItem {
     public function hasThemeConstant(name:String,?type:String):Bool;
     public function hasThemeConstantOverride(name:String):Bool;
     public function hasThemeFont(name:String,?type:String):Bool;
+    public function hasThemeFontOverride(name:String):Bool;
 }
