1ab06e74facc408cf3221d0bbafc37fd3b7a152f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-20 13:33:47 -0800

    Update emit method signature to accept variable arguments and add EventAbstract abstraction

--- sunaba/core/Event.hx
+++ sunaba/core/Event.hx
@@ -7,5 +7,9 @@ extern class Event extends BaseObject {
     public function new();
     public function connect(func: Function): Void;
     public function disconnect(func: Function): Void;
-    public function emit(args: Array<Dynamic>): Void;
+    public function emit(...args: Variant): Void;
+}
+
+abstract EventAbstract(Event) from Event to Event {
+    @:op()
 }
