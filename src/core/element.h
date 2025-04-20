338f890ee3af8b5cad77f4848b73fa72d3e262bc
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-20 13:11:51 -0800

    Rename bindMethodsElement to bindElementMethods for consistency in NodeProxy class

--- src/core/element.h
+++ src/core/element.h
@@ -24,7 +24,7 @@ namespace sunaba::core {
     public:
         Element* element = nullptr;
 
-        static void bindMethodsElement();
+        static void bindElementMethods();
 
         NodeProxy() {
 
