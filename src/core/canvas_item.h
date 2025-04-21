e74bcf1071999a8fed286069d314283da0055d14
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-20 17:39:44 -0800

    Add signal connection for hidden event in CanvasItem

--- src/core/canvas_item.h
+++ src/core/canvas_item.h
@@ -61,6 +61,19 @@ namespace sunaba::core {
             };
             Callable drawCallable = StlFunctionWrapper::create_callable_from_cpp_function(drawFunc);
             this->canvas_item->connect("draw", drawCallable);
+            std::function<Variant(std::vector<Variant>)> hiddenFunc =
+            [this](std::vector<Variant> av) {
+                Array args;
+                for (int i = 0; i < av.size(); ++i) {
+                    args.append(av[i]);
+                }
+                if (this->hidden != nullptr) {
+                    this->hidden->emit(args);
+                }
+                return Variant();
+            };
+            Callable hiddenCallable = StlFunctionWrapper::create_callable_from_cpp_function(hiddenFunc);
+            this->canvas_item->connect("hidden", hiddenCallable);
         }
     public:
         // Constructor with Node* parameter
