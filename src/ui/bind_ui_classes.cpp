dec2f369189bb2f4bbe9a69271b6e2b10f4dc4fb
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-23 12:35:49 -0800

    Add bindUIClasses function to bind UI components in Lua

---
+++ src/ui/bind_ui_classes.cpp
@@ -0,0 +1,19 @@
+#include "bind_ui_classes.h"
+
+#include "style_box.h"
+#include "style_box_empty.h"
+#include "style_box_flat.h"
+#include "style_box_line.h"
+#include "style_box_texture.h"
+#include "theme.h"
+#include "control.h"
+
+void sunaba::ui::bindUIClasses(sol::state& lua) {
+    bindStyleBox(lua);
+    bindStyleBoxEmpty(lua);
+    bindStyleBoxFlat(lua);
+    bindStyleBoxLine(lua);
+    bindStyleBoxTexture(lua);
+    bindTheme(lua);
+    bindControl(lua);
+}
