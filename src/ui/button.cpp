06776e60f66e72f214589e7b68cb8d53fdaf8294
mintkat <lucasbear9@gmail.com>
2025-04-25 11:24:33 -0800

    Add binding for Button class properties in Lua

---
+++ src/ui/button.cpp
@@ -0,0 +1,64 @@
+#include "button.h"
+
+namespace sunaba::ui {
+    void bindButton(sol::state &lua) {
+        using namespace sol;
+        using namespace sunaba::ui;
+
+        lua.new_usertype<Button>("Button",
+            sol::constructors<Button()>(),
+            sol::base_classes, sol::bases<sunaba::ui::BaseButton>(),
+            "alignment", sol::property(
+                &Button::getAlignment,
+                &Button::setAlignment
+            ),
+            "autowrapMode", sol::property(
+                &Button::getAutowrapMode,
+                &Button::setAutowrapMode
+            ),
+            "clipText", sol::property(
+                &Button::getClipText,
+                &Button::setClipText
+            ),
+            "expandIcon", sol::property(
+                &Button::isExpandIcon,
+                &Button::setExpandIcon
+            ),
+            "flat", sol::property(
+                &Button::isFlat,
+                &Button::setFlat
+            ),
+            "icon", sol::property(
+                &Button::getIcon,
+                &Button::setIcon
+            ),
+            "iconAlignment", sol::property(
+                &Button::getIconAlignment,
+                &Button::setIconAlignment
+            ),
+            "language", sol::property(
+                &Button::getLanguage,
+                &Button::setLanguage
+            ),
+            "text", sol::property(
+                &Button::getText,
+                &Button::setText
+            ),
+            "textDirection", sol::property(
+                &Button::getTextDirection,
+                &Button::setTextDirection
+            ),
+            "textOverrunBehavior", sol::property(
+                &Button::getTextOverrunBehavior,
+                &Button::setTextOverrunBehavior
+            ),
+            "verticalIconAlignment", sol::property(
+                &Button::getVerticalIconAlignment,
+                &Button::setVerticalIconAlignment
+            ),
+            "cast", [](sunaba::core::Resource* p_resource) {
+                return new Button(godot::Object::cast_to<ButtonNode>(p_resource->getResource()));
+            }
+        );
+    }
+}
