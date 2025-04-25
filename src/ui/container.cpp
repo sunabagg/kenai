fbc1c51d075c62518eaa3c19c5c1db5ae4eb9195
mintkat <lucasbear9@gmail.com>
2025-04-25 13:59:04 -0800

    Implement ContainerProxy methods for element interaction and data handling

---
+++ src/ui/container.cpp
@@ -0,0 +1,115 @@
+#include "container.h"
+
+namespace sunaba::ui {
+    void ContainerProxy::_enter_tree() {
+        if (this->element != nullptr) {
+            this->element->enterTree();
+        }
+    }
+    
+    void ContainerProxy::_exit_tree() {
+        if (this->element != nullptr) {
+            this->element->exitTree();
+        }
+    }
+    
+    void ContainerProxy::_ready() {
+        if (this->element != nullptr) {
+            this->element->ready();
+        }
+    }
+    
+    void ContainerProxy::_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->process(delta);
+        }
+    }
+    void ContainerProxy::_physics_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->physicsProcess(delta);
+        }
+    }
+    void ContainerProxy::_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->input(event);
+        }
+    }
+    void ContainerProxy::_unhandled_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->unhandledInput(event);
+        }
+    }
+    void ContainerProxy::_unhandled_key_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->unhandledKeyInput(event);
+        }
+    }
+    void ContainerProxy::_shortcut_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->shortcutInput(event);
+        }
+    }
+    void ContainerProxy::_draw() {
+        if (element != nullptr) {
+            element->_draw();
+        }
+    }
+    bool ContainerProxy::_can_drop_data(const Vector2& at_position, const Variant &data) const {
+        if (element != nullptr) {
+            return element->canDropData(at_position, data);
+        }
+        return false;
+    }
+    void ContainerProxy::_drop_data(const Vector2 &at_position, const Variant &data) {
+        if (element != nullptr) {
+            element->dropData(at_position, data);
+        }
+    }
+    Variant ContainerProxy::_get_drag_data(const Vector2 &at_position) {
+        if (element != nullptr) {
+            return element->getDragData(at_position);
+        }
+        return Variant();
+    }
+    Vector2 ContainerProxy::_get_minimum_size() const {
+        if (element != nullptr) {
+            return element->_getMinimumSize();
+        }
+        return Vector2();
+    }
+    String ContainerProxy::_get_tooltip(const Vector2 &at_position) const {
+        if (element != nullptr) {
+            return element->_getTooltip(at_position).c_str();
+        }
+        return String();
+    }
+    void ContainerProxy::_gui_input(const Ref<InputEvent>& event) {
+        if (element != nullptr) {
+            element->guiInput(event);
+        }
+    }
+    bool ContainerProxy::_has_point(const Vector2 &point) const {
+        if (element != nullptr) {
+            return element->hasPoint(point);
+        }
+        return false;
+    }
+    //Object* ContainerProxy::_make_custom_tooltip(const String &for_text) const {
+    //    if (element != nullptr) {
+    //        return element->makeCustomTooltip(for_text);
+    //    }
+    //    return nullptr;
+    //}
+    PackedInt32Array ContainerProxy::_get_allowed_size_flags_horizontal() const {
+        if (element != nullptr) {
+            return element->getAllowedSizeFlagsHorizontal();
+        }
+        return PackedInt32Array();
+    }
+    PackedInt32Array ContainerProxy::_get_allowed_size_flags_vertical() const {
+        if (element != nullptr) {
+            return element->getAllowedSizeFlagsVertical();
+        }
+        return PackedInt32Array();
+    }
+}
