b6b9655528248753c81f79dd094427db38302860
mintkat <lucasbear9@gmail.com>
2025-04-25 13:06:59 -0800

    Add CheckButton header file with necessary includes and definitions

---
+++ src/ui/check_button.h
@@ -0,0 +1,13 @@
+#ifndef CHECK_BUTTON_H
+#define CHECK_BUTTON_H
+
+#include <godot_cpp/classes/check_button.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define CheckButtonNode godot::CheckButton
+
+#include "button.h"
+
+
+#endif
