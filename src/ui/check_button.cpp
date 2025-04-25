0a4f5a7975ead4e785bdbb13a4ca6faa285055f1
mintkat <lucasbear9@gmail.com>
2025-04-25 13:15:54 -0800

    Refactor _structured_text_parser to use structuredTextParser method on element

--- src/ui/check_button.cpp
+++ src/ui/check_button.cpp
@@ -102,7 +102,7 @@ namespace sunaba::ui {
     // }
     TypedArray<Vector3i> CheckButtonProxy::_structured_text_parser(const Array &args, const String &text) const {
         if (element != nullptr) {
-            return element->_structuredTextParser(args, text);
+            return element->structuredTextParser(args, text);
         }
         return TypedArray<Vector3i>();
     }
