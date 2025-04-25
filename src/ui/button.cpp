4ab7d6be2d45a925469015848af10a0316d6aeae
mintkat <lucasbear9@gmail.com>
2025-04-25 11:35:42 -0800

    Refactor ButtonProxy methods to delegate functionality to the element

--- src/ui/button.cpp
+++ src/ui/button.cpp
@@ -1,6 +1,139 @@
 #include "button.h"
 
 namespace sunaba::ui {
+    void ButtonProxy::_enter_tree() {
+        if (this->element != nullptr) {
+            this->element->enterTree();
+        }
+    }
+    
+    void ButtonProxy::_exit_tree() {
+        if (this->element != nullptr) {
+            this->element->exitTree();
+        }
+    }
+    
+    void ButtonProxy::_ready() {
+        if (this->element != nullptr) {
+            this->element->ready();
+        }
+    }
+    
+    void ButtonProxy::_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->process(delta);
+        }
+    }
+    
+    void ButtonProxy::_physics_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->physicsProcess(delta);
+        }
+    }
+    
+    void ButtonProxy::_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->input(event);
+        }
+    }
+    
+    void ButtonProxy::_unhandled_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->unhandledInput(event);
+        }
+    }
+    
+    void ButtonProxy::_unhandled_key_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->unhandledKeyInput(event);
+        }
+    }
+    
+    void ButtonProxy::_shortcut_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->shortcutInput(event);
+        }
+    }
+
+    void ButtonProxy::_draw() {
+        if (element != nullptr) {
+            element->_draw();
+        }
+    }
+
+    bool ButtonProxy::_can_drop_data(const Vector2& at_position, const Variant &data) const {
+        if (element != nullptr) {
+            return element->canDropData(at_position, data);
+        }
+        return false;
+    }
+
+    void ButtonProxy::_drop_data(const Vector2 &at_position, const Variant &data) {
+        if (element != nullptr) {
+            element->dropData(at_position, data);
+        }
+    }
+
+    Variant ButtonProxy::_get_drag_data(const Vector2 &at_position) {
+        if (element != nullptr) {
+            return element->getDragData(at_position);
+        }
+        return Variant();
+    }
+
+    Vector2 ButtonProxy::_get_minimum_size() const {
+        if (element != nullptr) {
+            return element->_getMinimumSize();
+        }
+        return Vector2();
+    }
+
+    String ButtonProxy::_get_tooltip(const Vector2 &at_position) const {
+        if (element != nullptr) {
+            return element->_getTooltip(at_position).c_str();
+        }
+        return String();
+    }
+
+    void ButtonProxy::_gui_input(const Ref<InputEvent> &event) {
+        if (element != nullptr) {
+            element->guiInput(event);
+        }
+    }
+
+    bool ButtonProxy::_has_point(const Vector2 &point) const {
+        if (element != nullptr) {
+            return element->hasPoint(point);
+        }
+        return false;
+    }
+
+    //Object* ButtonProxy::_make_custom_tooltip(const String &for_text) const {
+    //    if (control_element != nullptr) {
+    //        return control_element->makeCustomTooltip(for_text);
+    //    }
+    //    return nullptr;
+    //}
+
+    TypedArray<Vector3i> ButtonProxy::_structured_text_parser(const Array &args, const String &text) const {
+        if (element != nullptr) {
+            return element->structuredTextParser(args, text);
+        }
+        return TypedArray<Vector3i>();
+    }
+
+    void ButtonProxy::_pressed() {
+        if (this->element != nullptr) {
+            this->element->pressed();
+        }
+    }
+
+    void ButtonProxy::_toggled(bool p_toggled_on) {
+        if (this->element != nullptr) {
+            this->element->toggled(p_toggled_on);
+        }
+    }
+
     void bindButton(sol::state &lua) {
         using namespace sol;
         using namespace sunaba::ui;
