9099b3e6942c204ae63a0b78941e6e7d637343a1
mintkat <lucasbear9@gmail.com>
2025-04-26 10:23:43 -0800

    Implement BoxContainerProxy methods and bind BoxContainer to Lua

---
+++ src/ui/box_container.cpp
@@ -0,0 +1,161 @@
+#include "box_container.h"
+
+namespace sunaba::ui {
+    void BoxContainerProxy::_enter_tree() {
+        if (this->element != nullptr) {
+            this->element->enterTree();
+        }
+    }
+    
+    void BoxContainerProxy::_exit_tree() {
+        if (this->element != nullptr) {
+            this->element->exitTree();
+        }
+    }
+    
+    void BoxContainerProxy::_ready() {
+        if (this->element != nullptr) {
+            this->element->ready();
+        }
+    }
+    
+    void BoxContainerProxy::_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->process(delta);
+        }
+    }
+
+    void BoxContainerProxy::_physics_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->physicsProcess(delta);
+        }
+    }
+
+    void BoxContainerProxy::_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->input(event);
+        }
+    }
+
+    void BoxContainerProxy::_unhandled_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->unhandledInput(event);
+        }
+    }
+
+    void BoxContainerProxy::_unhandled_key_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->unhandledKeyInput(event);
+        }
+    }
+
+    void BoxContainerProxy::_shortcut_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->shortcutInput(event);
+        }
+    }
+
+    void BoxContainerProxy::_draw() {
+        if (element != nullptr) {
+            element->_draw();
+        }
+    }
+
+    bool BoxContainerProxy::_can_drop_data(const Vector2& at_position, const Variant &data) const {
+        if (element != nullptr) {
+            return element->canDropData(at_position, data);
+        }
+        return false;
+    }
+
+    void BoxContainerProxy::_drop_data(const Vector2 &at_position, const Variant &data) {
+        if (element != nullptr) {
+            element->dropData(at_position, data);
+        }
+    }
+
+    Variant BoxContainerProxy::_get_drag_data(const Vector2 &at_position) {
+        if (element != nullptr) {
+            return element->getDragData(at_position);
+        }
+        return Variant();
+    }
+
+    Vector2 BoxContainerProxy::_get_minimum_size() const {
+        if (element != nullptr) {
+            return element->_getMinimumSize();
+        }
+        return Vector2();
+    }
+
+    String BoxContainerProxy::_get_tooltip(const Vector2 &at_position) const {
+        if (element != nullptr) {
+            return element->_getTooltip(at_position).c_str();
+        }
+        return String();
+    }
+
+    void BoxContainerProxy::_gui_input(const Ref<InputEvent>& event) {
+        if (element != nullptr) {
+            element->guiInput(event);
+        }
+    }
+
+    bool BoxContainerProxy::_has_point(const Vector2 &point) const {
+        if (element != nullptr) {
+            return element->hasPoint(point);
+        }
+        return false;
+    }
+
+    //Object* BoxContainerProxy::_make_custom_tooltip(const String &for_text) const {
+    //    if (element != nullptr) {
+    //        return element->makeCustomTooltip(for_text);
+    //    }
+    //    return nullptr;
+    //}
+
+    TypedArray<Vector3i> BoxContainerProxy::_structured_text_parser(const Array &args, const String &text) const {
+        if (element != nullptr) {
+            return element->structuredTextParser(args, text);
+        }
+        return TypedArray<Vector3i>();
+    }
+
+    PackedInt32Array BoxContainerProxy::_get_allowed_size_flags_horizontal() const {
+        if (element != nullptr) {
+            return element->getAllowedSizeFlagsHorizontal();
+        }
+        return PackedInt32Array();
+    }
+
+    PackedInt32Array BoxContainerProxy::_get_allowed_size_flags_vertical() const {
+        if (element != nullptr) {
+            return element->getAllowedSizeFlagsVertical();
+        }
+        return PackedInt32Array();
+    }
+
+    void bindBoxContainer(sol::state& lua) {
+        auto ut = lua.new_usertype<BoxContainer>("BoxContainer",
+            sol::constructors<BoxContainer()>(),
+            sol::base_classes, sol::bases<BaseObject, Element, sunaba::core::CanvasItem, Control, Container>(),
+            "alignment", sol::property(
+                &BoxContainer::getAlignment,
+                &BoxContainer::setAlignment
+            ),
+            "vertical", sol::property(
+                &BoxContainer::isVertical,
+                &BoxContainer::setVertical
+            ),
+            "addSpacer", &BoxContainer::addSpacer,
+            "cast", [](Element* e) {
+                BoxContainerNode* box = Object::cast_to<BoxContainerNode>(e->getNode());
+                if (box == nullptr) {
+                    return new BoxContainer(box);
+                }
+                return static_cast<BoxContainer*>(nullptr);
+            }
+        );
+    }
+}
