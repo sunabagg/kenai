84764a447acaf7c9735a6130664318bbb9f524b0
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-28 12:27:31 -0800

    Add binding for AABB type in Lua with various methods

---
+++ src/core/bind_aabb.cpp
@@ -0,0 +1,44 @@
+#include "lua_bind.h"
+
+void sunaba::core::bind_aabb(sol::state& lua) {
+    lua.new_usertype<godot::AABB>(
+        "AABB",
+        sol::constructors<
+            AABB(), 
+            AABB(Vector3, Vector3)>(),
+        "end", sol::property(
+            [](AABB& self) { 
+                return self.get_end(); 
+            }, 
+            [](AABB& self, Vector3 value) { 
+                self.set_end(value); 
+            }
+        ),
+        "position", &AABB::position,
+        "size", &AABB::size,
+        "abs", &AABB::abs,
+        "encloses", &AABB::encloses,
+        "getCenter", &AABB::get_center,
+        "getEndpoint", &AABB::get_endpoint,
+        "getLongestAxis", &AABB::get_longest_axis,
+        "getLongestAxisIndex", &AABB::get_longest_axis_index,
+        "getLongestAxisSize", &AABB::get_longest_axis_size, 
+        "getShortestAxis", &AABB::get_shortest_axis,
+        "getShortestAxisIndex", &AABB::get_shortest_axis_index,
+        "getShortestAxisSize", &AABB::get_shortest_axis_size,
+        "getSupport", &AABB::get_support,
+        "getVolume", &AABB::get_volume,
+        "grow", &AABB::grow,
+        "hasPoint", &AABB::has_point,
+        "hasSurface", &AABB::has_surface,
+        "hasVolume", &AABB::has_volume,
+        "intersection", &AABB::intersection,
+        "intersects", &AABB::intersects,
+        "intersectsPlane", &AABB::intersects_plane,
+        "intersectsRay", &AABB::intersects_ray,
+        "isEqualApprox", &AABB::is_equal_approx,
+        //"isFinite", &AABB::is_finite,
+        "merge", &AABB::merge,
+        "tostring", [](const AABB& a) { return std::string((a.operator String()).utf8().get_data()); }
+    );
+}
