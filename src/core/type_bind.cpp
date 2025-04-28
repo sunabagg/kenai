77b628e022201fa4e78e8f2b648fa30eed2d3be1
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-28 12:22:56 -0800

    Remove redundant Vector4i usertype definition and ensure binding for Vector4i in Lua

--- src/core/type_bind.cpp
+++ src/core/type_bind.cpp
@@ -25,31 +25,11 @@ void sunaba::core::bind_base_types(sol::state& lua) {
     bind_vector3(lua);
     bind_vector3i(lua);
     bind_vector4(lua);
-    
+    bind_vector4i(lua);
     
     
 
-    lua.new_usertype<godot::Vector4i>(
-        "Vector4i",
-        sol::constructors<Vector4i(), Vector4i(int, int, int, int)>(),
-        "x", &Vector4i::x,
-        "y", &Vector4i::y,
-        "z", &Vector4i::z,
-        "w", &Vector4i::w,
-        "abs", &Vector4i::abs,
-        "clamp", &Vector4i::clamp,
-        //"distanceSquaredTo", &Vector4i::distance_squared_to,
-        //"distanceTo", &Vector4i::distance_to,
-        "length", &Vector4i::length,
-        "lengthSquared", &Vector4i::length_squared,
-        "max", &Vector4i::max,
-        "maxAxisIndex", &Vector4i::max_axis_index,
-        "min", &Vector4i::min,
-        "minAxisIndex", &Vector4i::min_axis_index,
-        "sign", &Vector4i::sign,
-        "tostring", [](const Vector4i& v) { return std::string((v.operator String()).utf8().get_data()); }
-        //"snapped", &Vector4i::snapped
-    );
+    
 
     lua.new_usertype<godot::Quaternion> (
         "Quaternion",
