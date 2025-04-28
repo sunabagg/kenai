529493b54f1634f49dc1dc5f80700f4fce5675d7
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-28 12:23:58 -0800

    Add binding for Quaternion type in Lua with various methods

---
+++ src/core/bind_quaternion.cpp
@@ -0,0 +1,27 @@
+#include "lua_bind.h"
+
+void sunaba::core::bind_basis(sol::state& lua) {
+    lua.new_usertype<godot::Quaternion> (
+        "Quaternion",
+        sol::constructors<Quaternion(), Quaternion(float, float, float, float)>(),
+        "x", &Quaternion::x,
+        "y", &Quaternion::y,
+        "z", &Quaternion::z,
+        "w", &Quaternion::w,
+        "angleTo", &Quaternion::angle_to,
+        "dot", &Quaternion::dot,
+        "exp", &Quaternion::exp,
+        //"fromEuler", &Quaternion::from_euler,
+        "inverse", &Quaternion::inverse,
+        "isEqualApprox", &Quaternion::is_equal_approx,
+        "isNormalized", &Quaternion::is_normalized,
+        "length", &Quaternion::length,
+        "lengthSquared", &Quaternion::length_squared,
+        "log", &Quaternion::log,
+        "normalized", &Quaternion::normalized,
+        "slerp", &Quaternion::slerp,
+        "sphericalCubicInterpolate", &Quaternion::spherical_cubic_interpolate,
+        "sphericalLinearInterpolateInTime", &Quaternion::spherical_cubic_interpolate_in_time,
+        "tostring", [](const Quaternion& q) { return std::string((q.operator String()).utf8().get_data()); }
+    );
+}
