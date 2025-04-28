e32592ec0b2c5f7479d1125aea123af09f1b1f06
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-28 12:20:52 -0800

    Add binding for Vector3i type in Lua, removing redundant usertype definition

--- src/core/type_bind.cpp
+++ src/core/type_bind.cpp
@@ -23,27 +23,9 @@ void sunaba::core::bind_base_types(sol::state& lua) {
     bind_vector2i(lua);
     bind_rect2i(lua);
     bind_vector3(lua);
+    bind_vector3i(lua);
     
     
-    lua.new_usertype<godot::Vector3i>(
-        "Vector3i",
-        sol::constructors<Vector3i(), Vector3i(int, int, int)>(),
-        "x", &Vector3i::x,
-        "y", &Vector3i::y,
-        "z", &Vector3i::z,
-        "abs", &Vector3i::abs,
-        "clamp", &Vector3i::clamp,
-        //"distanceSquaredTo", &Vector3i::distance_squared_to,
-        //"distanceTo", &Vector3i::distance_to,
-        "length", &Vector3i::length,
-        "lengthSquared", &Vector3i::length_squared,
-        "max", &Vector3i::max,
-        "maxAxisIndex", &Vector3i::max_axis_index,
-        "min", &Vector3i::min,
-        "minAxisIndex", &Vector3i::min_axis_index,
-        "sign", &Vector3i::sign,
-        "tostring", [](const Vector3i& v) { return std::string((v.operator String()).utf8().get_data()); }
-    );
     
     lua.new_usertype<godot::Vector4>(
         "Vector4",
