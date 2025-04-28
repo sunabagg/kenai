2e919e2f54af831f5c949b410d0078e75e4dd159
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-28 13:10:08 -0800

    Refactor Lua bindings: rename bind_basis function to bind_quaternion

--- src/core/bind_quaternion.cpp
+++ src/core/bind_quaternion.cpp
@@ -1,6 +1,6 @@
 #include "lua_bind.h"
 
-void sunaba::core::bind_basis(sol::state& lua) {
+void sunaba::core::bind_quaternion(sol::state& lua) {
     lua.new_usertype<godot::Quaternion> (
         "Quaternion",
         sol::constructors<Quaternion(), Quaternion(float, float, float, float)>(),
