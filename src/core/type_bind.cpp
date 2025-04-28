91cca790422e802c7032168996b96ee3613cddf7
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-28 12:18:02 -0800

    Add binding for Rect2i type in Lua with various methods

--- src/core/type_bind.cpp
+++ src/core/type_bind.cpp
@@ -21,44 +21,8 @@ void sunaba::core::bind_base_types(sol::state& lua) {
     bind_vector2(lua);
     bind_rect2(lua);
     bind_vector2i(lua);
+    bind_rect2i(lua);
     
-    
-    
-
-    
-
-    lua.new_usertype<godot::Rect2i>(
-        "Rect2i", 
-        sol::constructors<
-            Rect2i(), 
-            Rect2i(Vector2i, Vector2i),
-            Rect2i(Rect2),
-            Rect2i(int, int, int, int)>(),
-            "position", &Rect2i::position, 
-            "size", &Rect2i::size,
-            "end", sol::property(
-                [](Rect2i& self) {
-                    return self.get_end();
-                },
-                [](Rect2i& self, Vector2i value) {
-                    self.set_end(value);
-                }
-            ),
-            "abs", &Rect2i::abs,
-            "encloses", &Rect2i::encloses,
-            "expand", &Rect2i::expand,
-            "getArea", &Rect2i::get_area,
-            "getCenter", &Rect2i::get_center,
-            "grow", &Rect2i::grow,
-            "growSide", &Rect2i::grow_side,
-            "hasArea", &Rect2i::has_area,
-            "hasPoint", &Rect2i::has_point,
-            "intersection", &Rect2i::intersection,
-            "intersects", &Rect2i::intersects,
-            "tostring", [](const Rect2i& r) { return std::string((r.operator String()).utf8().get_data()); }
-            //"isEqualApprox", &Rect2i::is_equal_approx,
-    );
-
     lua.new_usertype<godot::Vector3>(
         "Vector3",
         sol::constructors<Vector3(), Vector3(float, float, float)>(),
