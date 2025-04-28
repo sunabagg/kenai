f8eea36af4aecda2bb4eb77ce52f1cb09e13a349
mintkat <lucasbear9@gmail.com>
2025-04-27 19:05:56 -0800

    Implement CenterContainerProxy methods for element interaction and binding

---
+++ src/ui/center_container.cpp
@@ -0,0 +1,148 @@
+#include "center_container.h"
+
+namespace sunaba::ui {
+    void CenterContainerProxy::_enter_tree() {
+        if (this->element != nullptr) {
+            this->element->enterTree();
+        }
+    }
+
+    void CenterContainerProxy::_exit_tree() {
+        if (this->element != nullptr) {
+            this->element->exitTree();
+        }
+    }
+
+    void CenterContainerProxy::_ready() {
+        if (this->element != nullptr) {
+            this->element->ready();
+        }
+    }
+
+    void CenterContainerProxy::_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->process(delta);
+        }
+    }
+
+    void CenterContainerProxy::_physics_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->physicsProcess(delta);
+        }
+    }
+
+    void CenterContainerProxy::_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->input(event);
+        }
+    }
+
+    void CenterContainerProxy::_unhandled_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->unhandledInput(event);
+        }
+    }
+
+    void CenterContainerProxy::_unhandled_key_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->unhandledKeyInput(event);
+        }
+    }
+
+    void CenterContainerProxy::_shortcut_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->shortcutInput(event);
+        }
+    }
+
+    void CenterContainerProxy::_draw() {
+        if (element != nullptr) {
+            element->_draw();
+        }
+    }
+
+    bool CenterContainerProxy::_can_drop_data(const Vector2& at_position, const Variant &data) const {
+        if (element != nullptr) {
+            return element->canDropData(at_position, data);
+        }
+        return false;
+    }
+
+    void CenterContainerProxy::_drop_data(const Vector2 &at_position, const Variant &data) {
+        if (element != nullptr) {
+            element->dropData(at_position, data);
+        }
+    }
+
+    Variant CenterContainerProxy::_get_drag_data(const Vector2 &at_position) {
+        if (element != nullptr) {
+            return element->getDragData(at_position);
+        }
+        return Variant();
+    }
+
+    Vector2 CenterContainerProxy::_get_minimum_size() const {
+        if (element != nullptr) {
+            return element->_getMinimumSize();
+        }
+        return Vector2();
+    }
+
+    String CenterContainerProxy::_get_tooltip(const Vector2 &at_position) const {
+        if (element != nullptr) {
+            return element->_getTooltip(at_position).c_str();
+        }
+        return String();
+    }
+
+    void CenterContainerProxy::_gui_input(const Ref<InputEvent>& event) {
+        if (element != nullptr) {
+            element->guiInput(event);
+        }
+    }
+
+    bool CenterContainerProxy::_has_point(const Vector2 &point) const {
+        if (element != nullptr) {
+            return element->hasPoint(point);
+        }
+        return false;
+    }
+
+    TypedArray<Vector3i> CenterContainerProxy::_structured_text_parser(const Array &args, const String &text) const {
+        if (element != nullptr) {
+            return element->structuredTextParser(args, text);
+        }
+        return TypedArray<Vector3i>();
+    }
+
+    PackedInt32Array CenterContainerProxy::_get_allowed_size_flags_horizontal() const {
+        if (element != nullptr) {
+            return element->getAllowedSizeFlagsHorizontal();
+        }
+        return PackedInt32Array();
+    }
+
+    PackedInt32Array CenterContainerProxy::_get_allowed_size_flags_vertical() const {
+        if (element != nullptr) {
+            return element->getAllowedSizeFlagsVertical();
+        }
+        return PackedInt32Array();
+    }
+
+    void bindCenterContainerProxy(sol::state& lua) {
+        lua.new_usertype<CenterContainer>("CenterContainer",
+            sol::constructors<CenterContainer()>(),
+            sol::base_classes, sol::bases<BaseObject, Element, sunaba::core::CanvasItem, Control, Container>(),
+            "useTopLeft", sol::property(
+                &CenterContainer::isUsingTopLeft, 
+                &CenterContainer::setUseTopLeft
+            ),
+            "cast", [](Element* element) {
+                CenterContainerNode* node = Object::cast_to<CenterContainerNode>(element->getNode());
+                if (node != nullptr) {
+                    return new CenterContainer(node);
+                }
+                return nullptr;
+            }
+        );
+    }
