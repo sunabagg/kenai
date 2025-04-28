080a4ad90fa4b0e23b417b46ff54ada4f61e4806
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-28 12:24:11 -0800

    Remove redundant Quaternion usertype definition and ensure binding in Lua

--- src/core/type_bind.cpp
+++ src/core/type_bind.cpp
@@ -26,34 +26,12 @@ void sunaba::core::bind_base_types(sol::state& lua) {
     bind_vector3i(lua);
     bind_vector4(lua);
     bind_vector4i(lua);
+    bind_quaternion(lua);
     
+
     
 
     
-
-    lua.new_usertype<godot::Quaternion> (
-        "Quaternion",
-        sol::constructors<Quaternion(), Quaternion(float, float, float, float)>(),
-        "x", &Quaternion::x,
-        "y", &Quaternion::y,
-        "z", &Quaternion::z,
-        "w", &Quaternion::w,
-        "angleTo", &Quaternion::angle_to,
-        "dot", &Quaternion::dot,
-        "exp", &Quaternion::exp,
-        //"fromEuler", &Quaternion::from_euler,
-        "inverse", &Quaternion::inverse,
-        "isEqualApprox", &Quaternion::is_equal_approx,
-        "isNormalized", &Quaternion::is_normalized,
-        "length", &Quaternion::length,
-        "lengthSquared", &Quaternion::length_squared,
-        "log", &Quaternion::log,
-        "normalized", &Quaternion::normalized,
-        "slerp", &Quaternion::slerp,
-        "sphericalCubicInterpolate", &Quaternion::spherical_cubic_interpolate,
-        "sphericalLinearInterpolateInTime", &Quaternion::spherical_cubic_interpolate_in_time,
-        "tostring", [](const Quaternion& q) { return std::string((q.operator String()).utf8().get_data()); }
-    );
 
     lua.new_usertype<godot::Basis>(
         "Basis",
