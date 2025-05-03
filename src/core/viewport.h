ba437cb9c7059a6b1ac0988e24052132802cec2d
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-03 14:28:28 -0800

    Add getPositionalShadowAtlasQuad3 method to Viewport class

--- src/core/viewport.h
+++ src/core/viewport.h
@@ -280,6 +280,10 @@ namespace sunaba::core {
             void setPositionalShadowAtlasQuad2(int subdiv) {
                 viewport->set_positional_shadow_atlas_quadrant_subdiv(2, static_cast<godot::Viewport::PositionalShadowAtlasQuadrantSubdiv>(subdiv));
             }
+
+            int getPositionalShadowAtlasQuad3() {
+                return viewport->get_positional_shadow_atlas_quadrant_subdiv(3);
+            }
     };
 }
 
