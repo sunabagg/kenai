5c5d4d5ec3aaceee929007b8f95707a2ab47e8ab
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-02 23:21:13 -0800

    Implement LinkButtonProxy methods for event handling and data management

--- src/ui/link_button.cpp
+++ src/ui/link_button.cpp
@@ -1,5 +1,90 @@
 #include "link_button.h"
 
 namespace sunaba::ui {
-    
+    void LinkButtonProxy::_enter_tree() {
+        if (element != nullptr) {
+            element->enterTree();
+        }
+    }
+
+    void LinkButtonProxy::_exit_tree() {
+        if (element != nullptr) {
+            element->exitTree();
+        }
+    }
+
+    void LinkButtonProxy::_ready() {
+        if (element != nullptr) {
+            element->ready();
+        }
+    }
+
+    void LinkButtonProxy::_process(double delta) {
+        if (element != nullptr) {
+            element->process(delta);
+        }
+    }
+
+    void LinkButtonProxy::_physics_process(double delta) {
+        if (element != nullptr) {
+            element->physicsProcess(delta);
+        }
+    }
+
+    void LinkButtonProxy::_input(const Ref<InputEvent>& event) {
+        if (element != nullptr) {
+            element->input(event);
+        }
+    }
+
+    void LinkButtonProxy::_unhandled_input(const Ref<InputEvent>& event) {
+        if (element != nullptr) {
+            element->unhandledInput(event);
+        }
+    }
+
+    void LinkButtonProxy::_unhandled_key_input(const Ref<InputEvent>& event) {
+        if (element != nullptr) {
+            element->unhandledKeyInput(event);
+        }
+    }
+
+    void LinkButtonProxy::_shortcut_input(const Ref<InputEvent>& event) {
+        if (element != nullptr) {
+            element->shortcutInput(event);
+        }
+    }
+
+    void LinkButtonProxy::_draw() {
+        if (element != nullptr) {
+            element->_draw();
+        }
+    }
+
+    bool LinkButtonProxy::_can_drop_data(const Vector2& at_position, const Variant &data) const {
+        if (element != nullptr) {
+            return element->canDropData(at_position, data);
+        }
+        return false;
+    }
+
+    void LinkButtonProxy::_drop_data(const Vector2 &at_position, const Variant &data) {
+        if (element != nullptr) {
+            element->dropData(at_position, data);
+        }
+    }
+
+    Variant LinkButtonProxy::_get_drag_data(const Vector2 &at_position) {
+        if (element != nullptr) {
+            return element->getDragData(at_position);
+        }
+        return Variant();
+    }
+
+    Vector2 LinkButtonProxy::_get_minimum_size() const {
+        if (element != nullptr) {
+            return element->_getMinimumSize();
+        }
+        return Vector2();
+    }
 }
