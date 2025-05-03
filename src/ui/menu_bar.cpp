ba7a2e32d2311770f3839d7314f896d9a973ac69
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-03 13:04:40 -0800

    Add MenuBarProxy class with lifecycle methods for menu management

---
+++ src/ui/menu_bar.cpp
@@ -0,0 +1,33 @@
+#include "menu_bar.h"
+
+namespace sunaba::ui {
+    void MenuBarProxy::_enter_tree() {
+        if (element != nullptr) {
+            element->enterTree();
+        }
+    }
+    
+    void MenuBarProxy::_exit_tree() {
+        if (element != nullptr) {
+            element->exitTree();
+        }
+    }
+    
+    void MenuBarProxy::_ready() {
+        if (element != nullptr) {
+            element->ready();
+        }
+    }
+    
+    void MenuBarProxy::_process(double delta) {
+        if (element != nullptr) {
+            element->process(delta);
+        }
+    }
+    
+    void MenuBarProxy::_physics_process(double delta) {
+        if (element != nullptr) {
+            element->physicsProcess(delta);
+        }
+    }
+}
