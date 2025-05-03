3fcd0369e331b1c6fd535e1be9f4e7ba340146d2
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-02 23:22:13 -0800

    Add _gui_input method to LinkButtonProxy for handling GUI input events

--- src/ui/link_button.cpp
+++ src/ui/link_button.cpp
@@ -94,4 +94,10 @@ namespace sunaba::ui {
         }
         return String();
     }
+
+    void LinkButtonProxy::_gui_input(const Ref<InputEvent>& event) {
+        if (element != nullptr) {
+            element->guiInput(event);
+        }
+    }
 }
