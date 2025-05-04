6de21f5dd52b62f3dfd8e0a8837c47b27931e2d9
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-04 15:33:05 -0800

    Add hasThemeFontOverride method to Window class for theme font override retrieval

--- src/desktop/window.h
+++ src/desktop/window.h
@@ -722,6 +722,10 @@ namespace sunaba::desktop {
             bool hasThemeFont(std::string name, std::string type = "&amp;&quot;&quot;") {
                 return window->has_theme_font(name.c_str(), type.c_str());
             }
+
+            bool hasThemeFontOverride(std::string name) {
+                return window->has_theme_font_override(name.c_str());
+            }
     };
 }
 
