3a1e4711197828e950c1c60cb2fa2ed356cf721a
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-04 15:33:15 -0800

    Add hasThemeFontSize method to Window class for theme font size retrieval

--- src/desktop/window.h
+++ src/desktop/window.h
@@ -726,6 +726,10 @@ namespace sunaba::desktop {
             bool hasThemeFontOverride(std::string name) {
                 return window->has_theme_font_override(name.c_str());
             }
+
+            bool hasThemeFontSize(std::string name, std::string type = "&amp;&quot;&quot;") {
+                return window->has_theme_font_size(name.c_str(), type.c_str());
+            }
     };
 }
 
