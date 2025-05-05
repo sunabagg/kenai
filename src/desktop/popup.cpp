5a9a4fc626822879ca14b007f81e30fc87917b75
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-05 14:13:15 -0800

    Add PopupProxy methods for tree management and processing

---
+++ src/desktop/popup.cpp
@@ -0,0 +1,27 @@
+#include "popup.h"
+
+namespace sunaba::desktop {
+    void PopupProxy::_enter_tree() {
+        if (this->element != nullptr) {
+            this->element->enterTree();
+        }
+    }
+
+    void PopupProxy::_exit_tree() {
+        if (this->element != nullptr) {
+            this->element->exitTree();
+        }
+    }
+
+    void PopupProxy::_ready() {
+        if (this->element != nullptr) {
+            this->element->ready();
+        }
+    }
+
+    void PopupProxy::_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->process(delta);
+        }
+    }
+}
