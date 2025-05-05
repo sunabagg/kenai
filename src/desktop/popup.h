669e11c19d126e8afba94b4b03e13760ef262266
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-05 13:53:22 -0800

    Add Popup class and PopupProxy for window management in Lua binding

---
+++ src/desktop/popup.h
@@ -0,0 +1,73 @@
+#ifndef POPUP_H
+#define POPUP_H
+
+#include <godot_cpp/classes/popup.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define PopupNode godot::Popup
+
+#include "window.h"
+#include "../core/stl_function_wrapper.h"
+#include "../core/event.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+namespace sunaba::desktop {
+    void bindPopup(sol::state &lua);
+
+    class Popup;
+
+    class PopupProxy : public PopupNode {
+        public:
+            sunaba::desktop::Popup* element = nullptr;
+
+            void onInit() {
+                // Initialize the NodeProxy instance
+            }
+
+            void _enter_tree() override;
+            void _exit_tree() override;
+            void _ready() override ;
+            void _process(double delta) override;
+            void _physics_process(double delta) override;
+            void _input(const Ref<InputEvent>& event) override;
+            void _unhandled_input(const Ref<InputEvent>& event) override;
+            void _unhandled_key_input(const Ref<InputEvent>& event) override;
+            void _shortcut_input(const Ref<InputEvent>& event) override;
+
+            Vector2 _get_contents_minimum_size() const override;
+    };
+
+    class Popup : public sunaba::desktop::Window {
+        private:
+            PopupNode* popup = nullptr; // Pointer to the Popup instance
+            void connectPopupSignals() {
+                // Connect signals specific to Popup
+            }
+
+        public:
+            Popup() {
+                setPopup(memnew(PopupProxy));
+                onInit();
+            }
+
+            Popup(PopupNode* popup) {
+                setPopup(popup);
+                onInit();
+            }
+
+            PopupNode* getPopup() {
+                return popup;
+            }
+
+            void setPopup(PopupNode* popup) {
+                this->popup = popup;
+                connectPopupSignals();
+                setViewport(popup);
+            }
+    };
+}
+
+#endif // POPUP_H
