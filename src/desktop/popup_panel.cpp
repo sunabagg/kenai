22a1aa66d4c4ba65e039ef1e2c3b5f4cd553f527
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-06 12:56:25 -0800

    Add _shortcut_input method to PopupPanelProxy for handling shortcut input events

--- src/desktop/popup_panel.cpp
+++ src/desktop/popup_panel.cpp
@@ -48,4 +48,10 @@ namespace sunaba::desktop {
             this->element->unhandledKeyInput(event);
         }
     }
+
+    void PopupPanelProxy::_shortcut_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->shortcutInput(event);
+        }
+    }
 }
