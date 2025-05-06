a8ec0aaf14860689947ec21bfe7825c4a0a9ce28
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-06 12:47:17 -0800

    Add PopupPanel and PopupPanelProxy classes for enhanced popup functionality

---
+++ src/desktop/popup_panel.h
@@ -0,0 +1,78 @@
+#ifndef POPUP_PANEL_H
+#define POPUP_PANEL_H
+
+#include <godot_cpp/classes/popup_panel.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define PopupPanelNode godot::PopupPanel
+
+#include "popup.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+namespace sunaba::desktop {
+    void bindPopupPanel(sol::state& lua);
+
+    class PopupPanel;
+
+    class PopupPanelProxy : public PopupPanelNode {
+        public:
+            sunaba::desktop::PopupPanel* element = nullptr;
+
+            void onInit() {
+                // Initialize the NodeProxy instance
+            }
+
+            void _enter_tree() override;
+            void _exit_tree() override;
+            void _ready() override;
+            void _process(double delta) override;
+            void _physics_process(double delta) override;
+            void _input(const Ref<InputEvent>& event) override;
+            void _unhandled_input(const Ref<InputEvent>& event) override;
+            void _unhandled_key_input(const Ref<InputEvent>& event) override;
+            void _shortcut_input(const Ref<InputEvent>& event) override;
+
+            Vector2 _get_contents_minimum_size() const override;
+    };
+
+    class PopupPanel : public sunaba::desktop::Popup {
+        private:
+            PopupPanelNode* popup = nullptr; // Pointer to the Popup instance
+            void connectPopupSignals() {
+                // Connect signals specific to Popup
+            }
+
+        public:
+            PopupPanel() {
+                setPopupPanel(memnew(PopupPanelProxy));
+                onInit();
+            }
+
+            PopupPanel(PopupPanelNode* popup) {
+                setPopupPanel(popup);
+                onInit();
+            }
+
+            PopupPanelNode* getPopup() const {
+                return popup;
+            }
+
+            void setPopupPanel(PopupPanelNode* popup) {
+                this->popup = popup;
+                connectPopupSignals();
+                setPopup(popup);
+            }
+
+            void setPopupPanel(PopupPanelProxy* popup) {
+                this->popup = popup;
+                popup->element = this;
+                connectPopupSignals();
+                setPopup(popup);
+            }
+        };
+} // namespace sunaba::desktop
+
+#endif // POPUP_PANEL_H
