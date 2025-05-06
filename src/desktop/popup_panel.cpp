4a34006a4af12aab6e6931cf72ca92f9bcecb805
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-06 12:55:20 -0800

    Implement _enter_tree method in PopupPanelProxy for element tree management

--- src/desktop/popup_panel.cpp
+++ src/desktop/popup_panel.cpp
@@ -1,5 +1,9 @@
 #include "popup_panel.h"
 
 namespace sunaba::desktop {
-    
+    void PopupPanelProxy::_enter_tree() {
+        if (this->element != nullptr) {
+            this->element->enterTree();
+        }
+    }
 }
