973bb6efdccdfbce0db2720b5200a18341b325ac
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-08 11:53:13 -0800

    Rename addItemShortcut to setItemShortcut in PopupMenu for consistency with other setter methods

--- src/desktop/popup_menu.h
+++ src/desktop/popup_menu.h
@@ -426,8 +426,8 @@ namespace sunaba::desktop {
                 popup_menu->set_item_multistate_max(index, maxStates);
             }
 
-            void addItemShortcut(int index, sunaba::input::Shortcut* shortcut, bool global = false) {
-                popup_menu->add_item_shortcut(index, shortcut->getShortcut(), global);
+            void setItemShortcut(int index, sunaba::input::Shortcut* shortcut, bool global = false) {
+                popup_menu->set_item_shortcut(index, shortcut->getShortcut(), global);
             }
     };
 }
