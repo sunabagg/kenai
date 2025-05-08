a57d2cf04b32746b153bafbc355332baa6156452
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-08 12:05:08 -0800

    Add base class bindings for PopupMenu in Lua

--- src/desktop/popup_menu.cpp
+++ src/desktop/popup_menu.cpp
@@ -65,5 +65,6 @@ namespace sunaba::desktop {
     void bindPopupMenu(sol::state& lua) {
         lua.new_usertype<PopupMenu>("PopupMenu",
             sol::constructors<PopupMenu()>(),
+            sol::base_classes, sol::bases<BaseObject, Element, sunaba::core::Viewport, Window, Popup>(),
     }
 }
