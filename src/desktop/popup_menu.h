37b8d9b46aacbdab79183c35d655be1b4fbb891f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-08 10:54:54 -0800

    Add addIconCheckShortcut method to PopupMenu for adding checkable items with shortcuts

--- src/desktop/popup_menu.h
+++ src/desktop/popup_menu.h
@@ -201,6 +201,10 @@ namespace sunaba::desktop {
             void addIconCheckItem(sunaba::core::Texture2D* texture, std::string label, int id = -1, int accel = 0) {
                 popup_menu->add_icon_check_item(texture->getTexture2D(), label.c_str(), id, static_cast<godot::Key>(accel));
             }
+
+            void addIconCheckShortcut(sunaba::core::Texture2D* texture, sunaba::input::Shortcut* shortcut, int id = -1, bool global = false) {
+                popup_menu->add_icon_check_shortcut(texture->getTexture2D(), shortcut->getShortcut(), id, global);
+            }
     };
 }
 
