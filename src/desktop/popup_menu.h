191ca3317811b71f302c288e238e492c5b584244
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-08 10:39:04 -0800

    Add idPressedEvent member to PopupMenu for event handling

--- src/desktop/popup_menu.h
+++ src/desktop/popup_menu.h
@@ -161,6 +161,7 @@ namespace sunaba::desktop {
             void setIfFocusedEvent(Event* event) {
                 ifFocusedEvent = event;
             }
+            Event* idPressedEvent;
     };
 }
 
