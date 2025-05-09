a911a6d9c293a1cac3693facbb7635dd600aa154
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-09 12:57:58 -0800

    Implement _draw method in MenuButtonProxy for rendering the element

--- src/ui/menu_button.cpp
+++ src/ui/menu_button.cpp
@@ -54,4 +54,10 @@ namespace sunaba::ui {
             element->shortcutInput(event);
         }
     }
+
+    void MenuButtonProxy::_draw() {
+        if (element != nullptr) {
+            element->_draw();
+        }
+    }
 }
