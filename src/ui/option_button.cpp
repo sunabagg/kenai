ac9a4686a17b0601bdd84ae4ac8642e24c54f1f5
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-11 15:17:55 -0800

    Add OptionButtonProxy class with lifecycle methods for managing element state

---
+++ src/ui/option_button.cpp
@@ -0,0 +1,27 @@
+#include "option_button.h"
+
+namespace sunaba::ui {
+    void OptionButtonProxy::_enter_tree() {
+        if (element != nullptr) {
+            element->enterTree();
+        }
+    }
+
+    void OptionButtonProxy::_exit_tree() {
+        if (element != nullptr) {
+            element->exitTree();
+        }
+    }
+
+    void OptionButtonProxy::_ready() {
+        if (element != nullptr) {
+            element->ready();
+        }
+    }
+
+    void OptionButtonProxy::_process(double delta) {
+        if (element != nullptr) {
+            element->process(delta);
+        }
+    }
+}
