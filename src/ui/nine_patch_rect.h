60b7d1105b5b9d40b996718349ccd299b7f9316c
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-11 14:32:05 -0800

    Add texture2d.h include to NinePatchRect header for texture handling

--- src/ui/nine_patch_rect.h
+++ src/ui/nine_patch_rect.h
@@ -10,6 +10,7 @@
 #include "control.h"
 #include "../core/stl_function_wrapper.h"
 #include "../core/event.h"
+#include "../core/texture2d.h"
 
 using namespace godot;
 using namespace sunaba::core;
