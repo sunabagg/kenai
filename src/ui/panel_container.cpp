6e755f2c5503a8c7fc0836baaa02902995709c2e
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-11 22:22:21 -0800

    Add _get_allowed_size_flags_horizontal method to PanelContainerProxy for retrieving horizontal size flags

--- src/ui/panel_container.cpp
+++ src/ui/panel_container.cpp
@@ -114,4 +114,11 @@ namespace sunaba::ui {
         }
         return TypedArray<Vector3i>();
     }
+
+    PackedInt32Array PanelContainerProxy::_get_allowed_size_flags_horizontal() const {
+        if (element != nullptr) {
+            return element->getAllowedSizeFlagsHorizontal();
+        }
+        return PackedInt32Array();
+    }
 }
