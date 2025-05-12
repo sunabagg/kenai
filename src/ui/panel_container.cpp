9bbc0f2f3815684a0741b332263b61640e75f62f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-11 22:19:31 -0800

    Add _physics_process method to PanelContainerProxy for physics processing

--- src/ui/panel_container.cpp
+++ src/ui/panel_container.cpp
@@ -24,4 +24,10 @@ namespace sunaba::ui {
             this->element->process(delta);
         }
     }
+
+    void PanelContainerProxy::_physics_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->physicsProcess(delta);
+        }
+    }
 }
