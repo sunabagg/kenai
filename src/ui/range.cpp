33064b1dde79ccfdc0e106af7f73189756704af0
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-12 12:57:17 -0800

    Add RangeProxy class with lifecycle and processing methods

---
+++ src/ui/range.cpp
@@ -0,0 +1,33 @@
+#include "range.h"
+
+namespace sunaba::ui {
+    void RangeProxy::_enter_tree() {
+        if (element != nullptr) {
+            element->enterTree();
+        }
+    }
+    
+    void RangeProxy::_exit_tree() {
+        if (element != nullptr) {
+            element->exitTree();
+        }
+    }
+    
+    void RangeProxy::_ready() {
+        if (element != nullptr) {
+            element->ready();
+        }
+    }
+    
+    void RangeProxy::_process(double delta) {
+        if (element != nullptr) {
+            element->process(delta);
+        }
+    }
+
+    void RangeProxy::_physics_process(double delta) {
+        if (element != nullptr) {
+            element->physicsProcess(delta);
+        }
+    }
+}
