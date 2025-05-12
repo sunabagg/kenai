a85cf7fbbf35c7965bcdc01cec4406f03fdc2c6f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-12 15:42:13 -0800

    Add _value_changed method to HScrollBarProxy for value change handling

--- src/ui/h_scroll_bar.cpp
+++ src/ui/h_scroll_bar.cpp
@@ -114,4 +114,10 @@ namespace sunaba::ui {
         }
         return TypedArray<Vector3i>();
     }
+
+    void HScrollBarProxy::_value_changed(double value) {
+        if (element != nullptr) {
+            element->valueChanged(value);
+        }
+    }
 }
