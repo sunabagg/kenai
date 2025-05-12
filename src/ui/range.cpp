dcafc71e54537f1fdd845f28be29c702bb75d479
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-12 13:02:02 -0800

    Add _gui_input method to RangeProxy class for handling GUI input events

--- src/ui/range.cpp
+++ src/ui/range.cpp
@@ -94,4 +94,10 @@ namespace sunaba::ui {
         }
         return String();
     }
+
+    void RangeProxy::_gui_input(const Ref<InputEvent> &event) {
+        if (element != nullptr) {
+            element->guiInput(event);
+        }
+    }
 }
