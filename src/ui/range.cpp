25192013ab5d5b42ae7eab5e178892a4aa9d4cd7
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-12 12:58:19 -0800

    Add _unhandled_key_input method to RangeProxy class for handling unhandled key input events

--- src/ui/range.cpp
+++ src/ui/range.cpp
@@ -42,4 +42,10 @@ namespace sunaba::ui {
             element->unhandledInput(event);
         }
     }
+
+    void RangeProxy::_unhandled_key_input(const Ref<InputEvent>& event) {
+        if (element != nullptr) {
+            element->unhandledKeyInput(event);
+        }
+    }
 }
