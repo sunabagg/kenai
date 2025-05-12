0b32794d46384c70f918c54fe2774305de775646
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-12 15:51:58 -0800

    Add _physics_process method to VScrollBarProxy for physics processing

--- src/ui/v_scroll_bar.cpp
+++ src/ui/v_scroll_bar.cpp
@@ -24,4 +24,10 @@ namespace sunaba::ui {
             element->process(delta);
         }
     }
+
+    void VScrollBarProxy::_physics_process(double delta) {
+        if (element != nullptr) {
+            element->physicsProcess(delta);
+        }
+    }
 }
