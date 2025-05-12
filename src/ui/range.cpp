ef00286cbb9f48ad350a4d461a8d1d2152dac527
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-12 13:12:55 -0800

    Add allowLesser property to Range class for Lua binding

--- src/ui/range.cpp
+++ src/ui/range.cpp
@@ -126,6 +126,7 @@ namespace sunaba::ui {
             sol::constructors<Range()>(),
             sol::base_classes, sol::bases<BaseObject, Element, sunaba::core::CanvasItem, Control>(),
             "allowGreater", sol::property(&Range::getAllowGreater, &Range::setAllowGreater),
+            "allowLesser", sol::property(&Range::getAllowLesser, &Range::setAllowLesser),
         );
     }
 }
