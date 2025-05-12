5d6f160ba946263c195c04a3e04030b0b209c782
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-11 22:19:23 -0800

    Add PanelContainerProxy class for managing UI panel lifecycle events

---
+++ src/ui/panel_container.cpp
@@ -0,0 +1,27 @@
+#include "panel_container.h"
+
+namespace sunaba::ui {
+    void PanelContainerProxy::_enter_tree() {
+        if (this->element != nullptr) {
+            this->element->enterTree();
+        }
+    }
+    
+    void PanelContainerProxy::_exit_tree() {
+        if (this->element != nullptr) {
+            this->element->exitTree();
+        }
+    }
+    
+    void PanelContainerProxy::_ready() {
+        if (this->element != nullptr) {
+            this->element->ready();
+        }
+    }
+    
+    void PanelContainerProxy::_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->process(delta);
+        }
+    }
+}
