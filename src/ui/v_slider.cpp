2f9eea465594a9949708fcfc56ca8d1ca1e067ff
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-13 14:15:03 -0800

    Add VSliderProxy::_unhandled_key_input method to handle unhandled key input events

--- src/ui/v_slider.cpp
+++ src/ui/v_slider.cpp
@@ -42,4 +42,10 @@ namespace sunaba::ui {
             element->unhandledInput(event);
         }
     }
+
+    void VSliderProxy::_unhandled_key_input(const Ref<InputEvent>& event) {
+        if (element != nullptr) {
+            element->unhandledKeyInput(event);
+        }
+    }
 }
