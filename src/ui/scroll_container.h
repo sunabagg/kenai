e9e506a94bc9ee30c5fa054ceffeeca4125f5f7e
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-12 16:15:11 -0800

    Add ScrollContainer and ScrollContainerProxy classes with initialization and signal connection

---
+++ src/ui/scroll_container.h
@@ -0,0 +1,90 @@
+#ifndef SCROLL_CONTAINER_H
+#define SCROLL_CONTAINER_H
+
+#include <godot_cpp/classes/scroll_container.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define ScrollContainerNode godot::ScrollContainer
+
+#include "container.h"
+#include "../core/stl_function_wrapper.h"
+#include "../core/event.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+namespace sunaba::ui {
+    void bindScrollContainer(sol::state& lua);
+
+    class ScrollContainer;
+    class ScrollContainerProxy : public ScrollContainerNode {
+        public:
+            sunaba::ui::ScrollContainer* element = nullptr;
+
+            void onInit() {
+                // Initialize the NodeProxy instance
+            }
+
+            void _enter_tree() override;
+            void _exit_tree() override;
+            void _ready() override ;
+            void _process(double delta) override;
+            void _physics_process(double delta) override;
+            void _input(const Ref<InputEvent>& event) override;
+            void _unhandled_input(const Ref<InputEvent>& event) override;
+            void _unhandled_key_input(const Ref<InputEvent>& event) override;
+            void _shortcut_input(const Ref<InputEvent>& event) override;
+            void _draw() override;
+            bool _can_drop_data(const Vector2& at_position, const Variant &data) const override;
+            void _drop_data(const Vector2 &at_position, const Variant &data) override;
+            Variant _get_drag_data(const Vector2 &at_position) override;
+            Vector2 _get_minimum_size() const override;
+            String _get_tooltip(const Vector2 &at_position) const override;
+            void _gui_input(const Ref<InputEvent> &event) override;
+            bool _has_point(const Vector2 &point) const override;
+            //Object* _make_custom_tooltip(const String &for_text) const override;
+            TypedArray<Vector3i> _structured_text_parser(const Array &args, const String &text) const override;
+            
+            PackedInt32Array _get_allowed_size_flags_horizontal() const override;
+            PackedInt32Array _get_allowed_size_flags_vertical() const override;
+    };
+
+    class ScrollContainer : public sunaba::ui::Container {
+        private:
+            ScrollContainerNode* scrollContainer = nullptr; // Pointer to the ScrollContainer instance
+
+            void connectScrollContainerSignals() {
+                // Connect signals specific to ScrollContainer
+            }
+
+        public:
+            ScrollContainer() {
+                setScrollContainer(memnew(ScrollContainerProxy));
+                onInit();
+            }
+
+            ScrollContainer(ScrollContainerNode* p_node) {
+                setScrollContainer(p_node);
+            }
+
+            ScrollContainerNode* getScrollContainer() {
+                return scrollContainer;
+            }
+
+            void setScrollContainer(ScrollContainerNode* p_node) {
+                scrollContainer = p_node;
+                connectScrollContainerSignals();
+                setContainer(p_node);
+            }
+
+            void setScrollContainer(ScrollContainerProxy* p_node) {
+                scrollContainer = p_node;
+                p_node->element = this;
+                connectScrollContainerSignals();
+                setContainer(p_node);
+            }
+    };
+}
+
+#endif // SCROLL_CONTAINER_H
