5c7ec2370409d25688811c6c5520bc31db11f2da
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-13 12:53:12 -0800

    Fix missing newline at end of file in Slider binding

--- src/ui/slider.cpp
+++ src/ui/slider.cpp
@@ -134,7 +134,7 @@ namespace sunaba::ui {
             "dragEnded", sol::property(&Slider::getDragEndedEvent, &Slider::setDragEndedEvent),
             "dragStarted", sol::property(&Slider::getDragStartedEvent, &Slider::setDragStartedEvent),
             "cast", [] (Element* element) {
-            },
+            }
         );
     }
 }
