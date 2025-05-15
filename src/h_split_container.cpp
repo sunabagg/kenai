b537af83fd001fb6a648714f493b67e2868eaa79
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-15 12:57:24 -0800

    Add HSplitContainerProxy class with lifecycle methods

---
+++ src/h_split_container.cpp
@@ -0,0 +1,27 @@
+#include "h_split_container.h"
+
+namespace sunaba::ui {
+    void HSplitContainerProxy::_enter_tree() {
+        if (this->element != nullptr) {
+            this->element->enterTree();
+        }
+    }
+    
+    void HSplitContainerProxy::_exit_tree() {
+        if (this->element != nullptr) {
+            this->element->exitTree();
+        }
+    }
+    
+    void HSplitContainerProxy::_ready() {
+        if (this->element != nullptr) {
+            this->element->ready();
+        }
+    }
+    
+    void HSplitContainerProxy::_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->process(delta);
+        }
+    }
+}
