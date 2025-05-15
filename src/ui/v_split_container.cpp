a8c91a3fd404596a1c7802b4950c9b2165ec1779
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-15 15:07:17 -0800

    Add _get_allowed_size_flags_horizontal method to VSplitContainerProxy for size flag retrieval

--- src/ui/v_split_container.cpp
+++ src/ui/v_split_container.cpp
@@ -114,4 +114,11 @@ namespace sunaba::ui {
         }
         return TypedArray<Vector3i>();
     }
+
+    PackedInt32Array VSplitContainerProxy::_get_allowed_size_flags_horizontal() const {
+        if (element != nullptr) {
+            return element->_getAllowedSizeFlagsHorizontal();
+        }
+        return PackedInt32Array();
+    }
 }
