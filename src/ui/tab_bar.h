3180f9264d84b542fc45bc7eefa8efe245756230
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-17 22:14:21 -0800

    Add setDragToRearrangeEnabled method to TabBar class for managing drag to rearrange state

--- src/ui/tab_bar.h
+++ src/ui/tab_bar.h
@@ -119,6 +119,10 @@ namespace sunaba::ui {
             bool getDragToRearrangeEnabled() {
                 return container->get_drag_to_rearrange_enabled();
             }
+
+            void setDragToRearrangeEnabled(bool enabled) {
+                container->set_drag_to_rearrange_enabled(enabled);
+            }
     };
 }
 
