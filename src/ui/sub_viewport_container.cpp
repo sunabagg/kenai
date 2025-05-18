ea5da69f40ba99d9cd71a8b0bd5d358949f8d349
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-17 19:19:25 -0800

    Add _get_allowed_size_flags_vertical method to SubViewportContainerProxy for vertical size flag retrieval

--- src/ui/sub_viewport_container.cpp
+++ src/ui/sub_viewport_container.cpp
@@ -121,4 +121,11 @@ namespace sunaba::ui {
         }
         return PackedInt32Array();
     }
+
+    PackedInt32Array SubViewportContainerProxy::_get_allowed_size_flags_vertical() const {
+        if (this->element != nullptr) {
+            return this->element->getAllowedSizeFlagsVertical();
+        }
+        return PackedInt32Array();
+    }
 }
