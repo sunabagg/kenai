c6be384756ffdb5f7170bb52bd39ca2816fc752d
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-18 13:47:54 -0800

    Add getDeselectEnabled method to TabContainer for retrieving deselect state

--- src/ui/tab_container.h
+++ src/ui/tab_container.h
@@ -120,6 +120,10 @@ namespace sunaba::ui {
             void setCurrentTab(int tab) {
                 container->set_current_tab(tab);
             }
+
+            bool getDeselectEnabled() {
+                return container->get_deselect_enabled();
+            }
     };
 }
 
