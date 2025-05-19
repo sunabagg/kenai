7cb780940f90e48d8bcc14601bb83e51c948d2cf
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-18 19:17:52 -0800

    Add setTabButtonIcon method to TabContainer for setting an icon on a specific tab button

--- src/ui/tab_container.h
+++ src/ui/tab_container.h
@@ -317,6 +317,18 @@ namespace sunaba::ui {
             void setPopup(Element* popup) {
                 container->set_popup(popup->getNode());
             }
+
+            void setTabButtonIcon(int tab, sunaba::core::Texture2D* icon) {
+                godot::Texture2D* iconTexture = 
+                    icon != nullptr 
+                        ? icon->getTexture2D() 
+                        : nullptr;
+                Ref<godot::Texture2D> iconRef = 
+                    iconTexture != nullptr 
+                        ? Ref<godot::Texture2D>(iconTexture) 
+                        : Ref<godot::Texture2D>();
+                container->set_tab_button_icon(tab, iconRef);
+            }
     };
 }
 
