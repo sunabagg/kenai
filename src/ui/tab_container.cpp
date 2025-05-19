5597e4fc60ad2b97079e17114146fd09741658a8
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-18 23:25:44 -0800

    Add _physics_process method implementation to TabContainerProxy class

--- src/ui/tab_container.cpp
+++ src/ui/tab_container.cpp
@@ -24,4 +24,10 @@ namespace sunaba::ui {
             this->element->process(delta);
         }
     }
+
+    void TabContainerProxy::_physics_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->physicsProcess(delta);
+        }
+    }
 }
