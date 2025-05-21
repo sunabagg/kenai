6f01bfabce0647ee91cb4a072ea115f82a0f5ba9
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-20 16:18:28 -0800

    Add getSelectionLine method to TextEdit Lua bindings

--- src/ui/text_edit.cpp
+++ src/ui/text_edit.cpp
@@ -439,6 +439,7 @@ namespace sunaba::ui {
             "getSelectionColumn", &TextEdit::getSelectionColumn,
             "getSelectionFromColumn", &TextEdit::getSelectionFromColumn,
             "getSelectionFromLine", &TextEdit::getSelectionFromLine,
+            "getSelectionLine", &TextEdit::getSelectionLine,
         );
     }
 }
