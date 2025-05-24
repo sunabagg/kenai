2a785fe79ff33c308a51fb3b59614a7239f6d692
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-24 11:55:17 -0800

    Add _get_minimum_size method to TextureButtonProxy for minimum size retrieval

--- src/ui/texture_button.cpp
+++ src/ui/texture_button.cpp
@@ -82,4 +82,11 @@ namespace sunaba::ui {
         return Variant();
     }
 
+    Vector2 TextureButtonProxy::_get_minimum_size() const {
+        if (this->element != nullptr) {
+            return this->element->_getMinimumSize();
+        }
+        return Vector2();
+    }
+
 } // namespace sunaba::ui
