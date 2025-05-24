ee21f963be58d62ba7a016dcedb5d088a8541e2f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-24 10:45:17 -0800

    Add implementation of CodeEditProxy::_unhandled_key_input method

--- src/ui/code_edit.cpp
+++ src/ui/code_edit.cpp
@@ -42,6 +42,7 @@ namespace sunaba::ui {
             this->element->unhandledInput(event);
         }
     }
+    
     void CodeEditProxy::_unhandled_key_input(const Ref<InputEvent>& event) {
         if (this->element != nullptr) {
             this->element->unhandledKeyInput(event);
