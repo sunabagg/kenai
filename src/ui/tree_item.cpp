ec232826f1b4f2e29d7070955ae736c1082b10d1
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-25 11:20:03 -0800

    Add TreeItem class definition and header file

---
+++ src/ui/tree_item.cpp
@@ -0,0 +1,17 @@
+#ifndef TREE_ITEM_H
+#define TREE_ITEM_H
+
+#include <godot_cpp/classes/tree_item.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotTreeItem godot::TreeItem
+
+#include "../core/base_object.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+
+
+#endif // TREE_ITEM_H
