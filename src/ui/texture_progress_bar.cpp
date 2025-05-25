27a54f27c0cece7ad3bc9c5c00af768a5702328f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-24 17:12:24 -0800

    Add getStretchMargin method to TextureProgressBar Lua binding

--- src/ui/texture_progress_bar.cpp
+++ src/ui/texture_progress_bar.cpp
@@ -144,6 +144,7 @@ namespace sunaba::ui {
             "tintOver", sol::property(&TextureProgressBar::getTintOver, &TextureProgressBar::setTintOver),
             "tintProgress", sol::property(&TextureProgressBar::getTintProgress, &TextureProgressBar::setTintProgress),
             "tintUnder", sol::property(&TextureProgressBar::getTintUnder, &TextureProgressBar::setTintUnder),
+            "getStretchMargin", &TextureProgressBar::getStretchMargin,
         );
     }
 
