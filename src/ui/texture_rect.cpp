6ae69a77e54edd66ba6112bf8d6c6d2d1b703bfa
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-24 18:11:04 -0800

    Add _get_tooltip method to TextureRectProxy for tooltip retrieval

--- src/ui/texture_rect.cpp
+++ src/ui/texture_rect.cpp
@@ -88,4 +88,11 @@ namespace sunaba::ui {
         }
         return Vector2();
     }
+
+    String TextureRectProxy::_get_tooltip(const Vector2 &at_position) const {
+        if (this->element != nullptr) {
+            return this->element->getTooltip(at_position);
+        }
+        return String();
+    }
 }
