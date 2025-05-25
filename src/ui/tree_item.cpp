10dbcec77e988b29975cc1b697d6f215cba53e8b
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-25 14:53:00 -0800

    Add binding for 'getSuffix' method in TreeItem Lua registration

--- src/ui/tree_item.cpp
+++ src/ui/tree_item.cpp
@@ -54,6 +54,7 @@ namespace sunaba::ui {
             "getRangeConfig", &TreeItem::getRangeConfig,
             "getStructuredTextBidiOverride", &TreeItem::getStructuredTextBidiOverride,
             "getStructuredTextBdidOverrideOptions", &TreeItem::getStructuredTextBidiOverrideOptions,
+            "getSuffix", &TreeItem::getSuffix,
         );
     }
 }
