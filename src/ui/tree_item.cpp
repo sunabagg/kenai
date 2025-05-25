1c2e8606f397f5b6fe45340906703c99eb2c9375
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-25 14:13:27 -0800

    Add bindTreeItem function to register TreeItem type with Lua

--- src/ui/tree_item.cpp
+++ src/ui/tree_item.cpp
@@ -1,5 +1,9 @@
 #include "tree_item.h"
 
 namespace sunaba::ui {
-    
+    void bindTreeItem(sol::state& lua) {
+        lua.new_usertype<TreeItem>("TreeItem",
+            sol::constructors<TreeItem()>(),
+        );
+    }
 }
