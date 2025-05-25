96a65693a057822f141f01f0642d8257c3438aa2
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-24 16:47:09 -0800

    Fix tooltip return type in _get_tooltip method of TextureProgressBarProxy

--- src/ui/texture_progress_bar.cpp
+++ src/ui/texture_progress_bar.cpp
@@ -91,7 +91,7 @@ namespace sunaba::ui {
 
     String TextureProgressBarProxy::_get_tooltip(const Vector2 &at_position) const {
         if (this->element != nullptr) {
-            return this->element->_getTooltip(at_position);
+            return this->element->_getTooltip(at_position).c_str();
         }
         return String();
     }
