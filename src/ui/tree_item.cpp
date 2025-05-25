b926e3ff871a763d10fd18ca8594e2c24173da62
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-25 14:15:01 -0800

    Fix bindTreeItem function to correctly register base classes for TreeItem

--- src/ui/tree_item.cpp
+++ src/ui/tree_item.cpp
@@ -4,6 +4,7 @@ namespace sunaba::ui {
     void bindTreeItem(sol::state& lua) {
         lua.new_usertype<TreeItem>("TreeItem",
             sol::constructors<TreeItem()>(),
+            sol::base_classes, sol::bases<BaseObject>()
         );
     }
 }
