f0a3df02df3e4b4ce3aa1532c0a403570bf8d92f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-24 18:08:16 -0800

    Add TextureRectProxy class with lifecycle and input handling methods

---
+++ src/ui/texture_rect.cpp
@@ -0,0 +1,65 @@
+#include "texture_rect.h"
+
+namespace sunaba::ui {
+
+    void TextureRectProxy::_enter_tree() {
+        if (this->element != nullptr) {
+            this->element->enterTree();
+        }
+    }
+
+    void TextureRectProxy::_exit_tree() {
+        if (this->element != nullptr) {
+            this->element->exitTree();
+        }
+    }
+
+    void TextureRectProxy::_ready() {
+        if (this->element != nullptr) {
+            this->element->ready();
+        }
+    }
+
+    void TextureRectProxy::_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->process(delta);
+        }
+    }
+
+    void TextureRectProxy::_physics_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->physicsProcess(delta);
+        }
+    }
+
+    void TextureRectProxy::_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->input(event);
+        }
+    }
+
+    void TextureRectProxy::_unhandled_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->unhandledInput(event);
+        }
+    }
+
+    void TextureRectProxy::_unhandled_key_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->unhandledKeyInput(event);
+        }
+    }
+
+    void TextureRectProxy::_shortcut_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->shortcutInput(event);
+        }
+    }
+
+    void TextureRectProxy::_draw() {
+        if (this->element != nullptr) {
+            this->element->_draw();
+        }
+    }
+    
+}
