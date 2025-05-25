f269f1acf8efe230c3a6f06f4a38ea0aa5042fbf
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-24 16:45:21 -0800

    Implement TextureProgressBarProxy methods for handling tree and input events

---
+++ src/ui/texture_progress_bar.cpp
@@ -0,0 +1,65 @@
+#include "texture_progress_bar.h"
+
+namespace sunaba::ui {
+
+    void TextureProgressBarProxy::_enter_tree() {
+        if (this->element != nullptr) {
+            this->element->enterTree();
+        }
+    }
+
+    void TextureProgressBarProxy::_exit_tree() {
+        if (this->element != nullptr) {
+            this->element->exitTree();
+        }
+    }
+
+    void TextureProgressBarProxy::_ready() {
+        if (this->element != nullptr) {
+            this->element->ready();
+        }
+    }
+
+    void TextureProgressBarProxy::_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->process(delta);
+        }
+    }
+
+    void TextureProgressBarProxy::_physics_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->physicsProcess(delta);
+        }
+    }
+
+    void TextureProgressBarProxy::_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->input(event);
+        }
+    }
+
+    void TextureProgressBarProxy::_unhandled_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->unhandledInput(event);
+        }
+    }
+
+    void TextureProgressBarProxy::_unhandled_key_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->unhandledKeyInput(event);
+        }
+    }
+
+    void TextureProgressBarProxy::_shortcut_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->shortcutInput(event);
+        }
+    }
+
+    void TextureProgressBarProxy::_draw() {
+        if (this->element != nullptr) {
+            this->element->_draw();
+        }
+    }
+
+} // namespace sunaba::ui
