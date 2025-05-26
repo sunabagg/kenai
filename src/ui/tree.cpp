c1740125c63501247016be8be196f2a923deb02e
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-25 20:41:29 -0800

    Add _drop_data method to TreeProxy class for handling data drop functionality

--- src/ui/tree.cpp
+++ src/ui/tree.cpp
@@ -67,4 +67,10 @@ namespace sunaba::ui {
         }
         return false;
     }
+
+    void TreeProxy::_drop_data(const Vector2 &at_position, const Variant &data) {
+        if (this->element != nullptr) {
+            this->element->dropData(at_position, data);
+        }
+    }
 }
