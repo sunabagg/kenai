081ea3ebdf54f25e09ab2dec10f2205f11ecda2f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-26 11:19:25 -0800

    Add keywordColors property to CodeHighlighter Lua binding

--- src/ui/code_highlighter.cpp
+++ src/ui/code_highlighter.cpp
@@ -7,6 +7,7 @@ namespace sunaba::ui {
             sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, SyntaxHighlighter>(),
             "colorRegions", sol::property(&CodeHighlighter::getColorRegions, &CodeHighlighter::setColorRegions),
             "functionColor", sol::property(&CodeHighlighter::getFunctionColor, &CodeHighlighter::setFunctionColor),
+            "keywordColors", sol::property(&CodeHighlighter::getKeywordColors, &CodeHighlighter::setKeywordColors),
         );
     }
 }
