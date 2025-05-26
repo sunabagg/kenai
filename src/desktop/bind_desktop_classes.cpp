94675ae2e3ddc3e3a8193462154127da2589040a
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-26 12:02:21 -0800

    Add binding for PopupPanel class in bindDesktopClasses function

--- src/desktop/bind_desktop_classes.cpp
+++ src/desktop/bind_desktop_classes.cpp
@@ -1,10 +1,12 @@
 #include "bind_desktop_classes.h"
 #include "window.h"
 #include "popup.h"
+#include "popup_panel.h"
 
 namespace sunaba::desktop {
     void bindDesktopClasses(sol::state& lua) {
         bindWindow(lua);
         bindPopup(lua);
+        bindPopupPanel(lua);
     }
 }
