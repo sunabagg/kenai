c1f2c2d146b808dd4c5412672d402fc0d26c7db3
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-25 20:42:20 -0800

    Add _gui_input method to TreeProxy class for handling GUI input events

--- src/ui/tree.cpp
+++ src/ui/tree.cpp
@@ -94,4 +94,10 @@ namespace sunaba::ui {
         }
         return String();
     }
+
+    void TreeProxy::_gui_input(const Ref<InputEvent> &event) {
+        if (this->element != nullptr) {
+            this->element->guiInput(event);
+        }
+    }
 }
