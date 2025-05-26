b461a169b1c25a152c6099e7af4dd9051f56ab05
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-26 10:28:38 -0800

    Add v_flow_container.cpp to implement VFlowContainerProxy methods

---
+++ src/ui/v_flow_container.cpp
@@ -0,0 +1,65 @@
+#include "v_flow_container.h"
+
+namespace sunaba::ui {
+
+    void VFlowContainerProxy::_enter_tree() {
+        if (this->element != nullptr) {
+            this->element->enterTree();
+        }
+    }
+
+    void VFlowContainerProxy::_exit_tree() {
+        if (this->element != nullptr) {
+            this->element->exitTree();
+        }
+    }
+
+    void VFlowContainerProxy::_ready() {
+        if (this->element != nullptr) {
+            this->element->ready();
+        }
+    }
+
+    void VFlowContainerProxy::_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->process(delta);
+        }
+    }
+
+    void VFlowContainerProxy::_physics_process(double delta) {
+        if (this->element != nullptr) {
+            this->element->physicsProcess(delta);
+        }
+    }
+
+    void VFlowContainerProxy::_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->input(event);
+        }
+    }
+
+    void VFlowContainerProxy::_unhandled_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->unhandledInput(event);
+        }
+    }
+
+    void VFlowContainerProxy::_unhandled_key_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->unhandledKeyInput(event);
+        }
+    }
+
+    void VFlowContainerProxy::_shortcut_input(const Ref<InputEvent>& event) {
+        if (this->element != nullptr) {
+            this->element->shortcutInput(event);
+        }
+    }
+
+    void VFlowContainerProxy::_draw() {
+        if (this->element != nullptr) {
+            this->element->_draw();
+        }
+    }
+
+} // namespace sunaba::ui
