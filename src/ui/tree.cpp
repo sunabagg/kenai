90a6e48624ddf7288ea1b45e93170d9295f2c754
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-25 20:40:07 -0800

    Add _exit_tree method to TreeProxy class for exiting the tree structure

--- src/ui/tree.cpp
+++ src/ui/tree.cpp
@@ -6,4 +6,10 @@ namespace sunaba::ui {
             this->element->enterTree();
         }
     }
+
+    void TreeProxy::_exit_tree() {
+        if (this->element != nullptr) {
+            this->element->exitTree();
+        }
+    }
 }
