d2dad2ec919e5f44e653baa04684feeb07dc5f5c
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-28 13:32:39 -0800

    Add tabSelected method to TabBarSignalWrapper for tab selection events

--- src/ui/tab_bar.cpp
+++ src/ui/tab_bar.cpp
@@ -189,6 +189,14 @@ namespace sunaba::ui {
         }
     }
 
+    void TabBarSignalWrapper::tabSelected(int tab_idx) {
+        if (element) {
+            Array args;
+            args.push_back(tab_idx);
+            element->tabSelectedEvent->emit(args);
+        }
+    }
+
     void bindTabBar(sol::state& lua) {
         lua.new_usertype<TabBar>("TabBar",
             sol::constructors<TabBar()>(),
