6964a3235cf90180da5f5558336f62f2ca033fbd
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-28 13:31:55 -0800

    Add tabChanged method to TabBarSignalWrapper for tab change events

--- src/ui/tab_bar.cpp
+++ src/ui/tab_bar.cpp
@@ -149,6 +149,14 @@ namespace sunaba::ui {
         }
     }
 
+    void TabBarSignalWrapper::tabChanged(int tab_idx) {
+        if (element) {
+            Array args;
+            args.push_back(tab_idx);
+            element->tabChangedEvent->emit(args);
+        }
+    }
+
     void bindTabBar(sol::state& lua) {
         lua.new_usertype<TabBar>("TabBar",
             sol::constructors<TabBar()>(),
