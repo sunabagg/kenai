67f3e01aaa4debb8bdea14ed01daead834a5422e
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-28 14:26:25 -0800

    Add item_edited method binding to TreeSignalWrapper for handling item edit events

--- src/ui/tree.cpp
+++ src/ui/tree.cpp
@@ -216,6 +216,13 @@ namespace sunaba::ui {
         }
     }
 
+    void TreeSignalWrapper::item_edited() {
+        if (this->element != nullptr) {
+            Array args;
+            this->element->getItemEditedEvent()->emit(args);
+        }
+    }
+
     void bindTree(sol::state &lua) {
         lua.new_usertype<Tree>("Tree",
             sol::constructors<Tree()>(),
