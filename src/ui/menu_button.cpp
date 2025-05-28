eab11be407d9a65839531b8f7c381e13a8c2e0a0
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-28 13:06:23 -0800

    Bind about_to_popup method in MenuButtonSignalWrapper for signal handling

--- src/ui/menu_button.cpp
+++ src/ui/menu_button.cpp
@@ -127,6 +127,10 @@ namespace sunaba::ui {
         }
     }
 
+    void MenuButtonSignalWrapper::_bind_methods() {
+        ClassDB::bind_method(D_METHOD("about_to_popup"), &MenuButtonSignalWrapper::about_to_popup);
+    }
+
     void MenuButtonSignalWrapper::about_to_popup() {
         if (element != nullptr && element->aboutToPopupEvent != nullptr) {
             Array args;
