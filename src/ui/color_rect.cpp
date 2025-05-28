44a2c7758a7306a240fe29334cd84141a87a4ad3
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-28 15:19:44 -0800

    Add _gui_input method to ColorRectProxy for handling GUI input events

--- src/ui/color_rect.cpp
+++ src/ui/color_rect.cpp
@@ -94,4 +94,10 @@ namespace sunaba::ui {
         }
         return String();
     }
+
+    void ColorRectProxy::_gui_input(const Ref<InputEvent> &event) {
+        if (element != nullptr) {
+            element->guiInput(event);
+        }
+    }
 }
