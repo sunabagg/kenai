13deee675a42ade83fe94a63fe0ef1a9c02f40a0
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-28 14:16:51 -0800

    Add column_title_clicked method binding to TreeSignalWrapper

--- src/ui/tree.cpp
+++ src/ui/tree.cpp
@@ -126,6 +126,7 @@ namespace sunaba::ui {
         ClassDB::bind_method(D_METHOD("button_clicked", "item", "column", "id", "mouse_button_index"), &TreeSignalWrapper::button_clicked);
         ClassDB::bind_method(D_METHOD("cell_selected"), &TreeSignalWrapper::cell_selected);
         ClassDB::bind_method(D_METHOD("check_propagated_to_item", "item", "column"), &TreeSignalWrapper::check_propagated_to_item);
+        ClassDB::bind_method(D_METHOD("column_title_clicked", "column", "mouse_button_index"), &TreeSignalWrapper::column_title_clicked);
     }
 
     void bindTree(sol::state &lua) {
