49405e4016b3b083637a873eddf8d7815e325716
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-29 09:42:43 -0800

    Add _has_point method to RichTextLabelProxy for point detection

--- src/ui/rich_text_label.cpp
+++ src/ui/rich_text_label.cpp
@@ -100,4 +100,11 @@ namespace sunaba::ui {
             element->guiInput(event);
         }
     }
+
+    bool RichTextLabelProxy::_has_point(const Vector2 &point) const {
+        if (element != nullptr) {
+            return element->hasPoint(point);
+        }
+        return false;
+    }
 } // namespace sunaba::ui
