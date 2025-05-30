d1ae597eeefab0767f32267e0ae74cba30f060d4
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-30 14:53:01 -0800

    feat(Popup): add toPopup function for type casting to Popup

--- sunaba/desktop/Popup.hx
+++ sunaba/desktop/Popup.hx
@@ -5,4 +5,6 @@ import sunaba.core.Event;
 @:native("Popup")
 extern class Popup extends Window {
     public var popupHide: Event;
+    @:native("cast")
+    public function toPopup(type: Dynamic): Popup;
 }
