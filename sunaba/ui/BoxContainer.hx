58b733eb895c40fcfc4e4ca275eea16e3d0be90c
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-30 11:34:04 -0800

    feat(BoxContainer): add toBoxContainer method for type conversion

--- sunaba/ui/BoxContainer.hx
+++ sunaba/ui/BoxContainer.hx
@@ -5,4 +5,6 @@ extern class BoxContainer extends Container {
     public var alignment: Int;
     public var vertical: Bool;
     public function addSpacer(begin: Bool): Void;
+    @:native("cast")
+    public function toBoxContainer(type: Dynamic): BoxContainer;
 }
