8a5eaa184ed25d1f42316c9cfa6f5f735d225877
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-30 11:40:09 -0800

    feat(ColorRect): add toColorRect method for type casting

--- sunaba/ui/ColorRect.hx
+++ sunaba/ui/ColorRect.hx
@@ -6,4 +6,6 @@ import sunaba.core.Element;
 @:native("ColorRect")
 extern class ColorRect extends Control {
     public var color: Color;
+    @:native("cast")
+    public function toColorRect(type: Dynamic): ColorRect;
 }
