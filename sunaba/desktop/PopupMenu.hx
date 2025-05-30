d8c47f3e99166e3b335b6f56aa77af2d5cd39104
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-30 15:30:55 -0800

    feat(PopupMenu): add getItemIconMaxWidth function to PopupMenu class

--- sunaba/desktop/PopupMenu.hx
+++ sunaba/desktop/PopupMenu.hx
@@ -39,4 +39,5 @@ extern class PopupMenu extends Popup {
     public function getFocusedItem(): Int;
     public function getItemAccelerator(index: Int): Int;
     public function getItemIcon(index: Int): Texture2D;
+    public function getItemIconMaxWidth(index: Int): Int;
 }
