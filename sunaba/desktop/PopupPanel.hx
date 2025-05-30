4287973d3e496fe33bce2e7b810391dcd2a82b77
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-30 14:58:09 -0800

    feat(PopupPanel): add toPopupPanel function for type casting to PopupPanel

--- sunaba/desktop/PopupPanel.hx
+++ sunaba/desktop/PopupPanel.hx
@@ -2,5 +2,6 @@ package sunaba.desktop;
 
 @:native("PopupPanel")
 extern class PopupPanel extends Popup {
-    
+    @:native("cast")
+    public function toPopupPanel(type: Dynamic): PopupPanel;
 }
