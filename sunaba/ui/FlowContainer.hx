80f25191c5324c194dbb14c162d270b49a74add9
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-30 11:42:58 -0800

    feat(FlowContainer): add vertical property to FlowContainer extern class

--- sunaba/ui/FlowContainer.hx
+++ sunaba/ui/FlowContainer.hx
@@ -5,4 +5,5 @@ extern class FlowContainer extends Container {
     public var alignment: Int;
     public var lastWrapAlignment: Int;
     public var reverseFill: Bool;
+    public var vertical: Bool;
 }
