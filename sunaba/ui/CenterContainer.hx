7ebf7a34c1ab316814b79d3cecda47ce6f88ae62
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-30 11:37:39 -0800

    feat(CenterContainer): add CenterContainerAbstract for element conversion

--- sunaba/ui/CenterContainer.hx
+++ sunaba/ui/CenterContainer.hx
@@ -8,3 +8,14 @@ extern class CenterContainer extends Container {
     @:native("cast")
     public function toCenterContainer(type: Dynamic): CenterContainer;
 }
+
+abstract CenterContainerAbstract(CenterContainer) from CenterContainer to CenterContainer {
+    @:from
+    public static function fromElement(element: sunaba.core.Element): CenterContainerAbstract {
+        var container = CenterContainer.toCenterContainer(element);
+        if (container.isNull()) {
+            return null;
+        }
+        return container;
+    }
+}
