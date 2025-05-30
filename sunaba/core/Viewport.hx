64a72ec0ceeabf92f8410716421442952ce062da
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-30 11:56:52 -0800

    feat(Viewport): add physicsObjectPicking property to Viewport extern class

--- sunaba/core/Viewport.hx
+++ sunaba/core/Viewport.hx
@@ -22,4 +22,5 @@ extern class Viewport extends Element {
     public var msaa3D: Int;
     public var ownWorld3D: Bool;
     public var physicsInterpolationMode: Int;
+    public var physicsObjectPicking: Bool;
 }
