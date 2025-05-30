d67d4e541426bbb3a878a2b7a82680e8e45588c6
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-30 11:20:03 -0800

    feat(Container): add Event import and declare mouseFilter and preSortChildren variables

--- sunaba/ui/Container.hx
+++ sunaba/ui/Container.hx
@@ -1,6 +1,9 @@
 package sunaba.ui;
 
+import sunaba.core.Event;
+
 @:native("Container")
 public class Container extends Control {
-
+    public var mouseFilter: Int;
+    public var preSortChildren: Event;
 }
