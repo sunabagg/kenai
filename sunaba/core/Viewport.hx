ad4089867edaa2ad1245697beacfc65355c65776
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-30 12:03:50 -0800

    feat(Viewport): add sdfScale property to Viewport extern class

--- sunaba/core/Viewport.hx
+++ sunaba/core/Viewport.hx
@@ -35,4 +35,5 @@ extern class Viewport extends Element {
     public var scaling3DScale: Float;
     public var screenSpaceAA: Int;
     public var sdfOversize: Int;
+    public var sdfScale: Float;
 }
