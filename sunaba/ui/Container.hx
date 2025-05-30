588f97a63d8eea5c6607e28809c8991ffa09ca3d
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-30 11:21:35 -0800

    feat(Container): add fitChildInRect method to Container class

--- sunaba/ui/Container.hx
+++ sunaba/ui/Container.hx
@@ -7,4 +7,5 @@ public class Container extends Control {
     public var mouseFilter: Int;
     public var preSortChildren: Event;
     public var sortChildren: Event;
+    public function fitChildInRect(child: Control, rect: sunaba.core.Rect2): Void;
 }
