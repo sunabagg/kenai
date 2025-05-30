2526f779dbc23c087c33966c5b110187057937e8
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-30 15:17:43 -0800

    feat(PopupMenu): add addIconItem function to PopupMenu class

--- sunaba/desktop/PopupMenu.hx
+++ sunaba/desktop/PopupMenu.hx
@@ -22,4 +22,5 @@ extern class PopupMenu extends Popup {
     public function addCheckShortcut(shortcut: Shortcut, ?id: Int, ?global: Bool): Void;
     public function addIconCheckItem(texture: Texture2D, label: String, ?id: Int, ?accel: Int): Void;
     public function addIconCheckShortcut(texture: Texture2D, shortcut: Shortcut, ?id: Int, ?global: Bool): Void;
+    public function addIconItem(texture: Texture2D, label: String, ?id: Int, ?accel: Int): Void;
 }
