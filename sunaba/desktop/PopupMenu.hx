5b6e0be564b154f834d5b3b2ebd2f9cf6bfa117e
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-30 15:21:25 -0800

    feat(PopupMenu): add addItem function to PopupMenu class

--- sunaba/desktop/PopupMenu.hx
+++ sunaba/desktop/PopupMenu.hx
@@ -27,4 +27,5 @@ extern class PopupMenu extends Popup {
     public function addIconRadioCheckItem(texture: Texture2D, label: String, ?id: Int, ?accel: Int): Void;
     public function addIconRadioCheckShortcut(texture: Texture2D, shortcut: Shortcut, ?id: Int, ?global: Bool): Void;
     public function addIconShortcut(texture: Texture2D, shortcut: Shortcut, ?id: Int, ?global: Bool, ?allowEcho: Bool): Void;
+    public function addItem(label: String, id: Int, ?accel: Int): Void;
 }
