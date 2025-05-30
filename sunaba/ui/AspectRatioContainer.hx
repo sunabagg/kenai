4b6c5c600d27d6ec026582ed2ce1e8d21dca8eb5
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-30 11:30:08 -0800

    feat(AspectRatioContainer): add alignmentVertical property to AspectRatioContainer class

--- sunaba/ui/AspectRatioContainer.hx
+++ sunaba/ui/AspectRatioContainer.hx
@@ -3,4 +3,5 @@ package sunaba.ui;
 @:native("AspectRatioContainer")
 extern class AspectRatioContainer extends Container {
     public var alignmentHorizontal: Int;
+    public var alignmentVertical: Int;
 }
