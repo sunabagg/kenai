eea1d7d4d323292bdd210d06f08ca928ba58771f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-30 14:58:19 -0800

    feat(PopupPanel): add PopupPanelAbstract for type conversion from Element to PopupPanel

--- sunaba/desktop/PopupPanel.hx
+++ sunaba/desktop/PopupPanel.hx
@@ -5,3 +5,14 @@ extern class PopupPanel extends Popup {
     @:native("cast")
     public function toPopupPanel(type: Dynamic): PopupPanel;
 }
+
+abstract PopupPanelAbstract(PopupPanel) from PopupPanel to PopupPanel {
+    @:from
+    public static function fromElement(element: sunaba.core.Element): PopupPanelAbstract {
+        var panel = PopupPanel.toPopupPanel(element);
+        if (panel.isNull()) {
+            return null;
+        }
+        return panel;
+    }
+}
