58eb230b1c95774c021488a23c961901f1bb4672
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-31 12:09:44 -0800

    feat(ItemList): add setItemLanguage function to set the language for an item

--- sunaba/ui/ItemList.hx
+++ sunaba/ui/ItemList.hx
@@ -68,4 +68,5 @@ extern class ItemList extends Control {
     public function setItemIcon(index: Int, texture: Texture2D): Void;
     public function setItemIconModulate(index: Int, color: Color): Void;
     public function setItemIconRegion(index: Int, region: Rect2): Void;
+    public function setItemLanguage(index: Int, language: String): Void;
 }
