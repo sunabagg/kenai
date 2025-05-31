b3b8dfb109c47ea0e877e510483290880a8a88dd
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-31 11:31:52 -0800

    feat(GridContainer): add toGridContainer function for type casting to GridContainer

--- sunaba/ui/GridContainer.hx
+++ sunaba/ui/GridContainer.hx
@@ -3,4 +3,6 @@ package sunaba.ui;
 @:native("GridContainer")
 extern class GridContainer extends Container {
     public var columns: Int;
+    @:native("cast")
+    public function toGridContainer(type: Dynamic): GridContainer;
 }
