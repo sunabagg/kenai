4cea32a1f94046e53e1431e5399f864d49f9de17
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-31 11:33:20 -0800

    feat(HBoxContainer): add HBoxContainerAbstract for element conversion

--- sunaba/ui/HBoxContainer.hx
+++ sunaba/ui/HBoxContainer.hx
@@ -5,3 +5,14 @@ extern class HBoxContainer extends BoxContainer {
     @:native("cast")
     public function toHBoxContainer(type: Dynamic): HBoxContainer;
 }
+
+abstract HBoxContainerAbstract(HBoxContainer) from HBoxContainer to HBoxContainer {
+    @:from
+    public static function fromElement(element: sunaba.core.Element): HBoxContainerAbstract {
+        var hbox = HBoxContainer.toHBoxContainer(element);
+        if (hbox.isNull()) {
+            return null;
+        }
+        return hbox;
+    }
+}
