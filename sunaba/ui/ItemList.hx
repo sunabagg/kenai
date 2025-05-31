85ebf053d30e573317d1e5e87e9f5c25552141b3
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-31 11:51:14 -0800

    feat(ItemList): add ensureCurrentIsVisible function to ItemList class

--- sunaba/ui/ItemList.hx
+++ sunaba/ui/ItemList.hx
@@ -34,4 +34,5 @@ extern class ItemList extends Control {
     public function clear(): Void;
     public function deselect(index: Int): Void;
     public function deselectAll(): Void;
+    public function ensureCurrentIsVisible(): Void;
 }
