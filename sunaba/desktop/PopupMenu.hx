4e0c9d9b0d274369c7a7e4fc5adae9063f103cb1
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-30 17:24:06 -0800

    feat(PopupMenu): add toggleItemChecked function to toggle the checked state of menu items

--- sunaba/desktop/PopupMenu.hx
+++ sunaba/desktop/PopupMenu.hx
@@ -87,4 +87,5 @@ extern class PopupMenu extends Popup {
     public function setItemText(index: Int, text: String): Void;
     public function setItemTextDirection(index: Int, direction: Int): Void;
     public function setItemTooltip(index: Int, tooltip: String): Void;
+    public function toggleItemChecked(index: Int): Void;
 }
