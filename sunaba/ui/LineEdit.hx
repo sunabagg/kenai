f4ba6ea5db77026913781b59260753bb99f76e1b
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-01 13:35:01 -0800

    feat(LineEdit): add public emojiMenuEnabled variable to LineEdit class

--- sunaba/ui/LineEdit.hx
+++ sunaba/ui/LineEdit.hx
@@ -14,4 +14,5 @@ extern class LineEdit extends Control {
     public var dragAndDropSelectionEnabled: Bool;
     public var drawControlChars: Bool;
     public var editable: Bool;
+    public var emojiMenuEnabled: Bool;
 }
