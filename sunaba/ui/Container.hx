d21d470729d290ceae3a7a7cda1f3b6229a9c7c2
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-01 12:53:47 -0800

    feat(Container): remove unused mouseFilter variable for cleaner code

--- sunaba/ui/Container.hx
+++ sunaba/ui/Container.hx
@@ -5,7 +5,6 @@ import sunaba.core.Element;
 
 @:native("Container")
 extern class Container extends Control {
-    public var mouseFilter: Int;
     public var preSortChildren: Event;
     public var sortChildren: Event;
     public function fitChildInRect(child: Control, rect: sunaba.core.Rect2): Void;
