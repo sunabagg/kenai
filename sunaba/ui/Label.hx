ed843f0eeb1e624502b2c56d7a6891d7ab83272b
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-01 12:15:04 -0800

    feat(Label): add visibleCharactersBehavior property to Label class

--- sunaba/ui/Label.hx
+++ sunaba/ui/Label.hx
@@ -26,4 +26,5 @@ extern class Label extends Control {
     public var uppercase: Bool;
     public var verticalAlignment: Int;
     public var visibleCharacters: Int;
+    public var visibleCharactersBehavior: Int;
 }
