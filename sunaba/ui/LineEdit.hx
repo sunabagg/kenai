b4acfde60676b7df138c0860ab16899619b1dd57
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-01 13:42:29 -0800

    feat(LineEdit): add public selectingEnabled variable to LineEdit class

--- sunaba/ui/LineEdit.hx
+++ sunaba/ui/LineEdit.hx
@@ -27,4 +27,5 @@ extern class LineEdit extends Control {
     public var secret: Bool;
     public var secretCharacter: String;
     public var selectAllOnFocus: Bool;
+    public var selectingEnabled: Bool;
 }
