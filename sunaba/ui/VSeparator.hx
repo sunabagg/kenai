55dddbd518d8040f2b3b4a4c512902e3053745f8
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 15:10:49 -0800

    feat(VSeparator): add toVSeparator static function for casting to VSeparator

--- sunaba/ui/VSeparator.hx
+++ sunaba/ui/VSeparator.hx
@@ -2,4 +2,6 @@ package sunaba.ui;
 
 @:native("VSeparator")
 extern class VSeparator extends Control {
+    @:native("cast")
+    public static function toVSeparator(obj: Dynamic): sunaba.ui.VSeparator;
 }
