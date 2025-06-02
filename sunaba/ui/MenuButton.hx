bfebdc95279d9868926fc6f19a338d96e19f395b
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 12:00:24 -0800

    fix(MenuButton): remove toggleMode property from MenuButton class

--- sunaba/ui/MenuButton.hx
+++ sunaba/ui/MenuButton.hx
@@ -7,7 +7,6 @@ extern class MenuButton extends Button {
     public var flat: Bool;
     public var itemCount: Int;
     public var switchOnHover: Bool;
-    public var toggleMode: Bool;
     public var aboutToPopup: sunaba.core.Event;
     public function getPopup(): PopupMenu;
     public function setDisableShortcuts(disable: Bool): Void;
