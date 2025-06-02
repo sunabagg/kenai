e159269b60536341a142b47cf60c115417282cbc
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 10:54:35 -0800

    fix(LinkButton): format variable declarations in LinkButton class

--- sunaba/ui/LinkButton.hx
+++ sunaba/ui/LinkButton.hx
@@ -2,9 +2,9 @@ package sunaba.ui;
 
 @:native("LinkButton")
 extern class LinkButton extends BaseButton {
-    public var language:String;
-    public var mouseDefaultCursorShape:Int;
-    public var structuredTextBidiOverride:Int;
-    public var structuredTextBidiOverrideOptions:sunaba.core.ArrayList;
-    public var text:String;
+    public var language: String;
+    public var mouseDefaultCursorShape: Int;
+    public var structuredTextBidiOverride: Int;
+    public var structuredTextBidiOverrideOptions: sunaba.core.ArrayList;
+    public var text: String;
 }
