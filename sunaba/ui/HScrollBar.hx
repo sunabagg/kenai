fe806367069a47fd15f25a2a19e2c0eb7e5c6f16
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 14:51:31 -0800

    feat(HScrollBar): add static method toHScrollBar for type casting

--- sunaba/ui/HScrollBar.hx
+++ sunaba/ui/HScrollBar.hx
@@ -2,5 +2,6 @@ package sunaba.ui;
 
 @:native("HScrollBar")
 extern class HScrollBar extends ScrollBar {
-    
+    @:native("cast")
+    public static function toHScrollBar(obj: Dynamic): sunaba.ui.HScrollBar;
 }
