d01807ad1b29cd1cef76b6bcb9b58eb96dfc8beb
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 15:10:16 -0800

    feat(HSeparator): add HSeparatorAbstract for element conversion to HSeparator

--- sunaba/ui/HSeparator.hx
+++ sunaba/ui/HSeparator.hx
@@ -5,3 +5,14 @@ extern class HSeparator extends Separator {
     @:native("cast")
     public static function toHSeparator(obj: Dynamic): sunaba.ui.HSeparator;
 }
+
+abstract HSeparatorAbstract(HSeparator) from HSeparator to HSeparator {
+    @:from
+    public static function fromElement(element: sunaba.core.Element): sunaba.ui.HSeparator {
+        var hSeparator = sunaba.ui.HSeparator.toHSeparator(element);
+        if (hSeparator.isNull()) {
+            return null;
+        }
+        return hSeparator;
+    }
+}
