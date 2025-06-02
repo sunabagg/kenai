665aa43aa7813535891eb100155a8ba58c4f5b44
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 11:44:31 -0800

    fix(MenuBar): add setMenuTooltip method to MenuBar extern class

--- sunaba/ui/MenuBar.hx
+++ sunaba/ui/MenuBar.hx
@@ -21,4 +21,5 @@ extern class MenuBar extends Control {
     public function setMenuDisabled(index: Int, disabled: Bool): Void;
     public function setMenuHidden(index: Int, hidden: Bool): Void;
     public function setMenuTitle(index: Int, title: String): Void;
+    public function setMenuTooltip(index: Int, tooltip: String): Void;
 }
