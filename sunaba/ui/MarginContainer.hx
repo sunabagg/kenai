829505b622c320a6b721a77ca919969a103cb198
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 11:36:19 -0800

    feat(MarginContainer): add MarginContainerAbstract for element conversion

--- sunaba/ui/MarginContainer.hx
+++ sunaba/ui/MarginContainer.hx
@@ -5,3 +5,14 @@ extern class MarginContainer extends Container {
     @:native("cast")
     public static function toMarginContainer(obj: Dynamic): sunaba.ui.MarginContainer;
 }
+
+abstract MarginContainerAbstract(MarginContainer) from MarginContainer to MarginContainer {
+    @:from
+    public static function fromElement(element: sunaba.core.Element): sunaba.ui.MarginContainer {
+        var marginContainer = sunaba.ui.MarginContainer.toMarginContainer(element);
+        if (marginContainer.isNull()) {
+            return null;
+        }
+        return marginContainer;
+    }
+}
