d88cc29e6ed5afeb893209ce66cd85449fcd815a
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 15:08:53 -0800

    feat(Separator): add SeparatorAbstract for element conversion to Separator

--- sunaba/ui/Separator.hx
+++ sunaba/ui/Separator.hx
@@ -5,3 +5,14 @@ extern class Separator extends Control {
     @:native("cast")
     public static function toSeparator(obj: Dynamic): sunaba.ui.Separator;
 }
+
+abstract SeparatorAbstract(Separator) from Separator to Separator {
+    @:from
+    public static function fromElement(element: sunaba.core.Element): sunaba.ui.Separator {
+        var separator = sunaba.ui.Separator.toSeparator(element);
+        if (separator.isNull()) {
+            return null;
+        }
+        return separator;
+    }
+}
