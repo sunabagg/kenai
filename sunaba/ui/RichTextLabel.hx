0362ce83d6dbf146535e35fa93930efebc8e1fc4
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 13:35:05 -0800

    fix(RichTextLabel): add getCharacterLine method for character line retrieval

--- sunaba/ui/RichTextLabel.hx
+++ sunaba/ui/RichTextLabel.hx
@@ -45,4 +45,5 @@ extern class RichTextLabel extends Control {
     public function appendText(bbcode: String): Void;
     public function clear(): Void;
     public function deselect(): Void;
+    public function getCharacterLine(character: Int): Int;
 }
