e8af23850cd3ef93f095817433e830650dcd9f3c
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 15:19:15 -0800

    feat(VSlider): add VSliderAbstract for converting from Element to VSlider

--- sunaba/ui/VSlider.hx
+++ sunaba/ui/VSlider.hx
@@ -5,3 +5,14 @@ extern class VSlider extends Slider {
     @:native("cast")
     public static function toVSlider(obj: Dynamic): sunaba.ui.VSlider;
 }
+
+abstract VSliderAbstract(VSlider) from VSlider to VSlider {
+    @:from
+    public static function fromElement(element: sunaba.core.Element): sunaba.ui.VSlider {
+        var vSlider = sunaba.ui.VSlider.toVSlider(element);
+        if (vSlider.isNull()) {
+            return null;
+        }
+        return vSlider;
+    }
+}
