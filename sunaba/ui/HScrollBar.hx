bbaa84e7f5bc0868d3fb0add631c6149ea47088c
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 14:51:42 -0800

    feat(HScrollBar): add HScrollBarAbstract for element conversion

--- sunaba/ui/HScrollBar.hx
+++ sunaba/ui/HScrollBar.hx
@@ -5,3 +5,14 @@ extern class HScrollBar extends ScrollBar {
     @:native("cast")
     public static function toHScrollBar(obj: Dynamic): sunaba.ui.HScrollBar;
 }
+
+abstract HScrollBarAbstract(HScrollBar) from HScrollBar to HScrollBar {
+    @:from
+    public static function fromElement(element: sunaba.core.Element): sunaba.ui.HScrollBar {
+        var hScrollBar = sunaba.ui.HScrollBar.toHScrollBar(element);
+        if (hScrollBar.isNull()) {
+            return null;
+        }
+        return hScrollBar;
+    }
+}
