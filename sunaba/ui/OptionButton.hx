f7ac53fdf249cf58637a9793d8c16b181027e4f8
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 12:18:24 -0800

    feat(OptionButton): add addItem method to OptionButton class

--- sunaba/ui/OptionButton.hx
+++ sunaba/ui/OptionButton.hx
@@ -12,4 +12,5 @@ extern class OptionButton extends Button {
     public var itemFocused: sunaba.core.Event;
     public var itemSelected: sunaba.core.Event;
     public function addIconItem(texture: Texture2D, label: String, ?id: Int): Void;
+    public function addItem(label: String, ?id: Int): Void;
 }
