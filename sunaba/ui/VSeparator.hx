e3e5b19d936ac092def8380fbb069f0a81c04587
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 15:10:54 -0800

    feat(VSeparator): add VSeparatorAbstract for element conversion to VSeparator

--- sunaba/ui/VSeparator.hx
+++ sunaba/ui/VSeparator.hx
@@ -5,3 +5,14 @@ extern class VSeparator extends Control {
     @:native("cast")
     public static function toVSeparator(obj: Dynamic): sunaba.ui.VSeparator;
 }
+
+abstract VSeparatorAbstract(VSeparator) from VSeparator to VSeparator {
+    @:from
+    public static function fromElement(element: sunaba.core.Element): sunaba.ui.VSeparator {
+        var vSeparator = sunaba.ui.VSeparator.toVSeparator(element);
+        if (vSeparator.isNull()) {
+            return null;
+        }
+        return vSeparator;
+    }
+}
