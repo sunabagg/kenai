8d698a32f2fd1a7eb3de75a7007ebe984117ad2f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 11:41:46 -0800

    fix(MenuBar): add getMenuTooltip method to MenuBar extern class

--- sunaba/ui/MenuBar.hx
+++ sunaba/ui/MenuBar.hx
@@ -13,4 +13,5 @@ extern class MenuBar extends Control {
     public function getMenuCount(): Int;
     public function getMenuPopup(index: Int): PopupMenu;
     public function getMenuTitle(index: Int): String;
+    public function getMenuTooltip(index: Int): String;
 }
