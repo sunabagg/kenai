beab747b5c53ba40cf13faa2faa350a0dbb9949d
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 14:52:30 -0800

    feat(VScrollBar): add VScrollBarAbstract for element conversion

--- sunaba/ui/VScrollBar.hx
+++ sunaba/ui/VScrollBar.hx
@@ -5,3 +5,14 @@ extern class VScrollBar extends ScrollBar {
     @:native("cast")
     public static function toVScrollBar(obj: Dynamic): sunaba.ui.VScrollBar;
 }
+
+abstract VScrollBarAbstract(VScrollBar) from VScrollBar to VScrollBar {
+    @:from
+    public static function fromElement(element: sunaba.core.Element): sunaba.ui.VScrollBar {
+        var vScrollBar = sunaba.ui.VScrollBar.toVScrollBar(element);
+        if (vScrollBar.isNull()) {
+            return null;
+        }
+        return vScrollBar;
+    }
+}
