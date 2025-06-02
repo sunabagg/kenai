98726a9c787377cca05bd9437f2b820e291ce940
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 12:36:44 -0800

    feat(PanelContainer): add PanelContainerAbstract for element conversion

--- sunaba/ui/PanelContainer.hx
+++ sunaba/ui/PanelContainer.hx
@@ -5,3 +5,14 @@ extern class PanelContainer extends Container {
     @:native("cast")
     public static function toPanelContainer(obj: Dynamic): sunaba.ui.PanelContainer;
 }
+
+abstract PanelContainerAbstract(PanelContainer) from PanelContainer to PanelContainer {
+    @:from
+    public static function fromElement(element: sunaba.core.Element): sunaba.ui.PanelContainer {
+        var panelContainer = sunaba.ui.PanelContainer.toPanelContainer(element);
+        if (panelContainer.isNull()) {
+            return null;
+        }
+        return panelContainer;
+    }
+}
