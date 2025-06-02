26037b1389f835d65bb4f2b223e97f49b232bce2
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 12:04:46 -0800

    feat(NinePatchRect): add patchMarginLeft property to NinePatchRect class

--- sunaba/ui/NinePatchRect.hx
+++ sunaba/ui/NinePatchRect.hx
@@ -6,4 +6,5 @@ extern class NinePatchRect extends Control {
     public var axisStretchVertical: Int;
     public var drawCenter: Bool;
     public var mouseFilter: Int;
+    public var patchMarginLeft: Int;
 }
