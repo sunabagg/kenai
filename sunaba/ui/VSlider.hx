85773218d33f778ebd0da7317711d1e80c038980
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 15:19:09 -0800

    feat(VSlider): add toVSlider static function for type casting

--- sunaba/ui/VSlider.hx
+++ sunaba/ui/VSlider.hx
@@ -2,5 +2,6 @@ package sunaba.ui;
 
 @:native("VSlider")
 extern class VSlider extends Slider {
-    
+    @:native("cast")
+    public static function toVSlider(obj: Dynamic): sunaba.ui.VSlider;
 }
