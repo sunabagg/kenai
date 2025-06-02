8db8c821c30ba02d36549a1b54253f69e8c3d674
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 10:53:05 -0800

    feat(LinkButton): add structuredTextBidiOverride property to LinkButton class

--- sunaba/ui/LinkButton.hx
+++ sunaba/ui/LinkButton.hx
@@ -4,4 +4,5 @@ package sunaba.ui;
 extern class LinkButton extends BaseButton {
     public var language:String;
     public var mouseDefaultCursorShape:Int;
+    public var structuredTextBidiOverride:Int;
 }
