d7b6e7ae96c805f7270734af86ee15cdf46bcb91
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 14:49:53 -0800

    feat(ScrollBar): add abstract ScrollBarAbstract for element conversion

--- sunaba/ui/ScrollBar.hx
+++ sunaba/ui/ScrollBar.hx
@@ -10,3 +10,14 @@ extern class ScrollBar extends Range {
     @:native("cast")
     public static function toScrollBar(obj: Dynamic): sunaba.ui.ScrollBar;
 }
+
+abstract ScrollBarAbstract(ScrollBar) from ScrollBar to ScrollBar {
+    @:from
+    public static function fromElement(element: sunaba.core.Element): sunaba.ui.ScrollBar {
+        var scrollBar = sunaba.ui.ScrollBar.toScrollBar(element);
+        if (scrollBar.isNull()) {
+            return null;
+        }
+        return scrollBar;
+    }
+}
