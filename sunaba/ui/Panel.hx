e45bac82ba3bfc91db4e14affd2d5486783b4962
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 12:11:42 -0800

    feat(Panel): add abstract class PanelAbstract for element conversion

--- sunaba/ui/Panel.hx
+++ sunaba/ui/Panel.hx
@@ -5,3 +5,14 @@ extern class Panel extends Control {
     @:native("cast")
     public static function toPanel(obj: Dynamic): sunaba.ui.Panel;
 }
+
+abstract PanelAbstract(Panel) from Panel to Panel {
+    @:from
+    public static function fromElement(element: sunaba.core.Element): sunaba.ui.Panel {
+        var panel = sunaba.ui.Panel.toPanel(element);
+        if (panel.isNull()) {
+            return null;
+        }
+        return panel;
+    }
+}
