136659da2e944107e223aed03f4b25622eec005e
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 14:52:13 -0800

    feat(VScrollBar): add static method toVScrollBar for type casting

--- sunaba/ui/VScrollBar.hx
+++ sunaba/ui/VScrollBar.hx
@@ -2,5 +2,6 @@ package sunaba.ui;
 
 @:native("VScrollBar")
 extern class VScrollBar extends ScrollBar {
-
+    @:native("cast")
+    public static function toVScrollBar(obj: Dynamic): sunaba.ui.VScrollBar;
 }
