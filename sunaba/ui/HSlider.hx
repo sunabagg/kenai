196ee492ca3396d395f6c2eb56e31a767baf2399
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 15:18:17 -0800

    feat(HSlider): add HSliderAbstract for converting from Element to HSlider

--- sunaba/ui/HSlider.hx
+++ sunaba/ui/HSlider.hx
@@ -5,3 +5,14 @@ extern class HSlider extends Slider {
     @:native("cast")
     public static function toHSlider(obj: Dynamic): sunaba.ui.HSlider;
 }
+
+abstract HSliderAbstract(HSlider) from HSlider to HSlider {
+    @:from
+    public static function fromElement(element: sunaba.core.Element): sunaba.ui.HSlider {
+        var hSlider = sunaba.ui.HSlider.toHSlider(element);
+        if (hSlider.isNull()) {
+            return null;
+        }
+        return hSlider;
+    }
+}
