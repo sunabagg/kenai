a7e9c10ad18f03a692142c20234295ae090c8472
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 12:27:56 -0800

    feat(OptionButton): add getSelectedMetadata method to OptionButton class

--- sunaba/ui/OptionButton.hx
+++ sunaba/ui/OptionButton.hx
@@ -24,4 +24,5 @@ extern class OptionButton extends Button {
     public function getItemTooltip(idx: Int): String;
     public function getSelectedItem(?fromLast: Bool): Int;
     public function getSelectedId(): Int;
+    public function getSelectedMetadata(): Variant;
 }
