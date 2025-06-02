b6451334dd7246ad3d4f426386b72f1ddede2616
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 12:29:14 -0800

    feat(OptionButton): add setDisableShortcuts method to OptionButton class

--- sunaba/ui/OptionButton.hx
+++ sunaba/ui/OptionButton.hx
@@ -29,4 +29,5 @@ extern class OptionButton extends Button {
     public function isItemDisabled(idx: Int): Bool;
     public function removeItem(idx: Int): Void;
     public function select(idx: Int): Void;
+    public function setDisableShortcuts(disable: Bool): Void;
 }
