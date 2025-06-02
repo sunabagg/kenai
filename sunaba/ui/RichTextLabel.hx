9f90e1dcacc42f93f37e805dc6ce8148cb25405f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 13:39:34 -0800

    fix(RichTextLabel): replace Vector2 import with Vector2i for consistency

--- sunaba/ui/RichTextLabel.hx
+++ sunaba/ui/RichTextLabel.hx
@@ -4,7 +4,7 @@ import sunaba.core.ArrayList;
 import sunaba.core.Variant;
 import sunaba.core.Rect2;
 import sunaba.core.Color;
-import sunaba.core.Vector2;
+import sunaba.core.Vector2i;
 
 @:native("PanelContainer")
 extern class RichTextLabel extends Control {
