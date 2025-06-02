b84c1168b9d7d7ef0a669efbb3db8feab0396217
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 14:56:14 -0800

    fix(ScrollBar): remove unused step variable from ScrollBar class

--- sunaba/ui/ScrollBar.hx
+++ sunaba/ui/ScrollBar.hx
@@ -5,7 +5,6 @@ import sunaba.core.Event;
 @:native("ScrollBar")
 extern class ScrollBar extends Range {
     public var customStep: Float;
-    public var step: Float;
     public var scrolling: Event;
     @:native("cast")
     public static function toScrollBar(obj: Dynamic): sunaba.ui.ScrollBar;
