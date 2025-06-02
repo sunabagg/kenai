d51eed436c445d69632dda9d9067692f1c476257
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 13:01:42 -0800

    feat(ProgressBar): add static method toProgressBar for type casting

--- sunaba/ui/ProgressBar.hx
+++ sunaba/ui/ProgressBar.hx
@@ -6,4 +6,6 @@ extern class ProgressBar extends Range {
     public var fillMode: Int;
     public var indeterminate: Bool;
     public var showPercentage: Bool;
+    @:native("cast")
+    public static function toProgressBar(obj: Dynamic): sunaba.ui.ProgressBar;
 }
