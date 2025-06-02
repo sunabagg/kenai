63e7e01f5020c8fda52d00f06545a4ee221a66ea
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 11:36:11 -0800

    fix(MarginContainer): add static function toMarginContainer for casting

--- sunaba/ui/MarginContainer.hx
+++ sunaba/ui/MarginContainer.hx
@@ -2,5 +2,6 @@ package sunaba.ui;
 
 @:native("MarginContainer")
 extern class MarginContainer extends Container {
-    
+    @:native("cast")
+    public static function toMarginContainer(obj: Dynamic): sunaba.ui.MarginContainer;
 }
