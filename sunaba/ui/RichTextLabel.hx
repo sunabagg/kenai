30bffbdeb141690646acbd33a41c5f735fd51ba3
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 14:22:42 -0800

    fix(RichTextLabel): add pushFgcolor method for foreground color management

--- sunaba/ui/RichTextLabel.hx
+++ sunaba/ui/RichTextLabel.hx
@@ -87,4 +87,5 @@ extern class RichTextLabel extends Control {
     public function pushColor(color: Color): Void;
     public function pushContext(): Void;
     public function pushDropcap(string: String, font: Font, size: Int, ?dropcapMargins: Rect2, ?color: Color, ?outlineSize: Int, ?outlineColor: Color): Void;
+    public function pushFgcolor(fgcolor: Color): Void;
 }
