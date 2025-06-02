d4a779150343deba0992c2f9f0ecde3878cda67f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 14:23:08 -0800

    fix(RichTextLabel): add pushFontSize method for font size management

--- sunaba/ui/RichTextLabel.hx
+++ sunaba/ui/RichTextLabel.hx
@@ -89,4 +89,5 @@ extern class RichTextLabel extends Control {
     public function pushDropcap(string: String, font: Font, size: Int, ?dropcapMargins: Rect2, ?color: Color, ?outlineSize: Int, ?outlineColor: Color): Void;
     public function pushFgcolor(fgcolor: Color): Void;
     public function pushFont(font: Font): Void;
+    public function pushFontSize(size: Int): Void;
 }
