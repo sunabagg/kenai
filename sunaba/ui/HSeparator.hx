17456ad65e9166dac6c180ab8cdd9e4ca861f936
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 15:10:06 -0800

    feat(HSeparator): add toHSeparator static function for casting to HSeparator

--- sunaba/ui/HSeparator.hx
+++ sunaba/ui/HSeparator.hx
@@ -2,5 +2,6 @@ package sunaba.ui;
 
 @:native("HSeparator")
 extern class HSeparator extends Separator {
-    
+    @:native("cast")
+    public static function toHSeparator(obj: Dynamic): sunaba.ui.HSeparator;
 }
