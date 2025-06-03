1f361fa97ee3273bb7a1626c03f7a5bdc233e667
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 18:39:26 -0800

    feat(SubViewportContainer): add toSubViewportContainer static function for type casting

--- sunaba/ui/SubViewportContainer.hx
+++ sunaba/ui/SubViewportContainer.hx
@@ -7,4 +7,6 @@ extern class SubViewportContainer extends Container {
     public var mouseTarget: Bool;
     public var stretch: Bool;
     public var stretchShrink: Bool;
+    @:native("cast")
+    public static function toSubViewportContainer(obj: Dynamic): sunaba.ui.SubViewportContainer;
 }
