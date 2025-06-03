5f42225b7c926fa85b3215b557cc6cf594584345
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 18:18:13 -0800

    feat(VSplitContainer): add static toVSplitContainer function for type casting

--- sunaba/ui/VSplitContainer.hx
+++ sunaba/ui/VSplitContainer.hx
@@ -2,5 +2,6 @@ package sunaba.ui;
 
 @:native("VSplitContainer")
 extern class VSplitContainer extends SplitContainer {
-    
+    @:native("cast")
+    public static function toVSplitContainer(obj: Dynamic): sunaba.ui.VSplitContainer;
 }
