45412bc8193b2df1a8643ccacb6dcdd2c27c3b59
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 18:14:33 -0800

    feat(HSplitContainer): add HSplitContainerAbstract for element conversion

--- sunaba/ui/HSplitContainer.hx
+++ sunaba/ui/HSplitContainer.hx
@@ -5,3 +5,14 @@ extern class HSplitContainer extends SplitContainer {
     @:native("cast")
     public static function toHSplitContainer(obj: Dynamic): sunaba.ui.HSplitContainer;
 }
+
+abstract HSplitContainerAbstract(HSplitContainer) from HSplitContainer to HSplitContainer {
+    @:from
+    public static function fromElement(element: sunaba.core.Element): sunaba.ui.HSplitContainer {
+        var hsplitContainer = sunaba.ui.HSplitContainer.toHSplitContainer(element);
+        if (hsplitContainer.isNull()) {
+            return null;
+        }
+        return hsplitContainer;
+    }
+}
