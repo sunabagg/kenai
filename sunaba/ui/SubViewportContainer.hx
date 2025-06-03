1a53845dd12bd71f0adac73eb61501be4859c6df
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 18:39:33 -0800

    feat(SubViewportContainer): add abstract class for converting from Element to SubViewportContainer

--- sunaba/ui/SubViewportContainer.hx
+++ sunaba/ui/SubViewportContainer.hx
@@ -10,3 +10,14 @@ extern class SubViewportContainer extends Container {
     @:native("cast")
     public static function toSubViewportContainer(obj: Dynamic): sunaba.ui.SubViewportContainer;
 }
+
+abstract SubViewportContainerAbstract(SubViewportContainer) from SubViewportContainer to SubViewportContainer {
+    @:from
+    public static function fromElement(element: sunaba.core.Element): sunaba.ui.SubViewportContainer {
+        var subViewportContainer = sunaba.ui.SubViewportContainer.toSubViewportContainer(element);
+        if (subViewportContainer.isNull()) {
+            return null;
+        }
+        return subViewportContainer;
+    }
+}
