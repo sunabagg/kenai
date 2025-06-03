2646876c9ca4d2a93662e7f41f1f138e6b04e37b
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 16:49:45 -0800

    feat(SplitContainer): add dragAreaMarginEnd property

--- sunaba/ui/SplitContainer.hx
+++ sunaba/ui/SplitContainer.hx
@@ -5,4 +5,5 @@ extern class SplitContainer extends Container {
     public var collapsed: Bool;
     public var dragAreaHighlightInEditor: Bool;
     public var dragAreaMarginBegin: Int;
+    public var dragAreaMarginEnd: Int;
 }
