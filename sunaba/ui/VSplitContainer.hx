22fb13b06519d11925757b898465a348b1614cf0
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 18:18:19 -0800

    feat(VSplitContainer): add VSplitContainerAbstract for element conversion

--- sunaba/ui/VSplitContainer.hx
+++ sunaba/ui/VSplitContainer.hx
@@ -5,3 +5,14 @@ extern class VSplitContainer extends SplitContainer {
     @:native("cast")
     public static function toVSplitContainer(obj: Dynamic): sunaba.ui.VSplitContainer;
 }
+
+abstract VSplitContainerAbstract(VSplitContainer) from VSplitContainer to VSplitContainer {
+    @:from
+    public static function fromElement(element: sunaba.core.Element): sunaba.ui.VSplitContainer {
+        var vsplitContainer = sunaba.ui.VSplitContainer.toVSplitContainer(element);
+        if (vsplitContainer.isNull()) {
+            return null;
+        }
+        return vsplitContainer;
+    }
+}
