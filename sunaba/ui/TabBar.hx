1ac8ee569306d98d8e4b7b9c4a1b55cf41e1923b
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-04 11:59:52 -0800

    feat(TabBar): add getTabIdxAtPoint function to TabBar class

--- sunaba/ui/TabBar.hx
+++ sunaba/ui/TabBar.hx
@@ -34,4 +34,5 @@ extern class TabBar extends Control {
     public function getTabButtonIcon(tab: Int): Texture2D;
     public function getTabIcon(tab: Int): Texture2D;
     public function getTabIconMaxWidth(tab: Int): Int;
+    public function getTabIdxAtPoint(point: Vector2): Int;
 }
