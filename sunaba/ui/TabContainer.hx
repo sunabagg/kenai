2aad5a808eef45f8e5968cd20b78a3eee2aa0492
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-06 16:31:08 -0800

    feat(TabContainer): add deselectEnabled property to TabContainer class

--- sunaba/ui/TabContainer.hx
+++ sunaba/ui/TabContainer.hx
@@ -5,4 +5,5 @@ extern class TabContainer extends Container {
     public var allTabsInFront: Bool;
     public var clipTabs: Bool;
     public var currentTab: Int;
+    public var deselectEnabled: Bool;
 }
