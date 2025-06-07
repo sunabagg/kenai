878c6788326724c34d4ba63adc623c834ced975e
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-07 13:56:55 -0800

    fix(TextEdit): add missing gutterAdded event variable to TextEdit class

--- sunaba/ui/TextEdit.hx
+++ sunaba/ui/TextEdit.hx
@@ -51,4 +51,5 @@ extern class TextEdit extends Control {
     public var virtualKeyboardEnabled: Bool;
     public var wrapMode: Int;
     public var caretChanged: Event;
+    public var gutterAdded: Event;
 }
