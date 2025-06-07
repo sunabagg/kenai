dacd6b41848a8e54362fbad0bfb1e37fed90ef6b
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-07 15:37:50 -0800

    feat(TextEdit): add selectAll function to enable selection of all text

--- sunaba/ui/TextEdit.hx
+++ sunaba/ui/TextEdit.hx
@@ -178,4 +178,5 @@ extern class TextEdit extends Control {
     public function removeText(fromLine: Int, fromColumn: Int, toLine: Int, toColumn: Int): Void;
     public function search(text: String, flags: Int, fromLine: Int, fromColumn: Int): Vector2i;
     public function select(originLine: Int, originColumn: Int, caretLine: Int, caretColumn: Int, ?caretIndex: Int): Void;
+    public function selectAll(): Void;
 }
