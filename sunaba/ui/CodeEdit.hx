a02655a0e1d77bf3395b753c09b21e9b625f2724
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-09 11:49:16 -0800

    feat(CodeEdit): add codeCompletionEnabled property

--- sunaba/ui/CodeEdit.hx
+++ sunaba/ui/CodeEdit.hx
@@ -7,4 +7,5 @@ extern class CodeEdit extends TextEdit {
     public var autoBraceCompletionEnabled: Bool;
     public var autoBraceCompletionHighlightMatching: Bool;
     public var autoBraceCompletionPairs: Dictionary;
+    public var codeCompletionEnabled: Bool;
 }
