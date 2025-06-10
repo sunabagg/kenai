fdf6aafdf6abd320c868f04eeb99ad13c21607af
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-09 16:44:43 -0800

    fix(TextureRect): add TextureRectAbstract for element conversion

--- sunaba/ui/TextureRect.hx
+++ sunaba/ui/TextureRect.hx
@@ -9,3 +9,14 @@ extern class TextureRect extends Control {
     @:native("cast")
     public static function toTextureRect(obj: Dynamic): TextureRect;
 }
+
+abstract TextureRectAbstract(TextureRect) from TextureRect to TextureRect {
+    @:from
+    public static function fromElement(e: Element): TextureRectAbstract {
+        var tRect = TextureRect.toTextureRect(e);
+        if (tRect.isNull()) {
+            return null;
+        }
+        return tRect;
+    }
+}
