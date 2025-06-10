85c85ff29ade539b2d83fbe5e87fa925af228b56
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-09 17:10:51 -0800

    fix(TreeItem): add getIndex method to TreeItem class

--- sunaba/ui/TreeItem.hx
+++ sunaba/ui/TreeItem.hx
@@ -43,4 +43,5 @@ extern class TreeItem extends BaseObject {
     public function getIconModulate(column: Int): Color;
     public function getIconOverlay(column: Int): Texture2D;
     public function getIconRegion(column: Int): Rect2;
+    public function getIndex(): Int;
 }
