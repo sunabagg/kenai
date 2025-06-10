81f44bb6c2ac2e1c3ff83943e4a2726e424dbc52
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-09 18:18:33 -0800

    feat(TreeItem): add setCustomColor method to TreeItem class

--- sunaba/ui/TreeItem.hx
+++ sunaba/ui/TreeItem.hx
@@ -93,4 +93,5 @@ extern class TreeItem extends BaseObject {
     public function setCollapsedRecursive(collapsed: Bool): Void;
     public function setCustomAsButton(column: Int, enable: Bool): Void;
     public function setCustomBgColor(column: Int, color: Color): Void;
+    public function setCustomColor(column: Int, color: Color): Void;
 }
