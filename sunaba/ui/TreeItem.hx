1c060b853d425e652e4237ce26239286a2e3b98c
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-09 18:19:33 -0800

    feat(TreeItem): add setEditable method to TreeItem class

--- sunaba/ui/TreeItem.hx
+++ sunaba/ui/TreeItem.hx
@@ -97,4 +97,5 @@ extern class TreeItem extends BaseObject {
     public function setCustomFont(column: Int, font: Font): Void;
     public function setCustomFontSize(column: Int, size: Int): Void;
     public function setEditMultiline(column: Int, multiline: Bool): Void;
+    public function setEditable(column: Int, editable: Bool): Void;
 }
