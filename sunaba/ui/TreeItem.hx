e916ff2f63d2c067423a1779c337a6deb731f4f3
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-09 17:13:17 -0800

    fix(TreeItem): add getNextInTree method to TreeItem class

--- sunaba/ui/TreeItem.hx
+++ sunaba/ui/TreeItem.hx
@@ -48,4 +48,5 @@ extern class TreeItem extends BaseObject {
     public function getLanguage(column: Int): String;
     public function getMetadata(column: Int): Variant;
     public function getNext(): TreeItem;
+    public function getNextInTree(): TreeItem;
 }
