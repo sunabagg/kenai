d9efcd4d3e1fc7a043707a522a4580f24f93fdda
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-09 17:26:52 -0800

    fix(TreeItem): add isSelectable method to TreeItem class

--- sunaba/ui/TreeItem.hx
+++ sunaba/ui/TreeItem.hx
@@ -74,4 +74,5 @@ extern class TreeItem extends BaseObject {
     public function isEditMultiline(column: Int): Bool;
     public function isEditable(column: Int): Bool;
     public function isIndeterminate(column: Int): Bool;
+    public function isSelectable(column: Int): Bool;
 }
