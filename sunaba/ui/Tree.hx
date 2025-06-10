8136efb59fd55eb350a1708f40057ba17e95ca4b
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-10 13:43:02 -0800

    feat(Tree): add autoTooltip property to Tree class

--- sunaba/ui/Tree.hx
+++ sunaba/ui/Tree.hx
@@ -5,4 +5,5 @@ extern class Tree extends Control {
     public var allowReselect: Bool;
     public var allowRmbSelect: Bool;
     public var allowSearch: Bool;
+    public var autoTooltip: Bool;
 }
