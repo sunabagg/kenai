2b9874216731f3af20edd2e348b418b74339af83
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-09 16:53:50 -0800

    fix(TreeItem): add clearCustomBgColor method to TreeItem class

--- sunaba/ui/TreeItem.hx
+++ sunaba/ui/TreeItem.hx
@@ -12,4 +12,5 @@ extern class TreeItem extends BaseObject {
     public function addButton(colum: Int, button: Texture2D, ?id: Int, ?disabled: Bool, ?tooltipText: String): Void;
     public function addChild(item: TreeItem): Void;
     public function clearButtons(): Void;
+    public function clearCustomBgColor(column: Int): Void;
 }
