6bd38ca46358072eb79130d7093b342e8b4520d6
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-09 16:44:36 -0800

    fix(TextureRect): add toTextureRect static function for casting

--- sunaba/ui/TextureRect.hx
+++ sunaba/ui/TextureRect.hx
@@ -6,4 +6,6 @@ extern class TextureRect extends Control {
     public var flipH: Bool;
     public var flipV: Bool;
     public var texture: Texture2D;
+    @:native("cast")
+    public static function toTextureRect(obj: Dynamic): TextureRect;
 }
