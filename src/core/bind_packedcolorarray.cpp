75c341535c33d834661db4283e50f318a17c0597
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-11 10:20:17 -0800

    feat(lua_bind): add bind_packedcolorarray function for Lua binding

---
+++ src/core/bind_packedcolorarray.cpp
@@ -0,0 +1,39 @@
+#include "lua_bind.h"
+
+void sunaba::core::bind_packedcolorarray(sol::state& lua) {
+    lua.new_usertype<PackedColorArray>("PackedColorArray",
+        sol::constructors<PackedColorArray()>(),
+        "append", &PackedColorArray::append,
+        "appendArray", &PackedColorArray::append_array,
+        "bsearch", &PackedColorArray::bsearch,
+        "clear", &PackedColorArray::clear,
+        "duplicate", &PackedColorArray::duplicate,
+        "erase", &PackedColorArray::erase,
+        "fill", &PackedColorArray::fill,
+        "filter", &PackedColorArray::filter,
+        "get", &PackedColorArray::get,
+        "has", &PackedColorArray::has,
+        "insert", &PackedColorArray::insert,
+        "isEmpty", &PackedColorArray::is_empty,
+        "isReadOnly", &PackedColorArray::is_read_only,
+        "makeReadOnly", &PackedColorArray::make_read_only,
+        "max", &PackedColorArray::max,
+        "min", &PackedColorArray::min,
+        "popAt", &PackedColorArray::pop_at,
+        "popBack", &PackedColorArray::pop_back,
+        "popFront", &PackedColorArray::pop_front,
+        "pushBack", &PackedColorArray::push_back,
+        "pushFront", &PackedColorArray::push_front,
+        "removeAt", &PackedColorArray::remove_at,
+        "resize", &PackedColorArray::resize,
+        "reverse", &PackedColorArray::reverse,
+        "set", &PackedColorArray::set,
+        "shuffle", &PackedColorArray::shuffle,
+        "size", &PackedColorArray::size,
+        sol::meta_function::index, [](const PackedColorArray& arr, int index) { return arr[index]; },
+        sol::meta_function::new_index, [](PackedColorArray& arr, int index, const Color& value) {
+            arr.set(index, value);
+        },
+        sol::meta_function::length, &PackedColorArray::size
+    );
+}
