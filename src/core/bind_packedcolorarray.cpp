6bb813585d3da3ea23f2a8ae97604d0b112511d3
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-11 10:23:20 -0800

    feat(lua_bind): add slice function binding to PackedColorArray

--- src/core/bind_packedcolorarray.cpp
+++ src/core/bind_packedcolorarray.cpp
@@ -22,6 +22,7 @@ void sunaba::core::bind_packedcolorarray(sol::state& lua) {
         "rfind", &PackedColorArray::rfind,
         "set", &PackedColorArray::set,
         "size", &PackedColorArray::size,
+        "slice", &PackedColorArray::slice,
         sol::meta_function::index, [](const PackedColorArray& arr, int index) { return arr[index]; },
         sol::meta_function::new_index, [](PackedColorArray& arr, int index, const Color& value) {
             arr.set(index, value);
