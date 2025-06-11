373434f47c24df28cbf24dded5d4883576bb021f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-11 12:04:54 -0800

    feat(Float32Array): implement abstract for Float32Array with toArray and fromArray methods

---
+++ sunaba/core/Float32Array.hx
@@ -0,0 +1,15 @@
+package sunaba.core;
+
+abstract Float32Array(lua.Table<Int, Float>) from lua.Table<Int, Float> to lua.Table<Int, Float> {
+    @:to
+    public inline function toArray(): Array<Float> {
+        var table : lua.Table<Int, Float> = this;
+        return lua.Table.toArray(table);
+    }
+
+    @:from
+    public static function fromArray(array: Array<Float>): Float32Array {
+        var result = lua.Table.fromArray(array);
+        return result;
+    }
+}
