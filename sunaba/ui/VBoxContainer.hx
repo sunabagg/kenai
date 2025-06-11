19b5b28fa94e58ab6ec1695b63c8cdc3c57910f2
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-10 22:34:38 -0800

    feat(VBoxContainer): add VBoxContainerAbstract with fromElement function for Element conversion

--- sunaba/ui/VBoxContainer.hx
+++ sunaba/ui/VBoxContainer.hx
@@ -5,3 +5,14 @@ extern class VBoxContainer extends BoxContainer {
     @:native("cast")
     public static function toVBoxContainer(obj: Dynamic): VBoxContainer;
 }
+
+abstract VBoxContainerAbstract(VBoxContainer) from VBoxContainer to VBoxContainer {
+    @:from
+    public static function fromElement(element: Element): VBoxContainerAbstract {
+        var vBox: VBoxContainer = VBoxContainer.toVBoxContainer(element);
+        if (vBox.isNull()) {
+            return null;
+        }
+        return vBox;
+    }
+}
