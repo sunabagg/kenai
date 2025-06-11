1e5c737868bcd1a8caa7e8de47c8e408754cefe6
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-11 13:27:24 -0800

    fix(app): update start method to use test4 instead of test3

--- support_files/app.gd
+++ support_files/app.gd
@@ -4,7 +4,7 @@ extends App
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_window().theme = load("res://addons/lite/light.tres")
-	start("res://test3")
+	start("res://test4")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
