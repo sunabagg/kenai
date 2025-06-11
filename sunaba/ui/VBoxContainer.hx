d832881badb24772438211a0729e2000a9485c2b
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-10 22:34:26 -0800

    feat(VBoxContainer): add toVBoxContainer static function for casting Dynamic to VBoxContainer

--- sunaba/ui/VBoxContainer.hx
+++ sunaba/ui/VBoxContainer.hx
@@ -2,5 +2,6 @@ package sunaba.ui;
 
 @:native("VBoxContainer")
 extern class VBoxContainer extends BoxContainer {
-    
+    @:native("cast")
+    public static function toVBoxContainer(obj: Dynamic): VBoxContainer;
 }
