b627523f46cbc7e11eb678506c80e976630581af
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-11 10:44:40 -0800

    feat(bind_packedstringarray): update type usage to fully qualify godot::String for consistency

--- src/core/sol_gdstr.h
+++ src/core/sol_gdstr.h
@@ -4,26 +4,24 @@
 #include <godot_cpp/variant/string.hpp>
 #include <sol/sol.hpp>
 
-using namespace godot;
-
 template <typename Handler>
-bool sol_lua_check(sol::types<String>, lua_State* L, int index, Handler&& handler, sol::stack::record& tracking) {
+bool sol_lua_check(sol::types<godot::String>, lua_State* L, int index, Handler&& handler, sol::stack::record& tracking) {
     int absolute_index = lua_absindex(L, index);
     bool success = sol::stack::check<const char*>(L, absolute_index, handler, tracking);
     tracking.use(1);
     return success;
 }
 
-String sol_lua_get(sol::types<String>, lua_State* L, int index, sol::stack::record& tracking) {
+godot::String sol_lua_get(sol::types<godot::String>, lua_State* L, int index, sol::stack::record& tracking) {
     int absolute_index = lua_absindex(L, index);
     const char* str = sol::stack::get<const char*>(L, absolute_index, tracking);
     tracking.use(1);
-    return String(str);
+    return godot::String(str);
 }
 
-int sol_lua_push(sol::types<String>, lua_State* L, String str) {
-    const char* cstr = str.utf8().get_data();
-    return sol::stack::push<const char*>(L, cstr);
+int sol_lua_push(sol::types<godot::String>, lua_State* L, godot::String str) {
+    godot::CharString utf8 = str.utf8();
+    return sol::stack::push<std::string>(L, std::string(utf8.get_data()));
 }
 
 #endif // SOL_GDSTR_H
