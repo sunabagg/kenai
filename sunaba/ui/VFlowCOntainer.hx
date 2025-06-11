6dc12663b2634a6232419939b8f54833236e08fe
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-10 22:35:39 -0800

    feat(VFlowContainer): add VFlowContainerAbstract with fromElement function for Element conversion

--- sunaba/ui/VFlowCOntainer.hx
+++ sunaba/ui/VFlowCOntainer.hx
@@ -5,3 +5,14 @@ extern class VFlowCOntainer extends FlowContainer {
     @:native("cast")
     public static function toVFlowContainer(obj: Dynamic): VFlowCOntainer;
 }
+
+abstract VFlowContainerAbstract(VFlowCOntainer) from VFlowCOntainer to VFlowCOntainer {
+    @:from
+    public static function fromElement(element: Element): VFlowContainerAbstract {
+        var vFlow: VFlowCOntainer = VFlowCOntainer.toVFlowContainer(element);
+        if (vFlow.isNull()) {
+            return null;
+        }
+        return vFlow;
+    }
+}
