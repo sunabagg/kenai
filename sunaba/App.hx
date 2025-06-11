1b06460903a9d2553e45da1d16261925e72ba958
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-11 12:37:54 -0800

    Started work on gui test project

---
+++ sunaba/App.hx
@@ -0,0 +1,10 @@
+package sunaba;
+
+import sunaba.core.Element;
+
+class App {
+    public var rootElement(get, default): Element;
+    private function get_rootElement(): Element {
+        return untyped __lua__("_G.rootElement");
+    }
+}
