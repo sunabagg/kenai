576dceb64cb55042c03dbf96515eed93c425d10c
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-10 22:35:31 -0800

    feat(VFlowContainer): add toVFlowContainer static function for casting Dynamic to VFlowContainer

--- sunaba/ui/VFlowCOntainer.hx
+++ sunaba/ui/VFlowCOntainer.hx
@@ -2,5 +2,6 @@ package sunaba.ui;
 
 @:native("VFlowContainer")
 extern class VFlowCOntainer extends FlowContainer {
-    
+    @:native("cast")
+    public static function toVFlowContainer(obj: Dynamic): VFlowCOntainer;
 }
