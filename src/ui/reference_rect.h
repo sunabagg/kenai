08bcd71d519a23349c4227557c39df0db2548032
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-11 18:10:39 -0800

    fix(ReferenceRectProxy): implement _bind_methods for ReferenceRectProxy

--- src/ui/reference_rect.h
+++ src/ui/reference_rect.h
@@ -18,6 +18,11 @@ namespace sunaba::ui {
     class ReferenceRect;
     
     class ReferenceRectProxy : public ReferenceRectNode {
+        GDCLASS(ReferenceRectProxy, ReferenceRectNode)
+        protected:
+            static void _bind_methods() {
+                // Bind methods specific to ReferenceRectProxy
+            }
         public:
             sunaba::ui::ReferenceRect* element = nullptr;
 
