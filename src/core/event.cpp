8eeac96c520891570e5d243d2d7e05086119de41
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-13 14:18:33 -0800

    fix(Event): update connect/disconnect methods to use correct Lua bindings for improved functionality

--- src/core/event.cpp
+++ src/core/event.cpp
@@ -6,8 +6,8 @@ void sunaba::core::bindEvent(sol::state &lua) {
     lua.new_usertype<sunaba::core::Event>("Event",
         sol::constructors<sunaba::core::Event()>(),
         sol::base_classes, sol::bases<sunaba::core::BaseObject>(),
-        "connect", &sunaba::core::Event::connectLuaTable,
-        "disconnect", &sunaba::core::Event::disconnectLuaTable,
+        "connect", &sunaba::core::Event::connectLua,
+        "disconnect", &sunaba::core::Event::disconnectLua,
         "emit", &sunaba::core::Event::emitLua,
         "clear", &sunaba::core::Event::clear
     );
