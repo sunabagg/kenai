ed19f3a710312252b5653635ebb803e573960f32
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-14 00:31:34 -0800

    fix(io): add missing newline at end of IoInterface.hx

--- sunaba/core/io/IoInterface.hx
+++ sunaba/core/io/IoInterface.hx
@@ -5,4 +5,5 @@ import sunaba.core.BaseObject;
 @:native("IoInterface")
 extern class IoInterface extends BaseObject {
     public var pathUri: String;
+    public function getFilePath(path: String): String;
 }
