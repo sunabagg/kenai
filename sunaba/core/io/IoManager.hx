da98658f524ea2c3e78535c3ae10520bda287a82
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-14 00:40:07 -0800

    fix(io): restore getFileUri method signature in IoManager class

--- sunaba/core/io/IoManager.hx
+++ sunaba/core/io/IoManager.hx
@@ -5,5 +5,5 @@ extern class IoManager extends IoInterface {
     public function new();
     public function add(ioInterface: IoInterface): Void;
     public function remove(ioInterface: IoInterface): Void;
-    public function get
+    public function getFileUri(path: String, base_uri: String): String;
 }
