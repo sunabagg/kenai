97f903c38fe76dc3f8bea777b719a4bd9bed4e4f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-14 00:42:24 -0800

    fix(io): add missing method declaration for getFileUri in SystemIoInterface

--- sunaba/core/io/SystemIoInterface.hx
+++ sunaba/core/io/SystemIoInterface.hx
@@ -2,5 +2,5 @@ package sunaba.core.io;
 
 @:native("SystemIoInterface")
 extern class SystemIoInterface extends IoInterface {
-    
+    public function getFileUri(path: String, base_uri: String): String;
 }
