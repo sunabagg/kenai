1320195b83c4e544dfc3359d7075f64aa9c49369
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-14 00:46:57 -0800

    fix(io): add missing variable declarations in FileSystemIo class

--- sunaba/core/io/FileSystemIo.hx
+++ sunaba/core/io/FileSystemIo.hx
@@ -2,5 +2,7 @@ package sunaba.core.io;
 
 @:native("FileSystemIo")
 extern class FileSystemIo extends SystemIoInterface {
-    
+    public var basePath: String;
+    public var altPath: String;
+    public var altPath2: String;
 }
