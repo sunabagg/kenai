5ae5197780930f4e2b318c8941c6986debc56c7a
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-14 01:31:07 -0800

    fix(Main): add apiCodePath variable and initialize paths in _Ready method

--- bindgen/Main.cs
+++ bindgen/Main.cs
@@ -3,13 +3,20 @@ using System;
 
 public partial class Main : Node
 {
+
+	public string apiCodePath;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		var bindgenPath = ProjectSettings.GlobalizePath("res://");
+		var rootPath = bindgenPath + "/../";
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
+
+	pu
 }
