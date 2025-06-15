3052ea5d8817720835d616b99b15b6ad8a694b88
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-15 13:49:08 -0800

    fix(Console): add missing method binding for display_default_output in Console class

--- src/console.cpp
+++ src/console.cpp
@@ -13,6 +13,7 @@ using namespace sunaba;
 void Console::_bind_methods() {
     ClassDB::bind_method(D_METHOD("start_shell", "path"), &Console::start_shell);
     ClassDB::bind_method(D_METHOD("run_command", "command"), &Console::run_command);
+    ClassDB::bind_method(D_METHOD("display_default_output"), &Console::display_default_output);
 }
 
 Console::Console() {
