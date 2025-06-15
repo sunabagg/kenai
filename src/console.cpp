d5dc25b4c8f10a5355df5c1be4712df4e530414a
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-15 12:23:39 -0800

    fix(Console): correct constructor placement and formatting

--- src/console.cpp
+++ src/console.cpp
@@ -6,3 +6,11 @@ void Console::_bind_methods() {
     ClassDB::bind_method(D_METHOD("start_shell", "path"), &Console::start_shell);
     ClassDB::bind_method(D_METHOD("run_command", "command"), &Console::run_command);
 }
+
+Console::Console() {
+    ioManager = new sunaba::core::io::IoManager();
+    
+    default_output_handler = memnew(DefaultOutputHandler);
+    default_output_handler->set_console(this);
+    add_child(default_output_handler);
+}
