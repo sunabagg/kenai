34a85709094226ec3a165163e0e7150958973d85
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-15 12:23:11 -0800

    refactor(Console): remove unused methods from method binding

--- src/console.cpp
+++ src/console.cpp
@@ -5,6 +5,4 @@ using namespace sunaba;
 void Console::_bind_methods() {
     ClassDB::bind_method(D_METHOD("start_shell", "path"), &Console::start_shell);
     ClassDB::bind_method(D_METHOD("run_command", "command"), &Console::run_command);
-    ClassDB::bind_method(D_METHOD("run_executable", "path", "args"), &Console::run_executable);
-    ClassDB::bind_method(D_METHOD("run_executable_dir", "path", "args"), &Console::run_executable_dir);
 }
