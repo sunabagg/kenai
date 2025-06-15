332926b4cb2596a9c904d451397060eb0f97d875
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-15 12:01:55 -0800

    feat(DefaultOutputHandler): implement constructor to initialize UI components

---
+++ src/default_output_handler.cpp
@@ -0,0 +1,19 @@
+#include "console.h"
+
+using namespace sunaba;
+
+DefaultOutputHandler::DefaultOutputHandler() {
+    output_label = memnew(RichTextLabel);
+    input_line_edit = memnew(LineEdit);
+    container = memnew(VBoxContainer);
+
+    container->add_child(output_label);
+    container->add_child(input_line_edit);
+    
+    add_child(container);
+
+    output_label->set_v_size_flags(Control::SIZE_EXPAND_FILL);
+    input_line_edit->set_h_size_flags(Control::SIZE_EXPAND_FILL);
+
+    input_line_edit->connect("text_submitted", Callable(this, "on_line_edit_text_submitted"));
+}
