2186976dd17ea74913da8902e52ac7567e68d155
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-16 07:42:12 -0800

    fix(capsule): update Capsule binding to inherit from SharedObject for improved integration

--- src/spatial/mesh/capsule.cpp
+++ src/spatial/mesh/capsule.cpp
@@ -4,7 +4,7 @@ void sunaba::spatial::mesh::bindCapsule(sol::state& lua) {
     lua.new_usertype<Capsule>(
         "Capsule",
         sol::constructors<Capsule()>(),
-        sol::base_classes, sol::bases<Component>(),
+        sol::base_classes, sol::bases<std::shared_ptr<SharedObject>, std::shared_ptr<Component>>(),
         sol::meta_function::garbage_collect, sol::destructor([](Capsule* c) {  }),
         "height", sol::property(&Capsule::getHeight, &Capsule::setHeight),
         "radialSegments", sol::property(&Capsule::getRadialSegments, &Capsule::setRadialSegments),
