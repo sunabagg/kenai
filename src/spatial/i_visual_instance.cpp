87815e82bd37ed922ab97f01bed02ea6ef7b5f53
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-16 06:50:56 -0800

    fix(visual_instance): update bindVisualInstance to use shared_ptr for Component

--- src/spatial/i_visual_instance.cpp
+++ src/spatial/i_visual_instance.cpp
@@ -1,10 +1,10 @@
 #include "i_visual_instance.h"
 
 void sunaba::spatial::bindVisualInstance(sol::state& lua) {
-    lua.new_usertype<IVisualInstance>(
+    lua.new_usertype<std::shared_ptr<IVisualInstance>>(
         "IVisualInstance",
         sol::constructors<IVisualInstance()>(),
-        sol::base_classes, sol::bases<Component>(),
+        sol::base_classes, sol::bases<std::shared_ptr<Component>>(),
         sol::meta_function::garbage_collect, sol::destructor([](IVisualInstance* i) {  }),
         "layerMask", sol::property(&IVisualInstance::getLayerMask, &IVisualInstance::setLayerMask),
         "sortingOffset", sol::property(&IVisualInstance::getSortingOffset, &IVisualInstance::setSortingOffset),
