25e34b889816f85c32570c26578fcc225afde745
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-18 15:04:02 -0800

    feat(core): implement RegEx class in reg_ex.h

---
+++ src/core/reg_ex.h
@@ -0,0 +1,41 @@
+#ifndef REG_EX_H
+#define REG_EX_H
+
+#include <godot_cpp/classes/reg_ex.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotRegEx godot::RegEx
+
+#include "base_object.h"
+#include "reg_ex_match.h"
+
+using namespace godot;
+
+namespace sunaba::core {
+    void bindRegEx(sol::state& lua);
+
+    class RegEx: public BaseObject {
+        private:
+            godot::Ref<GodotRegEx> reg_ex = nullptr; // Pointer to the RegEx instance
+        public:
+            RegEx(GodotRegEx* regex) {
+                setRegEx(regex);
+            }
+
+            RegEx() {
+                setRegEx(godot::Ref<GodotRegEx>(memnew(GodotRegEx)));
+            }
+
+            godot::Ref<GodotRegEx> getRegEx() const {
+                return reg_ex;
+            }
+
+            void setRegEx(godot::Ref<GodotRegEx> regex) {
+                reg_ex = regex;
+                reg_ex->reference();
+            }
+    };
+}
+
+#endif // REG_EX_H
