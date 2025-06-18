56d89c137ed3253c22e6d36f9af515efa04a3f87
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-18 14:21:13 -0800

    feat(core): rename getNames to names for RegExMatch usertype

--- src/core/reg_ex_match.cpp
+++ src/core/reg_ex_match.cpp
@@ -4,7 +4,7 @@ namespace sunaba::core {
     void bindRegExMatch(sol::state& lua) {
         lua.new_usertype<RegExMatch>("RegExMatch",
             sol::constructors<RegExMatch(), RegExMatch(GodotRegExMatch*)>(),
-            "getNames", &RegExMatch::getNames,
+            "names", sol::readonly_property(&RegExMatch::getNames),
             "getStrings", &RegExMatch::getStrings,
             "getSubject", &RegExMatch::getSubject,
             "getEnd", &RegExMatch::getEnd,
