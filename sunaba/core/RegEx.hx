09ad29bfbf40fa2ba4767af44715f9cb32b3a6a9
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-18 20:29:13 -0800

    feat(core): add getNames method to RegEx class for retrieving named groups

--- sunaba/core/RegEx.hx
+++ sunaba/core/RegEx.hx
@@ -6,4 +6,5 @@ extern class RegEx extends BaseObject {
     public function compile(pattern: String, ?showError: Bool): Void;
     public function createFromString(pattern: String, ?showError: Bool): RegEx;
     public function getGroupCount(): Int;
+    public function getNames(): StringArray;
 }
