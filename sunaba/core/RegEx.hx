c802c4b8d3f14330482cd14b6d4a9533f0bdc00a
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-18 20:28:53 -0800

    feat(core): add getGroupCount method to RegEx class for group count retrieval

--- sunaba/core/RegEx.hx
+++ sunaba/core/RegEx.hx
@@ -5,4 +5,5 @@ extern class RegEx extends BaseObject {
     public function new();
     public function compile(pattern: String, ?showError: Bool): Void;
     public function createFromString(pattern: String, ?showError: Bool): RegEx;
+    public function getGroupCount(): Int;
 }
