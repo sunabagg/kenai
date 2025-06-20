5cc4414b3f80e07b4686fc26462a5a4ecd9d2292
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-19 19:10:36 -0800

    fix(app): update start scene from test4 to test5

--- support_files/app.gd
+++ support_files/app.gd
@@ -4,9 +4,4 @@ extends App
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_window().theme = load("res://addons/lite/light.tres")
-	start("res://test4")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	start("res://test5")
