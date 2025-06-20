8013bc9935c1d36ff2877249cec3799f8f643195
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 03:49:34 -0800

    fix(ui): add label variable and onClick function to ClickerCounter class

--- support_files/test5/src/ClickerCounter.hx
+++ support_files/test5/src/ClickerCounter.hx
@@ -3,7 +3,13 @@ package support_files.test5.src;
 import sunaba.ui.Widget;
 
 class ClickerCounter extends Widget {
+    var label: sunaba.ui.Label;
+    
     override function init() {
         load("app://assets/ClickerCounter.suml");
+    }
+
+    function onClick() {
+        
     }
 }
