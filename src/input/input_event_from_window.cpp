9db627c10553b92360a92cca240daf1ea4fe02e4
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 01:51:07 -0800

    fix(core): update bindInputEventFromWindow to use factory function for InputEventFromWindow instantiation

--- src/input/input_event_from_window.cpp
+++ src/input/input_event_from_window.cpp
@@ -2,7 +2,9 @@
 
 void sunaba::input::bindInputEventFromWindow(sol::state_view& lua) {
     lua.new_usertype<InputEventFromWindow>("InputEventFromWindow",
-        sol::constructors<InputEventFromWindow(), InputEventFromWindow(GodotInputEventFromWindow*)>(),
+        "new", sol::factories(
+            []() { return new InputEventFromWindow(); }
+        ),
         sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::input::InputEvent>(),
         "windowId", sol::property(&InputEventFromWindow::getWindowId, &InputEventFromWindow::setWindowId),
         "cast", [](sunaba::core::Resource* instance) {
