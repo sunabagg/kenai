3d361fdcbe0e22f3a6dc2b8e6b4335033dcd28f2
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 02:22:23 -0800

    fix(core): update bindOrmMaterial3D to use factory function for OrmMaterial3D instantiation

--- src/spatial/orm_material3d.cpp
+++ src/spatial/orm_material3d.cpp
@@ -2,7 +2,9 @@
 
 void sunaba::spatial::bindOrmMaterial3D(sol::state_view& lua) {
     lua.new_usertype<sunaba::spatial::OrmMaterial3D>("OrmMaterial3D",
-        sol::constructors<OrmMaterial3D()>(),
+        "new", sol::factories(
+            []() { return new sunaba::spatial::OrmMaterial3D(); }
+        ),
         sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::core::Material, sunaba::spatial::BaseMaterial3D>(),
         "cast", [](sunaba::core::Resource* instance) {
             return new OrmMaterial3D(godot::Object::cast_to<GodotOrmMaterial3D>(instance->getResource()));
