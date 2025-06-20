60d25915f69261ab642a383ace8523f1f9ea9d91
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 03:56:01 -0800

    fix(ui): update click counter label text and improve button functionality in ClickerCounter class

--- support_files/test5/src/ClickerCounter.hx
+++ support_files/test5/src/ClickerCounter.hx
@@ -1,16 +1,34 @@
 package support_files.test5.src;
 
+import sunaba.ui.Button;
+import sunaba.ui.Button.ButtonAbstract;
+import sunaba.ui.Label;
 import sunaba.ui.Widget;
 
 class ClickerCounter extends Widget {
-    var label: sunaba.ui.Label;
+    var label: Label;
     var counter: Int = 0;
     
     override function init() {
         load("app://assets/ClickerCounter.suml");
+        label = Label.toLabel(
+            rootElement.find(
+                "/centerContainer/vBoxContainer/clickCounterLabel"
+            )
+        );
+        var buttonA : ButtonAbstract = rootElement.find(
+            "/centerContainer/vBoxContainer/clickerButton"
+        );
+        var button : Button= buttonA;
+        button.pressed.connect(() -> {
+            onClick();
+        });
     }
 
     function onClick() {
-        
+        counter++;
+        if (label != null) {
+            label.text = "You clicked me " + counter + " times!";
+        }
     }
 }
