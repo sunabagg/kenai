231473edbefe29d3608fdb4e3ca22e985a43095d
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 00:14:35 -0800

    fix(audio): update AudioStreamMicrophone constructor to use factory function for instantiation

--- src/audio/audio_stream_microphone.cpp
+++ src/audio/audio_stream_microphone.cpp
@@ -2,7 +2,9 @@
 
 void sunaba::audio::bindAudioStreamMicrophone(sol::state_view& lua) {
     lua.new_usertype<AudioStreamMicrophone>("AudioStreamMicrophone",
-        sol::constructors<AudioStreamMicrophone()>(),
+        "new", sol::factories([]() {
+            return new AudioStreamMicrophone();
+        }),
         sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, AudioStream>(),
         "cast", [](sunaba::core::Resource* resource) { 
             return new AudioStreamMicrophone(
