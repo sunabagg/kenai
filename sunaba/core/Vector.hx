05ae003c6b3fb16bcd012e29b9746cdbea3e8190
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-19 20:04:37 -0800

    fix(core): rename Vector class to VectorNative for clarity

--- sunaba/core/Vector.hx
+++ sunaba/core/Vector.hx
@@ -1,12 +1,11 @@
 package sunaba.core;
 
 @:generic
-extern class Vector<T> {
+extern class VectorNative<T> {
     public function set(index:Int, value:T):Void;
     public function get(index:Int):T;
     public function find(value:T):Int;
     public function erase(index:Int):Void;
     public function insert(index:Int, value:T):Void;
     public function add(key:Int, value:T):Void;
-    public function size():Int;
 }
