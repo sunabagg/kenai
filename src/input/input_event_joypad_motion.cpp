391adf88942410c3b6b8faf5b98523d86ac24e54
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 02:12:10 -0800

    fix(core): update bindInputEventJoypadMotion to use factory function for InputEventJoypadMotion instantiation

--- src/input/input_event_joypad_motion.cpp
+++ src/input/input_event_joypad_motion.cpp
@@ -2,7 +2,9 @@
 
 void sunaba::input::bindInputEventJoypadMotion(sol::state_view& lua) {
     lua.new_usertype<InputEventJoypadMotion>("InputEventJoypadMotion",
-        sol::constructors<InputEventJoypadMotion(), InputEventJoypadMotion(GodotInputEventJoypadMotion*)>(),
+        "new", sol::factories(
+            []() { return new InputEventJoypadMotion(); }
+        ),
         sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::input::InputEvent>(),
         "axis", sol::property(&InputEventJoypadMotion::getAxis, &InputEventJoypadMotion::setAxis),
         "axisValue", sol::property(&InputEventJoypadMotion::getAxisValue, &InputEventJoypadMotion::setAxisValue),
