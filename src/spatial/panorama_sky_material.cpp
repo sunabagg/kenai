3f6c65ff36db68150ae531f763fe0783ec171abb
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 02:22:34 -0800

    fix(core): update bindPanoramaSkyMaterial to use factory function for PanoramaSkyMaterial instantiation

--- src/spatial/panorama_sky_material.cpp
+++ src/spatial/panorama_sky_material.cpp
@@ -2,7 +2,9 @@
 
 void sunaba::spatial::bindPanoramaSkyMaterial(sol::state_view& lua) {
     lua.new_usertype<sunaba::spatial::PanoramaSkyMaterial>("PanoramaSkyMaterial",
-        sol::constructors<PanoramaSkyMaterial()>(),
+        "new", sol::factories(
+            []() { return new sunaba::spatial::PanoramaSkyMaterial(); }
+        ),
         sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::core::Material>(),
         "energyMultiplier", sol::property(&PanoramaSkyMaterial::getEnergyMultiplier, &PanoramaSkyMaterial::setEnergyMultiplier),
         "filterEnabled", sol::property(&PanoramaSkyMaterial::getFilterEnabled, &PanoramaSkyMaterial::setFilterEnabled),
