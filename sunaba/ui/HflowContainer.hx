36b23597fda38bd41d1383366a21cb58542abf51
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 04:50:39 -0800

    fix(ui): standardize name casing for HflowContainer class and related functions

---
+++ sunaba/ui/HflowContainer.hx
@@ -0,0 +1,18 @@
+package sunaba.ui;
+
+@:native("HflowContainer")
+extern class HflowContainer extends FlowContainer {
+    @:native("cast")
+    public static function toHFlowContainer(type: Dynamic): HflowContainer;
+}
+
+abstract HflowContainerAbstract(HflowContainer) from HflowContainer to HflowContainer {
+    @:from
+    public static function fromElement(element: sunaba.core.Element): HflowContainerAbstract {
+        var hflow = HflowContainer.toHFlowContainer(element);
+        if (hflow.isNull()) {
+            return null;
+        }
+        return hflow;
+    }
+}
