cacfc210244b5830c8755f323961326e5bd08207
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 00:15:42 -0800

    fix(audio): update AudioStreamPolyphonic binding to use factory function for instantiation

--- src/audio/audio_stream_polyphonic.cpp
+++ src/audio/audio_stream_polyphonic.cpp
@@ -2,7 +2,9 @@
 
 void sunaba::audio::bindAudioStreamPolyphonic(sol::state_view& lua) {
     lua.new_usertype<AudioStreamPolyphonic>("AudioStreamPolyphonic",
-        sol::constructors<AudioStreamPolyphonic()>(),
+        "new", sol::factories([]() {
+            return new AudioStreamPolyphonic();
+        }),
         sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, AudioStream>(),
         "cast", [](sunaba::core::Resource* resource) { 
             return new AudioStreamPolyphonic(
