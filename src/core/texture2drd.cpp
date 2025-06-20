179dbeaee14c2998c4a49d51dff961c592d3d723
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 01:47:52 -0800

    fix(core): update Texture2DRD binding to use factory function for instantiation

--- src/core/texture2drd.cpp
+++ src/core/texture2drd.cpp
@@ -2,7 +2,9 @@
 
 void sunaba::core::bindTexture2DRD(sol::state &lua) {
     lua.new_usertype<Texture2DRD>("Texture2DRD",
-        sol::constructors<Texture2DRD()>(),
+        "new", sol::factories(
+            []() { return new Texture2DRD(); }
+        ),
         sol::base_classes, sol::bases<BaseObject, Resource, Texture, Texture2D>()
     );
 }
