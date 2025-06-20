f7632ffb639540e255062092af498982d0659290
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 02:23:35 -0800

    fix(core): update bindQuadOccluder3D to use factory function for QuadOccluder3D instantiation

--- src/spatial/quad_occluder3d.cpp
+++ src/spatial/quad_occluder3d.cpp
@@ -2,7 +2,9 @@
 
 void sunaba::spatial::bindQuadOccluder3D(sol::state_view& lua) {
     lua.new_usertype<sunaba::spatial::QuadOccluder3D>("QuadOccluder3D",
-        sol::constructors<QuadOccluder3D()>(),
+        "new", sol::factories(
+            []() { return new sunaba::spatial::QuadOccluder3D(); }
+        ),
         sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, Occluder3D>(),
         "size", sol::property(&QuadOccluder3D::getSize, &QuadOccluder3D::setSize),
         "cast", [](sunaba::core::Resource* instance) {
