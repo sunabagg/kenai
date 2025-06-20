79e87f724601ace65ce28af224a03ac68f254cc3
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 02:24:44 -0800

    fix(core): update bindStandardMaterial3D to use factory function for StandardMaterial3D instantiation

--- src/spatial/standard_material3d.cpp
+++ src/spatial/standard_material3d.cpp
@@ -2,7 +2,9 @@
 
 void sunaba::spatial::bindStandardMaterial3D(sol::state_view& lua) {
     lua.new_usertype<sunaba::spatial::StandardMaterial3D>("StandardMaterial3D",
-        sol::constructors<StandardMaterial3D()>(),
+        "new", sol::factories(
+            []() { return new sunaba::spatial::StandardMaterial3D(); }
+        ),
         sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::core::Material, sunaba::spatial::BaseMaterial3D>(),
         "cast", [](sunaba::core::Resource* instance) {
             return new StandardMaterial3D(godot::Object::cast_to<GodotStandardMaterial3D>(instance->getResource()));
