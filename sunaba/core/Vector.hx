b7e886f84067ffe5d3379d3a8efa07edc949e7af
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-19 19:57:27 -0800

    fix(core): add find method to Vector class

--- sunaba/core/Vector.hx
+++ sunaba/core/Vector.hx
@@ -5,4 +5,5 @@ extern class Vector<T> {
     public function set(index:Int, value:T):Void;
     public function at(index:Int):T;
     public function get(index:Int):T;
+    public function find(value:T):Int;
 }
