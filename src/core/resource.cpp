35d5698180479439c8451ff4a5deb9c5279482cd
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 00:41:38 -0800

    fix(core): update Resource binding to use factory function for instantiation

--- src/core/resource.cpp
+++ src/core/resource.cpp
@@ -2,7 +2,9 @@
 
 void sunaba::core::bindResource(sol::state &lua) {
     lua.new_usertype<sunaba::core::Resource>("Resource",
-        sol::constructors<sunaba::core::Resource()>(),
+        "new", sol::factories(
+            []() { return new sunaba::core::Resource(); }
+        ),
         sol::base_classes, sol::bases<sunaba::core::BaseObject>(),
         "duplicate", &sunaba::core::Resource::duplicate,
         "emitChanged", &sunaba::core::Resource::emitChanged,
