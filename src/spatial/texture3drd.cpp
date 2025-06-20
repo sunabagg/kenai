504b3fc6768346824b3f3b2abb8cf8e16c9c9eb1
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 02:25:09 -0800

    fix(core): update bindTexture3DRD to use factory function for Texture3DRD instantiation

--- src/spatial/texture3drd.cpp
+++ src/spatial/texture3drd.cpp
@@ -2,7 +2,9 @@
 
 void sunaba::spatial::bindTexture3DRD(sol::state_view& lua) {
     lua.new_usertype<sunaba::spatial::Texture3DRD>("Texture3DRD",
-        sol::constructors<Texture3DRD()>(),
+        "new", sol::factories(
+            []() { return new sunaba::spatial::Texture3DRD(); }
+        ),
         sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::core::Texture, Texture3D>(),
         "cast", [](sunaba::core::Resource* instance) {
             return new Texture3DRD(godot::Object::cast_to<GodotTexture3DRD>(instance->getResource()));
