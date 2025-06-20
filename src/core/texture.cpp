0d7794c71d4418d2ea9052fbd70e1ff659d9b4da
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 01:47:25 -0800

    fix(core): update Texture binding to use factory function for instantiation

--- src/core/texture.cpp
+++ src/core/texture.cpp
@@ -2,7 +2,9 @@
 
 void sunaba::core::bindTexture(sol::state &lua) {
     lua.new_usertype<sunaba::core::Texture>("Texture",
-        sol::constructors<sunaba::core::Texture()>(),
+        "new", sol::factories(
+            []() { return new sunaba::core::Texture(); }
+        ),
         sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource>(),
         "cast", [](Resource* instance) {
             return new Texture(godot::Object::cast_to<GodotTexture>(instance->getResource()));
