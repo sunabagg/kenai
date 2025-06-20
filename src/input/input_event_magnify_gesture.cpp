195b944ae90597d70b3e1cafe7961b2773bce63f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 02:12:38 -0800

    fix(core): update bindInputEventMagnifyGesture to use factory function for InputEventMagnifyGesture instantiation

--- src/input/input_event_magnify_gesture.cpp
+++ src/input/input_event_magnify_gesture.cpp
@@ -2,7 +2,9 @@
 
 void sunaba::input::bindInputEventMagnifyGesture(sol::state_view& lua) {
     lua.new_usertype<InputEventMagnifyGesture>("InputEventMagnifyGesture",
-        sol::constructors<InputEventMagnifyGesture(), InputEventMagnifyGesture(GodotInputEventMagnifyGesture*)>(),
+        "new", sol::factories(
+            []() { return new InputEventMagnifyGesture(); }
+        ),
         sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::input::InputEvent, sunaba::input::InputEventGesture>(),
         "factor", sol::property(&InputEventMagnifyGesture::getFactor, &InputEventMagnifyGesture::setFactor),
         "cast", [](sunaba::core::Resource* instance) {
