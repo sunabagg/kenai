c7358d749b284ba551e99848e9d1b90c57c5a94d
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 02:21:35 -0800

    fix(core): update bindFogMaterial to use factory function for FogMaterial instantiation

--- src/spatial/fog_material.cpp
+++ src/spatial/fog_material.cpp
@@ -2,7 +2,9 @@
 
 void sunaba::spatial::bindFogMaterial(sol::state_view &lua) {
     lua.new_usertype<FogMaterial>("FogMaterial",
-        sol::constructors<FogMaterial()>(),
+        "new", sol::factories(
+            []() { return new FogMaterial(); }
+        ),
         sol::base_classes, sol::bases<sunaba::core::BaseObject, sunaba::core::Resource, sunaba::core::Material>(),
         "albedo", sol::property(&FogMaterial::getAlbedo, &FogMaterial::setAlbedo),
         "density", sol::property(&FogMaterial::getDensity, &FogMaterial::setDensity),
