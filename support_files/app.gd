4c321be57414ee31b820ffcdd581dca648aa7096
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 16:08:54 -0800

    fix(app): update _ready function to initialize and load sbx file correctly

--- support_files/app.gd
+++ support_files/app.gd
@@ -4,4 +4,5 @@ extends App
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_window().theme = load("res://addons/lite/light.tres")
-	start("res://test5")
+	init(false)
+	load_and_execute_sbx("res://test5/bin/test5.sbx")
