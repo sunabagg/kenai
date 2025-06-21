ed330dc5c6802fdeb725b8076bd8f709e4e83113
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-21 14:58:59 -0800

    refactor(app): move initialization logic to _init function

--- support_files/app.gd
+++ support_files/app.gd
@@ -3,11 +3,13 @@ extends App
 var theme: Theme
 
 # Called when the node enters the scene tree for the first time.
+func _init() -> void:
+	init_state(false)
+	load_and_execute_sbx("res://test5/bin/test5.sbx")
+
 func _ready() -> void:
 	if (DisplayServer.is_dark_mode()):
 		theme = load("res://addons/lite/dark.tres")
 	else:
 		theme = load("res://addons/lite/light.tres")
 	get_tree().root.theme = theme
-	init_state(false)
-	load_and_execute_sbx("res://test5/bin/test5.sbx")
