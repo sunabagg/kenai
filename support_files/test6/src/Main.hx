52e50d7fdf498e767a8b40ff1828d62eb401fd3b
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-23 15:34:20 -0800

    added try catch

--- support_files/test6/src/Main.hx
+++ support_files/test6/src/Main.hx
@@ -2,22 +2,27 @@ package;
 
 class Main {
     static function main() {
-        var url = "https://httpbin.org/post";
-        var http = new haxe.Http(url);
+        try {
+            var url = "https://httpbin.org/post";
+            var http = new haxe.Http(url);
 
-        // Add form parameters
-        http.setParameter("username", "haxeuser");
-        http.setParameter("password", "secret");
+            // Add form parameters
+            http.setParameter("username", "haxeuser");
+            http.setParameter("password", "secret");
 
-        http.onData = function(data:String) {
-            trace("Response from POST:");
-            trace(data);
+            http.onData = function(data:String) {
+                trace("Response from POST:");
+                trace(data);
+            }
+
+            http.onError = function(error:String) {
+                trace("POST Error: " + error);
+            }
+
+            http.request(true); // true for POST
         }
-
-        http.onError = function(error:String) {
-            trace("POST Error: " + error);
+        catch(e) {
+            trace(e);
         }
-
-        http.request(true); // true for POST
     }
 }
