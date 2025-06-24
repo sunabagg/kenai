e0e0f53edefddfff64a70aadceeec75e509fe743
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-24 04:09:37 -0800

    feat: add initial project structure for test7
    Add Main.hx entry point and project configuration file with basic settings

---
+++ support_files/test7/src/Main.hx
@@ -0,0 +1,17 @@
+package;
+
+import sunaba.App;
+
+class Main extends App {
+    static function main() {
+        new Main();
+    }
+
+    override function init() {
+        try {
+        }
+        catch(e) {
+            trace(e);
+        }
+    }
+}
