6fff582a7887a1f38b98542ae1d6464e4f9e3830
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-25 15:59:42 -0800

    started work on AcceptDialog element

---
+++ src/desktop/accept_dialog.h
@@ -0,0 +1,65 @@
+#ifndef ACCEPT_DIALOG_H
+#define ACCEPT_DIALOG_H
+
+#include <godot_cpp/classes/accept_dialog.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define AcceptDialogNode godot::AcceptDialog
+
+#include "window.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+namespace sunaba::desktop {
+    void bindAcceptDialog(sol::state& lua);
+    
+    class AcceptDialog;
+
+    class AcceptDialogSignalWrapper : public Object {
+        GDCLASS(AcceptDialogSignalWrapper, Object)
+        protected:
+            static void _bind_methods();
+        public:
+            sunaba::desktop::AcceptDialog* element = nullptr;
+
+            AcceptDialogSignalWrapper() = default;
+            ~AcceptDialogSignalWrapper() = default;
+    };
+
+    class AcceptDialog : public Window {
+        private:
+            AcceptDialogNode* acceptDialog = nullptr;
+            AcceptDialogSignalWrapper* signalWrapper = nullptr;
+        
+            void connectAcceptDialogSignals() {
+                if (this->signalWrapper == nullptr) {
+                    signalWrapper == memnew(AcceptDialogSignalWrapper);
+                    signalWrapper->element = this;
+                }
+            }
+        public:
+
+            AcceptDialog() {
+                setAcceptDialog(memnew(AcceptDialogNode));
+                onInit();
+            }
+
+            AcceptDialog(AcceptDialogNode* node) {
+                setAcceptDialog(node);
+            }
+
+            AcceptDialogNode* getAcceptDialog() {
+                return acceptDialog;
+            }
+
+            void setAcceptDialog(AcceptDialogNode* aDialog) {
+                acceptDialog = aDialog;
+                connectAcceptDialogSignals();
+                setWindow(aDialog);
+            }
+    };
+}
+
+#endif
