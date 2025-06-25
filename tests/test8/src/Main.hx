cc167dd978b3a3c2f6da7ea03bb7e0642a6b376c
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-25 12:49:08 -0800

    added test8 project

---
+++ tests/test8/src/Main.hx
@@ -0,0 +1,13 @@
+package;
+
+import sunaba.App;
+
+class Main extends App{
+    public static function main() {
+        new Main();
+    }
+
+    override function init() {
+        trace("Hello, World!");
+    }
+}
