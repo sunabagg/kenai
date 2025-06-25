e46911e406bacce96f5410d972106610d17717a5
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-25 14:53:15 -0800

    added initial SubViewport lua binding with constructor

--- src/core/sub_viewport.cpp
+++ src/core/sub_viewport.cpp
@@ -2,6 +2,10 @@
 
 namespace sunaba::core {
     void bindSubViewport(sol::state& lua) {
-        
+        lua.new_usertype<SubViewport>("SubViewport",
+            "new", sol::factories(
+                []() { return new SubViewport; }
+            )
+        );
     }
 }
