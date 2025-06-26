d4be066efa117e60d4f209cf7b0e4fad603ff5fa
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-26 13:16:37 -0800

    added getRect function to StatusIndicator extern

--- api/sunaba/desktop/StatusIndicator.hx
+++ api/sunaba/desktop/StatusIndicator.hx
@@ -3,6 +3,7 @@ package sunaba.desktop;
 import sunaba.core.Element;
 import sunaba.core.Texture2D;
 import sunaba.core.Event;
+import sunaba.core.Rect2;
 
 @:native("StatusIndicator")
 extern class StatusIndicator extends Element {
@@ -11,4 +12,5 @@ extern class StatusIndicator extends Element {
     public var tooltip: String;
     public var visible: Bool;
     public var pressed: Event;
+    public function getRect(): Rect2;
 }
