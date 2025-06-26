d246757a9905af25cb722f80f4df352b1877380a
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-26 06:49:04 -0800

    started work on StatusIndicator element

---
+++ src/desktop/status_indicator.h
@@ -0,0 +1,71 @@
+#ifndef STATUS_INDICATOR_H
+#define STATUS_INDICATOR_H
+
+#include <godot_cpp/classes/status_indicator.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define StatusIndicatorNode godot::StatusIndicator
+
+#include "../core/element.h"
+#include "../core/texture2d.h"
+#include "../core/event.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+namespace sunaba::desktop {
+    void bindStatusIndicator(sol::state& lua);
+
+    class StatusIndicator;
+
+    class StatusIndicatorSignalWrapper : public Object {
+        GDCLASS(StatusIndicatorSignalWrapper, Object)
+        protected:
+            static void _bind_methods();
+
+        public:
+            StatusIndicator* element = nullptr;
+
+            StatusIndicatorSignalWrapper() = default;
+            ~StatusIndicatorSignalWrapper() = default;
+
+            void pressed(int mouse_button, Vector2i mouse_position);
+    };
+
+    class StatusIndicator : public Element {
+        private:
+            StatusIndicatorNode* statusIndicator = nullptr;
+            StatusIndicatorSignalWrapper* signalWrapper = nullptr;
+
+            void connectStatusIndicatorSignals() {
+                if (signalWrapper == nullptr) {
+                    signalWrapper = memnew(StatusIndicatorSignalWrapper);
+                    signalWrapper->element = this;
+                }
+
+                statusIndicator->connect("pressed", Callable(signalWrapper, "pressed"));
+            }
+        public:
+            StatusIndicator() {
+                setStatusIndicatorNode(memnew(StatusIndicatorNode));
+                onInit();
+            }
+
+            StatusIndicator(StatusIndicatorNode* node) {
+                setStatusIndicatorNode(node);
+            }
+
+            StatusIndicatorNode* getStatusIndicatorNode() {
+                return statusIndicator;
+            }
+
+            void setStatusIndicatorNode(StatusIndicatorNode* node) {
+                statusIndicator = node;
+                connectStatusIndicatorSignals();
+                setNode(node); 
+            }
+    };
+}
+
+#endif
