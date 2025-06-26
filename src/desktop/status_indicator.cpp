12bd6431993bec3ac2fcfcf8c76edc25b319e7cd
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-26 12:53:19 -0800

    bind pressed function to ClassDB

--- src/desktop/status_indicator.cpp
+++ src/desktop/status_indicator.cpp
@@ -2,6 +2,6 @@
 
 namespace sunaba::desktop {
     void StatusIndicatorSignalWrapper::_bind_methods() {
-        
+        ClassDB::bind_method(D_METHOD("pressed", "mouse_button", "mouse_position"), &StatusIndicatorSignalWrapper::pressed);
     }
 }
