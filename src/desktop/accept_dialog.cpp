e6acc94f6f9b95a6ebee0a13dfcdc62501764c3a
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-25 16:44:43 -0800

    bind confirmed function to ClassDB

--- src/desktop/accept_dialog.cpp
+++ src/desktop/accept_dialog.cpp
@@ -3,5 +3,6 @@
 namespace sunaba::desktop {
     void AcceptDialogSignalWrapper::_bind_methods() {
         ClassDB::bind_method(D_METHOD("canceled"), &AcceptDialogSignalWrapper::canceled);
+        ClassDB::bind_method(D_METHOD("confirmed"), &AcceptDialogSignalWrapper::confirmed);
     }
 }
