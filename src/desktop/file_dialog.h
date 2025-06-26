0c9d5f239f66d6901dabf03cc1d2654a48182296
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-25 22:14:04 -0800

    started work on FileDialog

---
+++ src/desktop/file_dialog.h
@@ -0,0 +1,65 @@
+#ifndef FILE_DIALOG_H
+#define FILE_DIALOG_H
+
+#include <godot_cpp/classes/file_dialog.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define FileDialogNode godot::FileDialog
+
+#include "confirmation_dialog.h"
+
+using namespace godot;
+using namespace sunaba::core;
+
+namespace sunaba::desktop {
+    void bindFileDialog(sol::state& lua);
+
+    class FileDialog;
+
+    class FileDialogSignalWrapper : public Object {
+        GDCLASS(FileDialogSignalWrapper, Object)
+        protected:
+            static void _bind_methods();
+        public:
+            FileDialog* element = nullptr;
+
+            FileDialogSignalWrapper() = default;
+            ~FileDialogSignalWrapper() = default;
+    };
+
+    class FileDialog : public ConfirmationDialog {
+        private:
+            FileDialogNode* fileDialog = nullptr;
+            FileDialogSignalWrapper* signalWrapper = nullptr;
+
+            void connectFileDialogSignals() {
+                if (signalWrapper == nullptr) {
+                    signalWrapper = memnew(FileDialogSignalWrapper);
+                    signalWrapper->element = this;
+                }
+            }
+
+        public:
+            FileDialog() {
+                setFileDialog(memnew(FileDialogNode));
+                onInit();
+            }
+
+            FileDialog(FileDialogNode* node) {
+                setFileDialog(node);
+            }
+
+            FileDialogNode* getFileDialog() {
+                return fileDialog;
+            }
+
+            void setFileDialog(FileDialogNode* node) {
+                fileDialog = node;
+                connectFileDialogSignals();
+                setConfirmationDialog(fileDialog);
+            }
+    };
+}
+
+#endif
