80d229d4458b169736fb1bb78370c24acbf8595c
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-25 18:59:56 -0800

    added getCancelButton function to ConfirmationDialog extern

--- api/sunaba/desktop/ConfirmationDialog.hx
+++ api/sunaba/desktop/ConfirmationDialog.hx
@@ -1,6 +1,9 @@
 package sunaba.desktop;
 
+import sunaba.ui.Button;
+
 @:native("ConfirmationDialog")
 extern class ConfirmationDialog extends AcceptDialog {
     public var cancelButtonText: String;
+    public function getCancelButton(): Button;
 }
