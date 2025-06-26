0bc25c7a0049abd531fc59833f471be65a53159f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-25 16:44:21 -0800

    bind canceled function to ClassDB

--- src/desktop/accept_dialog.cpp
+++ src/desktop/accept_dialog.cpp
@@ -1,3 +1,7 @@
 #include "accept_dialog.h"
 
-namespace sunaba::desktop {}
+namespace sunaba::desktop {
+    void AcceptDialogSignalWrapper::_bind_methods() {
+        ClassDB::bind_method(D_METHOD("canceled"), &AcceptDialogSignalWrapper::canceled);
+    }
+}
