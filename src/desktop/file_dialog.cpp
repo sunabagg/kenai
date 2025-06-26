33c8e06b93d59e079444e28c00479e54b61bc559
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-26 04:04:47 -0800

    bind dir_selected function to ClassDB

--- src/desktop/file_dialog.cpp
+++ src/desktop/file_dialog.cpp
@@ -1,5 +1,7 @@
 #include "file_dialog.h"
 
 namespace sunaba::desktop {
-    
+    void FileDialogSignalWrapper::_bind_methods() {
+        ClassDB::bind_method(D_METHOD("dir_selected"), &FileDialogSignalWrapper::file_selected);
+    }
 }
