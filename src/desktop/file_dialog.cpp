f49310ac94cd351e8796a9299a6872ec44a83dd3
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-26 04:11:19 -0800

    added dir_selected function implementation

--- src/desktop/file_dialog.cpp
+++ src/desktop/file_dialog.cpp
@@ -2,10 +2,17 @@
 
 namespace sunaba::desktop {
     void FileDialogSignalWrapper::_bind_methods() {
-        ClassDB::bind_method(D_METHOD("dir_selected"), &FileDialogSignalWrapper::file_selected);
+        ClassDB::bind_method(D_METHOD("dir_selected"), &FileDialogSignalWrapper::dir_selected);
         ClassDB::bind_method(D_METHOD("file_selected"), &FileDialogSignalWrapper::file_selected);
         ClassDB::bind_method(D_METHOD("filename_filter_changed"), &FileDialogSignalWrapper::filename_filter_changed);
         ClassDB::bind_method(D_METHOD("files_selected"), &FileDialogSignalWrapper::file_selected);
+    }
+
+    void FileDialogSignalWrapper::dir_selected() {
+        if (element != nullptr) {
+            Array args;
+            element->dirSelectedEvent->emit();
+        }
     }
 
     void FileDialogSignalWrapper::file_selected() {
