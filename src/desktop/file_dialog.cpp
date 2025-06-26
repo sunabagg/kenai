94b57728500b834776911349523e27d0662fb3b2
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-26 04:08:03 -0800

    bind files_selected function to ClassDB

--- src/desktop/file_dialog.cpp
+++ src/desktop/file_dialog.cpp
@@ -5,5 +5,6 @@ namespace sunaba::desktop {
         ClassDB::bind_method(D_METHOD("dir_selected"), &FileDialogSignalWrapper::file_selected);
         ClassDB::bind_method(D_METHOD("file_selected"), &FileDialogSignalWrapper::file_selected);
         ClassDB::bind_method(D_METHOD("filename_filter_changed"), &FileDialogSignalWrapper::filename_filter_changed);
+        ClassDB::bind_method(D_METHOD("files_selected"), &FileDialogSignalWrapper::file_selected);
     }
 }
