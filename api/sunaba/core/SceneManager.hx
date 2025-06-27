58dee30ade2801a89385ca01d056e8e88b86be28
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-26 16:02:48 -0800

    added cast function to SceneManager

--- api/sunaba/core/SceneManager.hx
+++ api/sunaba/core/SceneManager.hx
@@ -3,4 +3,6 @@ package sunaba.core;
 @:native("SceneManager")
 extern class SceneManager extends Element {
     public final scene: Scene;
+    @:native("cast")
+    public static function toSceneManager(obj:Any): SceneManager;
 }
