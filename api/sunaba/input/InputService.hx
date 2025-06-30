377ed9c243ae10e9426c64213f984145cefa406c
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-30 14:59:12 -0800

    added joyConnectionChanged event to InputService extern

--- api/sunaba/input/InputService.hx
+++ api/sunaba/input/InputService.hx
@@ -1,5 +1,6 @@
 package sunaba.input;
 
+import sunaba.core.Event;
 import sunaba.core.Service;
 
 @:native("InputService")
@@ -8,4 +9,5 @@ extern class InputService extends Service {
     public static var emulateTouchFromMouse: Bool;
     public static var mouseMode: Int;
     public static var useAccumulatedInput: Bool;
+    public static var joyConnectionChanged: Event;
 }
