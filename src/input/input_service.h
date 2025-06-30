c2aad08f6151f731176de500eb2412709a8dd92a
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-30 10:53:49 -0800

    added mouseMode getter to InputService

--- src/input/input_service.h
+++ src/input/input_service.h
@@ -32,6 +32,10 @@ namespace sunaba::input {
             static void setEmulateMouseFromTouch(bool value) {
                 return inputSingleton->set_emulate_touch_from_mouse(value);
             }
+
+            static int getMouseMode() {
+                return inputSingleton->get_mouse_mode();
+            }
     };
 }
 
