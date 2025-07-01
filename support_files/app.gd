9d3f6be04018f78c0a61d16c5d625f81911a3461
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-30 22:10:45 -0800

    updated app.gd

--- support_files/app.gd
+++ support_files/app.gd
@@ -5,13 +5,10 @@ var theme: Theme
 # Called when the node enters the scene tree for the first time.
 func _init() -> void:
 	init_state(false)
-
-func _ready() -> void:
 	if (DisplayServer.is_dark_mode()):
 		theme = load("res://addons/lite/dark.tres")
 	else:
 		theme = load("res://addons/lite/light.tres")
-	get_tree().root.theme = theme
 	set_theme(theme)
 	var args = OS.get_cmdline_args()
 	var sbx_path = ""
@@ -22,3 +19,6 @@ func _ready() -> void:
 		load_and_execute_sbx(sbx_path)
 	else:
 		load_and_execute_sbx("res://test5.sbx")
+
+func _ready() -> void:
+	get_tree().root.theme = theme
