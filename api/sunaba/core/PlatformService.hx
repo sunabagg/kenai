9ed8dfa1176637647bec0954d799192c00ba2574
Lucas C. Abbas <lucasbear9@gmail.com>
2025-07-09 19:00:29 -0800

    Add hasFeature method declaration to PlatformService for feature detection

--- api/sunaba/core/PlatformService.hx
+++ api/sunaba/core/PlatformService.hx
@@ -6,4 +6,5 @@ extern class PlatformService extends Service {
     public static final deviceType: DeviceType;
     public static final osName: String;
     public static function shellOpen(uri: String): Int;
+    public static function hasFeature(feature: String): Bool;
 }
