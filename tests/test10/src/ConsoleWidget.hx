8a6a5513584066bc2462c7a2c46e860284dc38eb
Lucas C. Abbas <lucasbear9@gmail.com>
2025-07-13 00:26:31 -0800

    refactor: reorganize ConsoleWidget initialization and enhance logging functionality

--- tests/test10/src/ConsoleWidget.hx
+++ tests/test10/src/ConsoleWidget.hx
@@ -1,5 +1,7 @@
 package;
 
+import sunaba.core.ArrayList;
+import sunaba.core.io.Console;
 import sunaba.ui.Widget;
 import sunaba.ui.RichTextLabel;
 import sunaba.ui.LineEdit;
@@ -8,9 +10,35 @@ class ConsoleWidget extends Widget {
     private var output:RichTextLabel;
     private var input:LineEdit;
 
+    private var console: Console;
+
     override function init() {
         load("app://Console.suml");
         output = RichTextLabel.toRichTextLabel(rootElement.find("vbox/control/output"));
+        var i = rootElement.find("vbox/input");
+        if (i == null) {
+            trace("Input element not found in ConsoleWidget");
+            return;
+        }
         input = LineEdit.toLineEdit(rootElement.find("vbox/input"));
+        console = new Console();
+        rootElement.addChild(console);
+        console.logHandler = (log: String) -> {
+            try {
+                trace("Console log: " + log);
+                output.appendText(log + "\n");
+                output.scrollFollowing = true; // Automatically scroll to the bottom
+            } catch (e: Dynamic) {
+                trace("Error appending log to output: " + e);
+            }
+        };
+        input.textSubmitted.connect((args: ArrayList) -> {
+            var textvar = args.get(0);
+            var text = textvar.toString();
+            if (text != "") {
+                trace("Console input: " + text);
+            }
+        });
+        console.eval("print('Welcome to the Sunaba Console!')");
     }
 }
