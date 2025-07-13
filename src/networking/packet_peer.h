49730d2a174952fae3092efc152ccfeb04d7d4f4
Lucas C. Abbas <lucasbear9@gmail.com>
2025-07-13 14:13:52 -0800

    added PacketPeer class

--- src/networking/packet_peer.h
+++ src/networking/packet_peer.h
@@ -1,4 +1,44 @@
 #ifndef PACKET_PEER_H
 #define PACKET_PEER_H
 
+#include <godot_cpp/classes/packet_peer.hpp>
+#include <godot_cpp/variant/variant.hpp>
+#include <sol/sol.hpp>
+
+#define GodotPacketPeer godot::PacketPeer
+
+#include "../core/base_object.h"
+#include "../core/io/binary_data.h"
+
+using namespace godot;
+using namespace sunaba::core;
+using namespace sunaba::core::io;
+
+namespace sunaba::networking {
+    void bindPacketPeer(sol::state& lua);
+
+    class PacketPeer : public BaseObject {
+        private:
+            Ref<GodotPacketPeer> packetPeer;
+        public:
+            PacketPeer() {
+                setPacketPeer(
+                    Ref<GodotPacketPeer>(memnew(GodotPacketPeer))
+                );
+            }
+
+            PacketPeer(Ref<GodotPacketPeer> peer) {
+                setPacketPeer(peer);
+            }
+
+            Ref<GodotPacketPeer> getPacketPeer() {
+                return packetPeer;
+            }
+
+            void setPacketPeer(Ref<GodotPacketPeer> peer) {
+                packetPeer = peer;
+            }
+    }
+}
+
 #endif //PACKET_PEER_H
