53eb0e9a153abab4446086b3901903c1453a71d1
Lucas C. Abbas <lucasbear9@gmail.com>
2025-07-12 20:42:46 -0800

    added eval function to Console extern

--- api/sunaba/core/io/Console.hx
+++ api/sunaba/core/io/Console.hx
@@ -9,5 +9,6 @@ extern class Console extends Element {
     public var logHandler: Function;
     public function addCommand(cmdNamd: String, func: Function): Void;
     public function getLogColor(log: String): Color;
-    public function cmd(command: String, args: Vector<String>): Int;
+    public function cmd(command: String, args: Vector<String>): Error;
+    public function eval(code: String): Error;
 }
