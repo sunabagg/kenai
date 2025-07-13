332a69a4f1e4f96281c9a9b48d68bec5b14366cd
Lucas C. Abbas <lucasbear9@gmail.com>
2025-07-12 20:36:49 -0800

    added logHandler property to Console extern

--- api/sunaba/core/io/Console.hx
+++ api/sunaba/core/io/Console.hx
@@ -1,7 +1,10 @@
 package sunaba.core.io;
 
+import haxe.macro.Expr.Function;
+
 @:native("Console")
 extern class Console extends Element {
     public var ioInterface: IoInterface;
     public final logs: Vector<String>;
+    public var logHandler: Function;
 }
