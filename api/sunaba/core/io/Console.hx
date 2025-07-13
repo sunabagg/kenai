f1248724d49945aa1562c5683525208d8969447c
Lucas C. Abbas <lucasbear9@gmail.com>
2025-07-12 20:43:35 -0800

    added casting function to Console extern

--- api/sunaba/core/io/Console.hx
+++ api/sunaba/core/io/Console.hx
@@ -11,4 +11,6 @@ extern class Console extends Element {
     public function getLogColor(log: String): Color;
     public function cmd(command: String, args: Vector<String>): Error;
     public function eval(code: String): Error;
+    @:native("cast")
+    public static function toConsole(obj:Any): Console;
 }
