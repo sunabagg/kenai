16843b21135b0bf7e637a6d8c2a482ead4d085a3
Lucas C. Abbas <lucasbear9@gmail.com>
2025-07-12 20:42:09 -0800

    added cmd function to Console extern

--- api/sunaba/core/io/Console.hx
+++ api/sunaba/core/io/Console.hx
@@ -9,4 +9,5 @@ extern class Console extends Element {
     public var logHandler: Function;
     public function addCommand(cmdNamd: String, func: Function): Void;
     public function getLogColor(log: String): Color;
+    public function cmd(command: String, args: Vector<String>): Int;
 }
