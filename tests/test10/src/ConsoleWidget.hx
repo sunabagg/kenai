25fdf07b75f10b4f137a639277d1b4f1a7c8f1f2
Lucas C. Abbas <lucasbear9@gmail.com>
2025-07-12 23:09:15 -0800

    refactor: move output and input variable declarations to the top for better readability

--- tests/test10/src/ConsoleWidget.hx
+++ tests/test10/src/ConsoleWidget.hx
@@ -1,9 +1,16 @@
 package;
 
 import sunaba.ui.Widget;
+import sunaba.ui.RichTextLabel;
+import sunaba.ui.LineEdit;
 
 class ConsoleWidget extends Widget {
+    private var output:RichTextLabel;
+    private var input:LineEdit;
+
     override function init() {
         load("app://Console.suml");
+        output = RichTextLabel.toRichTextLabel(rootElement.find("vbox/control/output"));
+        input = LineEdit.toLineEdit(rootElement.find("vbox/input"));
     }
 }
