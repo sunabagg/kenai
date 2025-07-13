362aac9f632232006025342d8c17671427719f42
Lucas C. Abbas <lucasbear9@gmail.com>
2025-07-13 01:21:25 -0800

    feat: add currentDir property to Console for directory management

--- api/sunaba/core/io/Console.hx
+++ api/sunaba/core/io/Console.hx
@@ -6,6 +6,7 @@ import haxe.Constraints.Function;
 extern class Console extends Element {
     public var ioInterface: IoInterface;
     public final logs: Vector<String>;
+    public var currentDir: String; // Current working directory
     public var logHandler: Function;
     public function addCommand(cmdNamd: String, func: Function): Void;
     public function getLogColor(log: String): Color;
