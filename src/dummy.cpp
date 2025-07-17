fdabd2ad2c97313c8a5ef524190ed5cc5a917878
Lucas C. Abbas <lucasbear9@gmail.com>
2025-07-16 20:40:25 -0800

    Add library creation and dummy source file for vcpkg compatibility

---
+++ src/dummy.cpp
@@ -0,0 +1,4 @@
+// dummy file to satisfy vcpkg add_library because some stupid fuckwad
+// at Microsoft thought it would be a good idea to hijack CMake's 'add_library' 
+// function
+void sunaba_dummy() {}
