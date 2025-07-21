fdc205abc691967923246b4a97667b172b6abaae
Lucas C. Abbas <lucasbear9@gmail.com>
2025-07-20 18:45:46 -0800

    updated test project

--- support_files/app.gd
+++ support_files/app.gd
@@ -18,7 +18,7 @@ func _init() -> void:
 	if not sbx_path.is_empty():
 		load_and_execute_sbx(sbx_path)
 	else:
-		load_and_execute_sbx("res://test5.sbx")
+		load_and_execute_sbx("res://test3.sbx")
 
 func _ready() -> void:
 	get_tree().root.theme = theme
