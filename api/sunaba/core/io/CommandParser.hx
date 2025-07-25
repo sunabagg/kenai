3990e064fecfe4b63fa586c6f8fc9b9026c457be
Lucas C. Abbas <lucasbear9@gmail.com>
2025-07-25 11:02:28 -0800

    Refactor parseCommand method for improved command string parsing

--- api/sunaba/core/io/CommandParser.hx
+++ api/sunaba/core/io/CommandParser.hx
@@ -4,15 +4,49 @@ class CommandParser {
 
     public static function parseCommand(cmdstr: String): Command {
         var cmdname = "";
-        var cmdargs : Array<String> = [];
-        var cmdstrarr = cmdstr.split(" ");
-        for (str in cmdstrarr) {
-            if (str != "" && str != "\t" && str != "\n") {
-                if (cmdname == "") {
-                    cmdname = str;
-                    continue;
+        var cmdargs: Array<String> = [];
+        var current = "";
+        var inQuotes = false;
+        var escapeNext = false;
+
+        for (i in 0...cmdstr.length) {
+            var c = cmdstr.charAt(i);
+
+            if (escapeNext) {
+                current += c;
+                escapeNext = false;
+                continue;
+            }
+
+            if (c == "\\") {
+                escapeNext = true;
+                continue;
+            }
+
+            if (c == "\"") {
+                inQuotes = !inQuotes;
+                continue;
+            }
+
+            if (!inQuotes && (c == " " || c == "\t" || c == "\n")) {
+                if (current != "") {
+                    if (cmdname == "") {
+                        cmdname = current;
+                    } else {
+                        cmdargs.push(current);
+                    }
+                    current = "";
                 }
-                cmdargs.push(str);
+            } else {
+                current += c;
+            }
+        }
+
+        if (current != "") {
+            if (cmdname == "") {
+                cmdname = current;
+            } else {
+                cmdargs.push(current);
             }
         }
 
