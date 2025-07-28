c3ec3ceb89b3a9bd21b44b1f34f9c02725573780
Lucas C. Abbas <lucasbear9@gmail.com>
2025-07-28 00:20:15 -0800

    Remove TextEditProxy class and associated methods from text_edit.h and text_edit.cpp

--- src/ui/text_edit.h
+++ src/ui/text_edit.h
@@ -21,64 +21,6 @@ using namespace sunaba::core;
 namespace sunaba::ui {
     void bindTextEdit(sol::state &lua);
 
-    class TextEdit;
-
-    class TextEditProxy : public TextEditNode {
-        GDCLASS(TextEditProxy, TextEditNode)
-        protected:
-            static void _bind_methods() {
-                // Bind methods specific to TextEditProxy
-            }
-        public:
-            sunaba::ui::TextEdit* element = nullptr;
-
-            void onInit() {
-                // Initialize the NodeProxy instance
-            }
-
-            void _enter_tree() override;
-            void _exit_tree() override;
-            void _ready() override ;
-            void _process(double delta) override;
-            void _physics_process(double delta) override;
-            void _input(const Ref<InputEvent>& event) override;
-            void _unhandled_input(const Ref<InputEvent>& event) override;
-            void _unhandled_key_input(const Ref<InputEvent>& event) override;
-            void _shortcut_input(const Ref<InputEvent>& event) override;
-
-            void _draw() override;
-
-            bool _can_drop_data(const Vector2& at_position, const Variant &data) const override;
-
-            void _drop_data(const Vector2 &at_position, const Variant &data) override;
-
-            Variant _get_drag_data(const Vector2 &at_position) override;
-
-            Vector2 _get_minimum_size() const override;
-
-            String _get_tooltip(const Vector2 &at_position) const override;
-
-            void _gui_input(const Ref<InputEvent> &event) override;
-
-            bool _has_point(const Vector2 &point) const override;
-
-            //Object* _make_custom_tooltip(const String &for_text) const override;
-
-            TypedArray<Vector3i> _structured_text_parser(const Array &args, const String &text) const override;
-
-            void _backspace(int caret_index) override;
-
-            void _copy(int caret_index) override;
-
-            void _cut(int caret_index) override;
-
-            void _handle_unicode_input(int unicode_char, int caret_index) override;
-
-            void _paste(int caret_index) override;
-
-            void _paste_primary_clipboard(int caret_index) override;
-    };
-
     class TextEditSignalWrapper : public Object{
         GDCLASS(TextEditSignalWrapper, Object)
         protected:
@@ -134,13 +76,6 @@ namespace sunaba::ui {
 
             void setTextEdit(TextEditNode* text_edit) {
                 this->text_edit = text_edit;
-                connectTextEditSignals();
-                setControl(text_edit);
-            }
-            
-            void setTextEdit(TextEditProxy* text_edit) {
-                this->text_edit = text_edit;
-                text_edit->element = this;
                 connectTextEditSignals();
                 setControl(text_edit);
             }
