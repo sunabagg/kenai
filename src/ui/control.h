20ddc85b92506e19af14b92651872f187bbc85da
Lucas C. Abbas <lucasbear9@gmail.com>
2025-07-28 00:23:29 -0800

    Remove ControlProxy class and its methods from control.h and control.cpp

--- src/ui/control.h
+++ src/ui/control.h
@@ -20,52 +20,6 @@ using namespace sunaba::core;
 namespace sunaba::ui {
     void bindControl(sol::state &lua);
 
-    class Control;
-
-    class ControlProxy : public ControlNode {
-        GDCLASS(ControlProxy, ControlNode)
-        protected:
-            static void _bind_methods() {
-                // Bind methods specific to ControlProxy
-            }
-        public:
-            sunaba::ui::Control* element = nullptr;
-
-            void onInit() {
-                // Initialize the NodeProxy instance
-            }
-    
-            void _enter_tree() override;
-            void _exit_tree() override;
-            void _ready() override ;
-            void _process(double delta) override;
-            void _physics_process(double delta) override;
-            void _input(const Ref<InputEvent>& event) override;
-            void _unhandled_input(const Ref<InputEvent>& event) override;
-            void _unhandled_key_input(const Ref<InputEvent>& event) override;
-            void _shortcut_input(const Ref<InputEvent>& event) override;
-
-            void _draw() override;
-
-            bool _can_drop_data(const Vector2& at_position, const Variant &data) const override;
-
-            void _drop_data(const Vector2 &at_position, const Variant &data) override;
-
-            Variant _get_drag_data(const Vector2 &at_position) override;
-
-            Vector2 _get_minimum_size() const override;
-
-            String _get_tooltip(const Vector2 &at_position) const override;
-
-            void _gui_input(const Ref<InputEvent> &event) override;
-
-            bool _has_point(const Vector2 &point) const override;
-
-            //Object* _make_custom_tooltip(const String &for_text) const override;
-
-            TypedArray<Vector3i> _structured_text_parser(const Array &args, const String &text) const override;
-    };
-
     class ControlSignalWrapper : public Object {
         GDCLASS(ControlSignalWrapper, Object)
         protected:
@@ -114,9 +68,6 @@ namespace sunaba::ui {
                 setControl(p_node);
             }
 
-            Control(ControlProxy* p_node) {
-                setControl(p_node);
-            }
         
             // Constructor with no parameters
             Control() {
@@ -135,13 +86,6 @@ namespace sunaba::ui {
                 control->set_theme(Ref<godot::Theme>(uiGlobals::getGlobalTheme()->getTheme()));
                 connectControlSignals();
                 setCanvasItem(control);
-            }
-
-            void setControl(ControlProxy* p_node) {
-                control = p_node;
-                p_node->element = this;
-                connectControlSignals();
-                setCanvasItem(p_node);
             }
 
             float getAnchorBottom() {
