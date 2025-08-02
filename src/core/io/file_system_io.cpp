8565ccdf5c6aef6d3be524e2252fef652e4c4f3a
Lucas C. Abbas <lucasbear9@gmail.com>
2025-08-01 18:55:54 -0800

    Fix base class bindings for FileSystemIo to include IoInterface

--- src/core/io/file_system_io.cpp
+++ src/core/io/file_system_io.cpp
@@ -10,7 +10,7 @@ namespace sunaba::core::io {
         lua.new_usertype<FileSystemIo>(
             "FileSystemIo",
             sol::no_constructor,
-            sol::base_classes, sol::bases<SystemIoInterface>(),
+            sol::base_classes, sol::bases<IoInterface, SystemIoInterface>(),
             "basePath", sol::property(
                 [](FileSystemIo& self) { return self.basePath; }
             ),
@@ -32,7 +32,7 @@ namespace sunaba::core::io {
         lua.new_usertype<FileSystemIo>(
             "FileSystemIo",
             sol::no_constructor,
-            sol::base_classes, sol::bases<SystemIoInterface>(),
+            sol::base_classes, sol::bases<IoInterface, SystemIoInterface>(),
             "basePath", sol::property(
                 [](FileSystemIo& self) { return self.basePath; }
             ),
