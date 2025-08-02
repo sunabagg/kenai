b94f11fed5310c30696c26c48144073639d4e3e9
Lucas C. Abbas <lucasbear9@gmail.com>
2025-08-01 18:50:22 -0800

    Add static create function to FileSystemIo class

--- api/sunaba/core/io/FileSystemIo.hx
+++ api/sunaba/core/io/FileSystemIo.hx
@@ -5,4 +5,5 @@ extern class FileSystemIo extends SystemIoInterface {
     public var basePath: String;
     public var altPath: String;
     public var altPath2: String;
+    public static function create(basePath: String, pathUri: String): FileSystemIo;
 }
