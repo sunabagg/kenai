5f8f3614a7277aa7cf4d783c29cd0447cdaee0ef
Lucas C. Abbas <lucasbear9@gmail.com>
2025-08-05 14:29:57 -0800

    Add initial test11 project files and main application logic

---
+++ tests/test11/src/Main.hx
@@ -0,0 +1,20 @@
+package;
+
+import sunaba.core.io.IoManager;
+import sunaba.ui.StyleBoxFlat;
+import sunaba.App;
+
+class Main extends App {
+    public static function main() {
+        new Main();
+    }
+
+    override function init() {
+        var stylebox = new StyleBoxFlat();
+        stylebox.ExpandMarginBottom - 0.56;
+        stylebox.borderWidthLeft = 3;
+        stylebox.cornerRadiusTopRight = 1;
+        var io : IoManager = untyped __lua__("_G.ioManager");
+        io.saveResource("user://test.sres", stylebox);
+    }
+}
