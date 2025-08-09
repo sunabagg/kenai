8df63b0d531b9cc83525e84277ee95f8463750dc
Lucas C. Abbas <lucasbear9@gmail.com>
2025-08-08 18:48:23 -0800

    Refactor EntityData class to extend ScriptableObject and implement fromEntity method for entity data conversion

--- api/sunaba/core/EntityData.hx
+++ api/sunaba/core/EntityData.hx
@@ -2,10 +2,29 @@ package sunaba.core;
 
 import sunaba.core.Dictionary;
 
-class EntityData {
-    var name: String;
-    var components: Array<Dictionary>;
-    var children: Array<EntityData>;
+class EntityData extends ScriptableObject {
+    public var name: String;
+    public var components: Dictionary = new Dictionary();
+    public var children: Array<EntityData> = new Array();
 
-    public function new() {}
+    public static function fromEntity(entity: Entity) {
+        var data = new EntityData();
+        data.name = entity.name;
+
+        var componentNameVector = entity.getComponentNames();
+        var componentNames = componentNameVector.toArray();
+        for (name in componentNames) {
+            var component = entity.getComponentByName(name);
+            var compData = component.getData();
+            data.components.set(name, compData);
+        }
+
+        for (i in 0...entity.getChildCount()) {
+            var child = entity.getChild(i);
+            var childData = fromEntity(child);
+            data.children.push(childData);
+        }
+
+        return data;
+    }
 }
