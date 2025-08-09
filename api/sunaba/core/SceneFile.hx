5ecc2af105a6558372c88cf1dc1e54add5f628b1
Lucas C. Abbas <lucasbear9@gmail.com>
2025-08-08 18:51:24 -0800

    Initialize entities array in SceneFile class and implement fromScene method for scene data conversion

--- api/sunaba/core/SceneFile.hx
+++ api/sunaba/core/SceneFile.hx
@@ -1,5 +1,17 @@
 package sunaba.core;
 
 class SceneFile extends ScriptableObject {
-    public var entities: Array<EntityData>;
+    public var entities: Array<EntityData> = new Array();
+
+    public static function fromScene(scene: Scene) {
+        var data = new SceneFile();
+        
+        for (i in 0...scene.getEntityCount()) {
+            var entity = scene.getEntity(i);
+            var entityData = EntityData.fromEntity(entity);
+            data.entities.push(entityData);
+        }
+
+        return data;
+    }
 }
