ce4eb49c0313ba5b3b68878f18fbe048ebf4a1b3
Lucas C. Abbas <lucasbear9@gmail.com>
2025-08-08 18:06:53 -0800

    Update EntityData to use Dictionary for components instead of Map<String, Any>

--- api/sunaba/core/EntityData.hx
+++ api/sunaba/core/EntityData.hx
@@ -1,7 +1,9 @@
 package sunaba.core;
 
+import sunaba.core.Dictionary;
+
 typedef EntityData = {
     var name: String;
-    var components: Array<Map<String, Any>>;
+    var components: Array<Dictionary>;
     var children: Array<EntityData>;
 }
