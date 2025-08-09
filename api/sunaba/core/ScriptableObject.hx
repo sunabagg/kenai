941ea896dbe3a8ddf10238fa440cb71294299951
Lucas C. Abbas <lucasbear9@gmail.com>
2025-08-08 17:28:19 -0800

    Add ScriptableObject class with getData and setData methods

---
+++ api/sunaba/core/ScriptableObject.hx
@@ -0,0 +1,10 @@
+package sunaba.core;
+
+class ScriptableObject {
+    public function getData() : Dictionary {
+        return new Dictionary();
+    }
+
+    public function setData(data: Dictionary) : Void {
+    }
+}
