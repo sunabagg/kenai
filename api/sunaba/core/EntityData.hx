eabde50595418aee34015d5c35beec04087c34d1
Lucas C. Abbas <lucasbear9@gmail.com>
2025-08-08 18:11:11 -0800

    Change EntityData from typedef to class for improved structure

--- api/sunaba/core/EntityData.hx
+++ api/sunaba/core/EntityData.hx
@@ -2,7 +2,7 @@ package sunaba.core;
 
 import sunaba.core.Dictionary;
 
-typedef EntityData = {
+class EntityData {
     var name: String;
     var components: Array<Dictionary>;
     var children: Array<EntityData>;
