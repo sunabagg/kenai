22e18012c95be8dd785f9352fcdb63b3a1338f5b
Lucas C. Abbas <lucasbear9@gmail.com>
2025-08-08 18:09:03 -0800

    Refactor setData method in ScriptableObject for consistency

--- api/sunaba/core/ScriptableObject.hx
+++ api/sunaba/core/ScriptableObject.hx
@@ -5,6 +5,7 @@ class ScriptableObject {
         return new Dictionary();
     }
 
-    public function setData(data: Dictionary) : Void {
-    }
+    public function setData(data: Dictionary) : Void {}
+
+    public function new() {}
 }
