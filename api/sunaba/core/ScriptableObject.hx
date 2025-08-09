03972f48cc6269bd444ad4e810e8da84c5e9ae19
Lucas C. Abbas <lucasbear9@gmail.com>
2025-08-08 18:05:26 -0800

    Revert "Remove getData and setData methods from ScriptableObject class"
    This reverts commit 4649045182c538667213c5cb3229590e9123625d.

--- api/sunaba/core/ScriptableObject.hx
+++ api/sunaba/core/ScriptableObject.hx
@@ -1,4 +1,10 @@
 package sunaba.core;
 
 class ScriptableObject {
+    public function getData() : Map<String, Any> {
+        return new Map<String, Any>();
+    }
+
+    public function setData(data: Map<String, Any>) : Void {
+    }
 }
