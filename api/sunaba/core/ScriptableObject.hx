4649045182c538667213c5cb3229590e9123625d
Lucas C. Abbas <lucasbear9@gmail.com>
2025-08-08 18:05:04 -0800

    Remove getData and setData methods from ScriptableObject class

--- api/sunaba/core/ScriptableObject.hx
+++ api/sunaba/core/ScriptableObject.hx
@@ -1,10 +1,4 @@
 package sunaba.core;
 
 class ScriptableObject {
-    public function getData() : Map<String, Any> {
-        return new Map<String, Any>();
-    }
-
-    public function setData(data: Map<String, Any>) : Void {
-    }
 }
