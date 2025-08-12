9b2ade650b83fc65140ddbfd4e2635650c39cac3
Lucas C. Abbas <lucasbear9@gmail.com>
2025-08-12 12:03:01 -0800

    Add printEntity and printScene methods to Main class for entity and scene visualization

--- tests/test11/src/Main.hx
+++ tests/test11/src/Main.hx
@@ -4,6 +4,13 @@ import sunaba.core.io.IoManager;
 import sunaba.ui.StyleBoxFlat;
 import sunaba.App;
 import sunaba.core.SceneFile;
+import sunaba.core.Scene;
+import sunaba.core.Entity;
+import sunaba.spatial.Camera;
+import sunaba.spatial.SpatialTransform;
+import sunaba.spatial.mesh.MeshRenderer;
+import sunaba.spatial.mesh.Box;
+import sunaba.core.SceneManager;
 
 
 class Main extends App {
@@ -19,5 +26,30 @@ class Main extends App {
         trace(io.loadText("app://TestScene.scnj"));
         sceneFile.setData(scenejson.toDictionary()); 
         trace(sceneFile.getData());
+    }
+
+    public static function printEntity(entity : Entity, indent : String = "    ") : Void {
+        Sys.println(indent + "Entity: " + entity.name);
+        /*var spatialTransform = SpatialTransform.getFromEntity(entity);
+        if (spatialTransform != null) {
+            var position = spatialTransform.position;
+            var rotation = spatialTransform.rotation;
+            var scale = spatialTransform.scale;
+            var global = spatialTransform.global;
+            Sys.println(indent + "    Transform: " + position.toString() + ", " + rotation.toString() + ", " + scale.toString());
+            Sys.println(indent + "    GlobalTransform: " + global.toString());
+        }*/        
+        for (child in 0...entity.getChildCount()) {
+            var childEntity = entity.getChild(child);
+            printEntity(childEntity, indent + "    ");
+        }
+    }
+
+    public static function printScene(scene : Scene) : Void {
+        Sys.println("Scene: " + scene);
+        for (entity in 0...scene.getEntityCount()) {
+            var entity = scene.getEntity(entity);
+            printEntity(entity);
+        }
     }
 }
